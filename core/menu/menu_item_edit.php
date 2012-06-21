21a4b0ca20e522a8ca7161f4cbdb510b1daf9bb4
Mark Crane <markjcrane@gmail.com>
2012-06-21 01:00:26 +0000

    If the menu item order is empty then assume an order of 0.

--- core/menu/menu_item_edit.php
+++ core/menu/menu_item_edit.php
@@ -210,7 +210,12 @@ else {
 				else {
 					$sql .= "menu_item_parent_uuid = '$menu_item_parent_uuid', ";
 				}
-				$sql .= "menu_item_order = '$menu_item_order', ";
+				if (strlen($menu_item_parent_uuid) == 0) {
+					$sql .= "menu_item_order = '0', ";
+				}
+				else {
+					$sql .= "menu_item_order = '$menu_item_order', ";
+				}
 				$sql .= "menu_item_mod_user = '".$_SESSION["username"]."', ";
 				$sql .= "menu_item_mod_date = now() ";
 				$sql .= "where menu_uuid = '$menu_uuid' ";
