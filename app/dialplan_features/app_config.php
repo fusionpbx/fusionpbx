15d853b0ccd7a88427e01bb4dc2d15ac23f4dcdd
Mark Crane <markjcrane@gmail.com>
2012-07-10 18:27:27 +0000

    Add the context features to the dialplan database.

---
+++ app/dialplan_features/app_config.php
@@ -0,0 +1,12 @@
+<?php
+	//application details
+		$apps[$x]['name'] = "Dialplan Featuers";
+		$apps[$x]['uuid'] = '42d2bf28-c90c-4a6d-a06c-796c5bc412aa';
+		$apps[$x]['category'] = 'Switch';
+		$apps[$x]['subcategory'] = '';
+		$apps[$x]['version'] = '';
+		$apps[$x]['license'] = 'Mozilla Public License 1.1';
+		$apps[$x]['url'] = 'http://www.fusionpbx.com';
+		$apps[$x]['description']['en'] = 'Provides dialplan context features including in call transfer and attended transfer and more.';
+
+?>
