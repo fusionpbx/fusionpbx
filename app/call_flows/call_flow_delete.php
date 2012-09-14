df888b6a03f4314f53474575477c60bc74b5d6bb
Mark Crane <markjcrane@gmail.com>
2012-09-14 22:07:03 +0000

    Delete the dialplan entry for call flows when the call flow is deleted.

--- app/call_flows/call_flow_delete.php
+++ app/call_flows/call_flow_delete.php
@@ -15,6 +15,17 @@ if (count($_GET)>0) {
 }
 
 if (strlen($id)>0) {
+
+	//get the dialplan uuid
+		$sql = "select * from v_call_flows ";
+		$sql .= "where domain_uuid = '$domain_uuid' ";
+		$sql .= "and call_flow_uuid = '$id' ";
+		$prep_statement = $db->prepare($sql);
+		$prep_statement->execute();
+		while($row = $prep_statement->fetch(PDO::FETCH_ASSOC)) {
+			$dialplan_uuid = $row['dialplan_uuid'];
+		}
+
 	//delete call_flow
 		$sql = "delete from v_call_flows ";
 		$sql .= "where domain_uuid = '$domain_uuid' ";
@@ -22,6 +33,26 @@ if (strlen($id)>0) {
 		$prep_statement = $db->prepare(check_sql($sql));
 		$prep_statement->execute();
 		unset($sql);
+
+	//delete the dialplan entry
+		$sql = "delete from v_dialplans ";
+		$sql .= "where domain_uuid = '$domain_uuid' ";
+		$sql .= "and dialplan_uuid = '$dialplan_uuid' ";
+		$db->query($sql);
+		unset($sql);
+
+	//delete the dialplan details
+		$sql = "delete from v_dialplan_details ";
+		$sql .= "where domain_uuid = '$domain_uuid' ";
+		$sql .= "and dialplan_uuid = '$dialplan_uuid' ";
+		$db->query($sql);
+		unset($sql);
+
+	//syncrhonize configuration
+		save_dialplan_xml();
+
+	//apply settings reminder
+		$_SESSION["reload_xml"] = true;
 }
 
 //redirect the user
