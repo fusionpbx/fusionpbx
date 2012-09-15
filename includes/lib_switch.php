f844e5a908158ea3872d494930a0a63c20b79da5
Mark Crane <markjcrane@gmail.com>
2012-09-15 23:39:59 +0000

    Remove */ on the default dialstring. This little setting has caused more harm than good.

--- includes/lib_switch.php
+++ includes/lib_switch.php
@@ -1666,7 +1666,7 @@ function save_extension_xml() {
 					$extension = preg_replace("/[\*\:\\/\<\>\|\'\"\?]/", "", $extension);
 					$dial_string = $row['dial_string'];
 					if (strlen($dial_string) == 0) {
-						$dial_string = "{sip_invite_domain=\${domain_name},presence_id=\${dialed_user}@\${dialed_domain}}\${sofia_contact(*/\${dialed_user}@\${dialed_domain})}";
+						$dial_string = "{sip_invite_domain=\${domain_name},presence_id=\${dialed_user}@\${dialed_domain}}\${sofia_contact(\${dialed_user}@\${dialed_domain})}";
 					}
 
 					$xml .= "<include>\n";
