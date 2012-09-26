bc1a87f415f776e4a02cb944019ad3e84e329be0
Mark Crane <markjcrane@gmail.com>
2012-09-26 01:38:48 +0000

    Add a few translations for fax.

--- app/fax/app_languages.php
+++ app/fax/app_languages.php
@@ -1,39 +1,39 @@
 <?php
 
 	$content['title']['en-us'] = 'Fax Server';
-	$content['title']['pt-pt'] = '';
+	$content['title']['pt-pt'] = 'Servidor de Fax';
 
 	$content['description']['en-us'] = 'To receive a FAX setup a fax extension and then direct the incoming to it.';
-	$content['description']['pt-pt'] = '';
+	$content['description']['pt-pt'] = 'Para receber um FAX, configure uma extensão de fax e encaminhe o tráfego de entrada para essa extensão.';
 
 	$content['confirm-delete']['en-us'] = 'Are you sure you want to delete this?';
-	$content['confirm-delete']['pt-pt'] = '';
+	$content['confirm-delete']['pt-pt'] = 'Tem a certeza que pretende apagar?';
 
 	$content['label-extension']['en-us'] = 'Extension';
-	$content['label-extension']['pt-pt'] = '';
+	$content['label-extension']['pt-pt'] = 'Extensão';
 
 	$content['label-name']['en-us'] = 'Name';
-	$content['label-name']['pt-pt'] = '';
+	$content['label-name']['pt-pt'] = 'Nome';
 
 	$content['label-email']['en-us'] = 'Email';
-	$content['label-email']['pt-pt'] = '';
+	$content['label-email']['pt-pt'] = 'Email';
 
 	$content['label-upload']['en-us'] = 'Upload';
-	$content['label-upload']['pt-pt'] = '';
+	$content['label-upload']['pt-pt'] = 'Carregar';
 
 	$content['button-back']['en-us'] = 'Back';
-	$content['button-back']['pt-pt'] = '';
+	$content['button-back']['pt-pt'] = 'Voltar atrás';
 
 	$content['button-settings']['en-us'] = 'Settings';
-	$content['button-settings']['pt-pt'] = '';
+	$content['button-settings']['pt-pt'] = 'Configurações';
 
 	$content['button-send']['en-us'] = 'Send';
-	$content['button-send']['pt-pt'] = '';
+	$content['button-send']['pt-pt'] = 'Enviar';
 
 	$content['button-copy']['en-us'] = 'Copy';
-	$content['button-copy']['pt-pt'] = '';
+	$content['button-copy']['pt-pt'] = 'Copiar';
 
 	$content['button-save']['en-us'] = 'Save';
-	$content['button-save']['pt-pt'] = '';
+	$content['button-save']['pt-pt'] = 'Guardar';
 
 ?>
