c7449c67aaff5afc4e774f19c8125e69faf4df17
Mark Crane <markjcrane@gmail.com>
2012-09-26 00:48:17 +0000

    After discussing with other programmers introducing the first app_languages.php file which will be used for multi-lingual support in the future.

---
+++ app/fax/app_languages.php
@@ -0,0 +1,39 @@
+<?php
+
+	$content['title']['en-us'] = 'Fax Server';
+	$content['title']['pt-pt'] = '';
+
+	$content['description']['en-us'] = 'To receive a FAX setup a fax extension and then direct the incoming to it.'
+	$content['description']['pt-pt'] = '';
+
+	$content['confirm-delete']['en-us'] = 'Are you sure you want to delete this?';
+	$content['confirm-delete']['pt-pt'] = '';
+
+	$content['label-extension']['en-us'] = 'Extension';
+	$content['label-extension']['pt-pt'] = '';
+
+	$content['label-name']['en-us'] = 'Name';
+	$content['label-name']['pt-pt'] = '';
+
+	$content['label-email']['en-us'] = 'Email';
+	$content['label-email']['pt-pt'] = '';
+
+	$content['label-upload']['en-us'] = 'Upload';
+	$content['label-upload']['pt-pt'] = '';
+
+	$content['button-back']['en-us'] = 'Back';
+	$content['button-back']['pt-pt'] = '';
+
+	$content['button-settings']['en-us'] = 'Settings';
+	$content['button-settings']['pt-pt'] = '';
+
+	$content['button-send']['en-us'] = 'Send';
+	$content['button-send']['pt-pt'] = '';
+
+	$content['button-copy']['en-us'] = 'Copy';
+	$content['button-copy']['pt-pt'] = '';
+
+	$content['button-save']['en-us'] = 'Save';
+	$content['button-save']['pt-pt'] = '';
+
+?>
