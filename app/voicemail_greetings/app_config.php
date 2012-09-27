7e009cf30b85f5b009c1abba624d699eda599ab6
Nate Jones <git@reliberate.com>
2012-09-27 05:06:04 +0000

    Corrected PK name for vm greetings table.

--- app/voicemail_greetings/app_config.php
+++ app/voicemail_greetings/app_config.php
@@ -66,7 +66,8 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en'] = '';
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = 'true';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'greeting_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'voicemail_greeting_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'greeting_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
