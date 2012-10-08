68b9593f075e68b65f29de1acddf22b7385a3099
Mark Crane <markjcrane@gmail.com>
2012-10-08 21:43:16 +0000

    Fix the back button translation and fix the white space problems. Remove the carriage return after ?> at the end of fax.php.

--- app/fax/app_languages.php
+++ app/fax/app_languages.php
@@ -17,18 +17,18 @@
 
 	$content_fax['label-email']['en-us'] = 'Email';
 	$content_fax['label-email']['pt-pt'] = 'E-mail';
-        
-        $content_fax['label-description']['en-us'] = 'Description';
+
+	$content_fax['label-description']['en-us'] = 'Description';
 	$content_fax['label-description']['pt-pt'] = 'Descrição';
 
 	$content_fax['label-upload']['en-us'] = 'Upload';
 	$content_fax['label-upload']['pt-pt'] = 'Carregar';
-        
-        $content_fax['label-uploadinfo']['en-us'] = 'Select the file to upload and send as a fax.';
+
+	$content_fax['label-uploadinfo']['en-us'] = 'Select the file to upload and send as a fax.';
 	$content_fax['label-uploadinfo']['pt-pt'] = 'Selecione o ficheiro para fazer o upload e enviar por fax.';
 
-	$content['button-back']['en-us'] = 'Back';
-	$content['button-back']['pt-pt'] = 'Voltar';
+	$content_fax['button-back']['en-us'] = 'Back';
+	$content_fax['button-back']['pt-pt'] = 'Voltar';
 
 	$content_fax['button-settings']['en-us'] = 'Settings';
 	$content_fax['button-settings']['pt-pt'] = 'Configurações';
@@ -41,110 +41,110 @@
 
 	$content_fax['button-save']['en-us'] = 'Save';
 	$content_fax['button-save']['pt-pt'] = 'Guardar';
-        
-        //fax_copy
-        $content_fax['confirm-copy']['en-us'] = 'Copy Complete';
+
+	//fax_copy
+	$content_fax['confirm-copy']['en-us'] = 'Copy Complete';
 	$content_fax['confirm-copy']['pt-pt'] = 'Cópia Completa';
-        
-        //fax_delete
-        $content_fax['confirm-delete']['en-us'] = 'Delete Complete';
+
+	//fax_delete
+	$content_fax['confirm-delete']['en-us'] = 'Delete Complete';
 	$content_fax['confirm-delete']['pt-pt'] = 'Eliminação Completa';
-        
-        //fax_edit
-        $content_fax['confirm-add']['en-us'] = 'Add Complete';
+
+	//fax_edit
+	$content_fax['confirm-add']['en-us'] = 'Add Complete';
 	$content_fax['confirm-add']['pt-pt'] = 'Adição Completa';
-        
-        $content_fax['confirm-update']['en-us'] = 'Update Complete';
+
+	$content_fax['confirm-update']['en-us'] = 'Update Complete';
 	$content_fax['confirm-update']['pt-pt'] = 'Actualização Completa';        
-        
-        $content_fax['confirm-ext']['en-us'] = 'Please provide: Extension';
+
+	$content_fax['confirm-ext']['en-us'] = 'Please provide: Extension';
 	$content_fax['confirm-ext']['pt-pt'] = 'Por favor, forneça: Extensão';
-        
-        $content_fax['confirm-fax']['en-us'] = 'Please provide: A file to Fax';
+
+	$content_fax['confirm-fax']['en-us'] = 'Please provide: A file to Fax';
 	$content_fax['confirm-fax']['pt-pt'] = 'Por favor forneça: um ficheiro para enviar Fax';
-        
-        $content_fax['confirm-faxadd']['en-us'] = 'Fax Add';
+
+	$content_fax['confirm-faxadd']['en-us'] = 'Fax Add';
 	$content_fax['confirm-faxadd']['pt-pt'] = 'Adição de Fax';
-        
-        $content_fax['confirm-faxedit']['en-us'] = 'Fax Edit';
+
+	$content_fax['confirm-faxedit']['en-us'] = 'Fax Edit';
 	$content_fax['confirm-faxedit']['pt-pt'] = 'Edição de Fax'; 
-        
-        $content_fax['confirm-copyinfo']['en-us'] = 'Do you really want to copy this?';
+
+	$content_fax['confirm-copyinfo']['en-us'] = 'Do you really want to copy this?';
 	$content_fax['confirm-copyinfo']['pt-pt'] = 'Deseja realmente copiar isto?';
-        
-        $content_fax['label-nameinfo']['en-us'] = 'Enter the name here.';
+
+	$content_fax['label-nameinfo']['en-us'] = 'Enter the name here.';
 	$content_fax['label-nameinfo']['pt-pt'] = 'Insira aqui o nome.';
-        
-        $content_fax['label-extensioninfo']['en-us'] = 'Enter the fax extension here.';
+
+	$content_fax['label-extensioninfo']['en-us'] = 'Enter the fax extension here.';
 	$content_fax['label-extensioninfo']['pt-pt'] = 'Insira aqui a extensão do fax';
-        
-        $content_fax['label-emailinfo']['en-us'] = 'Enter the email address to send the FAX to.';
+
+	$content_fax['label-emailinfo']['en-us'] = 'Enter the email address to send the FAX to.';
 	$content_fax['label-emailinfo']['pt-pt'] = 'Insira o endereço de e-mail para enviar o fax.';
-        
-        $content_fax['label-descriptioninfo']['en-us'] = 'Enter the description here.';
+
+	$content_fax['label-descriptioninfo']['en-us'] = 'Enter the description here.';
 	$content_fax['label-descriptioninfo']['pt-pt'] = 'Insira aqui a descrição';
-        
-        $content_fax['label-pin']['en-us'] = 'PIN Number';
+
+	$content_fax['label-pin']['en-us'] = 'PIN Number';
 	$content_fax['label-pin']['pt-pt'] = 'Numero PIN';
-        
-        $content_fax['label-pininfo']['en-us'] = 'Enter the PIN number here.';
+
+	$content_fax['label-pininfo']['en-us'] = 'Enter the PIN number here.';
 	$content_fax['label-pininfo']['pt-pt'] = 'Insira aqui o número do PIN.';
-        
-        $content_fax['label-calleridname']['en-us'] = 'Caller ID Name';
+
+	$content_fax['label-calleridname']['en-us'] = 'Caller ID Name';
 	$content_fax['label-calleridname']['pt-pt'] = 'Nome de Origem';
-        
-        $content_fax['label-calleridnumber']['en-us'] = 'Caller ID Number';
+
+	$content_fax['label-calleridnumber']['en-us'] = 'Caller ID Number';
 	$content_fax['label-calleridnumber']['pt-pt'] = 'Número de Origem';
-        
-        $content_fax['label-calleridnameinfo']['en-us'] = 'Enter the Caller ID name here.';
+
+	$content_fax['label-calleridnameinfo']['en-us'] = 'Enter the Caller ID name here.';
 	$content_fax['label-calleridnameinfo']['pt-pt'] = 'Insira aqui o nome de origem.';
-        
-        $content_fax['label-calleridnumberinfo']['en-us'] = 'Enter the Caller ID number here.';
+
+	$content_fax['label-calleridnumberinfo']['en-us'] = 'Enter the Caller ID number here.';
 	$content_fax['label-calleridnumberinfo']['pt-pt'] = 'Insira aqui o número de origem.';
-        
-        $content_fax['label-forward']['en-us'] = 'Forward Number';
+
+	$content_fax['label-forward']['en-us'] = 'Forward Number';
 	$content_fax['label-forward']['pt-pt'] = 'Número de Encaminhamento';
-        
-        $content_fax['label-forwardinfo']['en-us'] = 'Enter the forward number here. Used to forward the fax to a registered extension or external number.';
+
+	$content_fax['label-forwardinfo']['en-us'] = 'Enter the forward number here. Used to forward the fax to a registered extension or external number.';
 	$content_fax['label-forwardinfo']['pt-pt'] = 'Insira aqui o número de encaminhamento. Utilizado para encaminhar o fax para uma extensão registada ou número externo.';
-        
-        $content_fax['label-userlist']['en-us'] = 'User List';
+
+	$content_fax['label-userlist']['en-us'] = 'User List';
 	$content_fax['label-userlist']['pt-pt'] = 'Lista de utilizadores';
-        
-        $content_fax['button-add']['en-us'] = 'Add';
+
+	$content_fax['button-add']['en-us'] = 'Add';
 	$content_fax['button-add']['pt-pt'] = 'Adicionar';
-        
-        $content_fax['label-addinfo']['en-us'] = 'Assign the users that are can manage this fax extension.';
+
+	$content_fax['label-addinfo']['en-us'] = 'Assign the users that are can manage this fax extension.';
 	$content_fax['label-addinfo']['pt-pt'] = 'Atribuir os utilizadores que podem gerir esta extensão de fax.';
-        
-        $content_fax['label-file']['en-us'] = 'File not found.';
+
+	$content_fax['label-file']['en-us'] = 'File not found.';
 	$content_fax['label-file']['pt-pt'] = 'Ficheiro não encontrado.';
-        
-        $content_fax['description2']['en-us'] = 'To send a fax you can upload a .tif file or if ghost script has been installed then you can also send a fax by uploading a PDF.';
+
+	$content_fax['description2']['en-us'] = 'To send a fax you can upload a .tif file or if ghost script has been installed then you can also send a fax by uploading a PDF.';
 	$content_fax['description2']['pt-pt'] = 'Para enviar um fax, pode carregar um ficheiro .tif ou se o ghost script foi instalado, então também pode enviar um fax, carregando um ficheiro .pdf.';
-        
-        $content_fax['description3']['en-us'] = 'When sending a fax you can view status of the transmission by viewing the logs from the Status tab or by watching the response from the console.';
+
+	$content_fax['description3']['en-us'] = 'When sending a fax you can view status of the transmission by viewing the logs from the Status tab or by watching the response from the console.';
 	$content_fax['description3']['pt-pt'] = 'Ao enviar um fax, pode ver o estado da transmissão, visualizando os logs da tab Status ou assistindo a resposta através da consola.';
-        
-        $content_fax['label-faxnumber']['en-us'] = 'Fax Number';
+
+	$content_fax['label-faxnumber']['en-us'] = 'Fax Number';
 	$content_fax['label-faxnumber']['pt-pt'] = 'Número de Fax';
-        
-        $content_fax['label-faxnumberinfo']['en-us'] = 'Enter the number here.';
+
+	$content_fax['label-faxnumberinfo']['en-us'] = 'Enter the number here.';
 	$content_fax['label-faxnumberinfo']['pt-pt'] = 'Insira aqui o numero.';
-        
-        $content_fax['label-location']['en-us'] = 'location';
+
+	$content_fax['label-location']['en-us'] = 'location';
 	$content_fax['label-location']['pt-pt'] = 'localização';
-        
-        $content_fax['tabel-file']['en-us'] = 'File Name (download)';
+
+	$content_fax['tabel-file']['en-us'] = 'File Name (download)';
 	$content_fax['tabel-file']['pt-pt'] = 'Nome do Ficheiro (descarregado)';
-        
-        $content_fax['tabel-view']['en-us'] = 'View';
+
+	$content_fax['tabel-view']['en-us'] = 'View';
 	$content_fax['tabel-view']['pt-pt'] = 'Visualizar';
-        
-        $content_fax['tabel-modified']['en-us'] = 'Last Modified';
+
+	$content_fax['tabel-modified']['en-us'] = 'Last Modified';
 	$content_fax['tabel-modified']['pt-pt'] = 'Última alteração';
-        
-        $content_fax['tabel-size']['en-us'] = 'Size';
+
+	$content_fax['tabel-size']['en-us'] = 'Size';
 	$content_fax['tabel-size']['pt-pt'] = 'Tamanho';
-        
+
 ?>
