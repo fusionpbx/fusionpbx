849a69630ca856081798f9f3b464686e0d988acb
Mark Crane <markjcrane@gmail.com>
2012-10-24 06:54:27 +0000

    Remove superadmin group from click to call only used by the links in CDR and Contacts not needed in the menu.

--- app/click_to_call/app_config.php
+++ app/click_to_call/app_config.php
@@ -33,7 +33,6 @@
 		$apps[$x]['menu'][0]['parent_uuid'] = 'fd29e39c-c936-f5fc-8e2b-611681b266b5';
 		$apps[$x]['menu'][0]['category'] = 'internal';
 		$apps[$x]['menu'][0]['path'] = '/app/click_to_call/click_to_call.php';
-		$apps[$x]['menu'][0]['groups'][] = 'superadmin';
 
 	//permission details
 		$apps[$x]['permissions'][0]['name'] = 'click_to_call_view';
