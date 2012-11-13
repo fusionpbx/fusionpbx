140af2efeceafa8473cb56fdbe947c97de648b6b
Mark Crane <markjcrane@gmail.com>
2012-11-13 00:56:52 +0000

    Add destination_fax_number to app_config.php

--- app/destinations/app_config.php
+++ app/destinations/app_config.php
@@ -110,6 +110,10 @@
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = 'Enter the data.';
 		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'destination_fax_number';
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
+		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'destination_enabled';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
