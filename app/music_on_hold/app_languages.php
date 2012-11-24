f7c8aa10b31e68a8d2b587071925d17e8c32f08f
James Rose <james.o.rose@gmail.com>
2012-11-24 23:23:20 +0000

    music on hold, added preliminary support for translations.

---
+++ app/music_on_hold/app_languages.php
@@ -0,0 +1,53 @@
+﻿<?php
+/*
+	FusionPBX
+	Version: MPL 1.1
+
+	The contents of this file are subject to the Mozilla Public License Version
+	1.1 (the "License"); you may not use this file except in compliance with
+	the License. You may obtain a copy of the License at
+	http://www.mozilla.org/MPL/
+
+	Software distributed under the License is distributed on an "AS IS" basis,
+	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	for the specific language governing rights and limitations under the
+	License.
+
+	The Original Code is FusionPBX
+
+	The Initial Developer of the Original Code is
+	Mark J Crane <markjcrane@fusionpbx.com>
+	Portions created by the Initial Developer are Copyright (C) 2008-2012
+	the Initial Developer. All Rights Reserved.
+
+	Contributor(s):
+	Mark J Crane <markjcrane@fusionpbx.com>
+	James Rose <james.o.rose@gmail.com>
+*/
+	
+
+	$text['label-moh']['en-us'] = 'Music on Hold';
+	$text['desc-moh']['en-us'] = "Music on hold can be in WAV or MP3 format. To play an MP3 file you must have mod_shout enabled on the 'Modules' tab. You can adjust the volume of the MP3 audio from the 'Settings' tab. For best performance upload 16 bit, 8/16/32/48 kHz <i>mono</i> WAV files.";
+	$text['label-upload-moh']['en-us'] = 'Upload Music';
+	$text['label-file-path']['en-us'] = 'File Path';
+	$text['button-clear']['en-us'] = 'Clear';
+	$text['label-sampling']['en-us'] = 'Sampling';
+	$text['label-category']['en-us'] = 'Category';
+	$text['opt-default']['en-us'] = 'Default';
+	$text['opt-new']['en-us'] = 'New...';
+	$text['message-click-select']['en-us'] = 'Double-Click to Select an Existing Category';
+	$text['button-upload']['en-us'] = 'Upload';
+	$text['message-available-to-all']['en-us'] = 'Available to All Domains';
+	$text['label-download']['en-us'] = 'Download';
+	$text['label-play']['en-us'] = 'Play';
+	$text['label-uploaded']['en-us'] = 'Uploaded';
+	$text['label-file-size']['en-us'] = 'File Size';
+	$text['label-sampling']['en-us'] = 'Sampling';
+	$text['message-delete']['en-us'] = 'Do you really want to delete this file?';
+	$text['message-nofiles']['en-us'] = 'No files found.';
+	$text['label-delete-category']['en-us'] = 'Delete Category';
+	$text['label-location']['en-us'] = 'Location';
+	$text['label-default']['en-us'] = 'Default';
+	$text['message-event-socket']['en-us'] = 'Connection to Event Socket failed.';
+
+?>
