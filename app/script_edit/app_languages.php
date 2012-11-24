8e11ac639210483bb738c62d961b041b911953de
James Rose <james.o.rose@gmail.com>
2012-11-24 05:24:49 +0000

    cleanup.

--- app/script_edit/app_languages.php
+++ app/script_edit/app_languages.php
@@ -31,25 +31,20 @@
 	$text['label-notes']['en-us'] = 'Notes:';
 	$text['button-add']['en-us'] = 'Add';
 	$text['message-delete']['en-us'] = 'Delete Complete';
-	
 	$text['message-give-up']['en-us'] = 'Giving up :( Cannot create an XMLHTTP instance';
 	$text['message-problem']['en-us'] = 'There was a problem with the request.';
 	$text['label-clip-library']['en-us'] = 'Clip Library';
-
 	$text['button-edit-clip']['en-us'] = 'Edit Clip';
 	$text['button-add-clip']['en-us'] = 'Add Clip';
 	$text['message-delete-clip']['en-us'] = 'Are you sure you want to delete the selected clip?';
 	$text['button-delete']['en-us'] = '  Delete   ';
 	$text['button-close']['en-us'] = '   Close    ';
-
 	$text['label-path']['en-us'] = 'Path:';
 	$text['label-file-name']['en-us'] = 'File Name:';
 	$text['button-new-file']['en-us'] = 'New File';
 	$text['label-file-name-orig']['en-us'] = 'Original File Name:';
 	$text['label-rename-file-to']['en-us'] = 'Rename File To:';
-
 	$text['label-files']['en-us'] = 'Files';
-
 	$text['button-add-file']['en-us'] = 'Add File';
 	$text['button-add-dir']['en-us'] = 'Add Dir'; //add directory
 	$text['button-rename-file']['en-us'] = 'Rename File';
@@ -57,37 +52,11 @@
 	$text['button-del-file']['en-us'] = 'Delete File';
 	$text['message-delete-folder']['en-us'] = 'Are you sure you want to delete the selected folder?';
 	$text['button-del-dir']['en-us'] = 'Delete Dir'; //delete directory
-
 	$text['message-error']['en-us'] = 'Error!!';
-
 	$text['label-folder-name']['en-us'] = 'Folder Name:';
 	$text['button-new-folder']['en-us'] = 'New Folder';
-
 	$text['button-delete-this']['en-us'] = 'Are you sure want to delete this?';
-
 	$text['button-search']['en-us'] = 'Search';
 	$text['button-add']['en-us'] = 'Add';
-
-
-
-
-
-	$text['button-back']['en-us'] = 'Back';
-	$text['button-save']['en-us'] = 'Save';
-	$text['title']['en-us'] = 'Service Add';
-
-	$text['description']['en-us'] = 'Shows a list of processes and provides ability to start and stop them.';
-	$text['label-name']['en-us'] = 'Name:';
-	$text['description-name']['en-us'] = 'Enter the service name.';
-	$text['label-type']['en-us'] = 'Type:';
-	$text['description-type']['en-us'] = 'Select the service type.';
-	$text['label-data']['en-us'] =  'Data:';
-	$text['description-data']['en-us'] = 'Enter the service data.';
-	$text['label-start']['en-us'] = 'Start Command:';
-	$text['description-start']['en-us'] = 'Enter the command to start the service.';
-	$text['label-stop']['en-us'] = 'Stop Comand';
-	$text['description-stop']['en-us'] = 'Enter the command to stop the service.';
-	$text['label-description']['en-us'] = 'Description:';
-	$text['description-description']['en-us'] = 'Enter the service description.';
 
 ?>
