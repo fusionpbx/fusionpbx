84cf23236038f00000238e1dcdbe3e86f1939d9e
Rinor Hoxha <rinorhoxha@gmail.com>
2012-11-24 22:59:08 +0000

    Dialplan Inbound Multi lang rev.01

---
+++ app/dialplan_inbound/app_languages.php
@@ -0,0 +1,62 @@
+﻿<?php
+
+	$text['title-dialplan-inbound-add']['en-us'] = 'Inbound Call Routing';
+	$text['description-dialplan-inbound-add']['en-us'] = 'The public dialplan is used to route incoming calls to destinations based on one or more conditions and context. It can send incoming calls to an auto attendant, huntgroup, extension, external number, or a script.';
+
+	$text['button-back']['en-us'] = 'Back';
+	$text['button-advanced']['en-us'] = 'Advanced';
+
+//begin: Basic
+	$text['button-basic']['en-us'] = 'Basic';
+	
+	$text['label-name']['en-us'] = 'Name';
+	$text['description-name']['en-us'] = 'Please enter an inbound route name.'; 
+
+	$text['label-destination-number']['en-us'] = 'Destination Number';
+	
+	$text['label-select-inbound-destination-number']['en-us'] = 'Select the inbound destination number.';
+	
+	$text['button-add']['en-us'] = 'Add';
+
+	$text['label-action']['en-us'] = 'Action';
+	
+	$text['label-limit']['en-us'] = 'Limit';
+	
+	$text['label-order']['en-us'] = 'Order';
+	
+	$text['label-enabled']['en-us'] = 'Enabled';
+	
+	$text['label-description']['en-us'] = 'Description';
+//end: Basic
+
+//begin: Advanced
+	$text['label-condition_1']['en-us'] = 'Condition 1';
+
+	$text['label-condition_2']['en-us'] = 'Condition 2';
+
+	$text['label-action_1']['en-us'] = 'Action 1';
+
+	$text['label-action_2']['en-us'] = 'Action 2';
+	
+	$text['label-field']['en-us'] = 'Field';
+	
+	$text['label-expression']['en-us'] = 'Expression';
+//end: Advanced
+
+	$text['button-save']['en-us'] = 'Save';
+	
+	$text['label-required-domain_uuid']['en-us'] = 'Please provide: domain_uuid';
+	
+	$text['label-required-dialplan_name']['en-us'] = 'Please provide: Extension Name';
+	
+	$text['label-required-condition_field_1']['en-us'] = 'Please provide: Condition Field';
+	
+	$text['label-required-condition_expression_1']['en-us'] = 'Please provide: Condition Expression';
+	
+	$text['label-required-action_application_1']['en-us'] = 'Please provide: Action Application';
+	
+	$text['label-destination-must-be-num']['en-us'] = 'The destination number must be numeric.';
+	
+	$text['confirm-update-complete']['en-us'] = 'Update Complete';
+
+?>
