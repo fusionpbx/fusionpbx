5bbaa9926358e034c4d39b76c66a83f20f4fe3e4
James Rose <james.o.rose@gmail.com>
2012-11-24 02:29:52 +0000

    App->System, added initial support for translations

---
+++ app/system/app_languages.php
@@ -0,0 +1,51 @@
+<?php
+/*
+	FusionPBX
+	Version: MPL 1.1
+
+	The contents of this file are subject to the Mozilla Public License Version
+	1.1 (the "License"); you may not use this file except in compliance with
+	the License. You may obtain a copy of the License at
+	http://www.mozilla.org/MPL/
+
+	Software distributed under the License is distributed on an "AS IS" basis,
+	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	for the specific language governing rights and limitations under the
+	License.
+
+	The Original Code is FusionPBX
+
+	The Initial Developer of the Original Code is
+	Mark J Crane <markjcrane@fusionpbx.com>
+	Portions created by the Initial Developer are Copyright (C) 2008-2012
+	the Initial Developer. All Rights Reserved.
+
+	Contributor(s):
+	Mark J Crane <markjcrane@fusionpbx.com>
+	James Rose <james.o.rose@gmail.com>
+*/
+
+	$text['title-sys-info']['en-us'] = 'System Information';
+	$text['label-os']['en-us'] = 'Operating System:';
+	$text['title-mem']['en-us'] = 'Memory Information';
+	$text['label-mem']['en-us'] = 'Memory Status:';
+	$text['title-cpu']['en-us'] = 'CPU Information';
+	$text['label-cpu']['en-us'] = 'CPU Status:';
+	$text['title-drive']['en-us'] = 'Drive Information';
+	$text['label-drive']['en-us'] = 'Drive Space:';
+	$text['label-drive-capacity']['en-us'] = 'Capacity:';
+	$text['label-drive-free']['en-us'] = 'Free Space:';
+	$text['label-drive-percent']['en-us'] = 'Percent Free:';
+
+
+	$text['label-extension']['en-us'] = 'Extension';
+	$text['label-voicemail']['en-us'] = 'Voicemail Mail To';
+	$text['label-messages']['en-us'] = 'Messages';
+	$text['label-enabled']['en-us'] = 'Enabled';
+	$text['label-description']['en-us'] = 'Description';
+
+	$text['confirm-prefs-delete-alt']['en-us']= 'restore default preferences';
+	$text['confirm-prefs-delete-title']['en-us']='restore default preferences';
+	$text['confirm-prefs-delete']['en-us'] = 'Are you sure you want to remove the voicemail name and greeting?';
+
+?>
