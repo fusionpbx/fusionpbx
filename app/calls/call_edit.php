eb2a6c9039b9518d85257867f90b8df8671f16d4
Mark Crane <markjcrane@gmail.com>
2012-12-18 14:15:38 +0000

    Remove caller id prefix from follow me.

--- app/calls/call_edit.php
+++ app/calls/call_edit.php
@@ -117,7 +117,7 @@ if (count($_POST)>0 && strlen($_POST["persistformvar"]) == 0) {
 		if (count($_POST)>0) {
 			$forward_all_enabled = check_str($_POST["forward_all_enabled"]);
 			$forward_all_destination = check_str($_POST["forward_all_destination"]);
-			$cid_name_prefix = check_str($_POST["cid_name_prefix"]);
+			//$cid_name_prefix = check_str($_POST["cid_name_prefix"]);
 			$call_prompt = check_str($_POST["call_prompt"]);
 			$follow_me_enabled = check_str($_POST["follow_me_enabled"]);
 
@@ -230,7 +230,7 @@ if (count($_POST)>0 && strlen($_POST["persistformvar"]) == 0) {
 			$follow_me->domain_name = $_SESSION['domain_name'];
 			$follow_me->extension_uuid = $extension_uuid;
 			$follow_me->db_type = $db_type;
-			$follow_me->cid_name_prefix = $cid_name_prefix;
+			//$follow_me->cid_name_prefix = $cid_name_prefix;
 			$follow_me->call_prompt = $call_prompt;
 			$follow_me->follow_me_enabled = $follow_me_enabled;
 
@@ -317,7 +317,7 @@ if (count($_POST)>0 && strlen($_POST["persistformvar"]) == 0) {
 	$prep_statement->execute();
 	$result = $prep_statement->fetchAll(PDO::FETCH_NAMED);
 	foreach ($result as &$row) {
-		$cid_name_prefix = $row["cid_name_prefix"];
+		//$cid_name_prefix = $row["cid_name_prefix"];
 		$call_prompt = $row["call_prompt"];
 		$follow_me_enabled = $row["follow_me_enabled"];
 
@@ -576,16 +576,16 @@ if (count($_POST)>0 && strlen($_POST["persistformvar"]) == 0) {
 	echo "</td>\n";
 	echo "</tr>\n";
 
-	echo "<tr>\n";
-	echo "<td class='vncell' valign='top' align='left' nowrap='nowrap'>\n";
-	echo "	".$text['label-cid-prefix'].":\n";
-	echo "</td>\n";
-	echo "<td class='vtable' align='left'>\n";
-	echo "  <input class='formfld' type='text' name='cid_name_prefix' maxlength='255' value='$cid_name_prefix'>\n";
-	echo "<br />\n";
-	echo $text['description-cid-prefix']." \n";
-	echo "</td>\n";
-	echo "</tr>\n";
+	//echo "<tr>\n";
+	//echo "<td class='vncell' valign='top' align='left' nowrap='nowrap'>\n";
+	//echo "	".$text['label-cid-prefix'].":\n";
+	//echo "</td>\n";
+	//echo "<td class='vtable' align='left'>\n";
+	//echo "  <input class='formfld' type='text' name='cid_name_prefix' maxlength='255' value='$cid_name_prefix'>\n";
+	//echo "<br />\n";
+	//echo $text['description-cid-prefix']." \n";
+	//echo "</td>\n";
+	//echo "</tr>\n";
 
 	echo "<tr>\n";
 	echo "<td class='vncell' valign='top' align='left' nowrap>\n";
