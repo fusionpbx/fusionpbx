1dcec7514ccccab5900437dda2d2555f5d5c0bcf
Mark Crane <markjcrane@gmail.com>
2012-12-23 08:27:02 +0000

    Add the new gateway app/gateways/app_languages.php file.

---
+++ app/gateways/app_languages.php
@@ -0,0 +1,237 @@
+<?php
+
+	$text['title-gateways']['en-us'] = 'Gateways';
+	$text['title-gateways']['pt-pt'] = '';
+
+	$text['title-gateway']['en-us'] = 'Gateway';
+	$text['title-gateway']['pt-pt'] = '';
+
+	$text['description-gateway']['en-us'] = 'Gateways provide access into other voice networks. These can be voice providers or other systems that require SIP registration.';
+	$text['description-gateway']['pt-pt'] = '';
+
+	$text['description-gateway-edit']['en-us'] = 'Defines a connections to a SIP Provider or another SIP server.';
+	$text['description-gateway-edit']['pt-pt'] = '';
+
+	$text['label-gateway-name']['en-us'] = 'Gateway';
+	$text['label-gateway-name']['pt-pt'] = '';
+
+	$text['description-gateway-name']['en-us'] = 'Enter the gateway name here.';
+	$text['description-gateway-name']['pt-pt'] = '';
+
+	$text['label-username']['en-us'] = 'Username';
+	$text['label-username']['pt-pt'] = '';
+
+	$text['description-username']['en-us'] = 'Enter the username here.';
+	$text['description-username']['pt-pt'] = '';
+
+	$text['label-password']['en-us'] = 'Password';
+	$text['label-password']['pt-pt'] = '';
+
+	$text['description-password']['en-us'] = 'Enter the password here.';
+	$text['description-password']['pt-pt'] = '';
+
+	$text['label-distinct_to']['en-us'] = 'Distinct To';
+	$text['label-distinct_to']['pt-pt'] = '';
+
+	$text['description-distinct_to']['en-us'] = 'Enter the distinct_to here.';
+	$text['description-distinct_to']['pt-pt'] = '';
+
+	$text['label-auth_username']['en-us'] = 'Auth Username';
+	$text['label-auth_username']['pt-pt'] = '';
+
+	$text['description-auth_username']['en-us'] = 'Enter the auth-username here.';
+	$text['description-auth_username']['pt-pt'] = '';
+
+	$text['label-realm']['en-us'] = 'Realm';
+	$text['label-realm']['pt-pt'] = '';
+
+	$text['description-realm']['en-us'] = 'Enter the realm here.';
+	$text['description-realm']['pt-pt'] = '';
+
+	$text['label-from_user']['en-us'] = 'From User';
+	$text['label-from_user']['pt-pt'] = '';
+
+	$text['description-from_user']['en-us'] = 'Enter the from-user here.';
+	$text['description-from_user']['pt-pt'] = '';
+
+	$text['label-from_domain']['en-us'] = 'From Domain';
+	$text['label-from_domain']['pt-pt'] = '';
+
+	$text['description-from_domain']['en-us'] = 'Enter the from-domain here.';
+	$text['description-from_domain']['pt-pt'] = '';
+
+	$text['label-proxy']['en-us'] = 'Proxy';
+	$text['label-proxy']['pt-pt'] = '';
+
+	$text['description-proxy']['en-us'] = 'Enter the domain or IP address of the proxy.';
+	$text['description-proxy']['pt-pt'] = '';
+
+	$text['label-register_proxy']['en-us'] = 'Register Proxy';
+	$text['label-register_proxy']['pt-pt'] = '';
+
+	$text['description-register_proxy']['en-us'] = 'Enter the register proxy here.';
+	$text['description-register_proxy']['pt-pt'] = '';
+
+	$text['label-outbound_proxy']['en-us'] = 'Outbound Proxy';
+	$text['label-outbound_proxy']['pt-pt'] = '';
+
+	$text['description-outbound_proxy']['en-us'] = 'Enter the outbound proxy here.';
+	$text['description-outbound_proxy']['pt-pt'] = '';
+
+	$text['label-expire_seconds']['en-us'] = 'Expire Seconds';
+	$text['label-expire_seconds']['pt-pt'] = '';
+
+	$text['description-expire_seconds']['en-us'] = 'Enter the expire-seconds here.';
+	$text['description-expire_seconds']['pt-pt'] = '';
+
+	$text['label-register']['en-us'] = 'Register';
+	$text['label-register']['pt-pt'] = '';
+
+	$text['description-register']['en-us'] = 'Choose whether to register.';
+	$text['description-register']['pt-pt'] = '';
+
+	$text['label-register_transport']['en-us'] = 'Register Transport';
+	$text['label-register_transport']['pt-pt'] = '';
+
+	$text['description-register_transport']['en-us'] = 'Choose whether to register-transport.';
+	$text['description-register_transport']['pt-pt'] = '';
+
+	$text['label-retry_seconds']['en-us'] = 'Retry Seconds';
+	$text['label-retry_seconds']['pt-pt'] = '';
+
+	$text['description-retry_seconds']['en-us'] = 'Enter the retry-seconds here.';
+	$text['description-retry_seconds']['pt-pt'] = '';
+
+	$text['label-extension']['en-us'] = 'Extension';
+	$text['label-extension']['pt-pt'] = '';
+
+	$text['description-extension']['en-us'] = 'Enter the extension here.';
+	$text['description-extension']['pt-pt'] = '';
+
+	$text['label-ping']['en-us'] = 'Ping';
+	$text['label-ping']['pt-pt'] = '';
+
+	$text['description-ping']['en-us'] = 'Enter the ping interval here in seconds.';
+	$text['description-ping']['pt-pt'] = '';
+
+	$text['label-caller_id_in_from']['en-us'] = 'Caller Id In From';
+	$text['label-caller_id_in_from']['pt-pt'] = '';
+
+	$text['description-caller_id_in_from']['en-us'] = 'Enter the caller-id-in-from.';
+	$text['description-caller_id_in_from']['pt-pt'] = '';
+
+	$text['label-supress_cng']['en-us'] = 'Supress Cng';
+	$text['label-supress_cng']['pt-pt'] = '';
+
+	$text['description-supress_cng']['en-us'] = 'Enter the supress-cng';
+	$text['description-supress_cng']['pt-pt'] = '';
+
+	$text['label-sip_cid_type']['en-us'] = 'Sip CID Type';
+	$text['label-sip_cid_type']['pt-pt'] = '';
+
+	$text['description-sip_cid_type']['en-us'] = 'Enter the sip cid type: none, pid, and rpid.';
+	$text['description-sip_cid_type']['pt-pt'] = '';
+
+	$text['label-extension_in_contact']['en-us'] = 'Extension In Contact';
+	$text['label-extension_in_contact']['pt-pt'] = '';
+
+	$text['description-extension_in_contact']['en-us'] = 'Enter the Extension In Contact';
+	$text['description-extension_in_contact']['pt-pt'] = '';
+
+	$text['label-context']['en-us'] = 'Context';
+	$text['label-context']['pt-pt'] = '';
+
+	$text['description-context']['en-us'] = 'Enter the context here.';
+	$text['description-context']['pt-pt'] = '';
+
+	$text['label-profile']['en-us'] = 'Profile';
+	$text['label-profile']['pt-pt'] = '';
+
+	$text['description-profile']['en-us'] = 'Enter the profile here.';
+	$text['description-profile']['pt-pt'] = '';
+
+	$text['label-enabled']['en-us'] = 'Enabled';
+	$text['label-enabled']['pt-pt'] = '';
+
+	$text['description-enabled']['en-us'] = 'Enable or Disable the Gateway';
+	$text['description-enabled']['pt-pt'] = '';
+
+	$text['label-description']['en-us'] = 'Description';
+	$text['label-description']['pt-pt'] = '';
+
+	$text['description-description']['en-us'] = 'Enter the description of the gateway here.';
+	$text['description-description']['pt-pt'] = '';
+
+	$text['label-status']['en-us'] = 'Status';
+	$text['label-status']['pt-pt'] = '';
+
+	$text['label-status-stopped']['en-us'] = 'Stopped';
+	$text['label-status-stopped']['pt-pt'] = '';
+
+	$text['label-status-running']['en-us'] = 'Running';
+	$text['label-status-running']['pt-pt'] = '';
+
+	$text['label-action']['en-us'] = 'Action';
+	$text['label-action']['pt-pt'] = '';
+
+	$text['label-action-start']['en-us'] = 'Start';
+	$text['label-action-start']['pt-pt'] = '';
+
+	$text['label-action-stop']['en-us'] = 'Stop';
+	$text['label-action-stop']['pt-pt'] = '';
+
+	$text['label-state']['en-us'] = 'State';
+	$text['label-state']['pt-pt'] = '';
+
+	$text['label-true']['en-us'] = 'true';
+	$text['label-true']['pt-pt'] = '';
+
+	$text['label-false']['en-us'] = 'false';
+	$text['label-false']['pt-pt'] = 'falso';
+
+	$text['button-add']['en-us'] = 'Add';
+	$text['button-add']['pt-pt'] = '';
+
+	$text['button-edit']['en-us'] = 'Edit';
+	$text['button-edit']['pt-pt'] = '';
+
+	$text['button-delete']['en-us'] = 'Delete';
+	$text['button-delete']['pt-pt'] = '';
+
+	$text['button-save']['en-us'] = 'Save';
+	$text['button-save']['pt-pt'] = 'Guardar';
+
+	$text['button-view']['en-us'] = 'View';
+	$text['button-view']['pt-pt'] = '';
+
+	$text['button-back']['en-us'] = 'Back';
+	$text['button-back']['pt-pt'] = 'Voltar';
+
+	$text['button-refresh']['en-us'] = 'Refresh';
+	$text['button-refresh']['pt-pt'] = '';
+
+	$text['button-copy']['en-us'] = 'Copy';
+	$text['button-copy']['pt-pt'] = '';
+
+	$text['button-advanced']['en-us'] = 'Advanced';
+	$text['button-advanced']['pt-pt'] = '';
+
+	$text['button-advanced-show']['en-us'] = 'Show Advanced';
+	$text['button-advanced-show']['pt-pt'] = '';
+
+	$text['confirm-delete']['en-us'] = 'Do you really want to delete this?';
+	$text['confirm-delete']['pt-pt'] = '';
+
+	$text['message-add']['en-us'] = 'Add Completed';
+	$text['message-add']['pt-pt'] = '';
+
+	$text['message-update']['en-us'] = 'Update Completed';
+	$text['message-update']['pt-pt'] = '';
+
+	$text['message-delete']['en-us'] = 'Delete Completed';
+	$text['message-delete']['pt-pt'] = '';
+
+	$text['message-required']['en-us'] = 'Please provide: ';
+	$text['message-required']['pt-pt'] = '';
+
+?>
