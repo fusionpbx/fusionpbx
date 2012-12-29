307c21d21e7437ab35261ba9efa0043ebe632c32
Mark Crane <markjcrane@gmail.com>
2012-12-29 09:17:12 +0000

    Add the initial gui for the  voicemail front end.

---
+++ app/voicemails/app_languages.php
@@ -0,0 +1,100 @@
+<?php
+
+	//Voicemails
+		$text['title-voicemails']['en-us'] = 'Voicemails';
+		$text['title-voicemails']['pt-pt'] = '';
+
+		$text['title-voicemail']['en-us'] = 'Voicemail';
+		$text['title-voicemail']['pt-pt'] = '';
+
+		$text['description-voicemail']['en-us'] = 'Voicemail settings.';
+		$text['description-voicemail']['pt-pt'] = '';
+
+		$text['label-voicemail_id']['en-us'] = 'Voicemail ID';
+		$text['label-voicemail_id']['pt-pt'] = '';
+
+		$text['description-voicemail_id']['en-us'] = 'Enter the Voicemail ID';
+		$text['description-voicemail_id']['pt-pt'] = '';
+
+		$text['label-voicemail_password']['en-us'] = 'Password';
+		$text['label-voicemail_password']['pt-pt'] = '';
+
+		$text['description-voicemail_password']['en-us'] = 'Enter the Password';
+		$text['description-voicemail_password']['pt-pt'] = '';
+
+		$text['label-greeting_id']['en-us'] = 'Greeting ID';
+		$text['label-greeting_id']['pt-pt'] = '';
+
+		$text['description-greeting_id']['en-us'] = 'Enter the Greeting ID';
+		$text['description-greeting_id']['pt-pt'] = '';
+
+		$text['label-voicemail_mail_to']['en-us'] = 'Mail To';
+		$text['label-voicemail_mail_to']['pt-pt'] = '';
+
+		$text['description-voicemail_mail_to']['en-us'] = 'Enter the email address to send voicemail to. ';
+		$text['description-voicemail_mail_to']['pt-pt'] = '';
+
+		$text['label-voicemail_attach_file']['en-us'] = 'Attach File';
+		$text['label-voicemail_attach_file']['pt-pt'] = '';
+
+		$text['description-voicemail_attach_file']['en-us'] = 'Choose whether to attach the file to the email.';
+		$text['description-voicemail_attach_file']['pt-pt'] = '';
+
+		$text['label-voicemail_local_after_email']['en-us'] = 'Local After Email';
+		$text['label-voicemail_local_after_email']['pt-pt'] = '';
+
+		$text['description-voicemail_local_after_email']['en-us'] = 'Choose to keep the voicemail file after sending the email.';
+		$text['description-voicemail_local_after_email']['pt-pt'] = '';
+
+		$text['label-voicemail_enabled']['en-us'] = 'Enabled';
+		$text['label-voicemail_enabled']['pt-pt'] = '';
+
+		$text['description-voicemail_enabled']['en-us'] = 'Select to enable or disable this voicemail.';
+		$text['description-voicemail_enabled']['pt-pt'] = '';
+
+		$text['label-voicemail_description']['en-us'] = 'Description';
+		$text['label-voicemail_description']['pt-pt'] = '';
+
+		$text['description-voicemail_description']['en-us'] = 'Enter the description.';
+		$text['description-voicemail_description']['pt-pt'] = '';
+
+		$text['label-true']['en-us'] = 'true';
+		$text['label-true']['pt-pt'] = '';
+
+		$text['label-false']['en-us'] = 'false';
+		$text['label-false']['pt-pt'] = 'falso';
+
+		$text['button-add']['en-us'] = 'Add';
+		$text['button-add']['pt-pt'] = '';
+
+		$text['button-edit']['en-us'] = 'Edit';
+		$text['button-edit']['pt-pt'] = '';
+
+		$text['button-delete']['en-us'] = 'Delete';
+		$text['button-delete']['pt-pt'] = '';
+
+		$text['button-save']['en-us'] = 'Save';
+		$text['button-save']['pt-pt'] = 'Guardar';
+
+		$text['button-view']['en-us'] = 'View';
+		$text['button-view']['pt-pt'] = '';
+
+		$text['button-back']['en-us'] = 'Back';
+		$text['button-back']['pt-pt'] = 'Voltar';
+
+		$text['confirm-delete']['en-us'] = 'Do you really want to delete this?';
+		$text['confirm-delete']['pt-pt'] = '';
+
+		$text['message-add']['en-us'] = 'Add Completed';
+		$text['message-add']['pt-pt'] = '';
+
+		$text['message-update']['en-us'] = 'Update Completed';
+		$text['message-update']['pt-pt'] = '';
+
+		$text['message-delete']['en-us'] = 'Delete Completed';
+		$text['message-delete']['pt-pt'] = '';
+
+		$text['message-required']['en-us'] = 'Please provide: ';
+		$text['message-required']['pt-pt'] = '';
+
+?>
