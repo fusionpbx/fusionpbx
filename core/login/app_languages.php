07a772c0c68d9c069a1a1cdd8598f89a4839fad9
Mark Crane <markjcrane@gmail.com>
2013-01-05 09:28:33 +0000

    Add the core/login/app_languages.php file.

---
+++ core/login/app_languages.php
@@ -0,0 +1,9 @@
+<?php
+
+	$content_users['label-username']['en-us'] = 'Username';
+	$content_users['label-username']['pt-pt'] = 'Nome de Utilizador';
+
+	$content_users['label-password']['en-us'] = 'Password';
+	$content_users['label-password']['pt-pt'] = 'Palavra passe';
+
+?>
