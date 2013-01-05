25a685e7beae8f1557dbfd126dead99ea6191dc0
Mark Crane <markjcrane@gmail.com>
2013-01-05 05:37:11 +0000

    Update the user update link.

--- index2.php
+++ index2.php
@@ -53,6 +53,7 @@ echo "<br />";
 	//echo "</table>\n";
 	//echo "<br />\n";
 
+//start the user table
 	echo "<table width=\"100%\" border=\"0\" cellpadding=\"7\" cellspacing=\"0\">\n";
 	echo "<tr>\n";
 	echo "	<th class='th' colspan='2' align='left'>".$text['title-table']."&nbsp;</th>\n";
@@ -62,19 +63,24 @@ echo "<br />";
 	echo "		".$text['label-name'].": \n";
 	echo "	</td>\n";
 	echo "	<td class=\"row_style1\">\n";
-	echo "		<a href='".PROJECT_PATH."/app/users/usersupdate.php'>".$_SESSION["username"]."</a> \n";
+	echo "		<a href='".PROJECT_PATH."/app/user/user_edit.php'>".$_SESSION["username"]."</a> \n";
 	echo "	</td>\n";
 	echo "</tr>\n";
-	echo "<tr>\n";
-	echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
-	echo "		".$text['label-voicemail'].": \n";
-	echo "	</td>\n";
-	echo "	<td class=\"row_style1\">\n";
-	echo "		<a href='".PROJECT_PATH."/app/voicemails/voicemail_messages.php'>".$text['label-view-messages']."</a> \n";
-	echo "	</td>\n";
-	echo "</tr>\n";
-	echo "</table>\n";
 
+//voicemail
+	if (file_exists($_SERVER["DOCUMENT_ROOT"].PROJECT_PATH."/app/voicemails/voicemail_messages.php")) {
+		echo "<tr>\n";
+		echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
+		echo "		".$text['label-voicemail'].": \n";
+		echo "	</td>\n";
+		echo "	<td class=\"row_style1\">\n";
+		echo "		<a href='".PROJECT_PATH."/app/voicemails/voicemail_messages.php'>".$text['label-view-messages']."</a> \n";
+		echo "	</td>\n";
+		echo "</tr>\n";
+	}
+
+//end the table
+	echo "</table>\n";
 	echo "<br />\n";
 	echo "<br />\n";
 
