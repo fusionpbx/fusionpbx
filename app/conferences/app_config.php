4ae6cb05538202d4ebe46eeba80f118c408c0f4a
Mark Crane <markjcrane@gmail.com>
2013-01-05 00:20:39 +0000

    For new installs for the older conferences tool don't assign any groups to it. Then the server admin can assign groups to it if they want to use it.

--- app/conferences/app_config.php
+++ app/conferences/app_config.php
@@ -33,9 +33,9 @@
 		$apps[$x]['menu'][0]['parent_uuid'] = 'fd29e39c-c936-f5fc-8e2b-611681b266b5';
 		$apps[$x]['menu'][0]['category'] = 'internal';
 		$apps[$x]['menu'][0]['path'] = '/app/conferences/conferences.php';
-		$apps[$x]['menu'][0]['groups'][] = 'user';
-		$apps[$x]['menu'][0]['groups'][] = 'admin';
-		$apps[$x]['menu'][0]['groups'][] = 'superadmin';
+		//$apps[$x]['menu'][0]['groups'][] = 'user';
+		//$apps[$x]['menu'][0]['groups'][] = 'admin';
+		//$apps[$x]['menu'][0]['groups'][] = 'superadmin';
 
 	//permission details
 		$y = 0;
