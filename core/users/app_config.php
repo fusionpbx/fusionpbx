256afe0e8b1d7f8c4a4121b400302e61f5dc3baa
Mark Crane <markjcrane@gmail.com>
2013-01-05 09:16:46 +0000

    Remove login and logout from the core/users/app_config.php and put it in core/login/app_config.php

--- core/users/app_config.php
+++ core/users/app_config.php
@@ -18,44 +18,8 @@
 		$apps[$x]['description']['pt-pt'] = 'Adicionar, editar, apagar e pesquisa pelos utilizadores.';
 		$apps[$x]['description']['pt-br'] = '';
 
-	//menu details
-		$apps[$x]['menu'][0]['title']['en-us'] = 'Login';
-		$apps[$x]['menu'][0]['title']['es-mx'] = '';
-		$apps[$x]['menu'][0]['title']['de'] = '';
-		$apps[$x]['menu'][0]['title']['de-ch'] = '';
-		$apps[$x]['menu'][0]['title']['de-at'] = '';
-		$apps[$x]['menu'][0]['title']['fr'] = '';
-		$apps[$x]['menu'][0]['title']['fr-ca'] = '';
-		$apps[$x]['menu'][0]['title']['fr-ch'] = '';
-		$apps[$x]['menu'][0]['title']['pt-pt'] = 'Entrar';
-		$apps[$x]['menu'][0]['title']['pt-br'] = '';
-		$apps[$x]['menu'][0]['uuid'] = 'c85bf816-b88d-40fa-8634-11b456928afa';
-		$apps[$x]['menu'][0]['parent_uuid'] = '';
-		$apps[$x]['menu'][0]['category'] = 'internal';
-		$apps[$x]['menu'][0]['path'] = '/login.php';
-		$apps[$x]['menu'][0]['groups'][] = 'public';
-		$apps[$x]['menu'][0]['order'] = '99';
-
-		$apps[$x]['menu'][1]['title']['en-us'] = 'Logout';
-                $apps[$x]['menu'][1]['title']['es-mx'] = '';
-		$apps[$x]['menu'][1]['title']['de'] = '';
-		$apps[$x]['menu'][1]['title']['de-ch'] = '';
-		$apps[$x]['menu'][1]['title']['de-at'] = '';
-		$apps[$x]['menu'][1]['title']['fr'] = '';
-		$apps[$x]['menu'][1]['title']['fr-ca'] = '';
-		$apps[$x]['menu'][1]['title']['fr-ch'] = '';
-		$apps[$x]['menu'][1]['title']['pt-pt'] = 'Sair';
-		$apps[$x]['menu'][1]['title']['pt-br'] = '';
-		$apps[$x]['menu'][1]['uuid'] = '0d29e9f4-0c9b-9d8d-cd2d-454899dc9bc4';
-		$apps[$x]['menu'][1]['parent_uuid'] = '02194288-6d56-6d3e-0b1a-d53a2bc10788';
-		$apps[$x]['menu'][1]['category'] = 'internal';
-		$apps[$x]['menu'][1]['path'] = '/logout.php';
-		$apps[$x]['menu'][1]['groups'][] = 'user';
-		$apps[$x]['menu'][1]['groups'][] = 'admin';
-		$apps[$x]['menu'][1]['groups'][] = 'superadmin';
-
 		$apps[$x]['menu'][2]['title']['en-us'] = 'User Manager';
-                $apps[$x]['menu'][2]['title']['es-mx'] = '';
+		$apps[$x]['menu'][2]['title']['es-mx'] = '';
 		$apps[$x]['menu'][2]['title']['de'] = '';
 		$apps[$x]['menu'][2]['title']['de-ch'] = '';
 		$apps[$x]['menu'][2]['title']['de-at'] = '';
@@ -72,7 +36,7 @@
 		$apps[$x]['menu'][2]['groups'][] = 'superadmin';
 
 		$apps[$x]['menu'][3]['title']['en-us'] = 'Group Manager';
-                $apps[$x]['menu'][3]['title']['es-mx'] = '';
+		$apps[$x]['menu'][3]['title']['es-mx'] = '';
 		$apps[$x]['menu'][3]['title']['de'] = '';
 		$apps[$x]['menu'][3]['title']['de-ch'] = '';
 		$apps[$x]['menu'][3]['title']['de-at'] = '';
