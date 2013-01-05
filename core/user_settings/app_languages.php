9d3a58c26efc60ac55820e46445abd4863284d43
Mark Crane <markjcrane@gmail.com>
2013-01-05 09:59:27 +0000

    Remove change $content_user to $text to be more consistent with the other app_languages.php files.

--- core/user_settings/app_languages.php
+++ core/user_settings/app_languages.php
@@ -1,67 +1,67 @@
 <?php
 
-	$content_users['title']['en-us'] = 'Account Settings';
-	$content_users['title']['pt-pt'] = 'Configurações de Conta';
+	$text['title']['en-us'] = 'Account Settings';
+	$text['title']['pt-pt'] = 'Configurações de Conta';
 
-	$content_users['description']['en-us'] = 'Edit account information.';
-	$content_users['description']['pt-pt'] = 'Editar informações de conta.';
+	$text['description']['en-us'] = 'Edit account information.';
+	$text['description']['pt-pt'] = 'Editar informações de conta.';
 
-	$content_users['button-back']['en-us'] = 'Back';
-	$content_users['button-back']['pt-pt'] = 'Voltar';
+	$text['button-back']['en-us'] = 'Back';
+	$text['button-back']['pt-pt'] = 'Voltar';
 
-	$content_users['button-save']['en-us'] = 'Save';
-	$content_users['button-save']['pt-pt'] = 'Guardar';
+	$text['button-save']['en-us'] = 'Save';
+	$text['button-save']['pt-pt'] = 'Guardar';
 
-	$content_users['table-title']['en-us'] = 'User Info';
-	$content_users['table-title']['pt-pt'] = 'Informações de Utilizador';
+	$text['table-title']['en-us'] = 'User Info';
+	$text['table-title']['pt-pt'] = 'Informações de Utilizador';
 
-	$content_users['label-username']['en-us'] = 'Username';
-	$content_users['label-username']['pt-pt'] = 'Nome de Utilizador';
+	$text['label-username']['en-us'] = 'Username';
+	$text['label-username']['pt-pt'] = 'Nome de Utilizador';
 
-	$content_users['label-password']['en-us'] = 'Password';
-	$content_users['label-password']['pt-pt'] = 'Palavra passe';
+	$text['label-password']['en-us'] = 'Password';
+	$text['label-password']['pt-pt'] = 'Palavra passe';
 
-	$content_users['label-confirm-password']['en-us'] = 'Confirm Password';
-	$content_users['label-confirm-password']['pt-pt'] = 'Confirme Palavra Chave';
+	$text['label-confirm-password']['en-us'] = 'Confirm Password';
+	$text['label-confirm-password']['pt-pt'] = 'Confirme Palavra Chave';
 
-	$content_users['label-extension']['en-us'] = 'Extension';
-	$content_users['label-extension']['pt-pt'] = 'Extensão';
+	$text['label-extension']['en-us'] = 'Extension';
+	$text['label-extension']['pt-pt'] = 'Extensão';
 
-	$content_users['table2-title']['en-us'] = 'Additional Info';
-	$content_users['table2-title']['pt-pt'] = 'Informações Adicionais';
+	$text['table2-title']['en-us'] = 'Additional Info';
+	$text['table2-title']['pt-pt'] = 'Informações Adicionais';
 
-	$content_users['label-status']['en-us'] = 'Status';
-	$content_users['label-status']['pt-pt'] = 'Estado';
+	$text['label-status']['en-us'] = 'Status';
+	$text['label-status']['pt-pt'] = 'Estado';
 
-	$content_users['description-status']['en-us'] = 'Select a the user status.';
-	$content_users['description-status']['pt-pt'] = 'Selecione um estado para o utilizador.';
+	$text['description-status']['en-us'] = 'Select a the user status.';
+	$text['description-status']['pt-pt'] = 'Selecione um estado para o utilizador.';
 
-	$content_users['check-available-status']['en-us'] = 'Available';
-	$content_users['check-available-status']['pt-pt'] = 'Disponível';
+	$text['check-available-status']['en-us'] = 'Available';
+	$text['check-available-status']['pt-pt'] = 'Disponível';
 
-	$content_users['check-available-ondemand-status']['en-us'] = 'Available (On Demand)';
-	$content_users['check-available-ondemand-status']['pt-pt'] = 'Disponível (A pedido)';
+	$text['check-available-ondemand-status']['en-us'] = 'Available (On Demand)';
+	$text['check-available-ondemand-status']['pt-pt'] = 'Disponível (A pedido)';
 
-	$content_users['check-loggedout-status']['en-us'] = 'Logged Out';
-	$content_users['check-loggedout-status']['pt-pt'] = 'Desligado';
+	$text['check-loggedout-status']['en-us'] = 'Logged Out';
+	$text['check-loggedout-status']['pt-pt'] = 'Desligado';
 
-	$content_users['check-onbreak-status']['en-us'] = 'On Break';
-	$content_users['check-onbreak-status']['pt-pt'] = 'Em Pausa';
+	$text['check-onbreak-status']['en-us'] = 'On Break';
+	$text['check-onbreak-status']['pt-pt'] = 'Em Pausa';
 
-	$content_users['check-do-not-disturb-status']['en-us'] = 'Do Not Disturb';
-	$content_users['check-do-not-disturb-status']['pt-pt'] = 'Não Perturbar';
+	$text['check-do-not-disturb-status']['en-us'] = 'Do Not Disturb';
+	$text['check-do-not-disturb-status']['pt-pt'] = 'Não Perturbar';
 
-	$content_users['label-time']['en-us'] = 'Time Zone';
-	$content_users['label-time']['pt-pt'] = 'Fuso Horário';
+	$text['label-time']['en-us'] = 'Time Zone';
+	$text['label-time']['pt-pt'] = 'Fuso Horário';
 
-	$content_users['description-timezone']['en-us'] = 'Select the default time zone.';
-	$content_users['description-timezone']['pt-pt'] = 'Selecione o fuso horário padrão.';
+	$text['description-timezone']['en-us'] = 'Select the default time zone.';
+	$text['description-timezone']['pt-pt'] = 'Selecione o fuso horário padrão.';
 
-	$content_users['confirm-password']['en-us'] = 'Passwords did not match.';
-	$content_users['confirm-password']['pt-pt'] = 'As palavras chave não coincidem.';
+	$text['confirm-password']['en-us'] = 'Passwords did not match.';
+	$text['confirm-password']['pt-pt'] = 'As palavras chave não coincidem.';
 
-	$content_users['confirm-update']['en-us'] = 'Update Complete';
-	$content_users['confirm-update']['pt-pt'] = 'Actualização Completa';
+	$text['confirm-update']['en-us'] = 'Update Complete';
+	$text['confirm-update']['pt-pt'] = 'Actualização Completa';
 
 	$text['title-user-settings']['en-us'] = 'User Information';
 	$text['title-user-settings']['pt-pt'] = 'Informação do Utilizador';
