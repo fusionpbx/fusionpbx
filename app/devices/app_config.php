648ef006ccc391c3345e99e7d2fddb2ecebdeb5b
Mark Crane <markjcrane@gmail.com>
2013-03-20 16:53:37 +0000

    Rename the hardware phones to devices.

--- app/devices/app_config.php
+++ app/devices/app_config.php
@@ -1,13 +1,13 @@
 <?php
 	//application details
-		$apps[$x]['name'] = "Phones";
+		$apps[$x]['name'] = "Devices";
 		$apps[$x]['uuid'] = '4efa1a1a-32e7-bf83-534b-6c8299958a8e';
 		$apps[$x]['category'] = 'Switch';;
 		$apps[$x]['subcategory'] = '';
 		$apps[$x]['version'] = '';
 		$apps[$x]['license'] = 'Mozilla Public License 1.1';
 		$apps[$x]['url'] = 'http://www.fusionpbx.com';
-		$apps[$x]['description']['en-us'] = 'Phone provisioning list.';
+		$apps[$x]['description']['en-us'] = 'Devices for provisioning.';
 		$apps[$x]['description']['es-mx'] = '';
 		$apps[$x]['description']['de'] = '';
 		$apps[$x]['description']['de-ch'] = '';
@@ -19,7 +19,7 @@
 		$apps[$x]['description']['pt-br'] = '';
 
 	//menu details
-		$apps[$x]['menu'][0]['title']['en-us'] = 'Phones';
+		$apps[$x]['menu'][0]['title']['en-us'] = 'Devices';
 		$apps[$x]['menu'][0]['title']['es-mx'] = '';
 		$apps[$x]['menu'][0]['title']['de'] = '';
 		$apps[$x]['menu'][0]['title']['de-ch'] = '';
@@ -32,40 +32,42 @@
 		$apps[$x]['menu'][0]['uuid'] = 'f9dce498-b7f9-740f-e592-9e8ff3dac2a0';
 		$apps[$x]['menu'][0]['parent_uuid'] = 'bc96d773-ee57-0cdd-c3ac-2d91aba61b55';
 		$apps[$x]['menu'][0]['category'] = 'internal';
-		$apps[$x]['menu'][0]['path'] = '/app/hardware_phones/hardware_phones.php';
+		$apps[$x]['menu'][0]['path'] = '/app/devices/devices.php';
 		$apps[$x]['menu'][0]['groups'][] = 'admin';
 		$apps[$x]['menu'][0]['groups'][] = 'superadmin';
 
 	//permission details
-		$apps[$x]['permissions'][0]['name'] = 'phone_view';
+		$apps[$x]['permissions'][0]['name'] = 'device_view';
 		$apps[$x]['permissions'][0]['groups'][] = 'admin';
 		$apps[$x]['permissions'][0]['groups'][] = 'superadmin';
 
-		$apps[$x]['permissions'][1]['name'] = 'phone_add';
+		$apps[$x]['permissions'][1]['name'] = 'device_add';
 		$apps[$x]['permissions'][1]['groups'][] = 'admin';
 		$apps[$x]['permissions'][1]['groups'][] = 'superadmin';
 
-		$apps[$x]['permissions'][2]['name'] = 'phone_edit';
+		$apps[$x]['permissions'][2]['name'] = 'device_edit';
 		$apps[$x]['permissions'][2]['groups'][] = 'admin';
 		$apps[$x]['permissions'][2]['groups'][] = 'superadmin';
 
-		$apps[$x]['permissions'][3]['name'] = 'phone_delete';
+		$apps[$x]['permissions'][3]['name'] = 'device_delete';
 		$apps[$x]['permissions'][3]['groups'][] = 'admin';
 		$apps[$x]['permissions'][3]['groups'][] = 'superadmin';
 
 	//schema details
 		$y = 0; //table array index
 		$z = 0; //field array index
-		$apps[$x]['db'][$y]['table'] = 'v_hardware_phones';
+		$apps[$x]['db'][$y]['table'] = 'v_devices';
+		//$apps[$x]['db'][$y]['table']['deprecated'] = 'v_hardware_phones';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'id';
-		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'hardware_phone_id';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'hardware_device_id';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'serial';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'integer';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'INT NOT NULL AUTO_INCREMENT';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = 'true';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'hardware_phone_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'device_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'hardware_device_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
@@ -86,47 +88,99 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = 'true';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'phone_mac_address';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'device_mac_address';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'phone_mac_address';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'phone_label';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'device_label';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'phone_label';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'phone_vendor';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'device_vendor';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'phone_vendor';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'phone_model';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'device_model';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'phone_model';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'phone_firmware_version';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'device_firmware_version';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'phone_firmware_version';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'phone_provision_enable';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'device_provision_enable';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'phone_provision_enable';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'phone_template';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'device_template';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'phone_template';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'phone_username';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'device_username';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'phone_username';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'phone_password';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'device_password';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'phone_password';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'phone_time_zone';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'device_time_zone';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'phone_time_zone';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'phone_description';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'device_description';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'phone_description';
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
+
+		$y = 1; //table array index
+		$z = 0; //field array index
+		$apps[$x]['db'][$y]['table'] = 'v_device_extensions';
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'device_extension_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'primary';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'domain_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_domains';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'domain_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'device_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_devices';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'device_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'extension_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_extensions';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'extension_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'device_line';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 
--- app/devices/devices.php
+++ app/devices/devices.php
@@ -25,7 +25,7 @@
 require_once "root.php";
 require_once "includes/require.php";
 require_once "includes/checkauth.php";
-if (permission_exists('phone_view')) {
+if (permission_exists('device_view')) {
 	//access granted
 }
 else {
@@ -46,20 +46,20 @@ echo "		<br>";
 
 echo "<table width='100%' border='0'>\n";
 echo "<tr>\n";
-echo "<td width='50%' nowrap='nowrap' align='left'><b>Hardware Phone List</b></td>\n";
+echo "<td width='50%' nowrap='nowrap' align='left'><b>Devices</b></td>\n";
 echo "<td width='50%' align='right'>&nbsp;</td>\n";
 echo "</tr>\n";
 echo "<tr>\n";
 echo "<td colspan='2' align='left'>\n";
 
-echo "Phones in this list are automatically added to the list when they contact the provisioning \n";
+echo "Devices in this list are added to the list when they contact the provisioning \n";
 echo "server or added manually by an administrator. \n";
-echo "Items in this list can be assigned from the extensions page.<br /><br />\n";
+echo "Items in this list are assigned from the extensions page.<br /><br />\n";
 echo "</td>\n";
 echo "</tr>\n";
 echo "</tr></table>\n";
 
-$sql = "select * from v_hardware_phones ";
+$sql = "select * from v_devices ";
 $sql .= "where domain_uuid = '$domain_uuid' ";
 if (strlen($order_by)> 0) { $sql .= "order by $order_by $order "; }
 $prep_statement = $db->prepare(check_sql($sql));
@@ -74,7 +74,7 @@ if (strlen($page) == 0) { $page = 0; $_GET['page'] = 0; }
 list($paging_controls, $rows_per_page, $var_3) = paging($num_rows, $param, $rows_per_page); 
 $offset = $rows_per_page * $page; 
 
-$sql = "select * from v_hardware_phones ";
+$sql = "select * from v_devices ";
 $sql .= "where domain_uuid = '$domain_uuid' ";
 if (strlen($order_by)> 0) { $sql .= "order by $order_by $order "; }
 $sql .= " limit $rows_per_page offset $offset ";
@@ -91,37 +91,37 @@ $row_style["1"] = "row_style1";
 echo "<div align='center'>\n";
 echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
 echo "<tr>\n";
-echo th_order_by('phone_mac_address', 'MAC Address', $order_by, $order);
-echo th_order_by('phone_template', 'Template', $order_by, $order);
-echo th_order_by('phone_vendor', 'Vendor', $order_by, $order);
-//echo th_order_by('phone_model', 'Model', $order_by, $order);
-echo th_order_by('phone_provision_enable', 'Enabled', $order_by, $order);
-echo th_order_by('phone_description', 'Description', $order_by, $order);
+echo th_order_by('device_mac_address', 'MAC Address', $order_by, $order);
+echo th_order_by('device_template', 'Template', $order_by, $order);
+echo th_order_by('device_vendor', 'Vendor', $order_by, $order);
+//echo th_order_by('device_model', 'Model', $order_by, $order);
+echo th_order_by('device_provision_enable', 'Enabled', $order_by, $order);
+echo th_order_by('device_description', 'Description', $order_by, $order);
 echo "<td align='right' width='42'>\n";
-if (permission_exists('phone_add')) {
-	echo "	<a href='hardware_phone_edit.php' alt='add'>$v_link_label_add</a>\n";
+if (permission_exists('device_add')) {
+	echo "	<a href='device_edit.php' alt='add'>$v_link_label_add</a>\n";
 }
 echo "</td>\n";
 echo "<tr>\n";
 
 if ($result_count > 0) { //no results
 	foreach($result as $row) {
-		$phone_mac_address = $row[phone_mac_address];
-		$phone_mac_address = substr($phone_mac_address, 0,2).'-'.substr($phone_mac_address, 2,2).'-'.substr($phone_mac_address, 4,2).'-'.substr($phone_mac_address, 6,2).'-'.substr($phone_mac_address, 8,2).'-'.substr($phone_mac_address, 10,2);
+		$device_mac_address = $row[device_mac_address];
+		$device_mac_address = substr($device_mac_address, 0,2).'-'.substr($device_mac_address, 2,2).'-'.substr($device_mac_address, 4,2).'-'.substr($device_mac_address, 6,2).'-'.substr($device_mac_address, 8,2).'-'.substr($device_mac_address, 10,2);
 
 		echo "<tr >\n";
-		echo "	<td valign='top' class='".$row_style[$c]."'>".$phone_mac_address."&nbsp;</td>\n";
-		echo "	<td valign='top' class='".$row_style[$c]."'>".$row['phone_template']."&nbsp;</td>\n";
-		echo "	<td valign='top' class='".$row_style[$c]."'>".$row['phone_vendor']."&nbsp;</td>\n";
-		//echo "	<td valign='top' class='".$row_style[$c]."'>".$row['phone_mode'l]."&nbsp;</td>\n";
-		echo "	<td valign='top' class='".$row_style[$c]."' width='10px'>".$row['phone_provision_enable']."&nbsp;</td>\n";
-		echo "	<td valign='top' class='row_stylebg'>".$row['phone_description']."&nbsp;</td>\n";
+		echo "	<td valign='top' class='".$row_style[$c]."'>".$device_mac_address."&nbsp;</td>\n";
+		echo "	<td valign='top' class='".$row_style[$c]."'>".$row['device_template']."&nbsp;</td>\n";
+		echo "	<td valign='top' class='".$row_style[$c]."'>".$row['device_vendor']."&nbsp;</td>\n";
+		//echo "	<td valign='top' class='".$row_style[$c]."'>".$row['device_mode'l]."&nbsp;</td>\n";
+		echo "	<td valign='top' class='".$row_style[$c]."' width='10px'>".$row['device_provision_enable']."&nbsp;</td>\n";
+		echo "	<td valign='top' class='row_stylebg'>".$row['device_description']."&nbsp;</td>\n";
 		echo "	<td valign='top' align='right'>\n";
-		if (permission_exists('phone_edit')) {
-			echo "		<a href='hardware_phone_edit.php?id=".$row['hardware_phone_uuid']."' alt='edit'>$v_link_label_edit</a>\n";
+		if (permission_exists('device_edit')) {
+			echo "		<a href='device_edit.php?id=".$row['device_uuid']."' alt='edit'>$v_link_label_edit</a>\n";
 		}
-		if (permission_exists('phone_delete')) {
-			echo "		<a href='hardware_phone_delete.php?id=".$row['hardware_phone_uuid']."' alt='delete' onclick=\"return confirm('Do you really want to delete this?')\">$v_link_label_delete</a>\n";
+		if (permission_exists('device_delete')) {
+			echo "		<a href='hardware_device_delete.php?id=".$row['device_uuid']."' alt='delete' onclick=\"return confirm('Do you really want to delete this?')\">$v_link_label_delete</a>\n";
 		}
 		echo "	</td>\n";
 		echo "</tr>\n";
@@ -137,8 +137,8 @@ echo "	<tr>\n";
 echo "		<td width='33.3%' nowrap>&nbsp;</td>\n";
 echo "		<td width='33.3%' align='center' nowrap>$paging_controls</td>\n";
 echo "		<td width='33.3%' align='right'>\n";
-if (permission_exists('phone_add')) {
-	echo "			<a href='hardware_phone_edit.php' alt='add'>$v_link_label_add</a>\n";
+if (permission_exists('device_add')) {
+	echo "			<a href='device_edit.php' alt='add'>$v_link_label_add</a>\n";
 }
 echo "		</td>\n";
 echo "	</tr>\n";
