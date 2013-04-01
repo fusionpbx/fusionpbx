4530aa299e11eb2dc1a78e2b6454ec2f78a798ea
Mark Crane <markjcrane@gmail.com>
2013-04-01 20:25:40 +0000

    Change tables to schema in the virtual tables.

--- app/schemas/app_config.php
+++ app/schemas/app_config.php
@@ -1,6 +1,6 @@
 <?php
 	//application details
-		$apps[$x]['name'] = "Virtual Tables";
+		$apps[$x]['name'] = "Schemas";
 		$apps[$x]['uuid'] = '8e98d409-8134-d33c-be70-fcee63d67a64';
 		$apps[$x]['category'] = 'System';
 		$apps[$x]['subcategory'] = '';
@@ -19,7 +19,7 @@
 		$apps[$x]['description']['pt-br'] = '';
 
 	//menu details
-		$apps[$x]['menu'][0]['title']['en-us'] = 'Tables';
+		$apps[$x]['menu'][0]['title']['en-us'] = 'Schemas';
 		$apps[$x]['menu'][0]['title']['es-mx'] = '';
 		$apps[$x]['menu'][0]['title']['de'] = '';
 		$apps[$x]['menu'][0]['title']['de-ch'] = '';
@@ -32,47 +32,47 @@
 		$apps[$x]['menu'][0]['uuid'] = '6be94b46-2126-947f-2365-0bea23651a6b';
 		$apps[$x]['menu'][0]['parent_uuid'] = 'fd29e39c-c936-f5fc-8e2b-611681b266b5';
 		$apps[$x]['menu'][0]['category'] = 'internal';
-		$apps[$x]['menu'][0]['path'] = '/app/tables/tables.php';
+		$apps[$x]['menu'][0]['path'] = '/app/schemas/schemas.php';
 		$apps[$x]['menu'][0]['groups'][] = 'admin';
 		$apps[$x]['menu'][0]['groups'][] = 'superadmin';
 
 	//permission details
-		$apps[$x]['permissions'][0]['name'] = 'table_view';
+		$apps[$x]['permissions'][0]['name'] = 'schema_view';
 		$apps[$x]['permissions'][0]['groups'][] = 'admin';
 		$apps[$x]['permissions'][0]['groups'][] = 'superadmin';
 
-		$apps[$x]['permissions'][1]['name'] = 'table_add';
+		$apps[$x]['permissions'][1]['name'] = 'schema_add';
 		$apps[$x]['permissions'][1]['groups'][] = 'admin';
 		$apps[$x]['permissions'][1]['groups'][] = 'superadmin';
 
-		$apps[$x]['permissions'][2]['name'] = 'table_edit';
+		$apps[$x]['permissions'][2]['name'] = 'schema_edit';
 		$apps[$x]['permissions'][2]['groups'][] = 'admin';
 		$apps[$x]['permissions'][2]['groups'][] = 'superadmin';
 
-		$apps[$x]['permissions'][3]['name'] = 'table_delete';
+		$apps[$x]['permissions'][3]['name'] = 'schema_delete';
 		$apps[$x]['permissions'][3]['groups'][] = 'admin';
 		$apps[$x]['permissions'][3]['groups'][] = 'superadmin';
 
-		$apps[$x]['permissions'][4]['name'] = 'table_data_view';
+		$apps[$x]['permissions'][4]['name'] = 'schema_data_view';
 		$apps[$x]['permissions'][4]['groups'][] = 'admin';
 		$apps[$x]['permissions'][4]['groups'][] = 'superadmin';
 
-		$apps[$x]['permissions'][5]['name'] = 'table_data_add';
+		$apps[$x]['permissions'][5]['name'] = 'schema_data_add';
 		$apps[$x]['permissions'][5]['groups'][] = 'admin';
 		$apps[$x]['permissions'][5]['groups'][] = 'superadmin';
 
-		$apps[$x]['permissions'][6]['name'] = 'table_data_edit';
+		$apps[$x]['permissions'][6]['name'] = 'schema_data_edit';
 		$apps[$x]['permissions'][6]['groups'][] = 'admin';
 		$apps[$x]['permissions'][6]['groups'][] = 'superadmin';
 
-		$apps[$x]['permissions'][7]['name'] = 'table_data_delete';
+		$apps[$x]['permissions'][7]['name'] = 'schema_data_delete';
 		$apps[$x]['permissions'][7]['groups'][] = 'admin';
 		$apps[$x]['permissions'][7]['groups'][] = 'superadmin';
 
 	//schema details
 		$y = 0; //table array index
 		$z = 0; //field array index
-		$apps[$x]['db'][$y]['table']['text'] = 'v_table_data';
+		$apps[$x]['db'][$y]['table']['text'] = 'v_schema_data';
 		$apps[$x]['db'][$y]['table']['deprecated'] = 'v_virtual_table_data';
 
 		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'id';
@@ -83,7 +83,7 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = 'true';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_data_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_data_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_data_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
@@ -110,18 +110,17 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = 'true';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
 		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_tables';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'table_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_schemas';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'schema_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'data_row_uuid';
-		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_data_row_id';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_data_row_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
@@ -162,7 +161,7 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = 'true';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_parent_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_parent_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_parent_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
@@ -171,13 +170,12 @@
 		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'data_parent_row_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_data_parent_row_uuid';
-		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_data_parent_row_id';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 
 		$y = 1; //table array index
 		$z = 0; //field array index
-		$apps[$x]['db'][$y]['table']['text'] = 'v_table_name_values';
+		$apps[$x]['db'][$y]['table']['text'] = 'v_schema_name_values';
 		$apps[$x]['db'][$y]['table']['deprecated'] = 'v_virtual_table_name_values';
 
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'virtual_table_data_types_name_value_id';
@@ -187,9 +185,9 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = 'true';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_name_value_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_name_value_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_name_value_uuid';
-		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_data_types_name_value_uuid';
+		//$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_data_types_name_value_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
@@ -215,14 +213,14 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = 'true';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
 		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_tables';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'table_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_schemas';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'schema_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_field_id';
@@ -231,14 +229,14 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = 'true';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_field_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_field_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_field_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
 		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_table_fields';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'table_field_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_schema_fields';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'schema_field_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'data_types_name';
@@ -253,10 +251,10 @@
 
 		$y = 2; //table array index
 		$z = 0; //field array index
-		$apps[$x]['db'][$y]['table']['text'] = 'v_table_fields';
+		$apps[$x]['db'][$y]['table']['text'] = 'v_schema_fields';
 		$apps[$x]['db'][$y]['table']['deprecated'] = 'v_virtual_table_fields';
 
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_field_id';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_field_id';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_field_id';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'serial';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'integer';
@@ -264,7 +262,7 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = 'true';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_field_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_field_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_field_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
@@ -291,14 +289,14 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = 'true';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
 		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_tables';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'table_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_schemas';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'schema_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'field_label';
@@ -352,14 +350,13 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'field_description';
-		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'field_desc';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_field_description';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 
 		$y = 3; //table array index
 		$z = 0; //field array index
-		$apps[$x]['db'][$y]['table']['text'] = 'v_tables';
+		$apps[$x]['db'][$y]['table']['text'] = 'v_schemas';
 		$apps[$x]['db'][$y]['table']['deprecated'] = 'v_virtual_tables';
 
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'virtual_table_id';
@@ -369,7 +366,7 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = 'true';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
@@ -391,27 +388,27 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = 'true';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_category';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_category';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_category';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_label';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_label';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_label';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_name';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_name';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_name';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_auth';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_auth';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_auth';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_captcha';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_captcha';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_captcha';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
@@ -421,19 +418,18 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = 'true';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_parent_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_parent_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_parent_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
 		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_tables';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'table_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_schemas';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'schema_uuid';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'table_description';
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = 'schema_description';
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_description';
-		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = 'virtual_table_desc';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = '';
 
