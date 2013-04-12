7fde8c93d1995fbe0210cca0c87b01a4747a92cf
Mark Crane <markjcrane@gmail.com>
2013-04-12 18:27:56 +0000

    Add a mkdir function that is compatible to both unix and windows.

---
+++ includes/install/scripts/resources/functions/mkdir.lua
@@ -0,0 +1,14 @@
+
+--add the mkdir function
+	function mkdir(dir)
+		dir = dir:gsub("\\", "/");
+		if (package.config:sub(1,1) == "/") then
+			--unix
+			cmd = [[mkdir -p "]] .. dir .. [["]];
+		elseif (package.config:sub(1,1) == [[\]]) then
+			--windows
+			cmd = [[mkdir "]] .. dir .. [["]];
+		end
+		os.execute(cmd);
+		return cmd;
+	end
