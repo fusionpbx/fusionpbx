f420efe32d428fd228d7afb86f7cffdc77e94f7b
Mark Crane <markjcrane@gmail.com>
2013-05-02 03:19:38 +0000

    Fix the Call detail records download and play ob_clean(); seems to have done the trick.

--- app/recordings/recordings.php
+++ app/recordings/recordings.php
@@ -74,6 +74,7 @@ require_once "includes/paging.php";
 				header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 				header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 				header("Content-Length: " . filesize($_SESSION['switch']['recordings']['dir'].'/'.base64_decode($_GET['filename'])));
+				ob_clean();
 				fpassthru($fd);
 			}
 		}
