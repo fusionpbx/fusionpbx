37f4a885350ba056e3d1cd791ffc996c3e041cd3
Mark Crane <markjcrane@gmail.com>
2013-05-02 01:10:45 +0000

    Make the lua scripts that use config.lua to use the new config function.

--- includes/install/scripts/eavesdrop.lua
+++ includes/install/scripts/eavesdrop.lua
@@ -29,7 +29,8 @@ extension = argv[1];
 
 --include config.lua
 	scripts_dir = string.sub(debug.getinfo(1).source,2,string.len(debug.getinfo(1).source)-(string.len(argv[0])+1));
-	dofile(scripts_dir.."/resources/config.lua");
+	dofile(scripts_dir.."/resources/functions/config.lua");
+	dofile(config());
 
 --add the file_exists function
 	dofile(scripts_dir.."/resources/functions/file_exists.lua");
--- includes/install/scripts/park.lua
+++ includes/install/scripts/park.lua
@@ -16,7 +16,8 @@
 
 --include config.lua
 	scripts_dir = string.sub(debug.getinfo(1).source,2,string.len(debug.getinfo(1).source)-(string.len(argv[0])+1));
-	dofile(scripts_dir.."/resources/config.lua");
+	dofile(scripts_dir.."/resources/functions/config.lua");
+	dofile(config());
 
 --connect to the database
 	--dbh = freeswitch.Dbh("core:core"); -- when using sqlite
