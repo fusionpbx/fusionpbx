fb678b58936fe73f4183d2d23abf358057955933
Nate Jones <git@reliberate.com>
2013-05-04 02:53:37 +0000

    Multilinguified (SVG excluded).

---
+++ app/traffic_graph/app_languages.php
@@ -0,0 +1,23 @@
+<?php
+
+	//traffic_graph
+
+		$text['title-traffic_graph']['en-us'] = 'Traffic Graph';
+		$text['title-traffic_graph']['pt-pt'] = '';
+
+		$text['header-traffic_graph']['en-us'] = 'Traffic Graph';
+		$text['header-traffic_graph']['pt-pt'] = '';
+
+		$text['description-traffic_graph']['en-us'] = 'A browser (or plugin) that supports Scalable Vector Graphics (SVG) is required to view the traffic graph below.';
+		$text['description-traffic_graph']['pt-pt'] = '';
+
+		$text['label-interface']['en-us'] = 'Interface';
+		$text['label-interface']['pt-pt'] = '';
+
+		$text['description-no_svg']['en-us'] = 'Unfortunately your browser does not currently support Scalable Vector Graphics (SVG).  To view the traffic graph, please use a browser (or plugin) that supports Scalable Vector Graphics (SVG).';
+		$text['description-no_svg']['pt-pt'] = '';
+
+
+
+
+?>
