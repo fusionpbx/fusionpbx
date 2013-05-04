b4d8c820553202a88f58660e568fbc260170b344
Nate Jones <git@reliberate.com>
2013-05-04 00:35:21 +0000

    Multilingualified the Extensions app.  Added ability to preset the new extension when the Copy button is clicked.

---
+++ app/extensions/app_languages.php
@@ -0,0 +1,311 @@
+<?php
+
+	//Extensions
+
+		$text['title-extensions']['en-us'] = 'Extensions';
+		$text['title-extensions']['pt-pt'] = '';
+
+		$text['title-extension-edit']['en-us'] = 'Extension Edit';
+		$text['title-extension-edit']['pt-pt'] = '';
+
+		$text['title-extension-add']['en-us'] = 'Extension Add';
+		$text['title-extension-add']['pt-pt'] = '';
+
+		$text['header-extensions']['en-us'] = 'Extensions';
+		$text['header-extensions']['pt-pt'] = '';
+
+		$text['description-extensions']['en-us'] = 'Use this to configure your SIP extensions.';
+		$text['description-extensions']['pt-pt'] = '';
+
+		$text['header-extension-edit']['en-us'] = 'Extension Edit';
+		$text['header-extension-edit']['pt-pt'] = '';
+
+		$text['header-extension-add']['en-us'] = 'Extension Add';
+		$text['header-extension-add']['pt-pt'] = '';
+
+		$text['label-extension']['en-us'] = 'Extension';
+		$text['label-extension']['pt-pt'] = '';
+
+		$text['description-extension']['en-us'] = 'Enter the alphanumeric extension. The default configuration allows 2 - 7 digit extensions.';
+		$text['description-extension']['pt-pt'] = '';
+
+		$text['label-number_alias']['en-us'] = 'Number Alias';
+		$text['label-number_alias']['pt-pt'] = '';
+
+		$text['description-number_alias']['en-us'] = 'If the extension is numeric then number alias is optional.';
+		$text['description-number_alias']['pt-pt'] = '';
+
+		$text['label-password']['en-us'] = 'Password';
+		$text['label-password']['pt-pt'] = '';
+
+		$text['description-password']['en-us'] = 'Enter the password here.';
+		$text['description-password']['pt-pt'] = '';
+
+		$text['label-range']['en-us'] = 'Range';
+		$text['label-range']['pt-pt'] = '';
+
+		$text['description-range']['en-us'] = 'Enter the number of extensions to create. Increments each extension by 1.';
+		$text['description-range']['pt-pt'] = '';
+
+		$text['checkbox-range']['en-us'] = 'Auto-generate user with extension as login name';
+		$text['checkbox-range']['pt-pt'] = '';
+
+		$text['label-user_list']['en-us'] = 'User List';
+		$text['label-user_list']['pt-pt'] = '';
+
+		$text['description-user_list']['en-us'] = 'Assign the users that are assigned to this extension. ';
+		$text['description-user_list']['pt-pt'] = '';
+
+		$text['label-vm_password']['en-us'] = 'Voicemail Password';
+		$text['label-vm_password']['pt-pt'] = '';
+
+		$text['description-vm_password']['en-us'] = 'Enter the numeric voicemail  password here.';
+		$text['description-vm_password']['pt-pt'] = '';
+
+		$text['label-accountcode']['en-us'] = 'Account Code';
+		$text['label-accountcode']['pt-pt'] = '';
+
+		$text['description-accountcode']['en-us'] = 'Enter the account code here.';
+		$text['description-accountcode']['pt-pt'] = '';
+
+		$text['label-effective_caller_id_name']['en-us'] = 'Effective Caller ID Name';
+		$text['label-effective_caller_id_name']['pt-pt'] = '';
+
+		$text['description-effective_caller_id_name']['en-us'] = 'Enter the internal caller ID name here.';
+		$text['description-effective_caller_id_name']['pt-pt'] = '';
+
+		$text['label-effective_caller_id_number']['en-us'] = 'Effective Caller ID Number';
+		$text['label-effective_caller_id_number']['pt-pt'] = '';
+
+		$text['description-effective_caller_id_number']['en-us'] = 'Enter the internal caller ID number here.';
+		$text['description-effective_caller_id_number']['pt-pt'] = '';
+
+		$text['label-outbound_caller_id_name']['en-us'] = 'Outbound Caller ID Name';
+		$text['label-outbound_caller_id_name']['pt-pt'] = '';
+
+		$text['description-outbound_caller_id_name-select']['en-us'] = 'Select the external (public) caller ID name.';
+		$text['description-outbound_caller_id_name-select']['pt-pt'] = '';
+
+		$text['description-outbound_caller_id_name-custom']['en-us'] = 'Enter the external (public) caller ID name here.';
+		$text['description-outbound_caller_id_name-custom']['pt-pt'] = '';
+
+		$text['label-outbound_caller_id_number']['en-us'] = 'Outbound Caller ID Number';
+		$text['label-outbound_caller_id_number']['pt-pt'] = '';
+
+		$text['description-outbound_caller_id_number-select']['en-us'] = 'Select the external (public) caller ID number.';
+		$text['description-outbound_caller_id_number-select']['pt-pt'] = '';
+
+		$text['description-outbound_caller_id_number-custom']['en-us'] = 'Enter the external (public) caller ID number here.';
+		$text['description-outbound_caller_id_number-custom']['pt-pt'] = '';
+
+		$text['label-emergency_caller_id_number']['en-us'] = 'Emergency Caller ID Number';
+		$text['label-emergency_caller_id_number']['pt-pt'] = '';
+
+		$text['description-emergency_caller_id_number']['en-us'] = 'Enter the emergency caller ID number here.';
+		$text['description-emergency_caller_id_number']['pt-pt'] = '';
+
+		$text['label-directory_full_name']['en-us'] = 'Directory Full Name';
+		$text['label-directory_full_name']['pt-pt'] = '';
+
+		$text['description-directory_full_name']['en-us'] = 'Enter the first name followed by the last name.';
+		$text['description-directory_full_name']['pt-pt'] = '';
+
+		$text['label-directory_visible']['en-us'] = 'Directory Visible';
+		$text['label-directory_visible']['pt-pt'] = '';
+
+		$text['description-directory_visible']['en-us'] = 'Select whether to hide the name from the directory.';
+		$text['description-directory_visible']['pt-pt'] = '';
+
+		$text['label-directory_exten_visible']['en-us'] = 'Directory Extension Visible';
+		$text['label-directory_exten_visible']['pt-pt'] = '';
+
+		$text['description-directory_exten_visible']['en-us'] = 'Select whether announce the extension when calling the directory.';
+		$text['description-directory_exten_visible']['pt-pt'] = '';
+
+		$text['label-limit_max']['en-us'] = 'Limit Max';
+		$text['label-limit_max']['pt-pt'] = '';
+
+		$text['description-limit_max']['en-us'] = 'Enter the max number of outgoing calls for this user.';
+		$text['description-limit_max']['pt-pt'] = '';
+
+		$text['label-limit_destination']['en-us'] = 'Limit Destination';
+		$text['label-limit_destination']['pt-pt'] = '';
+
+		$text['description-limit_destination']['en-us'] = 'Enter the destination to send the calls when the max number of outgoing calls has been reached.';
+		$text['description-limit_destination']['pt-pt'] = '';
+
+		$text['label-provisioning']['en-us'] = 'Phone Provisioning';
+		$text['label-provisioning']['pt-pt'] = '';
+
+		$text['description-provisioning']['en-us'] = 'Select a device and line number to assign to this extension.';
+		$text['description-provisioning']['pt-pt'] = '';
+
+		$text['label-vm_enabled']['en-us'] = 'Voicemail Enabled';
+		$text['label-vm_enabled']['pt-pt'] = '';
+
+		$text['description-vm_enabled']['en-us'] = 'Enable/disable voicemail for this extension.';
+		$text['description-vm_enabled']['pt-pt'] = '';
+
+		$text['label-vm_mailto']['en-us'] = 'Voicemail Mail To';
+		$text['label-vm_mailto']['pt-pt'] = '';
+
+		$text['description-vm_mailto']['en-us'] = 'Enter the email address to send voicemail to (optional).';
+		$text['description-vm_mailto']['pt-pt'] = '';
+
+		$text['label-vm_attach_file']['en-us'] = 'Voicemail Attach File';
+		$text['label-vm_attach_file']['pt-pt'] = '';
+
+		$text['description-vm_attach_file']['en-us'] = 'Choose whether to attach the voicemail to the email as an audio file.';
+		$text['description-vm_attach_file']['pt-pt'] = '';
+
+		$text['label-vm_keep_local_after_email']['en-us'] = 'VM Keep Local After Email';
+		$text['label-vm_keep_local_after_email']['pt-pt'] = '';
+
+		$text['description-vm_keep_local_after_email']['en-us'] = 'Keep local file after sending the email.';
+		$text['description-vm_keep_local_after_email']['pt-pt'] = '';
+
+		$text['label-toll_allow']['en-us'] = 'Toll Allow';
+		$text['label-toll_allow']['pt-pt'] = '';
+
+		$text['description-toll_allow']['en-us'] = 'Enter the toll allow value here.  (Examples: domestic,international,local)';
+		$text['description-toll_allow']['pt-pt'] = '';
+
+		$text['label-call_timeout']['en-us'] = 'Call Timeout';
+		$text['label-call_timeout']['pt-pt'] = '';
+
+		$text['description-call_timeout']['en-us'] = 'Enter the call timeout.';
+		$text['description-call_timeout']['pt-pt'] = '';
+
+		$text['label-call_group']['en-us'] = 'Call Group';
+		$text['label-call_group']['pt-pt'] = '';
+
+		$text['description-call_group']['en-us'] = 'Enter the user call group here. Groups available by default: sales, support, billing.';
+		$text['description-call_group']['pt-pt'] = '';
+
+		$text['label-hold_music']['en-us'] = 'Hold Music';
+		$text['label-hold_music']['pt-pt'] = '';
+
+		$text['description-hold_music']['en-us'] = 'Select the MOH Category here.';
+		$text['description-hold_music']['pt-pt'] = '';
+
+		$text['label-user_context']['en-us'] = 'User Context';
+		$text['label-user_context']['pt-pt'] = '';
+
+		$text['description-user_context']['en-us'] = 'Enter the user context here.';
+		$text['description-user_context']['pt-pt'] = '';
+
+		$text['label-show_advanced']['en-us'] = 'Show Advanced';
+		$text['label-show_advanced']['pt-pt'] = '';
+
+		$text['button-advanced']['en-us'] = 'Advanced';
+		$text['button-advanced']['pt-pt'] = '';
+
+		$text['label-auth_acl']['en-us'] = 'Auth ACL';
+		$text['label-auth_acl']['pt-pt'] = '';
+
+		$text['description-auth_acl']['en-us'] = 'Enter the Auth ACL here.';
+		$text['description-auth_acl']['pt-pt'] = '';
+
+		$text['label-cidr']['en-us'] = 'CIDR';
+		$text['label-cidr']['pt-pt'] = '';
+
+		$text['description-cidr']['en-us'] = 'Enter the CIDR here.';
+		$text['description-cidr']['pt-pt'] = '';
+
+		$text['label-sip_force_contact']['en-us'] = 'SIP Force Contact';
+		$text['label-sip_force_contact']['pt-pt'] = '';
+
+		$text['description-sip_force_contact']['en-us'] = 'Choose whether to rewrite the contact port, or rewrite both the contact IP and port.';
+		$text['description-sip_force_contact']['pt-pt'] = '';
+
+		$text['label-sip_force_expires']['en-us'] = 'SIP Force Expires';
+		$text['label-sip_force_expires']['pt-pt'] = '';
+
+		$text['description-sip_force_expires']['en-us'] = 'Choose whether to rewrite the contact port, or rewrite both the contact IP and port.';
+		$text['description-sip_force_expires']['pt-pt'] = '';
+
+		$text['label-nibble_account']['en-us'] = 'Nibble Account';
+		$text['label-nibble_account']['pt-pt'] = '';
+
+		$text['description-nibble_account']['en-us'] = 'Enter the account number for nibblebill to use.';
+		$text['description-nibble_account']['pt-pt'] = '';
+
+		$text['label-mwi_account']['en-us'] = 'MWI Account';
+		$text['label-mwi_account']['pt-pt'] = '';
+
+		$text['description-mwi_account']['en-us'] = 'MWI Account with user@domain of the voicemail to monitor.';
+		$text['description-mwi_account']['pt-pt'] = '';
+
+		$text['label-sip_bypass_media']['en-us'] = 'SIP Bypass Media';
+		$text['label-sip_bypass_media']['pt-pt'] = '';
+
+		$text['description-sip_bypass_media']['en-us'] = 'Choose whether to send the media stream point to point or in transparent proxy mode.';
+		$text['description-sip_bypass_media']['pt-pt'] = '';
+
+		$text['label-dial_string']['en-us'] = 'Dial String';
+		$text['label-dial_string']['pt-pt'] = '';
+
+		$text['description-dial_string']['en-us'] = 'Location of the endpoint.';
+		$text['description-dial_string']['pt-pt'] = '';
+
+		$text['label-enabled']['en-us'] = 'Enabled';
+		$text['label-enabled']['pt-pt'] = '';
+
+		$text['description-enabled']['en-us'] = 'Set the status of the extension.';
+		$text['description-enabled']['pt-pt'] = '';
+
+		$text['label-description']['en-us'] = 'Description';
+		$text['label-description']['pt-pt'] = '';
+
+		$text['description-description']['en-us'] = '';
+		$text['description-description']['pt-pt'] = '';
+
+		$text['label-true']['en-us'] = 'true';
+		$text['label-true']['pt-pt'] = '';
+
+		$text['label-false']['en-us'] = 'false';
+		$text['label-false']['pt-pt'] = 'falso';
+
+		$text['button-add']['en-us'] = 'Add';
+		$text['button-add']['pt-pt'] = '';
+
+		$text['button-edit']['en-us'] = 'Edit';
+		$text['button-edit']['pt-pt'] = '';
+
+		$text['button-delete']['en-us'] = 'Delete';
+		$text['button-delete']['pt-pt'] = '';
+
+		$text['confirm-delete']['en-us'] = 'Do you really want to delete this?';
+		$text['confirm-delete']['pt-pt'] = '';
+
+		$text['button-save']['en-us'] = 'Save';
+		$text['button-save']['pt-pt'] = 'Guardar';
+
+		$text['button-copy']['en-us'] = 'Copy';
+		$text['button-copy']['pt-pt'] = '';
+
+		$text['confirm-copy']['en-us'] = 'Do you really want to copy this?';
+		$text['confirm-copy']['pt-pt'] = '';
+
+		$text['button-back']['en-us'] = 'Back';
+		$text['button-back']['pt-pt'] = 'Voltar';
+
+		$text['message-message']['en-us'] = 'Message';
+		$text['message-message']['pt-pt'] = '';
+
+		$text['message-add']['en-us'] = 'Add Completed';
+		$text['message-add']['pt-pt'] = '';
+
+		$text['message-update']['en-us'] = 'Update Completed';
+		$text['message-update']['pt-pt'] = '';
+
+		$text['message-delete']['en-us'] = 'Delete Completed';
+		$text['message-delete']['pt-pt'] = '';
+
+		$text['message-copy']['en-us'] = 'Copy Completed';
+		$text['message-copy']['pt-pt'] = '';
+
+		$text['message-required']['en-us'] = 'Please provide: ';
+		$text['message-required']['pt-pt'] = '';
+
+?>
