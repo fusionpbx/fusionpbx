252af521b2dd49a45920533e9a3facc42db33ad4
Nate Jones <git@reliberate.com>
2013-05-14 05:35:59 +0000

    Removed dev artifacts ("# ") from Domain Settings app_languages.php.

--- core/domain_settings/app_languages.php
+++ core/domain_settings/app_languages.php
@@ -2,161 +2,161 @@
 
 	//Domains
 
-		$text['title-domains']['en-us'] = '# Domains';
-		$text['title-domains']['pt-pt'] = '# ';
+		$text['title-domains']['en-us'] = 'Domains';
+		$text['title-domains']['pt-pt'] = '';
 
-		$text['header-domains']['en-us'] = '# Domains';
-		$text['header-domains']['pt-pt'] = '# ';
+		$text['header-domains']['en-us'] = 'Domains';
+		$text['header-domains']['pt-pt'] = '';
 
-		$text['description-domains']['en-us'] = '# Control the list of domains to manage.';
-		$text['description-domains']['pt-pt'] = '# ';
-
-		//------
-
-		$text['title-domain-edit']['en-us'] = '# Domain Edit';
-		$text['title-domain-edit']['pt-pt'] = '# ';
-
-		$text['header-domain-edit']['en-us'] = '# Domain Edit';
-		$text['header-domain-edit']['pt-pt'] = '# ';
-
-		$text['description-domain-edit']['en-us'] = '# Edit the details of this domain.';
-		$text['description-domain-edit']['pt-pt'] = '# ';
-
-		$text['header-settings']['en-us'] = '# Settings';
-		$text['header-settings']['pt-pt'] = '# ';
+		$text['description-domains']['en-us'] = 'Control the list of domains to manage.';
+		$text['description-domains']['pt-pt'] = '';
 
 		//------
 
-		$text['title-domain-add']['en-us'] = '# Domain Add';
-		$text['title-domain-add']['pt-pt'] = '# ';
+		$text['title-domain-edit']['en-us'] = 'Domain Edit';
+		$text['title-domain-edit']['pt-pt'] = '';
 
-		$text['header-domain-add']['en-us'] = '# Domain Add';
-		$text['header-domain-add']['pt-pt'] = '# ';
+		$text['header-domain-edit']['en-us'] = 'Domain Edit';
+		$text['header-domain-edit']['pt-pt'] = '';
 
-		$text['description-domain-add']['en-us'] = '# Enter the domain details below.';
-		$text['description-domain-add']['pt-pt'] = '# ';
+		$text['description-domain-edit']['en-us'] = 'Edit the details of this domain.';
+		$text['description-domain-edit']['pt-pt'] = '';
+
+		$text['header-settings']['en-us'] = 'Settings';
+		$text['header-settings']['pt-pt'] = '';
 
 		//------
 
-		$text['label-domain']['en-us'] = '# Domain';
-		$text['label-domain']['pt-pt'] = '# ';
+		$text['title-domain-add']['en-us'] = 'Domain Add';
+		$text['title-domain-add']['pt-pt'] = '';
 
-		$text['label-name']['en-us'] = '# Name';
-		$text['label-name']['pt-pt'] = '# ';
+		$text['header-domain-add']['en-us'] = 'Domain Add';
+		$text['header-domain-add']['pt-pt'] = '';
 
-		$text['description-name']['en-us'] = '# Enter the name of the domain.';
-		$text['description-name']['pt-pt'] = '# ';
+		$text['description-domain-add']['en-us'] = 'Enter the domain details below.';
+		$text['description-domain-add']['pt-pt'] = '';
 
-		$text['label-description']['en-us'] = '# Description';
-		$text['label-description']['pt-pt'] = '# ';
+		//------
 
-		$text['description-description']['en-us'] = '# Enter a description for this domain, if desired.';
-		$text['description-description']['pt-pt'] = '# ';
+		$text['label-domain']['en-us'] = 'Domain';
+		$text['label-domain']['pt-pt'] = '';
+
+		$text['label-name']['en-us'] = 'Name';
+		$text['label-name']['pt-pt'] = '';
+
+		$text['description-name']['en-us'] = 'Enter the name of the domain.';
+		$text['description-name']['pt-pt'] = '';
+
+		$text['label-description']['en-us'] = 'Description';
+		$text['label-description']['pt-pt'] = '';
+
+		$text['description-description']['en-us'] = 'Enter a description for this domain, if desired.';
+		$text['description-description']['pt-pt'] = '';
 
 		//------ settings
 
-		$text['title-domain_setting-edit']['en-us'] = '# Domain Setting Edit';
-		$text['title-domain_setting-edit']['pt-pt'] = '# ';
+		$text['title-domain_setting-edit']['en-us'] = 'Domain Setting Edit';
+		$text['title-domain_setting-edit']['pt-pt'] = '';
 
-		$text['header-domain_setting-edit']['en-us'] = '# Domain Setting Edit';
-		$text['header-domain_setting-edit']['pt-pt'] = '# ';
+		$text['header-domain_setting-edit']['en-us'] = 'Domain Setting Edit';
+		$text['header-domain_setting-edit']['pt-pt'] = '';
 
-		$text['description-domain_setting-edit']['en-us'] = '# Edit a setting for this domain.';
-		$text['description-domain_setting-edit']['pt-pt'] = '# ';
+		$text['description-domain_setting-edit']['en-us'] = 'Edit a setting for this domain.';
+		$text['description-domain_setting-edit']['pt-pt'] = '';
 
-
-		//------
-
-		$text['title-domain_setting-add']['en-us'] = '# Domain Setting Add';
-		$text['title-domain_setting-add']['pt-pt'] = '# ';
-
-		$text['header-domain_setting-add']['en-us'] = '# Domain Setting Add';
-		$text['header-domain_setting-add']['pt-pt'] = '# ';
-
-		$text['description-domain_setting-add']['en-us'] = '# Add a setting for this domain.';
-		$text['description-domain_setting-add']['pt-pt'] = '# ';
 
 		//------
 
-		$text['label-category']['en-us'] = '# Category';
-		$text['label-category']['pt-pt'] = '# ';
+		$text['title-domain_setting-add']['en-us'] = 'Domain Setting Add';
+		$text['title-domain_setting-add']['pt-pt'] = '';
 
-		$text['description-category']['en-us'] = '# Enter the category.';
-		$text['description-category']['pt-pt'] = '# ';
+		$text['header-domain_setting-add']['en-us'] = 'Domain Setting Add';
+		$text['header-domain_setting-add']['pt-pt'] = '';
 
-		$text['label-subcategory']['en-us'] = '# Subcategory';
-		$text['label-subcategory']['pt-pt'] = '# ';
+		$text['description-domain_setting-add']['en-us'] = 'Add a setting for this domain.';
+		$text['description-domain_setting-add']['pt-pt'] = '';
 
-		$text['description-subcategory']['en-us'] = '# Enter the subcategory.';
-		$text['description-subcategory']['pt-pt'] = '# ';
+		//------
 
-		$text['label-type']['en-us'] = '# Type';
-		$text['label-type']['pt-pt'] = '# ';
+		$text['label-category']['en-us'] = 'Category';
+		$text['label-category']['pt-pt'] = '';
 
-		$text['description-type']['en-us'] = '# Enter the setting type (ie. uuid, name, var, dir, etc).';
-		$text['description-type']['pt-pt'] = '# ';
+		$text['description-category']['en-us'] = 'Enter the category.';
+		$text['description-category']['pt-pt'] = '';
 
-		$text['label-value']['en-us'] = '# Value';
-		$text['label-value']['pt-pt'] = '# ';
+		$text['label-subcategory']['en-us'] = 'Subcategory';
+		$text['label-subcategory']['pt-pt'] = '';
 
-		$text['description-value']['en-us'] = '# Enter the value of this setting.';
-		$text['description-value']['pt-pt'] = '# ';
+		$text['description-subcategory']['en-us'] = 'Enter the subcategory.';
+		$text['description-subcategory']['pt-pt'] = '';
 
-		$text['label-enabled']['en-us'] = '# Enabled';
-		$text['label-enabled']['pt-pt'] = '# ';
+		$text['label-type']['en-us'] = 'Type';
+		$text['label-type']['pt-pt'] = '';
 
-		$text['description-enabled']['en-us'] = '# Set the status of this default setting.';
-		$text['description-enabled']['pt-pt'] = '# ';
+		$text['description-type']['en-us'] = 'Enter the setting type (ie. uuid, name, var, dir, etc).';
+		$text['description-type']['pt-pt'] = '';
 
-		$text['label-description']['en-us'] = '# Description';
-		$text['label-description']['pt-pt'] = '# ';
+		$text['label-value']['en-us'] = 'Value';
+		$text['label-value']['pt-pt'] = '';
 
-		$text['description-description']['en-us'] = '# Enter a description, if desired.';
-		$text['description-description']['pt-pt'] = '# ';
+		$text['description-value']['en-us'] = 'Enter the value of this setting.';
+		$text['description-value']['pt-pt'] = '';
 
-		$text['label-true']['en-us'] = '# True';
-		$text['label-true']['pt-pt'] = '# ';
+		$text['label-enabled']['en-us'] = 'Enabled';
+		$text['label-enabled']['pt-pt'] = '';
 
-		$text['label-false']['en-us'] = '# False';
-		$text['label-false']['pt-pt'] = '# falso';
+		$text['description-enabled']['en-us'] = 'Set the status of this default setting.';
+		$text['description-enabled']['pt-pt'] = '';
 
-		$text['label-none']['en-us'] = '# None';
-		$text['label-none']['pt-pt'] = '# ';
+		$text['label-description']['en-us'] = 'Description';
+		$text['label-description']['pt-pt'] = '';
 
-		$text['button-add']['en-us'] = '# Add';
-		$text['button-add']['pt-pt'] = '# ';
+		$text['description-description']['en-us'] = 'Enter a description, if desired.';
+		$text['description-description']['pt-pt'] = '';
 
-		$text['button-edit']['en-us'] = '# Edit';
-		$text['button-edit']['pt-pt'] = '# ';
+		$text['label-true']['en-us'] = 'True';
+		$text['label-true']['pt-pt'] = '';
 
-		$text['button-delete']['en-us'] = '# Delete';
-		$text['button-delete']['pt-pt'] = '# ';
+		$text['label-false']['en-us'] = 'False';
+		$text['label-false']['pt-pt'] = 'falso';
 
-		$text['confirm-delete']['en-us'] = '# Do you really want to delete this?';
-		$text['confirm-delete']['pt-pt'] = '# ';
+		$text['label-none']['en-us'] = 'None';
+		$text['label-none']['pt-pt'] = '';
 
-		$text['button-save']['en-us'] = '# Save';
-		$text['button-save']['pt-pt'] = '# Guardar';
+		$text['button-add']['en-us'] = 'Add';
+		$text['button-add']['pt-pt'] = '';
 
-		$text['button-back']['en-us'] = '# Back';
-		$text['button-back']['pt-pt'] = '# Voltar';
+		$text['button-edit']['en-us'] = 'Edit';
+		$text['button-edit']['pt-pt'] = '';
 
-		$text['button-export']['en-us'] = '# Export';
-		$text['button-export']['pt-pt'] = '# ';
+		$text['button-delete']['en-us'] = 'Delete';
+		$text['button-delete']['pt-pt'] = '';
 
-		$text['message-message']['en-us'] = '# Message';
-		$text['message-message']['pt-pt'] = '# ';
+		$text['confirm-delete']['en-us'] = 'Do you really want to delete this?';
+		$text['confirm-delete']['pt-pt'] = '';
 
-		$text['message-add']['en-us'] = '# Add Completed';
-		$text['message-add']['pt-pt'] = '# ';
+		$text['button-save']['en-us'] = 'Save';
+		$text['button-save']['pt-pt'] = 'Guardar';
 
-		$text['message-update']['en-us'] = '# Update Completed';
-		$text['message-update']['pt-pt'] = '# ';
+		$text['button-back']['en-us'] = 'Back';
+		$text['button-back']['pt-pt'] = 'Voltar';
 
-		$text['message-delete']['en-us'] = '# Delete Completed';
-		$text['message-delete']['pt-pt'] = '# ';
+		$text['button-export']['en-us'] = 'Export';
+		$text['button-export']['pt-pt'] = '';
 
-		$text['message-required']['en-us'] = '# Please provide: ';
-		$text['message-required']['pt-pt'] = '# ';
+		$text['message-message']['en-us'] = 'Message';
+		$text['message-message']['pt-pt'] = '';
+
+		$text['message-add']['en-us'] = 'Add Completed';
+		$text['message-add']['pt-pt'] = '';
+
+		$text['message-update']['en-us'] = 'Update Completed';
+		$text['message-update']['pt-pt'] = '';
+
+		$text['message-delete']['en-us'] = 'Delete Completed';
+		$text['message-delete']['pt-pt'] = '';
+
+		$text['message-required']['en-us'] = 'Please provide: ';
+		$text['message-required']['pt-pt'] = '';
 
 ?>
