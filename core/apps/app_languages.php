b207aa26db3cd7361565a0b19ec8be49008dafc8
Nate Jones <git@reliberate.com>
2013-05-15 02:19:44 +0000

    Multilinquified core/apps.  App Edit and Delete buttons hidden (for now, at least).

---
+++ core/apps/app_languages.php
@@ -0,0 +1,72 @@
+<?php
+
+	//Apps
+
+		$text['title-apps']['en-us'] = 'App Manager';
+		$text['title-apps']['pt-pt'] = '';
+
+		$text['header-apps']['en-us'] = 'App Manager';
+		$text['header-apps']['pt-pt'] = '';
+
+		$text['description-apps']['en-us'] = 'Manage the applications that are installed.';
+		$text['description-apps']['pt-pt'] = '';
+
+		//------
+
+		$text['title-app-edit']['en-us'] = 'App Edit';
+		$text['title-app-edit']['pt-pt'] = '';
+
+		$text['header-app-edit']['en-us'] = 'App Edit';
+		$text['header-app-edit']['pt-pt'] = '';
+
+		$text['description-app-edit']['en-us'] = 'Manage the applications that are installed.';
+		$text['description-app-edit']['pt-pt'] = '';
+
+		//------
+
+		$text['label-name']['en-us'] = 'Name';
+		$text['label-name']['pt-pt'] = '';
+
+		$text['label-category']['en-us'] = 'Category';
+		$text['label-category']['pt-pt'] = '';
+
+		$text['label-subcategory']['en-us'] = 'Subcategory';
+		$text['label-subcategory']['pt-pt'] = '';
+
+		$text['label-version']['en-us'] = 'Version';
+		$text['label-version']['pt-pt'] = '';
+
+		$text['label-description']['en-us'] = 'Description';
+		$text['label-description']['pt-pt'] = '';
+
+		$text['button-add']['en-us'] = 'Add';
+		$text['button-add']['pt-pt'] = '';
+
+		$text['button-edit']['en-us'] = 'Edit';
+		$text['button-edit']['pt-pt'] = '';
+
+		$text['button-delete']['en-us'] = 'Delete';
+		$text['button-delete']['pt-pt'] = '';
+
+		$text['confirm-delete']['en-us'] = 'Do you really want to delete this?';
+		$text['confirm-delete']['pt-pt'] = '';
+
+		$text['button-save']['en-us'] = 'Save';
+		$text['button-save']['pt-pt'] = 'Guardar';
+
+		$text['button-back']['en-us'] = 'Back';
+		$text['button-back']['pt-pt'] = 'Voltar';
+
+		$text['message-message']['en-us'] = 'Message';
+		$text['message-message']['pt-pt'] = '';
+
+		$text['message-add']['en-us'] = 'Add Completed';
+		$text['message-add']['pt-pt'] = '';
+
+		$text['message-update']['en-us'] = 'Update Completed';
+		$text['message-update']['pt-pt'] = '';
+
+		$text['message-delete']['en-us'] = 'Delete Completed';
+		$text['message-delete']['pt-pt'] = '';
+
+?>
