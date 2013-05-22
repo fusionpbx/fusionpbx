d8709267eaae69a32a089760b58ef32440c37c2c
Mark Crane <markjcrane@gmail.com>
2013-05-22 19:08:58 +0000

    Fix a regression for click to call regarding auto-answer

--- app/click_to_call/app_languages.php
+++ app/click_to_call/app_languages.php
@@ -24,33 +24,63 @@
 	Mark J Crane <markjcrane@fusionpbx.com>
 	James Rose <james.o.rose@gmail.com>
 */
-	
 
 	$text['label-click2call']['en-us'] = 'Click to Call';
+
 	$text['desc-click2call']['en-us'] = 'Provide the following information to make a call from the source number to the destination number.';
+
 	$text['label-src-caller-id-nam']['en-us'] = 'Source Caller ID Name';
+
 	$text['desc-src-caller-id-nam']['en-us'] = 'Enter the Caller ID name to send to your phone.';
+
 	$text['label-src-caller-id-num']['en-us'] = 'Source Caller ID Number';
+
 	$text['desc-src-caller-id-num']['en-us'] = 'Enter the Caller ID number to send to your phone (you probably want this to be the same as the destination number).';
+
 	$text['label-dest-caller-id-nam']['en-us'] = 'Destination Caller ID Name';
+
 	$text['desc-dest-caller-id-nam']['en-us'] = 'Enter the Caller ID name to send to the destination number.';
+
 	$text['label-dest-caller-id-num']['en-us'] = 'Destination Caller ID Number';
+
 	$text['desc-dest-caller-id-num']['en-us'] = 'Enter the Caller ID number to send to the destination number (you probably want this to be your phone number).';
+
+
+
 	$text['label-src-num']['en-us'] = 'Source Number';
+
 	$text['desc-src-num']['en-us'] = 'Enter your phone number.  This can be an extension on the system, or another number (eg: mobile phone).';
+
 	$text['label-dest-num']['en-us'] = 'Destination Number';
+
 	$text['desc-dest-num']['en-us'] = "Enter the number to call. This can be an extension on the system, another number, or a sip uri.  Sip URI's are of the form 5551234567@voip.example.com:5080 (5080 for freeswitch, or 5060 for other systems).";
+
+	$text['label-auto-answer']['en-us'] = 'Auto Answer';
+
+	$text['desc-auto-answer']['en-us'] = 'Select whether to enable auto answer.';
+
 	$text['label-record']['en-us'] = 'Record';
+
 	$text['label-true']['en-us'] = 'true';
+
 	$text['label-false']['en-us'] = 'false';
+
 	$text['desc-record']['en-us'] = 'Select whether to record the call.';
+
 	$text['label-ringback']['en-us'] = 'Ring Back';
+
 	$text['desc-ringback']['en-us'] = 'Defines what the you will hear while destination is being called. The choices are music (music on hold) ring (ring tone.)';
+
 	$text['opt-usring']['en-us'] = 'us-ring';
+
 	$text['opt-frring']['en-us'] = 'fr-ring';
+
 	$text['opt-ukring']['en-us'] = 'uk-ring';
+
 	$text['opt-rsring']['en-us'] = 'rs-ring';
+
 	$text['opt-moh']['en-us'] = 'music';
+
 	$text['button-call']['en-us'] = 'Call';
 
 ?>
