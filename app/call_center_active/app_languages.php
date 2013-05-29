ff8d303a2c8b83d3c081102d6605dc238fe538d7
Nate Jones <git@reliberate.com>
2013-05-29 21:16:24 +0000

    Forgot to svn add app_languages.php files. Doh!

---
+++ app/call_center_active/app_languages.php
@@ -0,0 +1,55 @@
+<?php
+
+	$text['title-active_call_center']['en-us'] = 'Active Call Center';
+	$text['title-call_center_queue_activity']['en-us'] = 'Call Center Queue Activity';
+
+	$text['header-active_call_center']['en-us'] = 'Active Call Center';
+	$text['header-agents']['en-us'] = 'Agents';
+
+	$text['description-active_call_center']['en-us'] = 'Select a Call Center Queue from the list below to view its activity.';
+	$text['description-agents']['en-us'] = 'A current list of agents is below.';
+	$text['description-queue']['en-us'] = 'A current list of callers in the queue is below.';
+
+	$text['label-queue_name']['en-us'] = 'Queue Name';
+	$text['label-extension']['en-us'] = 'Extension';
+	$text['label-strategy']['en-us'] = 'Strategy';
+	$text['label-music_on_hold']['en-us'] = 'Music on Hold';
+	$text['label-record_template']['en-us'] = 'Record Template';
+	$text['label-time_base_score']['en-us'] = 'Time Base Score';
+	$text['label-max_wait_time']['en-us'] = 'Max Wait Time';
+	$text['label-max_wait_time_with_no_agent']['en-us'] = 'Max Wait Time with No Agent';
+	$text['label-tier_rules_apply']['en-us'] = 'Tier Rules Apply';
+	$text['label-tier_rule_wait_second']['en-us'] = 'Tier Rule Wait Second';
+	$text['label-tier_rule_no_agent_no_wait']['en-us'] = 'Tier Rule No Agent No Wait';
+	$text['label-discard_abandoned_after']['en-us'] = 'Discard Abandoned After';
+	$text['label-abandoned_resume_allowed']['en-us'] = 'Abandoned Resume Allowed';
+	$text['label-tier_rule_wait_multiply_level']['en-us'] = 'Tier Rule Wait Multiply Level';
+	$text['label-description']['en-us'] = 'Description';
+	$text['label-name']['en-us'] = 'Name';
+	$text['label-extension']['en-us'] = 'Extension';
+	$text['label-status']['en-us'] = 'Status';
+	$text['label-state']['en-us'] = 'State';
+	$text['label-status_change']['en-us'] = 'Status Change';
+	$text['label-missed']['en-us'] = 'Missed';
+	$text['label-answered']['en-us'] = 'Answered';
+	$text['label-tier_state']['en-us'] = 'Tier State';
+	$text['label-tier_level']['en-us'] = 'Tier Level';
+	$text['label-tier_position']['en-us'] = 'Tier Position';
+	$text['label-options']['en-us'] = 'Options';
+	$text['label-eavesdrop']['en-us'] = 'Eavesdrop';
+	$text['label-transfer']['en-us'] = 'Transfer';
+	$text['label-call']['en-us'] = 'Call';
+	$text['label-queue']['en-us'] = 'Queue';
+	$text['label-time']['en-us'] = 'Time';
+	$text['label-system']['en-us'] = 'System';
+	$text['label-number']['en-us'] = 'Number';
+	$text['label-agent']['en-us'] = 'Agent';
+	$text['label-total_waiting']['en-us'] = 'Total Waiting';
+	$text['label-total_trying']['en-us'] = 'Total Trying';
+	$text['label-total_answered']['en-us'] = 'Total Answered';
+
+	$text['button-edit']['en-us'] = 'Edit';
+
+	$text['message-confirm']['en-us'] = 'Do you really want to do this?';
+
+?>
