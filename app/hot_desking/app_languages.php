d062df4e0c67b151122354fe3a660bd1192f683d
Nate Jones <git@reliberate.com>
2013-06-06 21:31:19 +0000

    Multilinguified Hot Desking, fixed bug where editing an existing Extension detected its own Unique ID and prevented the update.

---
+++ app/hot_desking/app_languages.php
@@ -0,0 +1,40 @@
+<?php
+
+	//Hot Desking
+
+		$text['title-hot_desking']['en-us'] = 'Hot Desking';
+
+		$text['header-hot_desking']['en-us'] = 'Hot Desking';
+		$text['header-message']['en-us'] = 'Message';
+
+		$text['description-hot_desking']['en-us'] = 'Login into hot desking with an ID and your voicemail password to direct your calls to a remote extension. Then make and receive calls as if you were at your extension. ';
+
+		$text['label-extension']['en-us'] = 'Extension';
+		$text['label-unique_id']['en-us'] = 'Unique ID';
+		$text['label-forward_to']['en-us'] = 'Forward To';
+		$text['label-description']['en-us'] = 'Description';
+		$text['label-voicemail_password']['en-us'] = 'Voicemail Password';
+		$text['label-dial_string']['en-us'] = 'Dial String';
+
+		$text['description-extension-add']['en-us'] = 'Select the extension number.';
+		$text['description-extension-edit']['en-us'] = 'Extension number.';
+		$text['description-unique_id']['en-us'] = 'A unique NUMERIC ID to identify the extension and domain.';
+		$text['description-voicemail_password']['en-us'] = 'Enter the voicemail password here.';
+		$text['description-password']['en-us'] = 'Password';
+		$text['description-dial_string']['en-us'] = 'Location of the endpoint.';
+
+		$text['button-add']['en-us'] = 'Add';
+		$text['button-edit']['en-us'] = 'Edit';
+		$text['button-delete']['en-us'] = 'Delete';
+		$text['button-back']['en-us'] = 'Back';
+		$text['button-save']['en-us'] = 'Save';
+
+		$text['confirm-delete']['en-us'] = 'Do you really want to delete this?';
+
+		$text['message-add']['en-us'] = 'Add Completed';
+		$text['message-update']['en-us'] = 'Update Completed';
+		$text['message-delete']['en-us'] = 'Delete Completed';
+		$text['message-required']['en-us'] = 'Please provide: ';
+		$text['message-unique']['en-us'] = 'The Unique ID is not unique.  Please provide a numeric ID not already in use by a different extension.';
+
+?>
