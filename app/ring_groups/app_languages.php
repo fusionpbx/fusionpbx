beed4272026365056a7c76c8510302d36873abb5
Mark Crane <markjcrane@gmail.com>
2013-08-16 01:07:39 +0000

    Add pt-pt to app/ring_groups/app_languages.php. Also add label-user_list, and description-user_list.

--- app/ring_groups/app_languages.php
+++ app/ring_groups/app_languages.php
@@ -26,98 +26,151 @@
 */
 
 	$text['title']['en-us'] = 'Ring Groups';
+	$text['title']['pt-pt'] = '';
 
 	$text['description']['en-us'] = 'A ring group is a set of destinations that can be called with a ring strategy.';
+	$text['description']['pt-pt'] = '';
 
 	$text['label-name']['en-us'] = 'Name';
+	$text['label-name']['pt-pt'] = '';
 
 	$text['label-extension']['en-us'] = 'Extension';
+	$text['label-extension']['pt-pt'] = '';
 
 	$text['label-ring-group-extension']['en-us'] = 'Ring Group Extension';
+	$text['label-ring-group-extension']['pt-pt'] = '';
 
 	$text['header-ring-group-forward']['en-us'] = 'Ring Group Forward';
+	$text['header-ring-group-forward']['pt-pt'] = '';
 
 	$text['description-ring-group-forward']['en-us'] = 'Forwards calls to an alternate destination for extension ';
+	$text['description-ring-group-forward']['pt-pt'] = '';
 
 	$text['label-call-forward']['en-us'] = 'Call Forward:';
+	$text['label-call-forward']['pt-pt'] = '';
 
 	$text['label-forward_destination']['en-us'] = 'Number:';
+	$text['label-forward_destination']['pt-pt'] = '';
 
 	$text['label-tools']['en-us'] = 'Tools';
+	$text['label-tools']['pt-pt'] = '';
 
 	$text['label-enabled']['en-us'] = 'Enabled';
+	$text['label-enabled']['pt-pt'] = '';
 
 	$text['label-description']['en-us'] = 'Description';
+	$text['label-description']['pt-pt'] = '';
 
 	$text['message-delete']['en-us'] = 'Delete Complete';
+	$text['message-delete']['pt-pt'] = '';
 
 	$text['message-name']['en-us'] = 'Please provide: Name';
+	$text['message-name']['pt-pt'] = '';
 
 	$text['message-strategy']['en-us'] = 'Please provide: Strategy';
+	$text['message-strategy']['pt-pt'] = '';
 
 	$text['message-timeout']['en-us'] = 'Please provide: Timeout';
+	$text['message-timeout']['pt-pt'] = '';
 
 	$text['message-timeout-action']['en-us'] = 'Please provide: Timeout Action';
+	$text['message-timeout-action']['pt-pt'] = '';
 
 	$text['message-enabled']['en-us'] = 'Please provide: Enabled';
+	$text['message-enabled']['pt-pt'] = '';
 
 	$text['message-add']['en-us'] = 'Add Complete';
+	$text['message-add']['pt-pt'] = '';
 
 	$text['message-update']['en-us'] = 'Update Complete';
+	$text['message-update']['pt-pt'] = '';
 
 	$text['label-ring-group']['en-us'] = 'Ring Group';
+	$text['label-ring-group']['pt-pt'] = '';
 
 	$text['button-back']['en-us'] = 'Back';
+	$text['button-back']['pt-pt'] = '';
 
 	$text['description-name']['en-us'] = 'Enter a name.';
+	$text['description-name']['pt-pt'] = '';
 
 	$text['label-context']['en-us'] = 'Context';
+	$text['label-context']['pt-pt'] = '';
 
 	$text['description-enter-context']['en-us'] = 'Enter the context.';
+	$text['description-enter-context']['pt-pt'] = '';
 
 	$text['label-strategy']['en-us'] = 'Strategy';
+	$text['label-strategy']['pt-pt'] = '';
 
 	$text['option-sequence']['en-us'] = 'Sequence';
+	$text['option-sequence']['pt-pt'] = '';
 
 	$text['option-simultaneous']['en-us'] = 'Simultaneous';
+	$text['option-simultaneous']['pt-pt'] = '';
 
 	$text['option-enterprise']['en-us'] = 'Enterprise';
+	$text['option-enterprise']['pt-pt'] = '';
 
 	$text['description-strategy']['en-us'] = 'Select the ring strategy.';
+	$text['description-strategy']['pt-pt'] = '';
 
 	$text['label-duration']['en-us'] = 'Duration';
+	$text['label-duration']['pt-pt'] = '';
 
 	$text['label-call-timeout']['en-us'] = 'Call Timeout';
+	$text['label-call-timeout']['pt-pt'] = '';
 
 	$text['description-timeout']['en-us'] = 'Enter the total call timeout in seconds';
+	$text['description-timeout']['pt-pt'] = '';
 
 	$text['label-timeout_destination']['en-us'] = 'Timeout Destination';
+	$text['label-timeout_destination']['pt-pt'] = '';
 
 	$text['description-timeout_destination']['en-us'] = 'Select the timeout destination for this ring group.';
+	$text['description-timeout_destination']['pt-pt'] = '';
 
 	$text['label-cid-prefix']['en-us'] = 'CID Prefix';
+	$text['label-cid-prefix']['pt-pt'] = '';
 
 	$text['description-cid-prefix']['en-us'] = 'Set a prefix on the caller ID name.';
+	$text['description-cid-prefix']['pt-pt'] = '';
 
 	$text['label-ringback']['en-us'] = 'Ring Back';
+	$text['label-ringback']['pt-pt'] = '';
 
 	$text['option-usring']['en-us'] = 'us-ring';
+	$text['option-usring']['pt-pt'] = '';
 
 	$text['option-frring']['en-us'] = 'fr-ring';
+	$text['option-frring']['pt-pt'] = '';
 
 	$text['option-ukring']['en-us'] = 'uk-ring';
+	$text['option-ukring']['pt-pt'] = '';
 
 	$text['option-rsring']['en-us'] = 'rs-ring';
+	$text['option-rsring']['pt-pt'] = '';
 
 	$text['description-ringback']['en-us'] = 'Defines what the caller will hear while the destination is being called.';
+	$text['description-ringback']['pt-pt'] = '';
 
 	$text['option-true']['en-us'] = 'True';
+	$text['option-true']['pt-pt'] = '';
 
 	$text['option-false']['en-us'] = 'False';
+	$text['option-false']['pt-pt'] = '';
+
+	$text['label-user_list']['en-us'] = 'User List';
+	$text['label-user_list']['pt-pt'] = '';
+
+	$text['description-user_list']['en-us'] = 'Assign the users that are assigned to this ring group. ';
+	$text['description-user_list']['pt-pt'] = '';
 
 	$text['description-enabled']['en-us'] ='Set the status of this ring group.';
+	$text['description-enabled']['pt-pt'] = '';
 
 	$text['description-description']['en-us'] = 'Enter a description (optional).';
+	$text['description-description']['pt-pt'] = '';
 
 	$text['label-delay']['en-us'] = 'Delay';
 	$text['label-delay']['pt-pt'] = '';
