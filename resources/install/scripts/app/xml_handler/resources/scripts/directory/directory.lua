e5b72715f0dcd7f945259984e46bbc17b42a7b1d
Mark Crane <markjcrane@gmail.com>
2013-10-08 22:26:25 +0000

    Comment out the voicemail fields in the xml handler.

--- resources/install/scripts/app/xml_handler/resources/scripts/directory/directory.lua
+++ resources/install/scripts/app/xml_handler/resources/scripts/directory/directory.lua
@@ -100,24 +100,24 @@
 						end
 					--params
 						password = row.password;
-						vm_enabled = "true";
-						if (string.len(row.vm_enabled) > 0) then
-							vm_enabled = row.vm_enabled;
-						end
-						vm_password = row.vm_password;
-						vm_attach_file = "true";
-						if (string.len(row.vm_attach_file) > 0) then
-							vm_attach_file = row.vm_attach_file;
-						end
-						vm_keep_local_after_email = "true";
-						if (string.len(row.vm_keep_local_after_email) > 0) then
-							vm_keep_local_after_email = row.vm_keep_local_after_email;
-						end
-						if (string.len(row.vm_mailto) > 0) then
-							vm_mailto = row.vm_mailto;
-						else
-							vm_mailto = "";
-						end
+						--vm_enabled = "true";
+						--if (string.len(row.vm_enabled) > 0) then
+						--	vm_enabled = row.vm_enabled;
+						--end
+						--vm_password = row.vm_password;
+						--vm_attach_file = "true";
+						--if (string.len(row.vm_attach_file) > 0) then
+						--	vm_attach_file = row.vm_attach_file;
+						--end
+						--vm_keep_local_after_email = "true";
+						--if (string.len(row.vm_keep_local_after_email) > 0) then
+						--	vm_keep_local_after_email = row.vm_keep_local_after_email;
+						--end
+						--if (string.len(row.vm_mailto) > 0) then
+						--	vm_mailto = row.vm_mailto;
+						--else
+						--	vm_mailto = "";
+						--end
 						mwi_account = row.mwi_account;
 						auth_acl = row.auth_acl;
 					--variables
@@ -217,14 +217,14 @@
 					end
 					table.insert(xml, [[							<params>]]);
 					table.insert(xml, [[									<param name="password" value="]] .. password .. [["/>]]);
-					table.insert(xml, [[									<param name="vm-enabled" value="]] .. vm_enabled .. [["/>]]);
-					if (string.len(vm_mailto) > 0) then
-						table.insert(xml, [[								<param name="vm-password" value="]] .. vm_password  .. [["/>]]);
-						table.insert(xml, [[								<param name="vm-email-all-messages" value="]] .. vm_enabled  ..[["/>]]);
-						table.insert(xml, [[								<param name="vm-attach-file" value="]] .. vm_attach_file .. [["/>]]);
-						table.insert(xml, [[								<param name="vm-keep-local-after-email" value="]] .. vm_keep_local_after_email .. [["/>]]);
-						table.insert(xml, [[								<param name="vm-mailto" value="]] .. vm_mailto .. [["/>]]);
-					end
+					--table.insert(xml, [[									<param name="vm-enabled" value="]] .. vm_enabled .. [["/>]]);
+					--if (string.len(vm_mailto) > 0) then
+					--	table.insert(xml, [[								<param name="vm-password" value="]] .. vm_password  .. [["/>]]);
+					--	table.insert(xml, [[								<param name="vm-email-all-messages" value="]] .. vm_enabled  ..[["/>]]);
+					--	table.insert(xml, [[								<param name="vm-attach-file" value="]] .. vm_attach_file .. [["/>]]);
+					--	table.insert(xml, [[								<param name="vm-keep-local-after-email" value="]] .. vm_keep_local_after_email .. [["/>]]);
+					--	table.insert(xml, [[								<param name="vm-mailto" value="]] .. vm_mailto .. [["/>]]);
+					--end
 					if (string.len(mwi_account) > 0) then
 						table.insert(xml, [[								<param name="MWI-Account" value="]] .. mwi_account .. [["/>]]);
 					end
