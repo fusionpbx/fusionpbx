dd210c942279161bf32be1f21c369d894263971a
Mark Crane <markjcrane@gmail.com>
2013-10-31 17:52:56 +0000

    Add line spaces between the different translations and add button-copy.

--- core/users/app_languages.php
+++ core/users/app_languages.php
@@ -5,21 +5,27 @@
 	$text['title-user_manager']['en-us'] = "User Manager";
 	$text['title-user_manager']['pt-pt'] = "Gestor de Utilizadores";
 	$text['title-user_manager']['fr-fr'] = "Gestion Usagers";
+
 	$text['title-user_add']['en-us'] = "User Add";
 	$text['title-user_add']['pt-pt'] = "Adicionar Utilizador";
 	$text['title-user_add']['fr-fr'] = "Ajout de l'Usager";
+
 	$text['title-user_edit']['en-us'] = "User Edit";
 	$text['title-user_edit']['pt-pt'] = "Editar Utilizador";
 	$text['title-user_edit']['fr-fr'] = "Edition de l'Usager";
+
 	$text['title-group_manager']['en-us'] = "Group Manager";
 	$text['title-group_manager']['pt-pt'] = "Gestor de Grupos";
 	$text['title-group_manager']['fr-fr'] = "Gestion de Groupe";
+
 	$text['title-group_add']['en-us'] = "Group Add";
 	$text['title-group_add']['pt-pt'] = "Adicionar Grupo";
 	$text['title-group_add']['fr-fr'] = "Ajout de Groupe";
+
 	$text['title-group_permissions']['en-us'] = "Group Permissions";
 	$text['title-group_permissions']['pt-pt'] = "Permissões do Grupo";
 	$text['title-group_permissions']['fr-fr'] = "Permissions du Groupe";
+
 	$text['title-group_members']['en-us'] = "Group Members";
 	$text['title-group_members']['pt-pt'] = "Membros do Grupo";
 	$text['title-group_members']['fr-fr'] = "Membres du Groupe";
@@ -27,21 +33,27 @@
 	$text['header-user_manager']['en-us'] = "User Manager";
 	$text['header-user_manager']['pt-pt'] = "Gestor de Utilizadores";
 	$text['header-user_manager']['fr-fr'] = "Gestion Usagers";
+
 	$text['header-user_add']['en-us'] = "User Add";
 	$text['header-user_add']['pt-pt'] = "Adicionar Utilizador";
 	$text['header-user_add']['fr-fr'] = "Ajout de l'Usager";
+
 	$text['header-user_edit']['en-us'] = "User Edit";
 	$text['header-user_edit']['pt-pt'] = "Editar Utilizador";
 	$text['header-user_edit']['fr-fr'] = "Edition de l'Usager";
+
 	$text['header-group_manager']['en-us'] = "Group Manager";
 	$text['header-group_manager']['pt-pt'] = "Gestor de Grupos";
 	$text['header-group_manager']['fr-fr'] = "Gestion de Groupe";
+
 	$text['header-group_add']['en-us'] = "Group Add";
 	$text['header-group_add']['pt-pt'] = "Adicionar Grupo";
 	$text['header-group_add']['fr-fr'] = "Ajout de Groupe";
+
 	$text['header-group_permissions']['en-us'] = "Group Permissions: ";
 	$text['header-group_permissions']['pt-pt'] = "Permissões do Grupo";
 	$text['header-group_permissions']['fr-fr'] = "Permissions du Groupe";
+
 	$text['header-group_members']['en-us'] = "Group Members";
 	$text['header-group_members']['pt-pt'] = "Membros do Grupo";
 	$text['header-group_members']['fr-fr'] = "Membres du Groupe";
@@ -49,15 +61,19 @@
 	$text['description-user_manager']['en-us'] = "Add, edit, delete, and search users. ";
 	$text['description-user_manager']['pt-pt'] = "Adicionar, editar, remover e pesquisar utilizadores. ";
 	$text['description-user_manager']['fr-fr'] = "Ajouter, éditer, supprimer et chercher des utilisateurs. ";
+
 	$text['description-user_add']['en-us'] = "To add a user, please fill out this form completely. All fields are required. ";
 	$text['description-user_add']['pt-pt'] = "Para adicionar um utilizador por favor preencha este formulário. Todos os campos são de preenchimento obrigatório. ";
 	$text['description-user_add']['fr-fr'] = "Afin d'ajouter des utilisateurs, remplissez complètement ce formulaire. Tous les champs sont requis. ";
+
 	$text['description-user_edit']['en-us'] = "Edit user information and group membership. ";
 	$text['description-user_edit']['pt-pt'] = "Editar a informação do utilizador e os membros do grupo. ";
 	$text['description-user_edit']['fr-fr'] = "Editer les informations de l'utilisateur et l'appartenance au groupe.";
+
 	$text['description-group_add']['en-us'] = "Please choose a group name.";
 	$text['description-group_add']['pt-pt'] = "Por favor escolha um nome para o grupo.";
-	$text['description-group_add']['fr-fr'] = "Choisir un nom pour le groupe."; 
+	$text['description-group_add']['fr-fr'] = "Choisir un nom pour le groupe.";
+
 	$text['description-group_permissions']['en-us'] = "Assign permissions for this group.";
 	$text['description-group_permissions']['pt-pt'] = "Atribua permissões para este grupo.";
 	$text['description-group_permissions']['fr-fr'] = "Assigner des permissions à ce groupe.";
@@ -65,63 +81,83 @@
 	$text['label-search_by']['en-us'] = "Search by";
 	$text['label-search_by']['pt-pt'] = "Pesquisar por";
 	$text['label-search_by']['fr-fr'] = "Chercher par";
+
 	$text['label-username']['en-us'] = "Username";
 	$text['label-username']['pt-pt'] = "Nome de Utilizador";
 	$text['label-username']['fr-fr'] = "Nom d'Utilisateur";
+
 	$text['label-password']['en-us'] = "Password";
 	$text['label-password']['pt-pt'] = "Palavra-Passe";
 	$text['label-password']['fr-fr'] = "Mot de passe";
+
 	$text['label-confirm_password']['en-us'] = "Confirm Password";
 	$text['label-confirm_password']['pt-pt'] = "Confirmar Palavra-Passe";
 	$text['label-confirm_password']['fr-fr'] = "Confirmer le mot de passe";
+
 	$text['label-email']['en-us'] = "Email";
 	$text['label-email']['pt-pt'] = "Email";
 	$text['label-email']['fr-fr'] = "Courriel";
+
 	$text['label-first_name']['en-us'] = "First Name";
 	$text['label-first_name']['pt-pt'] = "Primeiro Nome";
 	$text['label-first_name']['fr-fr'] = "Prénom";
+
 	$text['label-last_name']['en-us'] = "Last Name";
 	$text['label-last_name']['pt-pt'] = "Último Nome";
 	$text['label-last_name']['fr-fr'] = "Nom";
+
 	$text['label-company_name']['en-us'] = "Company Name";
 	$text['label-company_name']['pt-pt'] = "Nome da Empresa";
 	$text['label-company_name']['fr-fr'] = "Compagnie";
+
 	$text['label-user_info']['en-us'] = "User Info";
 	$text['label-user_info']['pt-pt'] = "Informação do Utilizador";
 	$text['label-user_info']['fr-fr'] = "Informations sur l'utilisateur";
+
 	$text['label-groups']['en-us'] = "Groups";
 	$text['label-groups']['pt-pt'] = "Grupos";
 	$text['label-groups']['fr-fr'] = "Groupes";
+
 	$text['label-additional_info']['en-us'] = "Additional Info";
 	$text['label-additional_info']['pt-pt'] = "Informação Adicional";
 	$text['label-additional_info']['fr-fr'] = "Informations additionnelles";
+
 	$text['label-contact']['en-us'] = "Contact";
 	$text['label-contact']['pt-pt'] = "Contacto";
 	$text['label-contact']['fr-fr'] = "Contact";
+
 	$text['label-status']['en-us'] = "Status";
 	$text['label-status']['pt-pt'] = "Estado";
 	$text['label-status']['fr-fr'] = "Etat";
+
 	$text['label-time_zone']['en-us'] = "Time Zone";
 	$text['label-time_zone']['pt-pt'] = "Fuso Horário";
 	$text['label-time_zone']['fr-fr'] = "Fuseau Horaire";
+
 	$text['label-enabled']['en-us'] = "Enabled";
 	$text['label-enabled']['pt-pt'] = "Activado";
 	$text['label-enabled']['fr-fr'] = "Actif";
+
 	$text['label-group_name']['en-us'] = "Name";
 	$text['label-group_name']['pt-pt'] = "Nome";
 	$text['label-group_name']['fr-fr'] = "Nom";
+
 	$text['label-group_description']['en-us'] = "Description";
 	$text['label-group_description']['pt-pt'] = "Descrição";
 	$text['label-group_description']['fr-fr'] = "Description";
+
 	$text['label-group_permissions']['en-us'] = "Permissions";
 	$text['label-group_permissions']['pt-pt'] = "Permissões";
 	$text['label-group_permissions']['fr-fr'] = "Permissions";
+
 	$text['label-group_members']['en-us'] = "Members";
 	$text['label-group_members']['pt-pt'] = "Membros";
 	$text['label-group_members']['fr-fr'] = "Membres";
+
 	$text['label-permission_permissions']['en-us'] = "Permissions";
 	$text['label-permission_permissions']['pt-pt'] = "Permissões";
 	$text['label-permission_permissions']['fr-fr'] = "Permissions";
+
 	$text['label-permission_description']['en-us'] = "Description";
 	$text['label-permission_description']['pt-pt'] = "Descrição";
 	$text['label-permission_description']['fr-fr'] = "Description";
@@ -129,15 +165,19 @@
 	$text['description-contact']['en-us'] = "Assign a contact to this user account.";
 	$text['description-contact']['pt-pt'] = "Atribuir um contacto a esta conta de utilizador.";
 	$text['description-contact']['fr-fr'] = "Attacher un contact à ce compte utilisateur";
+
 	$text['description-contact_view']['en-us'] = "View";
 	$text['description-contact_view']['pt-pt'] = "Vista";
 	$text['description-contact_view']['fr-fr'] = "Voir";
+
 	$text['description-status']['en-us'] = "Set the user\'s presence.";
 	$text['description-status']['pt-pt'] = "Escolher o estado de presença do utilizador.";
 	$text['description-status']['fr-fr'] = "Choisir l'état de présence de l'utilisateur.";
+
 	$text['description-time_zone']['en-us'] = "Select the default time zone.";
 	$text['description-time_zone']['pt-pt'] = "Escolha o time zone por omissão.";
 	$text['description-time_zone']['fr-fr'] = "Choisir le fuseau horaire par défaut.";
+
 	$text['description-enabled']['en-us'] = "Set the status of this account.";
 	$text['description-enabled']['pt-pt'] = "Escolha o estado desta conta.";
 	$text['description-enabled']['fr-fr'] = "Choisir le statut de ce compte.";
@@ -145,21 +185,27 @@
 	$text['option-true']['en-us'] = "True";
 	$text['option-true']['pt-pt'] = "Sim";
 	$text['option-true']['fr-fr'] = "Oui";
+
 	$text['option-false']['en-us'] = "False";
 	$text['option-false']['pt-pt'] = "Não";
 	$text['option-false']['fr-fr'] = "Non";
+
 	$text['option-available']['en-us'] = "Available";
 	$text['option-available']['pt-pt'] = "Disponível";
 	$text['option-available']['fr-fr'] = "Disponible";
+
 	$text['option-available_on_demand']['en-us'] = "Available (On Demand)";
 	$text['option-available_on_demand']['pt-pt'] = "Disponível (On Demand)";
 	$text['option-available_on_demand']['fr-fr'] = "Disponible (sur demande)";
+
 	$text['option-logged_out']['en-us'] = "Logged Out";
 	$text['option-logged_out']['pt-pt'] = "Desligado";
 	$text['option-logged_out']['fr-fr'] = "Déconnecté";
+
 	$text['option-on_break']['en-us'] = "On Break";
 	$text['option-on_break']['pt-pt'] = "Em Pausa";
 	$text['option-on_break']['fr-fr'] = "En Pause";
+
 	$text['option-do_not_disturb']['en-us'] = "Do Not Disturb";
 	$text['option-do_not_disturb']['pt-pt'] = "Não Perturbar";
 	$text['option-do_not_disturb']['fr-fr'] = "Ne pas Déranger";
@@ -167,27 +213,39 @@
 	$text['button-search']['en-us'] = "Search";
 	$text['button-search']['pt-pt'] = "Pesquisar";
 	$text['button-search']['fr-fr'] = "Chercher";
+
 	$text['button-create_account']['en-us'] = "Create Account";
 	$text['button-create_account']['pt-pt'] = "Criar Conta";
 	$text['button-create_account']['fr-fr'] = "Créé le Compte";
+
 	$text['button-add']['en-us'] = "Add";
 	$text['button-add']['pt-pt'] = "Adicionar";
 	$text['button-add']['fr-fr'] = "Ajouter";
+
 	$text['button-edit']['en-us'] = "Edit";
 	$text['button-edit']['pt-pt'] = "Editar";
 	$text['button-edit']['fr-fr'] = "Editer";
+
 	$text['button-delete']['en-us'] = "Delete";
 	$text['button-delete']['pt-pt'] = "Remoção";
 	$text['button-delete']['fr-fr'] = "Supprimer";
+
 	$text['button-back']['en-us'] = "Back";
 	$text['button-back']['pt-pt'] = "Voltar";
 	$text['button-back']['fr-fr'] = "Retour";
+
 	$text['button-save']['en-us'] = "Save";
 	$text['button-save']['pt-pt'] = "Guardar";
 	$text['button-save']['fr-fr'] = "Sauvegarder";
+
 	$text['button-restore']['en-us'] = "Restore Default";
 	$text['button-restore']['pt-pt'] = "Restaurar Valores por Omissão";
 	$text['button-restore']['fr-fr'] = "Restaurer";
+
+	$text['button-copy']['en-us'] = "Copy";
+	$text['button-copy']['pt-pt'] = "Copiar";
+	$text['button-copy']['fr-fr'] = "Copier";
+
 	$text['button-add_member']['en-us'] = "Add Member";
 	$text['button-add_member']['pt-pt'] = "Adicionar Membro";
 	$text['button-add_member']['fr-fr'] = "Ajouter un Membre";
@@ -199,21 +257,27 @@
 	$text['message-add']['en-us'] = "Add Completed";
 	$text['message-add']['pt-pt'] = "Adição Efectuada";
 	$text['message-add']['fr-fr'] = "Ajouté";
+
 	$text['message-update']['en-us'] = "Update Completed";
 	$text['message-update']['pt-pt'] = "Actualização Efectuada";
 	$text['message-update']['fr-fr'] = "Mis à Jour";
+
 	$text['message-delete']['en-us'] = "Delete Completed";
 	$text['message-delete']['pt-pt'] = "Remoção Efectuada";
 	$text['message-delete']['fr-fr'] = "Supprimé";
+
 	$text['message-restore']['en-us'] = "Restore Completed";
 	$text['message-restore']['pt-pt'] = "Restauração Efectuada";
 	$text['message-restore']['fr-fr'] = "Restauré";
+
 	$text['message-required']['en-us'] = "Please provide: ";
 	$text['message-required']['pt-pt'] = "Por favor indique: ";
 	$text['message-required']['fr-fr'] = "Merci d'indiquer: ";
+
 	$text['message-password_blank']['en-us'] = "Password cannot be blank.";
 	$text['message-password_blank']['pt-pt'] = "O campo password não pode ser vazio.";
 	$text['message-password_blank']['fr-fr'] = "Le mot de passe ne peut pas être vide.";
+
 	$text['message-password_mismatch']['en-us'] = "Passwords did not match.";
 	$text['message-password_mismatch']['pt-pt'] = "As passwords não correspondem.";
 	$text['message-password_mismatch']['fr-fr'] = "les mots de passe ne correspondent pas";
