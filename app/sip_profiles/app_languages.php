31f2d565bf3b9e7de9bbe9d454bb62928a86cd8a
Mark Crane <markjcrane@gmail.com>
2013-11-12 15:56:26 +0000

    Add hostname to sip profiles.

--- app/sip_profiles/app_languages.php
+++ app/sip_profiles/app_languages.php
@@ -1,122 +1,159 @@
 <?php
 
-	//SIP Profiles
+	$text['title-sip_profiles']['en-us'] = "SIP Profiles";
+	$text['title-sip_profiles']['pt-pt'] = "Perfis SIP";
+	$text['title-sip_profiles']['fr-fr'] = "Profiles SIP";
 
-		$text['title-sip_profiles']['en-us'] = "SIP Profiles";
-		$text['title-sip_profiles']['pt-pt'] = "Perfis SIP";
-		$text['title-sip_profiles']['fr-fr'] = "Profiles SIP";
-		$text['title-sip_profile']['en-us'] = "SIP Profile";
-		$text['title-sip_profile']['pt-pt'] = "Perfil SIP";
-		$text['title-sip_profile']['fr-fr'] = "Profile SIP";
-		$text['title-setting']['en-us'] = "Profile Setting";
-		$text['title-setting']['pt-pt'] = "Definições de Perfil";
-		$text['title-setting']['fr-fr'] = "Paramètres du Profile";
+	$text['title-sip_profile']['en-us'] = "SIP Profile";
+	$text['title-sip_profile']['pt-pt'] = "Perfil SIP";
+	$text['title-sip_profile']['fr-fr'] = "Profile SIP";
 
-		$text['header-sip_profiles']['en-us'] = "SIP Profiles";
-		$text['header-sip_profiles']['pt-pt'] = "Perfis SIP";
-		$text['header-sip_profiles']['fr-fr'] = "Profiles SIP";
-		$text['header-sip_profile']['en-us'] = "SIP Profile";
-		$text['header-sip_profile']['pt-pt'] = "Perfil SIP";
-		$text['header-sip_profile']['fr-fr'] = "Profile SIP";
-		$text['header_settings']['en-us'] = "Settings";
-		$text['header_settings']['pt-pt'] = "Definições";
-		$text['header_settings']['fr-fr'] = "Paramètres";
-		$text['header-setting']['en-us'] = "Profile Setting";
-		$text['header-setting']['pt-pt'] = "Definições de Perfil";
-		$text['header-setting']['fr-fr'] = "Paramètres du Profile";
+	$text['title-setting']['en-us'] = "Profile Setting";
+	$text['title-setting']['pt-pt'] = "Definições de Perfil";
+	$text['title-setting']['fr-fr'] = "Paramètres du Profile";
 
-		$text['description-sip_profiles']['en-us'] = "Manage settings for SIP profiles.";
-		$text['description-sip_profiles']['pt-pt'] = "Gerir definições para os perfis SIP.";
-		$text['description-sip_profiles']['fr-fr'] = "Gérer les paramètres du profile SIP";
-		$text['description-sip_profile']['en-us'] = "Manage settings for the SIP profile.";
-		$text['description-sip_profile']['pt-pt'] = "Gerir definições para o perfil SIP.";
-		$text['description-sip_profile']['fr-fr'] = "Gérer les paramètres du profile SIP";
+	$text['header-sip_profiles']['en-us'] = "SIP Profiles";
+	$text['header-sip_profiles']['pt-pt'] = "Perfis SIP";
+	$text['header-sip_profiles']['fr-fr'] = "Profiles SIP";
 
-		$text['label-name']['en-us'] = "Name";
-		$text['label-name']['pt-pt'] = "Nome";
-		$text['label-name']['fr-fr'] = "Nom";
-		$text['label-description']['en-us'] = "Description";
-		$text['label-description']['pt-pt'] = "Descrição";
-		$text['label-description']['fr-fr'] = "Description";
-		$text['label-setting_name']['en-us'] = "Name";
-		$text['label-setting_name']['pt-pt'] = "Nome";
-		$text['label-setting_name']['fr-fr'] = "Nom";
-		$text['label-setting_value']['en-us'] = "Value";
-		$text['label-setting_value']['pt-pt'] = "Valor";
-		$text['label-setting_value']['fr-fr'] = "Valeur";
-		$text['label-setting_enabled']['en-us'] = "Enabled";
-		$text['label-setting_enabled']['pt-pt'] = "Activado";
-		$text['label-setting_enabled']['fr-fr'] = "Actif";
-		$text['label-setting_description']['en-us'] = "Description";
-		$text['label-setting_description']['pt-pt'] = "Descrição";
-		$text['label-setting_description']['fr-fr'] = "Description";
+	$text['header-sip_profile']['en-us'] = "SIP Profile";
+	$text['header-sip_profile']['pt-pt'] = "Perfil SIP";
+	$text['header-sip_profile']['fr-fr'] = "Profile SIP";
 
-		$text['description-name']['en-us'] = "Enter the SIP Profile name.";
-		$text['description-name']['pt-pt'] = "Introduza o nome do perfil SIP.";
-		$text['description-name']['fr-fr'] = "Entrer le nom du Profile SIP.";
-		$text['description-description']['en-us'] = "Enter the description.";
-		$text['description-description']['pt-pt'] = "Introduza a descrição.";
-		$text['description-description']['fr-fr'] = "Entrer la description.";
-		$text['description-setting_name']['en-us'] = "Enter the name.";
-		$text['description-setting_name']['pt-pt'] = "Introduza o nome.";
-		$text['description-setting_name']['fr-fr'] = "Entrer le nom.";
-		$text['description-setting_value']['en-us'] = "Enter the value.";
-		$text['description-setting_value']['pt-pt'] = "Introduza o valor.";
-		$text['description-setting_value']['fr-fr'] = "Entrer la valeur.";
-		$text['description-setting_enabled']['en-us'] = "Choose to enable or disable this.";
-		$text['description-setting_enabled']['pt-pt'] = "Escolha se o parâmetro será activo ou desactivo.";
-		$text['description-setting_enabled']['fr-fr'] = "Choisir si le paramètre sera actif ou non.";
-		$text['description-setting_description']['en-us'] = "Enter the description.";
-		$text['description-setting_description']['pt-pt'] = "Introduza uma descrição.";
-		$text['description-setting_description']['fr-fr'] = "Entrer la description.";
+	$text['header_settings']['en-us'] = "Settings";
+	$text['header_settings']['pt-pt'] = "Definições";
+	$text['header_settings']['fr-fr'] = "Paramètres";
 
-		$text['option-true']['en-us'] = "True";
-		$text['option-true']['pt-pt'] = "Sim";
-		$text['option-true']['fr-fr'] = "Oui";
-		$text['option-false']['en-us'] = "False";
-		$text['option-false']['pt-pt'] = "Não";
-		$text['option-false']['fr-fr'] = "Non";
+	$text['header-setting']['en-us'] = "Profile Setting";
+	$text['header-setting']['pt-pt'] = "Definições de Perfil";
+	$text['header-setting']['fr-fr'] = "Paramètres du Profile";
 
-		$text['button-add']['en-us'] = "Add";
-		$text['button-add']['pt-pt'] = "Adicionar";
-		$text['button-add']['fr-fr'] = "Ajouter";
-		$text['button-edit']['en-us'] = "Edit";
-		$text['button-edit']['pt-pt'] = "Editar";
-		$text['button-edit']['fr-fr'] = "Editer";
-		$text['button-delete']['en-us'] = "Delete";
-		$text['button-delete']['pt-pt'] = "Remover";
-		$text['button-delete']['fr-fr'] = "Supprimer";
-		$text['button-back']['en-us'] = "Back";
-		$text['button-back']['pt-pt'] = "voltar";
-		$text['button-back']['fr-fr'] = "Retour";
-		$text['button-copy']['en-us'] = "Copy";
-		$text['button-copy']['pt-pt'] = "Copiar";
-		$text['button-copy']['fr-fr'] = "Copier";
-		$text['button-save']['en-us'] = "Save";
-		$text['button-save']['pt-pt'] = "Guardar";
-		$text['button-save']['fr-fr'] = "Sauvegarder";
+	$text['description-sip_profiles']['en-us'] = "Manage settings for SIP profiles.";
+	$text['description-sip_profiles']['pt-pt'] = "Gerir definições para os perfis SIP.";
+	$text['description-sip_profiles']['fr-fr'] = "Gérer les paramètres du profile SIP";
 
-		$text['confirm-delete']['en-us'] = "Do you really want to delete this?";
-		$text['confirm-delete']['pt-pt'] = "Tem a certeza que pretende remover isto?";
-		$text['confirm-delete']['fr-fr'] = "Voulez-vous vraiment supprimer cela?";
-		$text['confirm-copy']['en-us'] = "Do you really want to copy this?";
-		$text['confirm-copy']['pt-pt'] = "Tem a certeza que pretende copiar isto?";
-		$text['confirm-copy']['fr-fr'] = "Voulez-vous vraiment copier cela?";
+	$text['description-sip_profile']['en-us'] = "Manage settings for the SIP profile.";
+	$text['description-sip_profile']['pt-pt'] = "Gerir definições para o perfil SIP.";
+	$text['description-sip_profile']['fr-fr'] = "Gérer les paramètres du profile SIP";
 
-		$text['message-add']['en-us'] = "Add Completed";
-		$text['message-add']['pt-pt'] = "Adição Efectuada";
-		$text['message-add']['fr-fr'] = "Ajouté";
-		$text['message-update']['en-us'] = "Update Completed";
-		$text['message-update']['pt-pt'] = "Actualização Efectuada";
-		$text['message-update']['fr-fr'] = "Mis à jour";
-		$text['message-delete']['en-us'] = "Delete Completed";
-		$text['message-delete']['pt-pt'] = "Remoção Efectuada";
-		$text['message-delete']['fr-fr'] = "Supprimé";
-		$text['message-copy']['en-us'] = "Copy Completed";
-		$text['message-copy']['pt-pt'] = "Cópia Efectuada";
-		$text['message-copy']['fr-fr'] = "Copié";
-		$text['message-required']['en-us'] = "Please provide: ";
-		$text['message-required']['pt-pt'] = "Por favor indique: ";
-		$text['message-required']['fr-fr'] = "Merci d'indiquer: ";
+	$text['label-name']['en-us'] = "Name";
+	$text['label-name']['pt-pt'] = "Nome";
+	$text['label-name']['fr-fr'] = "Nom";
+
+	$text['label-description']['en-us'] = "Description";
+	$text['label-description']['pt-pt'] = "Descrição";
+	$text['label-description']['fr-fr'] = "Description";
+
+	$text['label-setting_name']['en-us'] = "Name";
+	$text['label-setting_name']['pt-pt'] = "Nome";
+	$text['label-setting_name']['fr-fr'] = "Nom";
+
+	$text['label-setting_value']['en-us'] = "Value";
+	$text['label-setting_value']['pt-pt'] = "Valor";
+	$text['label-setting_value']['fr-fr'] = "Valeur";
+
+	$text['label-setting_enabled']['en-us'] = "Enabled";
+	$text['label-setting_enabled']['pt-pt'] = "Activado";
+	$text['label-setting_enabled']['fr-fr'] = "Actif";
+
+	$text['label-setting_description']['en-us'] = "Description";
+	$text['label-setting_description']['pt-pt'] = "Descrição";
+	$text['label-setting_description']['fr-fr'] = "Description";
+
+	$text['description-name']['en-us'] = "Enter the SIP Profile name.";
+	$text['description-name']['pt-pt'] = "Introduza o nome do perfil SIP.";
+	$text['description-name']['fr-fr'] = "Entrer le nom du Profile SIP.";
+
+	$text['description-description']['en-us'] = "Enter the description.";
+	$text['description-description']['pt-pt'] = "Introduza a descrição.";
+	$text['description-description']['fr-fr'] = "Entrer la description.";
+
+	$text['description-setting_name']['en-us'] = "Enter the name.";
+	$text['description-setting_name']['pt-pt'] = "Introduza o nome.";
+	$text['description-setting_name']['fr-fr'] = "Entrer le nom.";
+
+	$text['description-setting_value']['en-us'] = "Enter the value.";
+	$text['description-setting_value']['pt-pt'] = "Introduza o valor.";
+	$text['description-setting_value']['fr-fr'] = "Entrer la valeur.";
+
+	$text['description-setting_enabled']['en-us'] = "Choose to enable or disable this.";
+	$text['description-setting_enabled']['pt-pt'] = "Escolha se o parâmetro será activo ou desactivo.";
+	$text['description-setting_enabled']['fr-fr'] = "Choisir si le paramètre sera actif ou non.";
+
+	$text['description-setting_description']['en-us'] = "Enter the description.";
+	$text['description-setting_description']['pt-pt'] = "Introduza uma descrição.";
+	$text['description-setting_description']['fr-fr'] = "Entrer la description.";
+
+	$text['header-hostname']['en-us'] = "Hostname";
+	$text['header-hostname']['pt-pt'] = "";
+	$text['header-hostname']['fr-fr'] = "";
+
+	$text['label-hostname']['en-us'] = 'Hostname';
+	$text['label-hostname']['pt-pt'] = '';
+	$text['label-hostname']['fr-fr'] = '';
+
+	$text['description-hostname']['en-us'] = 'Enter the hostname / switchname.';
+	$text['description-hostname']['pt-pt'] = '';
+	$text['description-hostname']['fr-fr'] = '';
+
+	$text['option-true']['en-us'] = "True";
+	$text['option-true']['pt-pt'] = "Sim";
+	$text['option-true']['fr-fr'] = "Oui";
+
+	$text['option-false']['en-us'] = "False";
+	$text['option-false']['pt-pt'] = "Não";
+	$text['option-false']['fr-fr'] = "Non";
+
+	$text['button-add']['en-us'] = "Add";
+	$text['button-add']['pt-pt'] = "Adicionar";
+	$text['button-add']['fr-fr'] = "Ajouter";
+
+	$text['button-edit']['en-us'] = "Edit";
+	$text['button-edit']['pt-pt'] = "Editar";
+	$text['button-edit']['fr-fr'] = "Editer";
+
+	$text['button-delete']['en-us'] = "Delete";
+	$text['button-delete']['pt-pt'] = "Remover";
+	$text['button-delete']['fr-fr'] = "Supprimer";
+
+	$text['button-back']['en-us'] = "Back";
+	$text['button-back']['pt-pt'] = "voltar";
+	$text['button-back']['fr-fr'] = "Retour";
+
+	$text['button-copy']['en-us'] = "Copy";
+	$text['button-copy']['pt-pt'] = "Copiar";
+	$text['button-copy']['fr-fr'] = "Copier";
+
+	$text['button-save']['en-us'] = "Save";
+	$text['button-save']['pt-pt'] = "Guardar";
+	$text['button-save']['fr-fr'] = "Sauvegarder";
+
+	$text['confirm-delete']['en-us'] = "Do you really want to delete this?";
+	$text['confirm-delete']['pt-pt'] = "Tem a certeza que pretende remover isto?";
+	$text['confirm-delete']['fr-fr'] = "Voulez-vous vraiment supprimer cela?";
+
+	$text['confirm-copy']['en-us'] = "Do you really want to copy this?";
+	$text['confirm-copy']['pt-pt'] = "Tem a certeza que pretende copiar isto?";
+	$text['confirm-copy']['fr-fr'] = "Voulez-vous vraiment copier cela?";
+
+	$text['message-add']['en-us'] = "Add Completed";
+	$text['message-add']['pt-pt'] = "Adição Efectuada";
+	$text['message-add']['fr-fr'] = "Ajouté";
+
+	$text['message-update']['en-us'] = "Update Completed";
+	$text['message-update']['pt-pt'] = "Actualização Efectuada";
+	$text['message-update']['fr-fr'] = "Mis à jour";
+
+	$text['message-delete']['en-us'] = "Delete Completed";
+	$text['message-delete']['pt-pt'] = "Remoção Efectuada";
+	$text['message-delete']['fr-fr'] = "Supprimé";
+
+	$text['message-copy']['en-us'] = "Copy Completed";
+	$text['message-copy']['pt-pt'] = "Cópia Efectuada";
+	$text['message-copy']['fr-fr'] = "Copié";
+
+	$text['message-required']['en-us'] = "Please provide: ";
+	$text['message-required']['pt-pt'] = "Por favor indique: ";
+	$text['message-required']['fr-fr'] = "Merci d'indiquer: ";
 
 ?>
