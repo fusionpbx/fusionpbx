1a766efaf3110c5764ef1144ea1b6ee934f4de19
Mark Crane <markjcrane@gmail.com>
2013-11-15 04:20:56 +0000

    Hide the gateway copy button when adding a gateway.

--- app/gateways/gateway_edit.php
+++ app/gateways/gateway_edit.php
@@ -381,7 +381,9 @@ if (count($_POST)>0 && strlen($_POST["persistformvar"]) == 0) {
 	echo "		</td>";
 	echo "		<td width='50%' align='right'>\n";
 	echo "			<input type='submit' name='submit' class='btn' value='".$text['button-save']."'>\n";
-	echo "			<input type='button' class='btn' name='' alt='".$text['button-copy']."' onclick=\"if (confirm('".$text['confirm-copy']."')){window.location='gateway_copy.php?id=".$gateway_uuid."';}\" value='".$text['button-copy']."'>\n";
+	if ($action == "update") {
+		echo "			<input type='button' class='btn' name='' alt='".$text['button-copy']."' onclick=\"if (confirm('".$text['confirm-copy']."')){window.location='gateway_copy.php?id=".$gateway_uuid."';}\" value='".$text['button-copy']."'>\n";
+	}
 	echo "			<input type='button' class='btn' name='' alt='".$text['button-back']."' onclick=\"window.location='gateways.php'\" value='".$text['button-back']."'>\n";
 	echo "		</td>\n";
 	echo "	</tr>";
