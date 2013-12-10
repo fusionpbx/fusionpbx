b5c09c793412f5d426f589765b9ba1d3f0d9c09d
Mark Crane <markjcrane@gmail.com>
2013-12-10 14:00:54 +0000

    After adding or editing a device setting redirect back to device_edit.php.

--- app/devices/device_setting_delete.php
+++ app/devices/device_setting_delete.php
@@ -57,7 +57,7 @@ if (strlen($id)>0) {
 
 //redirect the user
 	require_once "resources/header.php";
-	echo "<meta http-equiv=\"refresh\" content=\"2;url=device_settings.php?id=$device_uuid\">\n";
+	echo "<meta http-equiv=\"refresh\" content=\"2;url=device_edit.php?id=$device_uuid\">\n";
 	echo "<div align='center'>\n";
 	echo $text['message-delete']."\n";
 	echo "</div>\n";
--- app/devices/device_setting_edit.php
+++ app/devices/device_setting_edit.php
@@ -129,7 +129,7 @@ if (count($_POST)>0 && strlen($_POST["persistformvar"]) == 0) {
 
 			//redirect the browser
 				require_once "resources/header.php";
-				echo "<meta http-equiv=\"refresh\" content=\"2;url=device_settings.php?id=$device_uuid\">\n";
+				echo "<meta http-equiv=\"refresh\" content=\"2;url=device_edit.php?id=$device_uuid\">\n";
 				echo "<div align='center'>\n";
 				if ($action == "add") {
 					echo $text['message-add']."\n";
