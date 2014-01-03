08c772cde4adf7e3ec8ea0f45081c09f8382677d
Mark Crane <markjcrane@gmail.com>
2014-01-03 08:05:46 +0000

    Add device_key_line

--- app/devices/app_config.php
+++ app/devices/app_config.php
@@ -361,6 +361,10 @@
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = 'Select the type.';
 		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'device_key_line';
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = 'Enter the value.';
+		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'device_key_value';
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = 'Enter the value.';
