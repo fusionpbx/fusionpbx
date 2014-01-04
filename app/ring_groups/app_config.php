5afad8ad8c4d4f226b4160dd87e23b158a81c7da
Mark Crane <markjcrane@gmail.com>
2014-01-04 07:16:05 +0000

    Add ring groups menu permission for users in the admin group.

--- app/ring_groups/app_config.php
+++ app/ring_groups/app_config.php
@@ -33,9 +33,8 @@
 		$apps[$x]['menu'][0]['parent_uuid'] = "fd29e39c-c936-f5fc-8e2b-611681b266b5";
 		$apps[$x]['menu'][0]['category'] = "internal";
 		$apps[$x]['menu'][0]['path'] = "/app/ring_groups/ring_groups.php";
-		//$apps[$x]['menu'][0]['groups'][] = "user";
-		//$apps[$x]['menu'][0]['groups'][] = "admin";
 		$apps[$x]['menu'][0]['groups'][] = "superadmin";
+		$apps[$x]['menu'][0]['groups'][] = "admin";
 
 	//permission details
 		$y = 0;
