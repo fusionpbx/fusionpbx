485b40fc4fe192e26f128b812cabcba7be3c89f8
Mark Crane <markjcrane@gmail.com>
2014-01-16 20:14:33 +0000

    Add spacing between the translations.

--- core/upgrade/app_languages.php
+++ core/upgrade/app_languages.php
@@ -1,57 +1,66 @@
 <?php
 
-	// Upgrade Schema
+	$text['title-upgrade_schema']['en-us'] = "Upgrade Schema";
+	$text['title-upgrade_schema']['pt-pt'] = "Actualizar Esquema";
+	$text['title-upgrade_schema']['fr-fr'] = "Mise à jour du Schéma";
 
-		$text['title-upgrade_schema']['en-us'] = "Upgrade Schema";
-		$text['title-upgrade_schema']['pt-pt'] = "Actualizar Esquema";
-		$text['title-upgrade_schema']['fr-fr'] = "Mise à jour du Schéma";
+	$text['header-database_type']['en-us'] = "Database Type";
+	$text['header-database_type']['pt-pt'] = "Tipo de Base de Dados";
+	$text['header-database_type']['fr-fr'] = "Type de base de données";
 
-		$text['header-database_type']['en-us'] = "Database Type";
-		$text['header-database_type']['pt-pt'] = "Tipo de Base de Dados";
-		$text['header-database_type']['fr-fr'] = "Type de base de données";
-		$text['header-message']['en-us'] = "Message";
-		$text['header-message']['pt-pt'] = "Mensagem";
-		$text['header-message']['fr-fr'] = "Message";
+	$text['header-message']['en-us'] = "Message";
+	$text['header-message']['pt-pt'] = "Mensagem";
+	$text['header-message']['fr-fr'] = "Message";
 
-		$text['label-sql_changes']['en-us'] = "SQL Changes";
-		$text['label-sql_changes']['pt-pt'] = "Modificações SQL";
-		$text['label-sql_changes']['fr-fr'] = "Modifications SQL";
-		$text['label-table']['en-us'] = "Table";
-		$text['label-table']['pt-pt'] = "Tabela";
-		$text['label-table']['fr-fr'] = "Table";
-		$text['label-exists']['en-us'] = "Exists";
-		$text['label-exists']['pt-pt'] = "Existe";
-		$text['label-exists']['fr-fr'] = "Existe";
-		$text['label-details']['en-us'] = "Details";
-		$text['label-details']['pt-pt'] = "Detalhes";
-		$text['label-details']['fr-fr'] = "Détails";
-		$text['label-name']['en-us'] = "Name";
-		$text['label-name']['pt-pt'] = "Nome";
-		$text['label-name']['fr-fr'] = "Nom";
-		$text['label-type']['en-us'] = "Type";
-		$text['label-type']['pt-pt'] = "Tipo";
-		$text['label-type']['fr-fr'] = "Type";
-		$text['label-upgrade']['en-us'] = "Upgrade";
-		$text['label-upgrade']['pt-pt'] = "Actualizar";
-		$text['label-upgrade']['fr-fr'] = "Mise à jour";
-		$text['label-database']['en-us'] = "Database";
-		$text['label-database']['pt-pt'] = "Base de Dados";
-		$text['label-database']['fr-fr'] = "Base de Données";
-		$text['label-schema']['en-us'] = "Schema";
-		$text['label-schema']['pt-pt'] = "Esquema";
-		$text['label-schema']['fr-fr'] = "Schéma";
-		$text['label-no_change']['en-us'] = "No Change";
-		$text['label-no_change']['pt-pt'] = "Sem Mudança";
-		$text['label-no_change']['fr-fr'] = "Pas de changement";
+	$text['label-sql_changes']['en-us'] = "SQL Changes";
+	$text['label-sql_changes']['pt-pt'] = "Modificações SQL";
+	$text['label-sql_changes']['fr-fr'] = "Modifications SQL";
 
-		$text['option-true']['en-us'] = "True";
-		$text['option-true']['pt-pt'] = "Sim";
-		$text['option-true']['fr-fr'] = "Oui";
-		$text['option-false']['en-us'] = "False";
-		$text['option-false']['pt-pt'] = "Não";
-		$text['option-false']['fr-fr'] = "Non";
+	$text['label-table']['en-us'] = "Table";
+	$text['label-table']['pt-pt'] = "Tabela";
+	$text['label-table']['fr-fr'] = "Table";
 
-		$text['message-upgrade']['en-us'] = "Upgrade Completed";
-		$text['message-upgrade']['pt-pt'] = "Actualização Efectuada";
-		$text['message-upgrade']['fr-fr'] = "Mis à jour";
+	$text['label-exists']['en-us'] = "Exists";
+	$text['label-exists']['pt-pt'] = "Existe";
+	$text['label-exists']['fr-fr'] = "Existe";
+
+	$text['label-details']['en-us'] = "Details";
+	$text['label-details']['pt-pt'] = "Detalhes";
+	$text['label-details']['fr-fr'] = "Détails";
+
+	$text['label-name']['en-us'] = "Name";
+	$text['label-name']['pt-pt'] = "Nome";
+	$text['label-name']['fr-fr'] = "Nom";
+
+	$text['label-type']['en-us'] = "Type";
+	$text['label-type']['pt-pt'] = "Tipo";
+	$text['label-type']['fr-fr'] = "Type";
+
+	$text['label-upgrade']['en-us'] = "Upgrade";
+	$text['label-upgrade']['pt-pt'] = "Actualizar";
+	$text['label-upgrade']['fr-fr'] = "Mise à jour";
+
+	$text['label-database']['en-us'] = "Database";
+	$text['label-database']['pt-pt'] = "Base de Dados";
+	$text['label-database']['fr-fr'] = "Base de Données";
+
+	$text['label-schema']['en-us'] = "Schema";
+	$text['label-schema']['pt-pt'] = "Esquema";
+	$text['label-schema']['fr-fr'] = "Schéma";
+
+	$text['label-no_change']['en-us'] = "No Change";
+	$text['label-no_change']['pt-pt'] = "Sem Mudança";
+	$text['label-no_change']['fr-fr'] = "Pas de changement";
+
+	$text['option-true']['en-us'] = "True";
+	$text['option-true']['pt-pt'] = "Sim";
+	$text['option-true']['fr-fr'] = "Oui";
+
+	$text['option-false']['en-us'] = "False";
+	$text['option-false']['pt-pt'] = "Não";
+	$text['option-false']['fr-fr'] = "Non";
+
+	$text['message-upgrade']['en-us'] = "Upgrade Completed";
+	$text['message-upgrade']['pt-pt'] = "Actualização Efectuada";
+	$text['message-upgrade']['fr-fr'] = "Mis à jour";
 ?>
