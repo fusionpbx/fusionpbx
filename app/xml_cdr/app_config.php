b4df5264e152db6d6364cd6638d88674d0716174
Mark Crane <markjcrane@gmail.com>
2014-02-13 04:31:33 +0000

    Move the 'Apps' menu to a different directory.

--- app/xml_cdr/app_config.php
+++ app/xml_cdr/app_config.php
@@ -19,25 +19,6 @@
 		$apps[$x]['description']['pt-br'] = "";
 
 	//menu details
-		$apps[$x]['menu'][0]['title']['en-us'] = "Apps";
-		$apps[$x]['menu'][0]['title']['es-mx'] = "";
-		$apps[$x]['menu'][0]['title']['de-de'] = "";
-		$apps[$x]['menu'][0]['title']['de-ch'] = "";
-		$apps[$x]['menu'][0]['title']['de-at'] = "";
-		$apps[$x]['menu'][0]['title']['fr-fr'] = "Apps";
-		$apps[$x]['menu'][0]['title']['fr-ca'] = "";
-		$apps[$x]['menu'][0]['title']['fr-ch'] = "";
-		$apps[$x]['menu'][0]['title']['pt-pt'] = "Aplicações";
-		$apps[$x]['menu'][0]['title']['pt-br'] = "";
-		$apps[$x]['menu'][0]['uuid'] = "fd29e39c-c936-f5fc-8e2b-611681b266b5";
-		$apps[$x]['menu'][0]['parent_uuid'] = "";
-		$apps[$x]['menu'][0]['category'] = "internal";
-		$apps[$x]['menu'][0]['path'] = "/app/xml_cdr/xml_cdr.php";
-		$apps[$x]['menu'][0]['order'] = "20";
-		$apps[$x]['menu'][0]['groups'][] = "user";
-		$apps[$x]['menu'][0]['groups'][] = "admin";
-		$apps[$x]['menu'][0]['groups'][] = "superadmin";
-
 		$apps[$x]['menu'][1]['title']['en-us'] = "Call Detail Records";
 		$apps[$x]['menu'][1]['title']['es-mx'] = "";
 		$apps[$x]['menu'][1]['title']['de-de'] = "";
