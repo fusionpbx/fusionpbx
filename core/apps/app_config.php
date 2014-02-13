fdfe07a78799dd3d1fb6d293060b752d24be319f
Mark Crane <markjcrane@gmail.com>
2014-02-13 04:51:03 +0000

    Add the Apps menu to core/apps/app_config.php

--- core/apps/app_config.php
+++ core/apps/app_config.php
@@ -19,21 +19,45 @@
 		$apps[$x]['description']['pt-br'] = "";
 
 	//menu details
-		$apps[$x]['menu'][0]['title']['en-us'] = "App Manager";
+		$apps[$x]['menu'][0]['title']['en-us'] = "Apps";
 		$apps[$x]['menu'][0]['title']['es-mx'] = "";
 		$apps[$x]['menu'][0]['title']['de-de'] = "";
 		$apps[$x]['menu'][0]['title']['de-ch'] = "";
 		$apps[$x]['menu'][0]['title']['de-at'] = "";
-		$apps[$x]['menu'][0]['title']['fr-fr'] = "Gestion App";
+		$apps[$x]['menu'][0]['title']['fr-fr'] = "Apps";
 		$apps[$x]['menu'][0]['title']['fr-ca'] = "";
 		$apps[$x]['menu'][0]['title']['fr-ch'] = "";
-		$apps[$x]['menu'][0]['title']['pt-pt'] = "Gestor de Aplicações";
+		$apps[$x]['menu'][0]['title']['pt-pt'] = "Aplicações";
 		$apps[$x]['menu'][0]['title']['pt-br'] = "";
-		$apps[$x]['menu'][0]['uuid'] = "ef00f229-7890-00c2-bf23-fed5b8fa9fe7";
-		$apps[$x]['menu'][0]['parent_uuid'] = "594d99c5-6128-9c88-ca35-4b33392cec0f";
+		$apps[$x]['menu'][0]['uuid'] = "fd29e39c-c936-f5fc-8e2b-611681b266b5";
+		$apps[$x]['menu'][0]['parent_uuid'] = "";
 		$apps[$x]['menu'][0]['category'] = "internal";
-		$apps[$x]['menu'][0]['path'] = "/core/apps/apps.php";
+		if (file_exists($_SERVER["DOCUMENT_ROOT"].PROJECT_PATH.'/app/xml_cdr')) {
+			$apps[$x]['menu'][0]['path'] = "/app/xml_cdr/xml_cdr.php";
+		}
+		else {
+			$apps[$x]['menu'][0]['path'] = PROJECT_PATH;
+		}
+		$apps[$x]['menu'][0]['order'] = "20";
+		$apps[$x]['menu'][0]['groups'][] = "user";
+		$apps[$x]['menu'][0]['groups'][] = "admin";
 		$apps[$x]['menu'][0]['groups'][] = "superadmin";
+
+		$apps[$x]['menu'][1]['title']['en-us'] = "App Manager";
+		$apps[$x]['menu'][1]['title']['es-mx'] = "";
+		$apps[$x]['menu'][1]['title']['de-de'] = "";
+		$apps[$x]['menu'][1]['title']['de-ch'] = "";
+		$apps[$x]['menu'][1]['title']['de-at'] = "";
+		$apps[$x]['menu'][1]['title']['fr-fr'] = "Gestion App";
+		$apps[$x]['menu'][1]['title']['fr-ca'] = "";
+		$apps[$x]['menu'][1]['title']['fr-ch'] = "";
+		$apps[$x]['menu'][1]['title']['pt-pt'] = "Gestor de Aplicações";
+		$apps[$x]['menu'][1]['title']['pt-br'] = "";
+		$apps[$x]['menu'][1]['uuid'] = "ef00f229-7890-00c2-bf23-fed5b8fa9fe7";
+		$apps[$x]['menu'][1]['parent_uuid'] = "594d99c5-6128-9c88-ca35-4b33392cec0f";
+		$apps[$x]['menu'][1]['category'] = "internal";
+		$apps[$x]['menu'][1]['path'] = "/core/apps/apps.php";
+		$apps[$x]['menu'][1]['groups'][] = "superadmin";
 
 	//permission details
 		$y = 0;
