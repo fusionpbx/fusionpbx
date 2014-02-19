37c58aa15cf1e386f5adb7fbfe5f4f589148dd95
Mark Crane <markjcrane@gmail.com>
2014-02-19 08:20:15 +0000

    Add a button to Call Broadcast to Cancel the calls.

---
+++ app/call_broadcast/call_broadcast_cancel.php
@@ -0,0 +1,64 @@
+<?php
+/*
+	FusionPBX
+	Version: MPL 1.1
+
+	The contents of this file are subject to the Mozilla Public License Version
+	1.1 (the "License"); you may not use this file except in compliance with
+	the License. You may obtain a copy of the License at
+	http://www.mozilla.org/MPL/
+
+	Software distributed under the License is distributed on an "AS IS" basis,
+	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	for the specific language governing rights and limitations under the
+	License.
+
+	The Original Code is FusionPBX
+
+	The Initial Developer of the Original Code is
+	Mark J Crane <markjcrane@fusionpbx.com>
+	Portions created by the Initial Developer are Copyright (C) 2008-2012
+	the Initial Developer. All Rights Reserved.
+
+	Contributor(s):
+	Mark J Crane <markjcrane@fusionpbx.com>
+*/
+include "root.php";
+require_once "resources/require.php";
+require_once "resources/check_auth.php";
+if (permission_exists('call_broadcast_send')) {
+	//access granted
+}
+else {
+	echo "access denied";
+	exit;
+}
+
+//add multi-lingual support
+	require_once "app_languages.php";
+	foreach($text as $key => $value) {
+		$text[$key] = $value[$_SESSION['domain']['language']['code']];
+	}
+
+//get the html values and set them as variables
+	if (count($_GET)>0) {
+		$uuid = trim($_GET["id"]);
+	}
+
+//show the header
+	require_once "resources/header.php";
+	echo "<meta http-equiv=\"refresh\" content=\"2;url=call_broadcast_edit.php?id=$uuid\">\n";
+
+//show the result
+	if (count($_GET) > 0) {
+		$fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
+		if ($fp) {
+			$cmd = "sched_del ".$uuid;
+			$result = event_socket_request($fp, 'api '.$cmd);
+			echo htmlentities($result);
+		}
+	}
+
+//show the footer
+	require_once "resources/footer.php";
+?>
