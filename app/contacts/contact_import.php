e7c3918eb6cb355e49dffb64130c571501dda13c
Nate Jones <git@reliberate.com>
2014-02-26 04:54:25 +0000

    Changed class on file input.

--- app/contacts/contact_import.php
+++ app/contacts/contact_import.php
@@ -407,7 +407,7 @@ else {
 	echo "			".$text['label-import_file_upload'].":\n";
 	echo "</td>\n";
 	echo "<td class='vtable' align='left'>\n";
-	echo "			<input name=\"ulfile\" type=\"file\" class=\"btn\" id=\"ulfile\">\n";
+	echo "			<input name=\"ulfile\" type=\"file\" class=\"formfld\" id=\"ulfile\">\n";
 	echo "<br />\n";
 	//echo "Select the enclosure.\n";
 	echo "</td>\n";
