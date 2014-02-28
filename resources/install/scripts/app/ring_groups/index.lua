bbea9c2f9e136d8438977c81a123e4df1f914d69
Mark Crane <markjcrane@gmail.com>
2014-02-28 00:06:19 +0000

    Removed } which caused a problem when pressing *2 to make a recording.

--- resources/install/scripts/app/ring_groups/index.lua
+++ resources/install/scripts/app/ring_groups/index.lua
@@ -358,7 +358,7 @@
 
 				--set bind meta app
 					session:execute("bind_meta_app", "1 ab s execute_extension::dx XML features");
-					session:execute("bind_meta_app", "2 ab s record_session::"..recordings_dir.."}/archive/"..os.date("%Y").."/"..os.date("%m").."/"..os.date("%d").."}/"..uuid..".wav");
+					session:execute("bind_meta_app", "2 ab s record_session::"..recordings_dir.."/archive/"..os.date("%Y").."/"..os.date("%m").."/"..os.date("%d").."}/"..uuid..".wav");
 					session:execute("bind_meta_app", "3 ab s execute_extension::cf XML features");
 					session:execute("bind_meta_app", "4 ab s execute_extension::att_xfer XML features");
 
