e1c991d6ea41c2545cceb6140b57fdc3f3c2ca4f
Mark Crane <markjcrane@gmail.com>
2014-03-22 01:00:46 +0000

    Add code to change switch gateways dir to switch sip_profiles dir. Update the domain settings.

--- core/upgrade/upgrade_domains.php
+++ core/upgrade/upgrade_domains.php
@@ -137,10 +137,20 @@
 				$category = $row['default_setting_category'];
 				$subcategory = $row['default_setting_subcategory'];
 				if (strlen($subcategory) == 0) {
-					$_SESSION[$category][$name] = $row['default_setting_value'];
-				}
-				else {
-					$_SESSION[$category][$subcategory][$name] = $row['default_setting_value'];
+					if ($name == "array") {
+						$_SESSION[$category][] = $row['default_setting_value'];
+					}
+					else {
+						$_SESSION[$category][$name] = $row['default_setting_value'];
+					}
+				} else {
+					if ($name == "array") {
+						$_SESSION[$category][$subcategory][] = $row['default_setting_value'];
+					}
+					else {
+						$_SESSION[$category][$subcategory]['uuid'] = $row['default_setting_uuid'];
+						$_SESSION[$category][$subcategory][$name] = $row['default_setting_value'];
+					}
 				}
 			}
 
