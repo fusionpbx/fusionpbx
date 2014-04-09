8704cb69d30053399188df5361c1a866e6b238b8
Mark Crane <markjcrane@gmail.com>
2014-04-09 04:36:17 +0000

    remove the s

--- root.php
+++ root.php
@@ -1,7 +1,7 @@
 <?php
 /*
 	FusionPBX
-	Version: MPL 1.1s
+	Version: MPL 1.1
 
 	The contents of this file are subject to the Mozilla Public License Version
 	1.1 (the "License"); you may not use this file except in compliance with
