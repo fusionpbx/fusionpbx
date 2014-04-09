025f733ac283d6b4af957999fe606f586da342b7
Mark Crane <markjcrane@gmail.com>
2014-04-09 04:33:50 +0000

    if the debian package scripts directory exists then use it for the source

--- root.php
+++ root.php
@@ -1,7 +1,7 @@
 <?php
 /*
 	FusionPBX
-	Version: MPL 1.1
+	Version: MPL 1.1s
 
 	The contents of this file are subject to the Mozilla Public License Version
 	1.1 (the "License"); you may not use this file except in compliance with
