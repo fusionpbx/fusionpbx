898d35ae8cbf3efde84054e60962421fa2ccef8d
Mark Crane <markjcrane@gmail.com>
2014-04-29 17:51:29 +0000

    During the install check for /etc/freeswitch/vars.xml before using it

--- resources/install.php
+++ resources/install.php
@@ -166,7 +166,7 @@ require_once "resources/functions.php";
 					$switch_sip_profiles_dir = $switch_conf_dir.'/sip_profiles';
 					$switch_dialplan_dir = $switch_conf_dir.'/dialplan';
 				}
-				if (file_exists('/etc/freeswitch')) {
+				if (file_exists('/etc/freeswitch/vars.xml')) {
 					$switch_conf_dir = '/etc/freeswitch';
 					$switch_extensions_dir = $switch_conf_dir.'/directory';
 					$switch_sip_profiles_dir = $switch_conf_dir.'/sip_profiles';
