a871d6b28c8268d77de48b6e0319e8755caa4fc6
Mark Crane <markjcrane@gmail.com>
2014-05-02 05:45:57 +0000

    Add conference center mute.lua

---
+++ resources/install/scripts/app/conference_center/resources/scripts/mute.lua
@@ -0,0 +1,16 @@
+
+--get the argv values
+	script_name = argv[0];
+
+--options all, last, non_moderator, member_id
+	data = argv[1];
+
+--prepare the api object
+	api = freeswitch.API();
+
+--get the session variables
+	conference_name = session:getVariable("conference_name");
+
+--send the conferenc mute command
+	cmd = "conference " .. conference_name .. " mute " .. data;
+	response = api:executeString(cmd);
