515483e6a72aa45c95b8210d9fb71e9dc05a1f79
Nate Jones <git@reliberate.com>
2014-05-06 00:03:39 +0000

    Voicemail Greetings: Minor language mods, re-enabled editing of greeting.

--- app/voicemail_greetings/voicemail_greeting_edit.php
+++ app/voicemail_greetings/voicemail_greeting_edit.php
@@ -186,7 +186,10 @@ if (count($_POST)>0 && strlen($_POST["persistformvar"]) == 0) {
 	if ($action == "update") {
 		echo "<td align='left' width='30%' nowrap><b>".$text['label-edit']."</b></td>\n";
 	}
-	echo "<td width='70%' align='right'><input type='button' class='btn' name='' alt='back' onclick=\"window.location='voicemail_greetings.php?id=".$voicemail_id."'\" value='".$text['button-back']."'></td>\n";
+	echo "<td width='70%' align='right'>\n";
+	echo "	<input type='button' class='btn' name='' alt='".$text['button-back']."' onclick=\"window.location='voicemail_greetings.php?id=".$voicemail_id."'\" value='".$text['button-back']."'>";
+	echo "	<input type='submit' name='submit' class='btn' value='".$text['button-save']."'>\n";
+	echo "</td>\n";
 	echo "</tr>\n";
 
 	echo "<tr>\n";
