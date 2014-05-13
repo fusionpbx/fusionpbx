417375abd461ac1f51ca14e501cb23dd130e1a5c
Mark Crane <markjcrane@gmail.com>
2014-05-13 05:45:04 +0000

    Fix the table alignment

--- core/users/usersupdate.php
+++ core/users/usersupdate.php
@@ -643,8 +643,6 @@ else {
 	echo "		".$text['description-user_language']."<br />\n";
 	echo "	</td>\n";
 	echo "	</tr>\n";
-	echo "	</table>";
-	echo "<br>";
 
 	echo "	<tr>\n";
 	echo "	<td width='20%' class=\"vncell\">\n";
