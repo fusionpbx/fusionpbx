1c20d2465324202de23d351c013b0bcfdde169f6
Nate Jones <git@reliberate.com>
2014-06-22 01:10:24 +0000

    Usability Enhancement: Click on list rows to Edit items.

--- app/call_center/call_center_queues.php
+++ app/call_center/call_center_queues.php
@@ -112,7 +112,7 @@ require_once "resources/paging.php";
 	$row_style["1"] = "row_style1";
 
 	echo "<div align='center'>\n";
-	echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
+	echo "<table class='tr_hover' width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
 	echo "<tr>\n";
 	echo th_order_by('queue_name', $text['label-queue_name'], $order_by, $order);
 	echo th_order_by('queue_extension', $text['label-extension'], $order_by, $order);
@@ -134,11 +134,12 @@ require_once "resources/paging.php";
 		echo "<a href='call_center_queue_edit.php' alt='".$text['button-add']."'>$v_link_label_add</a>";
 	}
 	echo "</td>\n";
-	echo "<tr>\n";
+	echo "</tr>\n";
 
 	if ($result_count > 0) {
 		foreach($result as $row) {
-			echo "<tr >\n";
+			$tr_link = (permission_exists('call_center_queue_edit')) ? "href='call_center_queue_edit.php?id=".$row[call_center_queue_uuid]."'" : null;
+			echo "<tr ".$tr_link.">\n";
 			echo "	<td valign='top' class='".$row_style[$c]."'>";
 			if (permission_exists('call_center_queue_edit')) {
 				echo "<a href='call_center_queue_edit.php?id=".$row[call_center_queue_uuid]."'>".$row[queue_name]."</a>";
