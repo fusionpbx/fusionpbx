0c5b38819f9b4cacda4dada77c81bfc427fb9939
Mark Crane <markjcrane@gmail.com>
2014-06-22 00:02:04 +0000

    Add cdr format and storage settings to default settings

--- app/xml_cdr/app_defaults.php
+++ app/xml_cdr/app_defaults.php
@@ -25,8 +25,9 @@
 */
 
 //make sure that prefix-a-leg is set to true in the xml_cdr.conf.xml file
-/*
+
 	if ($domains_processed == 1) {
+		/*
 		$file_contents = file_get_contents($_SESSION['switch']['conf']['dir']."/autoload_configs/xml_cdr.conf.xml");
 		$file_contents_new = str_replace("param name=\"prefix-a-leg\" value=\"false\"/", "param name=\"prefix-a-leg\" value=\"true\"/", $file_contents);
 		if ($file_contents != $file_contents_new) {
@@ -37,6 +38,65 @@
 				echo "	xml_cdr.conf.xml: 	updated\n";
 			}
 		}
+		*/
+
+		//ensure that the language code is set
+			$sql = "select count(*) as num_rows from v_default_settings ";
+			$sql .= "where default_setting_category = 'cdr' ";
+			$sql .= "and default_setting_subcategory = 'format' ";
+			$sql .= "and default_setting_name = 'text' ";
+			$prep_statement = $db->prepare($sql);
+			if ($prep_statement) {
+				$prep_statement->execute();
+				$row = $prep_statement->fetch(PDO::FETCH_ASSOC);
+				if ($row['num_rows'] == 0) {
+					$sql = "insert into v_default_settings ";
+					$sql .= "(";
+					$sql .= "default_setting_uuid, ";
+					$sql .= "default_setting_category, ";
+					$sql .= "default_setting_subcategory, ";
+					$sql .= "default_setting_name, ";
+					$sql .= "default_setting_value, ";
+					$sql .= "default_setting_enabled, ";
+					$sql .= "default_setting_description ";
+					$sql .= ")";
+					$sql .= "values ";
+					$sql .= "(";
+					$sql .= "'".uuid()."', ";
+					$sql .= "'cdr', ";
+					$sql .= "'format', ";
+					$sql .= "'text', ";
+					$sql .= "'json', ";
+					$sql .= "'true', ";
+					$sql .= "'' ";
+					$sql .= ")";
+					$db->exec(check_sql($sql));
+					unset($sql);
+
+					$sql = "insert into v_default_settings ";
+					$sql .= "(";
+					$sql .= "default_setting_uuid, ";
+					$sql .= "default_setting_category, ";
+					$sql .= "default_setting_subcategory, ";
+					$sql .= "default_setting_name, ";
+					$sql .= "default_setting_value, ";
+					$sql .= "default_setting_enabled, ";
+					$sql .= "default_setting_description ";
+					$sql .= ")";
+					$sql .= "values ";
+					$sql .= "(";
+					$sql .= "'".uuid()."', ";
+					$sql .= "'cdr', ";
+					$sql .= "'storage', ";
+					$sql .= "'text', ";
+					$sql .= "'db', ";
+					$sql .= "'true', ";
+					$sql .= "'' ";
+					$sql .= ")";
+					$db->exec(check_sql($sql));
+					unset($sql);
+				}
+			}
 	}
 */
 ?>
