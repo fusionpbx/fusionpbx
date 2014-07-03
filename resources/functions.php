39eddf4b0a0d6e1c755c1fbfb1c4605b4f7a04be
Nate Jones <git@reliberate.com>
2014-07-03 23:19:30 +0000

    Added valid_email() function for verifying email address syntax.

--- resources/functions.php
+++ resources/functions.php
@@ -950,4 +950,16 @@ function number_pad($number,$n) {
 	return str_pad((int) $number,$n,"0",STR_PAD_LEFT);
 }
 
+// validate email address syntax
+if(!function_exists('validate_email')) {
+	function valid_email($email) {
+		$regex = '/^[A-z0-9][\w.-]*@[A-z0-9][\w\-\.]+\.[A-z0-9]{2,6}$/';
+		if ($email != "" && preg_match($regex, $email) == 0) {
+			return false; // email address does not have valid syntax
+		}
+		else {
+			return true; // email address has valid syntax
+		}
+	}
+}
 ?>
