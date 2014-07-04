669ff44a02b77590a57485bd98077dc573cbf458
Nate Jones <git@reliberate.com>
2014-07-04 22:45:43 +0000

    New Project Notifications feature, collect installation demographics.

---
+++ app/notifications/app_config.php
@@ -0,0 +1,58 @@
+<?php
+	//application details
+		$apps[$x]['name'] = "Notifications";
+		$apps[$x]['uuid'] = "e746fbcb-f67f-4e0e-ab64-c414c01fac11";
+		$apps[$x]['category'] = "Switch";
+		$apps[$x]['subcategory'] = "";
+		$apps[$x]['version'] = "";
+		$apps[$x]['url'] = "http://www.fusionpbx.com";
+		$apps[$x]['description']['en-us'] = "Configure notification preferences.";
+		$apps[$x]['description']['es-cl'] = "";
+		$apps[$x]['description']['de-de'] = "";
+		$apps[$x]['description']['de-ch'] = "";
+		$apps[$x]['description']['de-at'] = "";
+		$apps[$x]['description']['fr-fr'] = "";
+		$apps[$x]['description']['fr-ca'] = "";
+		$apps[$x]['description']['fr-ch'] = "";
+		$apps[$x]['description']['pt-pt'] = "";
+		$apps[$x]['description']['pt-br'] = "";
+
+	//menu details
+		$apps[$x]['menu'][0]['title']['en-us'] = "Notifications";
+		$apps[$x]['menu'][0]['title']['es-cl'] = "";
+		$apps[$x]['menu'][0]['title']['de-de'] = "";
+		$apps[$x]['menu'][0]['title']['de-ch'] = "";
+		$apps[$x]['menu'][0]['title']['de-at'] = "";
+		$apps[$x]['menu'][0]['title']['fr-fr'] = "";
+		$apps[$x]['menu'][0]['title']['fr-ca'] = "";
+		$apps[$x]['menu'][0]['title']['fr-ch'] = "";
+		$apps[$x]['menu'][0]['title']['pt-pt'] = "";
+		$apps[$x]['menu'][0]['title']['pt-br'] = "";
+		$apps[$x]['menu'][0]['uuid'] = "f8d65f91-0f4a-405a-b5ac-24cb3c4f10ba";
+		$apps[$x]['menu'][0]['parent_uuid'] = "02194288-6d56-6d3e-0b1a-d53a2bc10788";
+		$apps[$x]['menu'][0]['category'] = "internal";
+		$apps[$x]['menu'][0]['path'] = "/app/notifications/notifications_edit.php";
+		$apps[$x]['menu'][0]['groups'][] = "superadmin";
+
+	//permission details
+		$apps[$x]['permissions'][0]['name'] = "notifications_edit";
+		$apps[$x]['permissions'][0]['menu']['uuid'] = "f8d65f91-0f4a-405a-b5ac-24cb3c4f10ba";
+		$apps[$x]['permissions'][0]['groups'][] = "superadmin";
+
+	//schema details
+		$y = 0; //table array index
+		$z = 0; //field array index
+		$apps[$x]['db'][$y]['table'] = "v_notifications";
+
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = "notification_uuid";
+		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = "uuid";
+		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = "text";
+		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = "char(36)";
+		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = "primary";
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = "project_notifications";
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
+		$z++;
+
+?>
---
+++ app/notifications/app_languages.php
@@ -0,0 +1,137 @@
+<?php
+
+	$text['title-notifications']['en-us'] = "Notifications";
+	$text['title-notifications']['es-cl'] = "Notificaciones";
+	$text['title-notifications']['pt-pt'] = "Notificações";
+	$text['title-notifications']['fr-fr'] = "Notifications";
+
+	$text['header-notifications']['en-us'] = "Notifications";
+	$text['header-notifications']['es-cl'] = "Notificaciones";
+	$text['header-notifications']['pt-pt'] = "Notificações";
+	$text['header-notifications']['fr-fr'] = "Notifications";
+
+	$text['description-notifications']['en-us'] = "Set notification preferences for FusionPBX project-related communication and other local system alerts.";
+	$text['description-notifications']['es-cl'] = "Definición de las preferencias de notificación de la comunicación relacionada con el proyecto FusionPBX y otras alertas del sistema local.";
+	$text['description-notifications']['pt-pt'] = "Definir preferências de notificação para a comunicação relacionada com o projecto FusionPBX e outros alertas do sistema local.";
+	$text['description-notifications']['fr-fr'] = "Definición de las preferencias de notificación de la comunicación relacionada con el proyecto FusionPBX y otras alertas del sistema local.";
+
+	$text['label-project_notifications']['en-us'] = "Project Notifications";
+	$text['label-project_notifications']['es-cl'] = "Proyecto Notificaciones";
+	$text['label-project_notifications']['pt-pt'] = "Projeto Notificações";
+	$text['label-project_notifications']['fr-fr'] = "Notifications Projet";
+
+	$text['description-project_notifications']['en-us'] = "Enable FusionPBX project notifications.";
+	$text['description-project_notifications']['es-cl'] = "Habilitar las notificaciones de proyectos FusionPBX.";
+	$text['description-project_notifications']['pt-pt'] = "Ativar notificações de projetos FusionPBX.";
+	$text['description-project_notifications']['fr-fr'] = "Ativar notificações de projetos FusionPBX.";
+
+	$text['label-project_security']['en-us'] = "Security";
+	$text['label-project_security']['es-cl'] = "Segurança";
+	$text['label-project_security']['pt-pt'] = "Segurança";
+	$text['label-project_security']['fr-fr'] = "Sécurité";
+
+	$text['description-project_security']['en-us'] = "Receive notifications of critical security updates.";
+	$text['description-project_security']['es-cl'] = "Recibir notificaciones de actualizaciones de seguridad críticas.";
+	$text['description-project_security']['pt-pt'] = "Receber notificações de atualizações críticas de segurança.";
+	$text['description-project_security']['fr-fr'] = "Receber notificações de atualizações críticas de segurança.";
+
+	$text['label-project_releases']['en-us'] = "Releases";
+	$text['label-project_releases']['es-cl'] = "Estrenos";
+	$text['label-project_releases']['pt-pt'] = "Lançamentos";
+	$text['label-project_releases']['fr-fr'] = "Communiqués";
+
+	$text['description-project_releases']['en-us'] = "Receive notifications when new stable version of FusionPBX are released and available for download.";
+	$text['description-project_releases']['es-cl'] = "Reciba notificaciones cuando una nueva versión estable de FusionPBX se liberan y disponible para su descarga.";
+	$text['description-project_releases']['pt-pt'] = "Receba notificações quando nova versão estável do FusionPBX são liberados e disponíveis para download.";
+	$text['description-project_releases']['fr-fr'] = "Recevez des alertes lorsque la nouvelle version stable de FusionPBX sont libérés et disponibles pour le téléchargement.";
+
+	$text['label-project_events']['en-us'] = "Events";
+	$text['label-project_events']['es-cl'] = "Eventos";
+	$text['label-project_events']['pt-pt'] = "Eventos";
+	$text['label-project_events']['fr-fr'] = "événements";
+
+	$text['description-project_events']['en-us'] = "Receive notifications of upcoming FusionPBX training classes, convention booths, and other events related to the project.";
+	$text['description-project_events']['es-cl'] = "Recibir notificaciones de próximas clases FusionPBX formación, cabinas de convenciones y otros eventos relacionados con el proyecto.";
+	$text['description-project_events']['pt-pt'] = "Receber notificações de futuras aulas FusionPBX formação, cabines de convenções e outros eventos relacionados ao projeto.";
+	$text['description-project_events']['fr-fr'] = "Recevoir des notifications de venir les classes FusionPBX de formation, des stands de congrès et d'autres événements liés au projet.";
+
+	$text['label-project_news']['en-us'] = "News";
+	$text['label-project_news']['es-cl'] = "Noticias";
+	$text['label-project_news']['pt-pt'] = "Notícia";
+	$text['label-project_news']['fr-fr'] = "Nouvelles";
+
+	$text['description-project_news']['en-us'] = "Receive notifications of general FusionPBX related news and information.";
+	$text['description-project_news']['es-cl'] = "Recibir notificaciones de noticias FusionPBX relacionadas e información general.";
+	$text['description-project_news']['pt-pt'] = "Receber notificações de notícias e informações gerais FusionPBX relacionados.";
+	$text['description-project_news']['fr-fr'] = "Recevoir des notifications de nouvelles et d'information générale FusionPBX liés.";
+
+	$text['option-enabled']['en-us'] = "Enabled";
+	$text['option-enabled']['es-cl'] = "Activado";
+	$text['option-enabled']['pt-pt'] = "Ativado";
+	$text['option-enabled']['fr-fr'] = "Activé";
+
+	$text['option-disabled']['en-us'] = "Disabled";
+	$text['option-disabled']['es-cl'] = "Discapacitado";
+	$text['option-disabled']['pt-pt'] = "Inválido";
+	$text['option-disabled']['fr-fr'] = "Handicapés";
+
+	$text['label-project_notification_method']['en-us'] = "Method";
+	$text['label-project_notification_method']['es-cl'] = "Método";
+	$text['label-project_notification_method']['pt-pt'] = "Método";
+	$text['label-project_notification_method']['fr-fr'] = "Méthode";
+
+//	$text['option-ticker']['en-us'] = "Ticker";
+//	$text['option-ticker']['es-cl'] = "";
+//	$text['option-ticker']['pt-pt'] = "";
+//	$text['option-ticker']['fr-fr'] = "";
+
+	$text['option-email']['en-us'] = "Email";
+	$text['option-email']['es-cl'] = "Email";
+	$text['option-email']['pt-pt'] = "Email";
+	$text['option-email']['fr-fr'] = "Email";
+
+// 	$text['option-text']['en-us'] = "Text/SMS";
+// 	$text['option-text']['es-cl'] = "Texto/SMS";
+// 	$text['option-text']['pt-pt'] = "Texto/SMS";
+// 	$text['option-text']['fr-fr'] = "Texte/SMS";
+
+	$text['description-project_notification_method']['en-us'] = "Select the desired method of communication.";
+	$text['description-project_notification_method']['es-cl'] = "Seleccione el método deseado de la comunicación.";
+	$text['description-project_notification_method']['pt-pt'] = "Seleccione o método desejado de comunicação.";
+	$text['description-project_notification_method']['fr-fr'] = "Sélectionnez la méthode souhaitée de communication.";
+
+	$text['label-project_notification_recipient']['en-us'] = "Recipient";
+	$text['label-project_notification_recipient']['es-cl'] = "Beneficiario";
+	$text['label-project_notification_recipient']['pt-pt'] = "Beneficiário";
+	$text['label-project_notification_recipient']['fr-fr'] = "Destinataire";
+
+	$text['description-project_notification_recipient']['en-us'] = "Enter the desired Email Address or Phone Number recipient.";
+	$text['description-project_notification_recipient']['es-cl'] = "Introduzca la deseada correo electrónico o número de teléfono del destinatario.";
+	$text['description-project_notification_recipient']['pt-pt'] = "Digite o endereço de email desejado ou número de telefone do destinatário.";
+	$text['description-project_notification_recipient']['fr-fr'] = "Entrez l'adresse e-mail souhaitée ou bénéficiaire d'un numéro de téléphone.";
+
+	$text['message-update']['en-us'] = "Update Completed";
+	$text['message-update']['es-cl'] = "ActualizaciÃ³n Completada";
+	$text['message-update']['pt-pt'] = "ActualizaÃ§Ã£o Efectuada";
+	$text['message-update']['fr-fr'] = "Mis Ã Jour";
+
+	$text['message-failed']['en-us'] = "Update Failed - Contact Support";
+	$text['message-failed']['es-cl'] = "Update Error - Contact Support";
+	$text['message-failed']['pt-pt'] = "Falha na atualização - Contato com o suporte";
+	$text['message-failed']['fr-fr'] = "Mise à jour a échoué - Contacter le support technique";
+
+	$text['message-invalid_recipient']['en-us'] = "Invalid Notification Recipient";
+	$text['message-invalid_recipient']['es-cl'] = "Inválido destinatario de la notificación";
+	$text['message-invalid_recipient']['pt-pt'] = "Inválido Notificação Destinatário";
+	$text['message-invalid_recipient']['fr-fr'] = "Notification invalide bénéficiaire";
+
+	$text['button-save']['en-us'] = "Save";
+	$text['button-save']['es-cl'] = "Guardar";
+	$text['button-save']['pt-pt'] = "Guardar";
+	$text['button-save']['fr-fr'] = "Sauvegarder";
+
+	$text['message-disclaimer']['en-us'] = "<u>Disclosure</u>: Enabling FusionPBX Project Notifications implies consent to the collection of <i>anonymous</i> and <i>general</i> demographic information about your installation environment.  Information requested consists of the following: 1) FusionPBX version, 2) PHP version, 3) web server and version, 4) voice switch version, 5) database type and version, 6) operating system platform and version, and 7) public IP address (only for approximating your geographical location). Any information collected will not be used to contact you individually, and will not be selectively shared with any 3rd party. If you do not wish for this information to be submitted, simply leave Project Notifications disabled, and no information will be submitted about your system to the developers of FusionPBX.";
+	$text['message-disclaimer']['es-cl'] = "";
+	$text['message-disclaimer']['pt-pt'] = "";
+	$text['message-disclaimer']['fr-fr'] = "";
+?>
