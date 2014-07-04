44d661e92e7f510fc2a89a7f4ded3483b1ac363c
Nate Jones <git@reliberate.com>
2014-07-04 22:45:43 +0000

    New Project Notifications feature, collect installation demographics.

---
+++ app/notifications/app_defaults.php
@@ -0,0 +1,54 @@
+<?php
+/*
+	FusionPBX
+	Version: MPL 1.1
+
+	The contents of this file are subject to the Mozilla Public License Version
+	1.1 (the "License"); you may not use this file except in compliance with
+	the License. You may obtain a copy of the License at
+	http://www.mozilla.org/MPL/
+
+	Software distributed under the License is distributed on an "AS IS" basis,
+	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	for the specific language governing rights and limitations under the
+	License.
+
+	The Original Code is FusionPBX
+
+	The Initial Developer of the Original Code is
+	Mark J Crane <markjcrane@fusionpbx.com>
+	Portions created by the Initial Developer are Copyright (C) 2008-2012
+	the Initial Developer. All Rights Reserved.
+
+	Contributor(s):
+	Mark J Crane <markjcrane@fusionpbx.com>
+*/
+
+if (strlen($_SESSION['switch']['scripts']['dir']) > 0) {
+	if ($domains_processed == 1) {
+
+		//update the notifications table
+			$sql = "select count(*) as num_rows from v_notifications ";
+			$prep_statement = $db->prepare($sql);
+			if ($prep_statement) {
+				$prep_statement->execute();
+				$row = $prep_statement->fetch(PDO::FETCH_ASSOC);
+				if ($row['num_rows'] == 0) {
+					$sql = "insert into v_notifications ";
+					$sql .= "(";
+					$sql .= "notification_uuid, ";
+					$sql .= "project_notifications ";
+					$sql .= ")";
+					$sql .= "values ";
+					$sql .= "(";
+					$sql .= "'".uuid()."', ";
+					$sql .= "'false' ";
+					$sql .= ")";
+					$db->exec(check_sql($sql));
+					unset($sql);
+				}
+			}
+	}
+}
+
+?>
