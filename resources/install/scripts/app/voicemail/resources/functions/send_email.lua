7713c9a6e96ff0eb939b099aa41c1fc5ed5689a7
Nate Jones <git@reliberate.com>
2014-07-08 02:30:57 +0000

    Voicemail-to-Email: Add custom headers containing domain_uuid, domain_name, call_uuid and email_type.  Send email using Domain smtp settings, if defined under Default Settings.

--- resources/install/scripts/app/voicemail/resources/functions/send_email.lua
+++ resources/install/scripts/app/voicemail/resources/functions/send_email.lua
@@ -84,6 +84,12 @@
 						file_body = scripts_dir.."/app/voicemail/resources/templates/en/us/email_body.tpl";
 					end
 
+				--prepare the headers
+					headers = '{"X-FusionPBX-Domain-UUID":"'..domain_uuid..'",';
+					headers = headers..'"X-FusionPBX-Domain-Name":"'..domain_name..'",';
+					headers = headers..'"X-FusionPBX-Call-UUID":"'..uuid..'",';
+					headers = headers..'"X-FusionPBX-Email-Type":"voicemail"}';
+
 				--prepare the subject
 					local f = io.open(file_subject, "r");
 					local subject = f:read("*all");
@@ -124,9 +130,9 @@
 							delete = "false";
 						end
 						file = voicemail_dir.."/"..id.."/msg_"..uuid.."."..vm_message_ext;
-						cmd = "luarun email.lua "..voicemail_mail_to.." "..voicemail_mail_to.." '"..subject.."' '"..body.."' '"..file.."' "..delete;
+						cmd = "luarun email.lua "..voicemail_mail_to.." "..voicemail_mail_to.." "..headers.." '"..subject.."' '"..body.."' '"..file.."' "..delete;
 					else
-						cmd = "luarun email.lua "..voicemail_mail_to.." "..voicemail_mail_to.." '"..subject.."' '"..body.."'";
+						cmd = "luarun email.lua "..voicemail_mail_to.." "..voicemail_mail_to.." "..headers.." '"..subject.."' '"..body.."'";
 					end
 					if (debug["info"]) then
 						freeswitch.consoleLog("notice", "[voicemail] cmd: " .. cmd .. "\n");
