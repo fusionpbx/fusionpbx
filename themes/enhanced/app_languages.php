07e263bce2b51d7f1c47d7b6d7fc14f87527a700
Nate Jones <git@reliberate.com>
2014-07-09 22:02:34 +0000

    Enhanced Theme: Modified header to show domain of current user, added labels.

--- themes/enhanced/app_languages.php
+++ themes/enhanced/app_languages.php
@@ -1,8 +1,24 @@
 <?php
 	// Theme
+
+	$text['theme-label-user']['en-us'] = "User:";
+	$text['theme-label-user']['es-cl'] = "Usuario:";
+	$text['theme-label-user']['pt-pt'] = "Usuário:";
+	$text['theme-label-user']['fr-fr'] = "Utilisateur:";
+
+	$text['theme-label-domain']['en-us'] = "Domain:";
+	$text['theme-label-domain']['es-cl'] = "Dominio:";
+	$text['theme-label-domain']['pt-pt'] = "Domínio:";
+	$text['theme-label-domain']['fr-fr'] = "Domaine:";
+
+	$text['theme-label-open_selector']['en-us'] = "Open Domain Selector";
+	$text['theme-label-open_selector']['es-cl'] = "Selector de Dominio Abierto";
+	$text['theme-label-open_selector']['pt-pt'] = "Seletor de Domínio Aberto";
+	$text['theme-label-open_selector']['fr-fr'] = "Sélecteur Domaine Ouvert";
+
 	$text['theme-title-domains']['en-us'] = "Domains";
 	$text['theme-title-domains']['es-cl'] = "Dominios";
-	$text['theme-title-domains']['pt-pt'] = "DomÃ­nios";
+	$text['theme-title-domains']['pt-pt'] = "Domínios";
 	$text['theme-title-domains']['fr-fr'] = "Domaines";
 
 	$text['theme-button-close']['en-us'] = "Close";
@@ -14,4 +30,10 @@
 	$text['theme-label-search']['es-cl'] = "Buscar...";
 	$text['theme-label-search']['pt-pt'] = "Pesquisa...";
 	$text['theme-label-search']['fr-fr'] = "Recherche...";
+
+	$text['theme-label-search']['en-us'] = "Search...";
+	$text['theme-label-search']['es-cl'] = "Buscar...";
+	$text['theme-label-search']['pt-pt'] = "Pesquisa...";
+	$text['theme-label-search']['fr-fr'] = "Recherche...";
+
 ?>
