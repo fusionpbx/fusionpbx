d79b45663e0d141c4f3ab2f816c17e644dffbb54
Mark Crane <markjcrane@gmail.com>
2014-07-12 05:13:15 +0000

    Change limit_excecute to limit

--- app/dialplan_inbound/dialplan_inbound_add.php
+++ app/dialplan_inbound/dialplan_inbound_add.php
@@ -302,7 +302,7 @@ if (count($_POST)>0 && strlen($_POST["persistformvar"]) == 0) {
 			$sql .= "'$dialplan_uuid', ";
 			$sql .= "'$dialplan_detail_uuid', ";
 			$sql .= "'action', ";
-			$sql .= "'limit_execute', ";
+			$sql .= "'limit', ";
 			$sql .= "'db \${domain} inbound ".$limit." !USER_BUSY', ";
 			$sql .= "'70' ";
 			$sql .= ")";
