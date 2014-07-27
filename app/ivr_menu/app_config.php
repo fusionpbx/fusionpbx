dca9d859beb9740b074ce1c8d7d101bddf9b0004
Mark Crane <markjcrane@gmail.com>
2014-07-27 00:54:12 +0000

    FreeSWITCH allows a named extension so allowed ivr menu extension to be text

--- app/ivr_menu/app_config.php
+++ app/ivr_menu/app_config.php
@@ -120,7 +120,7 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
 		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = "ivr_menu_extension";
-		$apps[$x]['db'][$y]['fields'][$z]['type'] = "numeric";
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
 		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = "ivr_menu_greet_long";
