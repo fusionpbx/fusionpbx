bd0f8a8471980019f367abde9fcc9857e1e1050d
Mark Crane <markjcrane@gmail.com>
2014-07-29 05:58:12 +0000

    Add provision enabled and other variables to default settings when provision is installed

--- app/provision/app_defaults.php
+++ app/provision/app_defaults.php
@@ -48,51 +48,122 @@
 		unset($prep_statement, $result);
 	}
 
-//move the dynamic provision variables that from v_vars table to v_default_settings
-	if (count($_SESSION['provision']) == 0 && $domains_processed == 1) {
-		$sql = "select * from v_vars ";
-		$sql .= "where var_cat = 'Provision' ";
-		$prep_statement = $db->prepare(check_sql($sql));
-		$prep_statement->execute();
-		$result = $prep_statement->fetchAll(PDO::FETCH_NAMED);
-		foreach ($result as &$row) {
-			//set the variable
-				$var_name = check_str($row['var_name']);
-			//remove the 'v_' prefix from the variable name
-				if (substr($var_name, 0, 2) == "v_") {
-					$var_name = substr($var_name, 2);
+//process this code online once
+	if ($domains_processed == 1) {
+		//add provision category to default settings
+			$sql = "select count(*) as num_rows from v_default_settings ";
+			$sql .= "where default_setting_category = 'provision' ";
+			$sql .= "and default_setting_subcategory = 'enabled' ";
+			$sql .= "and default_setting_name = 'text' ";
+			$prep_statement = $db->prepare($sql);
+			if ($prep_statement) {
+				$prep_statement->execute();
+				$row = $prep_statement->fetch(PDO::FETCH_ASSOC);
+				unset($prep_statement);
+				if ($row['num_rows'] == 0) {
+					$x = 0;
+					$array[$x]['default_setting_category'] = 'provision';
+					$array[$x]['default_setting_subcategory'] = 'enabled';
+					$array[$x]['default_setting_name'] = 'text';
+					$array[$x]['default_setting_value'] = 'true';
+					$array[$x]['default_setting_enabled'] = 'true';
+					$array[$x]['default_setting_description'] = '';
+					$x++;
+					$array[$x]['default_setting_category'] = 'provision';
+					$array[$x]['default_setting_subcategory'] = 'http_auth_username';
+					$array[$x]['default_setting_name'] = 'text';
+					$array[$x]['default_setting_value'] = '';
+					$array[$x]['default_setting_enabled'] = 'false';
+					$array[$x]['default_setting_description'] = '';
+					$x++;
+					$array[$x]['default_setting_category'] = 'provision';
+					$array[$x]['default_setting_subcategory'] = 'http_auth_password';
+					$array[$x]['default_setting_name'] = 'text';
+					$array[$x]['default_setting_value'] = '';
+					$array[$x]['default_setting_enabled'] = 'false';
+					$array[$x]['default_setting_description'] = '';
+					$x++;
+					$array[$x]['default_setting_category'] = 'provision';
+					$array[$x]['default_setting_subcategory'] = 'cidr';
+					$array[$x]['default_setting_name'] = 'array';
+					$array[$x]['default_setting_value'] = '';
+					$array[$x]['default_setting_enabled'] = 'false';
+					$array[$x]['default_setting_description'] = '';
+					$x++;
+					$array[$x]['default_setting_category'] = 'provision';
+					$array[$x]['default_setting_subcategory'] = 'admin_name';
+					$array[$x]['default_setting_name'] = 'text';
+					$array[$x]['default_setting_value'] = '';
+					$array[$x]['default_setting_enabled'] = 'false';
+					$array[$x]['default_setting_description'] = '';
+					$x++;
+					$array[$x]['default_setting_category'] = 'provision';
+					$array[$x]['default_setting_subcategory'] = 'admin_password';
+					$array[$x]['default_setting_name'] = 'text';
+					$array[$x]['default_setting_value'] = '';
+					$array[$x]['default_setting_enabled'] = 'false';
+					$array[$x]['default_setting_description'] = '';
+					$x++;
+					$orm = new orm;
+					$orm->name('default_settings');
+					$orm->save($array[0]);
+					$orm->save($array[1]);
+					$orm->save($array[2]);
+					$orm->save($array[3]);
+					$orm->save($array[4]);
+					$orm->save($array[5]);
+					$message = $orm->message;
+					//print_r($message);
 				}
-			//add the provision variable to the default settings table
-				$sql = "insert into v_default_settings ";
-				$sql .= "(";
-				$sql .= "default_setting_uuid, ";
-				$sql .= "default_setting_category, ";
-				$sql .= "default_setting_subcategory, ";
-				$sql .= "default_setting_name, ";
-				$sql .= "default_setting_value, ";
-				$sql .= "default_setting_enabled, ";
-				$sql .= "default_setting_description ";
-				$sql .= ") ";
-				$sql .= "values ";
-				$sql .= "(";
-				$sql .= "'".uuid()."', ";
-				$sql .= "'provision', ";
-				$sql .= "'".$var_name."', ";
-				$sql .= "'var', ";
-				$sql .= "'".check_str($row['var_value'])."', ";
-				$sql .= "'".check_str($row['var_enabled'])."', ";
-				$sql .= "'".check_str($row['var_description'])."' ";
-				$sql .= ")";
-				$db->exec(check_sql($sql));
-				unset($sql);
-		}
-		unset($prep_statement);
-		//delete the provision variables from system -> variables
-		//$sql = "delete from v_vars ";
-		//$sql .= "where var_cat = 'Provision' ";
-		//echo $sql ."\n";
-		//$db->exec(check_sql($sql));
-		//echo "$var_name $var_value \n";
+				unset($row);
+			}
+
+		//move the dynamic provision variables that from v_vars table to v_default_settings
+			if (count($_SESSION['provision']) == 0) {
+				$sql = "select * from v_vars ";
+				$sql .= "where var_cat = 'Provision' ";
+				$prep_statement = $db->prepare(check_sql($sql));
+				$prep_statement->execute();
+				$result = $prep_statement->fetchAll(PDO::FETCH_NAMED);
+				foreach ($result as &$row) {
+					//set the variable
+						$var_name = check_str($row['var_name']);
+					//remove the 'v_' prefix from the variable name
+						if (substr($var_name, 0, 2) == "v_") {
+							$var_name = substr($var_name, 2);
+						}
+					//add the provision variable to the default settings table
+						$sql = "insert into v_default_settings ";
+						$sql .= "(";
+						$sql .= "default_setting_uuid, ";
+						$sql .= "default_setting_category, ";
+						$sql .= "default_setting_subcategory, ";
+						$sql .= "default_setting_name, ";
+						$sql .= "default_setting_value, ";
+						$sql .= "default_setting_enabled, ";
+						$sql .= "default_setting_description ";
+						$sql .= ") ";
+						$sql .= "values ";
+						$sql .= "(";
+						$sql .= "'".uuid()."', ";
+						$sql .= "'provision', ";
+						$sql .= "'".$var_name."', ";
+						$sql .= "'var', ";
+						$sql .= "'".check_str($row['var_value'])."', ";
+						$sql .= "'".check_str($row['var_enabled'])."', ";
+						$sql .= "'".check_str($row['var_description'])."' ";
+						$sql .= ")";
+						$db->exec(check_sql($sql));
+						unset($sql);
+				}
+				unset($prep_statement);
+				//delete the provision variables from system -> variables
+				//$sql = "delete from v_vars ";
+				//$sql .= "where var_cat = 'Provision' ";
+				//echo $sql ."\n";
+				//$db->exec(check_sql($sql));
+				//echo "$var_name $var_value \n";
+			}
 	}
 
 ?>
