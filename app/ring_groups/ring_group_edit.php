9129890be374c59165e28183c537b3e3824a829e
Mark Crane <markjcrane@gmail.com>
2014-08-13 09:13:22 +0000

    set the style class

--- app/ring_groups/ring_group_edit.php
+++ app/ring_groups/ring_group_edit.php
@@ -683,7 +683,7 @@ else {
 	echo "	</tr>";
 
 	echo "<tr>\n";
-	echo "<td class='vncellreq' valign='top' align='left' nowrap='nowrap'>\n";
+	echo "<td class='vncell' valign='top' align='left' nowrap='nowrap'>\n";
 	echo "	".$text['label-skip_active'].":\n";
 	echo "</td>\n";
 	echo "<td class='vtable' align='left'>\n";
