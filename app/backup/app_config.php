37eb5b203ac999c3d2fc8fcf1d570556df9b4592
Mark Crane <markjcrane@gmail.com>
2014-08-15 23:33:20 +0000

    Add a backup feature

---
+++ app/backup/app_config.php
@@ -0,0 +1,50 @@
+<?php
+	//application details
+		$apps[$x]['name'] = "Backup";
+		$apps[$x]['uuid'] = "1df70270-8274-44e9-a43e-5074684649ad";
+		$apps[$x]['category'] = "App";
+		$apps[$x]['subcategory'] = "";
+		$apps[$x]['version'] = "";
+		$apps[$x]['license'] = "Mozilla Public License 1.1";
+		$apps[$x]['url'] = "http://www.fusionpbx.com";
+		$apps[$x]['description']['en-us'] = "";
+		$apps[$x]['description']['es-cl'] = "";
+		$apps[$x]['description']['es-mx'] = "";
+		$apps[$x]['description']['de-de'] = "";
+		$apps[$x]['description']['de-ch'] = "";
+		$apps[$x]['description']['de-at'] = "";
+		$apps[$x]['description']['fr-fr'] = "";
+		$apps[$x]['description']['fr-ca'] = "";
+		$apps[$x]['description']['fr-ch'] = "";
+		$apps[$x]['description']['pt-pt'] = "";
+		$apps[$x]['description']['pt-br'] = "";
+
+	//menu details
+		$apps[$x]['menu'][0]['title']['en-us'] = "Backup";
+		$apps[$x]['menu'][0]['title']['es-cl'] = "";
+		$apps[$x]['menu'][0]['title']['es-mx'] = "";
+		$apps[$x]['menu'][0]['title']['de-de'] = "";
+		$apps[$x]['menu'][0]['title']['de-ch'] = "";
+		$apps[$x]['menu'][0]['title']['de-at'] = "";
+		$apps[$x]['menu'][0]['title']['fr-fr'] = "";
+		$apps[$x]['menu'][0]['title']['fr-ca'] = "";
+		$apps[$x]['menu'][0]['title']['fr-ch'] = "";
+		$apps[$x]['menu'][0]['title']['pt-pt'] = "";
+		$apps[$x]['menu'][0]['title']['pt-br'] = "";
+		$apps[$x]['menu'][0]['uuid'] = "7e174c3c-e494-4bb0-a52a-4ea55209ffeb";
+		$apps[$x]['menu'][0]['parent_uuid'] = "fd29e39c-c936-f5fc-8e2b-611681b266b5";
+		$apps[$x]['menu'][0]['category'] = "internal";
+		$apps[$x]['menu'][0]['path'] = "/app/backup/index.php";
+		$apps[$x]['menu'][0]['groups'][] = "superadmin";
+
+	//permission details
+		$y = 0;
+		$apps[$x]['permissions'][$y]['name'] = "backup_download";
+		$apps[$x]['permissions'][$y]['menu']['uuid'] = "7e174c3c-e494-4bb0-a52a-4ea55209ffeb";
+		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
+		$y++;
+		$apps[$x]['permissions'][$y]['name'] = "backup_upload";
+		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
+		$y++;
+
+?>
---
+++ app/backup/app_languages.php
@@ -0,0 +1,73 @@
+<?php
+
+	$text['title-zzz']['en-us'] = "zzz";
+	$text['title-zzz']['es-cl'] = "zzz";
+	$text['title-zzz']['pt-pt'] = "zzz";
+	$text['title-zzz']['fr-fr'] = "zzz";
+
+	$text['description-zzz']['en-us'] = "zzz";
+	$text['description-zzz']['es-cl'] = "zzz";
+	$text['description-zzz']['pt-pt'] = "zzz";
+	$text['description-zzz']['fr-fr'] = "zzz";
+
+	$text['button-add']['en-us'] = "Add";
+	$text['button-add']['es-cl'] = "Agregar";
+	$text['button-add']['pt-pt'] = "Adicionar";
+	$text['button-add']['fr-fr'] = "Ajouter";
+
+	$text['button-edit']['en-us'] = "Edit";
+	$text['button-edit']['es-cl'] = "Editar";
+	$text['button-edit']['pt-pt'] = "Editar";
+	$text['button-edit']['fr-fr'] = "Editer";
+
+	$text['button-delete']['en-us'] = "Delete";
+	$text['button-delete']['es-cl'] = "Eliminar";
+	$text['button-delete']['pt-pt'] = "Remoção";
+	$text['button-delete']['fr-fr'] = "Supprimer";
+
+	$text['button-save']['en-us'] = "Save";
+	$text['button-save']['es-cl'] = "Guardar";
+	$text['button-save']['pt-pt'] = "Guardar";
+	$text['button-save']['fr-fr'] = "Sauvegarder";
+
+	$text['button-back']['en-us'] = "Back";
+	$text['button-back']['es-cl'] = "Volver";
+	$text['button-back']['pt-pt'] = "Voltar";
+	$text['button-back']['fr-fr'] = "Retour";
+
+	$text['button-search']['en-us'] = "Search";
+	$text['button-search']['es-cl'] = "Buscar";
+	$text['button-search']['pt-pt'] = "Pesquisar";
+	$text['button-search']['fr-fr'] = "Chercher";
+
+	$text['confirm-delete']['en-us'] = "Do you really want to delete this?";
+	$text['confirm-delete']['es-cl'] = "¿Realmente desea eliminar esto?";
+	$text['confirm-delete']['pt-pt'] = "Deseja realmente remover isto?";
+	$text['confirm-delete']['fr-fr'] = "Voulez-vous vraiment supprimer cela?";
+
+	$text['message-add']['en-us'] = "Add Completed";
+	$text['message-add']['es-cl'] = "Agregar Completo";
+	$text['message-add']['pt-pt'] = "Adição Realizada";
+	$text['message-add']['fr-fr'] = "Ajouté";
+
+	$text['message-update']['en-us'] = "Update Completed";
+	$text['message-update']['es-cl'] = "Actualización Completa";
+	$text['message-update']['pt-pt'] = "Actualização Realizada";
+	$text['message-update']['fr-fr'] = "Mis à jour";
+
+	$text['message-delete']['en-us'] = "Delete Completed";
+	$text['message-delete']['es-cl'] = "Eliminación Completa";
+	$text['message-delete']['pt-pt'] = "Remoção Realizada";
+	$text['message-delete']['fr-fr'] = "Supprimé";
+
+	$text['message-required']['en-us'] = "Please provide: ";
+	$text['message-required']['es-cl'] = "Por favor ingrese: ";
+	$text['message-required']['pt-pt'] = "Por favor forneça: ";
+	$text['message-required']['fr-fr'] = "Merci d'indiquer: ";
+
+	$text['message-duplicate']['en-us'] = "Duplicate detected";
+	$text['message-duplicate']['es-cl'] = "Duplicar detectado";
+	$text['message-duplicate']['pt-pt'] = "Duplicar detectado";
+	$text['message-duplicate']['fr-fr'] = "Dupliquer détecté";
+
+?>
