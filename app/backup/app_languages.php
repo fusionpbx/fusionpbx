3b9daf63fbd99e311c2e18dd213cc05c759701e6
Nate Jones <git@reliberate.com>
2014-08-16 22:15:01 +0000

    Backup: Completed functionality to back up and restore paths defined in Default Settings.

--- app/backup/app_languages.php
+++ app/backup/app_languages.php
@@ -1,73 +1,77 @@
 <?php
 
-	$text['title-zzz']['en-us'] = "zzz";
-	$text['title-zzz']['es-cl'] = "zzz";
-	$text['title-zzz']['pt-pt'] = "zzz";
-	$text['title-zzz']['fr-fr'] = "zzz";
+	$text['title-backup']['en-us'] = "Backup";
+	$text['title-backup']['es-cl'] = "Copia de seguridad";
+	$text['title-backup']['pt-pt'] = "Backup";
+	$text['title-backup']['fr-fr'] = "Sauvegarde";
 
-	$text['description-zzz']['en-us'] = "zzz";
-	$text['description-zzz']['es-cl'] = "zzz";
-	$text['description-zzz']['pt-pt'] = "zzz";
-	$text['description-zzz']['fr-fr'] = "zzz";
+	$text['header-backup']['en-us'] = "Backup";
+	$text['header-backup']['es-cl'] = "Copia de seguridad";
+	$text['header-backup']['pt-pt'] = "Backup";
+	$text['header-backup']['fr-fr'] = "Sauvegarde";
 
-	$text['button-add']['en-us'] = "Add";
-	$text['button-add']['es-cl'] = "Agregar";
-	$text['button-add']['pt-pt'] = "Adicionar";
-	$text['button-add']['fr-fr'] = "Ajouter";
+	$text['description-backup']['en-us'] = "To create a backup of paths defined in Default Settings, click the 'Backup' button below and then choose a location on your local system to save the file.";
+	$text['description-backup']['es-cl'] = "Para crear una copia de seguridad de rutas definidas en la configuración predeterminada, haga clic en el botón 'Copia de seguridad' a continuación y luego elija una ubicación en el sistema local para guardar el archivo.";
+	$text['description-backup']['pt-pt'] = "Para criar uma cópia de segurança dos caminhos definidos em configurações padrão, clique no botão 'Backup' abaixo e, em seguida, escolher um local em seu sistema local para salvar o arquivo.";
+	$text['description-backup']['fr-fr'] = "Pour créer une sauvegarde de chemins définis dans les paramètres par défaut, cliquez sur le bouton 'Sauvegarder' ci-dessous, puis choisissez un emplacement sur votre système local pour enregistrer le fichier.";
 
-	$text['button-edit']['en-us'] = "Edit";
-	$text['button-edit']['es-cl'] = "Editar";
-	$text['button-edit']['pt-pt'] = "Editar";
-	$text['button-edit']['fr-fr'] = "Editer";
+	$text['label-file_format']['en-us'] = "File Format";
+	$text['label-file_format']['es-cl'] = "Formato de Archivo";
+	$text['label-file_format']['pt-pt'] = "Formato de arquivo";
+	$text['label-file_format']['fr-fr'] = "Format de Fichier";
 
-	$text['button-delete']['en-us'] = "Delete";
-	$text['button-delete']['es-cl'] = "Eliminar";
-	$text['button-delete']['pt-pt'] = "Remoção";
-	$text['button-delete']['fr-fr'] = "Supprimer";
+	$text['header-restore']['en-us'] = "Restore";
+	$text['header-restore']['es-cl'] = "Restaurar";
+	$text['header-restore']['pt-pt'] = "Restaurar";
+	$text['header-restore']['fr-fr'] = "Restaurer";
 
-	$text['button-save']['en-us'] = "Save";
-	$text['button-save']['es-cl'] = "Guardar";
-	$text['button-save']['pt-pt'] = "Guardar";
-	$text['button-save']['fr-fr'] = "Sauvegarder";
+	$text['description-restore']['en-us'] = "To restore a backup from a file on your local system, click 'Browse' then select the desired backup file (eg. 'backup_[YYMMDD]_[HHMMSS].tgz'), then click the 'Restore' button.";
+	$text['description-restore']['es-cl'] = "Para restaurar una copia de seguridad de un archivo en el sistema local, haga clic en 'Examinar' y seleccione el archivo de copia de seguridad que desee (por ejemplo, 'backup_[YYMMDD]_[HHMMSS].tgz'), a continuación, haga clic en el botón 'Restaurar'.";
+	$text['description-restore']['pt-pt'] = "Para restaurar um backup de um arquivo em seu sistema local, clique em 'Procurar' e selecione o arquivo de backup desejado (por exemplo, 'backup_[YYMMDD]_[HHMMSS].tgz'), então clique no botão 'Restaurar'.";
+	$text['description-restore']['fr-fr'] = "Pour restaurer une sauvegarde à partir d'un fichier sur votre système local, cliquez sur 'Parcourir' puis sélectionnez le fichier de sauvegarde souhaité (par exemple, 'backup_[YYMMDD]_[HHMMSS].tgz'), puis cliquez sur le bouton 'Restaurer'.";
 
-	$text['button-back']['en-us'] = "Back";
-	$text['button-back']['es-cl'] = "Volver";
-	$text['button-back']['pt-pt'] = "Voltar";
-	$text['button-back']['fr-fr'] = "Retour";
+	$text['description-restore_warning']['en-us'] = "Warning: Existing files, if any, will be overwritten.";
+	$text['description-restore_warning']['es-cl'] = "Advertencia: Los archivos existentes, si los hay, se sobrescribirá.";
+	$text['description-restore_warning']['pt-pt'] = "Aviso: Os arquivos existentes, se houver, serão substituídos.";
+	$text['description-restore_warning']['fr-fr'] = "Attention: les fichiers existants, le cas échéant, sera écrasé.";
 
-	$text['button-search']['en-us'] = "Search";
-	$text['button-search']['es-cl'] = "Buscar";
-	$text['button-search']['pt-pt'] = "Pesquisar";
-	$text['button-search']['fr-fr'] = "Chercher";
+	$text['label-select_backup']['en-us'] = "Select Backup File";
+	$text['label-select_backup']['es-cl'] = "Seleccione Copia de Seguridad";
+	$text['label-select_backup']['pt-pt'] = "Selecione Backup";
+	$text['label-select_backup']['fr-fr'] = "Sélectionnez Sauvegarde";
 
-	$text['confirm-delete']['en-us'] = "Do you really want to delete this?";
-	$text['confirm-delete']['es-cl'] = "¿Realmente desea eliminar esto?";
-	$text['confirm-delete']['pt-pt'] = "Deseja realmente remover isto?";
-	$text['confirm-delete']['fr-fr'] = "Voulez-vous vraiment supprimer cela?";
+	$text['button-backup']['en-us'] = "Backup";
+	$text['button-backup']['es-cl'] = "Copia de Seguridad";
+	$text['button-backup']['pt-pt'] = "Backup";
+	$text['button-backup']['fr-fr'] = "Sauvegarde";
 
-	$text['message-add']['en-us'] = "Add Completed";
-	$text['message-add']['es-cl'] = "Agregar Completo";
-	$text['message-add']['pt-pt'] = "Adição Realizada";
-	$text['message-add']['fr-fr'] = "Ajouté";
+	$text['button-restore']['en-us'] = "Restore";
+	$text['button-restore']['es-cl'] = "Restaurar";
+	$text['button-restore']['pt-pt'] = "Restaurar";
+	$text['button-restore']['fr-fr'] = "Restaurer";
 
-	$text['message-update']['en-us'] = "Update Completed";
-	$text['message-update']['es-cl'] = "Actualización Completa";
-	$text['message-update']['pt-pt'] = "Actualização Realizada";
-	$text['message-update']['fr-fr'] = "Mis à jour";
+	$text['message-backup_failed_format']['en-us'] = "Backup Failed - Unable to Create Backup File";
+	$text['message-backup_failed_format']['es-cl'] = "Copia de seguridad Error - No se puede crear archivo de copia de seguridad";
+	$text['message-backup_failed_format']['pt-pt'] = "Falha no backup - Não é possível criar arquivo de backup";
+	$text['message-backup_failed_format']['fr-fr'] = "Échec de la sauvegarde - Impossible de créer le fichier de sauvegarde";
 
-	$text['message-delete']['en-us'] = "Delete Completed";
-	$text['message-delete']['es-cl'] = "Eliminación Completa";
-	$text['message-delete']['pt-pt'] = "Remoção Realizada";
-	$text['message-delete']['fr-fr'] = "Supprimé";
+	$text['message-backup_failed_paths']['en-us'] = "Backup Failed - No Backup Paths Defined";
+	$text['message-backup_failed_paths']['es-cl'] = "Fallidos de copia de seguridad - No hay rutas de copia de seguridad definida";
+	$text['message-backup_failed_paths']['pt-pt'] = "Backup falhou - Sem Caminhos backup definido";
+	$text['message-backup_failed_paths']['fr-fr'] = "Échec de sauvegarde - Pas de chemins sauvegarde définie";
 
-	$text['message-required']['en-us'] = "Please provide: ";
-	$text['message-required']['es-cl'] = "Por favor ingrese: ";
-	$text['message-required']['pt-pt'] = "Por favor forneça: ";
-	$text['message-required']['fr-fr'] = "Merci d'indiquer: ";
+	$text['message-restore_completed']['en-us'] = "Restore Completed";
+	$text['message-restore_completed']['es-cl'] = "Restaurar Completado";
+	$text['message-restore_completed']['pt-pt'] = "Concluída a Restauração";
+	$text['message-restore_completed']['fr-fr'] = "Restaurer Terminé";
 
-	$text['message-duplicate']['en-us'] = "Duplicate detected";
-	$text['message-duplicate']['es-cl'] = "Duplicar detectado";
-	$text['message-duplicate']['pt-pt'] = "Duplicar detectado";
-	$text['message-duplicate']['fr-fr'] = "Dupliquer détecté";
+	$text['message-restore_failed_format']['en-us'] = "Restore Failed - Invalid File Format";
+	$text['message-restore_failed_format']['es-cl'] = "Restaurar Error - Formato de archivo no válido";
+	$text['message-restore_failed_format']['pt-pt'] = "Falha na restauração - Formato de arquivo inválido";
+	$text['message-restore_failed_format']['fr-fr'] = "Échec de la restauration - Format de fichier incorrect";
 
+	$text['message-restore_failed_upload']['en-us'] = "Restore Failed - File Upload Failed";
+	$text['message-restore_failed_upload']['es-cl'] = "Falló la recup - Cargar archivo Error";
+	$text['message-restore_failed_upload']['pt-pt'] = "Falha de restauração - Upload de arquivo falhou";
+	$text['message-restore_failed_upload']['fr-fr'] = "Échec de la restauration - File Upload Échec";
 ?>
