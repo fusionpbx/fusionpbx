d68440acea71ce77f1e06ca2fdc151e7d145b2d9
Nate Jones <git@reliberate.com>
2014-08-17 13:41:57 +0000

    Added a variation of the Enhanced Theme: Minimized.

---
+++ themes/minimized/app_languages.php
@@ -0,0 +1,41 @@
+<?php
+	// Theme
+
+	$text['theme-label-logout']['en-us'] = "Logout";
+	$text['theme-label-logout']['es-cl'] = "Salir";
+	$text['theme-label-logout']['pt-pt'] = "Sair";
+	$text['theme-label-logout']['fr-fr'] = "Déconnexion";
+
+	$text['theme-confirm-logout']['en-us'] = "Do you really want to logout?";
+	$text['theme-confirm-logout']['es-cl'] = "¿Realmente desea cerrar la sesi?";
+	$text['theme-confirm-logout']['pt-pt'] = "Você realmente quer sair?";
+	$text['theme-confirm-logout']['fr-fr'] = "Voulez-vous vraiment vous déconnecter?";
+
+	$text['theme-label-open_selector']['en-us'] = "Domain Selector [ESC]";
+	$text['theme-label-open_selector']['es-cl'] = "Selector de Dominio Abierto";
+	$text['theme-label-open_selector']['pt-pt'] = "Seletor de Domínio Aberto";
+	$text['theme-label-open_selector']['fr-fr'] = "Sélecteur Domaine Ouvert";
+
+	$text['theme-title-domains']['en-us'] = "Domains";
+	$text['theme-title-domains']['es-cl'] = "Dominios";
+	$text['theme-title-domains']['pt-pt'] = "Domínios";
+	$text['theme-title-domains']['fr-fr'] = "Domaines";
+
+	$text['theme-button-close']['en-us'] = "Close";
+	$text['theme-button-close']['es-cl'] = "Cerca";
+	$text['theme-button-close']['pt-pt'] = "Perto";
+	$text['theme-button-close']['fr-fr'] = "Proche";
+
+	$text['theme-label-search']['en-us'] = "Search...";
+	$text['theme-label-search']['es-cl'] = "Buscar...";
+	$text['theme-label-search']['pt-pt'] = "Pesquisa...";
+	$text['theme-label-search']['fr-fr'] = "Recherche...";
+
+	$text['theme-label-search']['en-us'] = "Search...";
+	$text['theme-label-search']['es-cl'] = "Buscar...";
+	$text['theme-label-search']['pt-pt'] = "Pesquisa...";
+	$text['theme-label-search']['fr-fr'] = "Recherche...";
+
+
+
+?>
