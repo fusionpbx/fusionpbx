4bc6a781612e3105a62613bb765ed5a24b2a2563
Nate Jones <git@reliberate.com>
2014-08-17 13:41:57 +0000

    Added a variation of the Enhanced Theme: Minimized.

---
+++ themes/minimized/config.php
@@ -0,0 +1,9 @@
+<?php
+
+//define the link labels
+	$v_link_label_edit = "<img src='".PROJECT_PATH."/themes/enhanced/images/icon_edit.png' class='list_control_icon'>";
+	$v_link_label_add = "<img src='".PROJECT_PATH."/themes/enhanced/images/icon_add.png' class='list_control_icon'>";
+	$v_link_label_delete = "<img src='".PROJECT_PATH."/themes/enhanced/images/icon_delete.png' class='list_control_icon'>";
+	$v_link_label_view = "<img src='".PROJECT_PATH."/themes/enhanced/images/icon_view.png' class='list_control_icon'>";
+
+?>
