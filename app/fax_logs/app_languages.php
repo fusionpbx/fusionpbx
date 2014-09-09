73069cac7f429834b7bb037e5148944c26f2aee8
Mark Crane <markjcrane@gmail.com>
2014-09-09 05:43:09 +0000

    Add App -> Fax Logs

---
+++ app/fax_logs/app_languages.php
@@ -0,0 +1,163 @@
+<?php
+
+	//FAX Logs
+		$text['title-fax_logs']['en-us'] = 'FAX Logs';
+		$text['title-fax_logs']['pt-pt'] = '';
+
+		$text['title-fax_log']['en-us'] = 'Fax Log';
+		$text['title-fax_log']['pt-pt'] = '';
+
+		$text['description-fax_log']['en-us'] = 'The details for sending and receiving a fax included are whether fax was successful, ECM (Error Correction Mode), number of retries, destination number, date, etc.   ';
+		$text['description-fax_log']['pt-pt'] = '';
+
+		$text['label-fax_success']['en-us'] = 'Success';
+		$text['label-fax_success']['pt-pt'] = '';
+
+		$text['description-fax_success']['en-us'] = '';
+		$text['description-fax_success']['pt-pt'] = '';
+
+		$text['label-fax_result_code']['en-us'] = 'Code';
+		$text['label-fax_result_code']['pt-pt'] = '';
+
+		$text['description-fax_result_code']['en-us'] = '';
+		$text['description-fax_result_code']['pt-pt'] = '';
+
+		$text['label-fax_result_text']['en-us'] = 'Result';
+		$text['label-fax_result_text']['pt-pt'] = '';
+
+		$text['description-fax_result_text']['en-us'] = '';
+		$text['description-fax_result_text']['pt-pt'] = '';
+
+		$text['label-fax_file']['en-us'] = 'File';
+		$text['label-fax_file']['pt-pt'] = '';
+
+		$text['description-fax_file']['en-us'] = '';
+		$text['description-fax_file']['pt-pt'] = '';
+
+		$text['label-fax_ecm_used']['en-us'] = 'ECM';
+		$text['label-fax_ecm_used']['pt-pt'] = '';
+
+		$text['description-fax_ecm_used']['en-us'] = '';
+		$text['description-fax_ecm_used']['pt-pt'] = '';
+
+		$text['label-fax_local_station_id']['en-us'] = 'Local Station ID';
+		$text['label-fax_local_station_id']['pt-pt'] = '';
+
+		$text['description-fax_local_station_id']['en-us'] = '';
+		$text['description-fax_local_station_id']['pt-pt'] = '';
+
+		$text['label-fax_document_transferred_pages']['en-us'] = 'Document Transferred Pages';
+		$text['label-fax_document_transferred_pages']['pt-pt'] = '';
+
+		$text['description-fax_document_transferred_pages']['en-us'] = '';
+		$text['description-fax_document_transferred_pages']['pt-pt'] = '';
+
+		$text['label-fax_document_total_pages']['en-us'] = 'Document Total Pages';
+		$text['label-fax_document_total_pages']['pt-pt'] = '';
+
+		$text['description-fax_document_total_pages']['en-us'] = '';
+		$text['description-fax_document_total_pages']['pt-pt'] = '';
+
+		$text['label-fax_image_resolution']['en-us'] = 'Image Resolution';
+		$text['label-fax_image_resolution']['pt-pt'] = '';
+
+		$text['description-fax_image_resolution']['en-us'] = '';
+		$text['description-fax_image_resolution']['pt-pt'] = '';
+
+		$text['label-fax_image_size']['en-us'] = 'Image Size';
+		$text['label-fax_image_size']['pt-pt'] = '';
+
+		$text['description-fax_image_size']['en-us'] = '';
+		$text['description-fax_image_size']['pt-pt'] = '';
+
+		$text['label-fax_bad_rows']['en-us'] = 'Bad Rows';
+		$text['label-fax_bad_rows']['pt-pt'] = '';
+
+		$text['description-fax_bad_rows']['en-us'] = '';
+		$text['description-fax_bad_rows']['pt-pt'] = '';
+
+		$text['label-fax_transfer_rate']['en-us'] = 'Transfer Rate';
+		$text['label-fax_transfer_rate']['pt-pt'] = '';
+
+		$text['description-fax_transfer_rate']['en-us'] = '';
+		$text['description-fax_transfer_rate']['pt-pt'] = '';
+
+		$text['label-fax_retry_attempts']['en-us'] = 'Retry';
+		$text['label-fax_retry_attempts']['pt-pt'] = '';
+
+		$text['description-fax_retry_attempts']['en-us'] = '';
+		$text['description-fax_retry_attempts']['pt-pt'] = '';
+
+		$text['label-fax_retry_limit']['en-us'] = 'Retry Limit';
+		$text['label-fax_retry_limit']['pt-pt'] = '';
+
+		$text['description-fax_retry_limit']['en-us'] = '';
+		$text['description-fax_retry_limit']['pt-pt'] = '';
+
+		$text['label-fax_retry_sleep']['en-us'] = 'Retry Sleep';
+		$text['label-fax_retry_sleep']['pt-pt'] = '';
+
+		$text['description-fax_retry_sleep']['en-us'] = '';
+		$text['description-fax_retry_sleep']['pt-pt'] = '';
+
+		$text['label-fax_destination']['en-us'] = 'Destination';
+		$text['label-fax_destination']['pt-pt'] = '';
+
+		$text['label-fax_uri']['en-us'] = 'URI';
+		$text['label-fax_uri']['pt-pt'] = '';
+
+		$text['description-fax_uri']['en-us'] = '';
+		$text['description-fax_uri']['pt-pt'] = '';
+
+		$text['label-fax_date']['en-us'] = 'Date';
+		$text['label-fax_date']['pt-pt'] = '';
+
+		$text['description-fax_date']['en-us'] = '';
+		$text['description-fax_date']['pt-pt'] = '';
+
+		$text['label-fax_epoch']['en-us'] = 'Epoch';
+		$text['label-fax_epoch']['pt-pt'] = '';
+
+		$text['description-fax_epoch']['en-us'] = '';
+		$text['description-fax_epoch']['pt-pt'] = '';
+
+		$text['label-true']['en-us'] = 'true';
+		$text['label-true']['pt-pt'] = '';
+
+		$text['label-false']['en-us'] = 'false';
+		$text['label-false']['pt-pt'] = 'falso';
+
+		$text['button-add']['en-us'] = 'Add';
+		$text['button-add']['pt-pt'] = '';
+
+		$text['button-edit']['en-us'] = 'Edit';
+		$text['button-edit']['pt-pt'] = '';
+
+		$text['button-delete']['en-us'] = 'Delete';
+		$text['button-delete']['pt-pt'] = '';
+
+		$text['button-save']['en-us'] = 'Save';
+		$text['button-save']['pt-pt'] = 'Guardar';
+
+		$text['button-view']['en-us'] = 'View';
+		$text['button-view']['pt-pt'] = '';
+
+		$text['button-back']['en-us'] = 'Back';
+		$text['button-back']['pt-pt'] = 'Voltar';
+
+		$text['confirm-delete']['en-us'] = 'Do you really want to delete this?';
+		$text['confirm-delete']['pt-pt'] = '';
+
+		$text['message-add']['en-us'] = 'Add Completed';
+		$text['message-add']['pt-pt'] = '';
+
+		$text['message-update']['en-us'] = 'Update Completed';
+		$text['message-update']['pt-pt'] = '';
+
+		$text['message-delete']['en-us'] = 'Delete Completed';
+		$text['message-delete']['pt-pt'] = '';
+
+		$text['message-required']['en-us'] = 'Please provide: ';
+		$text['message-required']['pt-pt'] = '';
+
+?>
