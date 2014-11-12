c88491895badb50fc9373ef807a87c82338a8965
Mark Crane <markjcrane@gmail.com>
2014-11-12 02:15:57 +0000

    Deprecate contact_groups_uuid and change it to contact_group_uuid.

--- app/contacts/app_config.php
+++ app/contacts/app_config.php
@@ -393,7 +393,8 @@
 
 		$y = 4; //table array index
 		$apps[$x]['db'][$y]['table'] = "v_contact_groups";
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = "contact_group_uuid";
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = "contact_group_uuid";
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = "contact_groups_uuid";
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = "uuid";
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = "char(36)";
