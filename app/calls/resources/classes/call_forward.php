ad9ca8af81eff96b70512dbaf6bc7381eab23cc9
Mark Crane <markjcrane@gmail.com>
2014-11-18 10:38:11 +0000

    When disabling call forward do not remove the call forward destination number only disable it.

--- app/calls/resources/classes/call_forward.php
+++ app/calls/resources/classes/call_forward.php
@@ -91,7 +91,9 @@ include "root.php";
 			//update the extension
 				$sql = "update v_extensions set ";
 				if (strlen($this->forward_all_destination) == 0 || $this->forward_all_enabled == "false") {
-					$sql .= "forward_all_destination = null, ";
+					if (strlen($this->forward_all_destination) == 0) {
+						$sql .= "forward_all_destination = null, ";
+					}
 					$sql .= "dial_string = null, ";
 					$sql .= "forward_all_enabled = 'false' ";
 				}
