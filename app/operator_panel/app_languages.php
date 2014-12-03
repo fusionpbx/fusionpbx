f64b010c2871108fda81e817f1291b4bb0ccd415
Nate Jones <git@reliberate.com>
2014-12-03 11:05:13 +0000

    Operator Panel: Drag n' drop call transfer and origination, extension monitoring, etc.

---
+++ app/operator_panel/app_languages.php
@@ -0,0 +1,23 @@
+<?php
+
+	$text['title-operator_panel']['en-us'] = 'Operator Panel';
+	$text['title-operator_panel']['es-cl'] = 'Panel de Operador';
+	$text['title-operator_panel']['pt-pt'] = 'Painel do Operador';
+	$text['title-operator_panel']['fr-fr'] = 'Operator Panel';
+
+	$text['label-call_group']['en-us'] = "Call Group";
+	$text['label-call_group']['es-cl'] = "Llamar a Grupo";
+	$text['label-call_group']['pt-pt'] = "Grupo";
+	$text['label-call_group']['fr-fr'] = "Groupe d'appel";
+
+	$text['label-other_extensions']['en-us'] = "Other Extensions";
+	$text['label-other_extensions']['es-cl'] = "Otras Extensiones";
+	$text['label-other_extensions']['pt-pt'] = "Outros Extensions";
+	$text['label-other_extensions']['fr-fr'] = "Autres Extensions";
+
+	$text['label-no_extensions_found']['en-us'] = "No extensions found.";
+	$text['label-no_extensions_found']['es-cl'] = "No hay extensiones encontrados.";
+	$text['label-no_extensions_found']['pt-pt'] = "Sem extensões encontrado.";
+	$text['label-no_extensions_found']['fr-fr'] = "Aucun extensions trouvés.";
+
+?>
