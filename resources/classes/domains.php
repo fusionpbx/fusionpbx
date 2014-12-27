4c69bd1cde412270e7476d20ae62cfc677601c37
Mark Crane <markjcrane@gmail.com>
2014-12-27 17:28:07 +0000

    Change the | to a comma had to review the syntax on the glob command.

--- resources/classes/domains.php
+++ resources/classes/domains.php
@@ -230,8 +230,8 @@
 			//get the PROJECT PATH
 				include "root.php";
 
-			//get the list of installed apps from the core and mod directories
-				$config_list = glob($_SERVER["DOCUMENT_ROOT"] . PROJECT_PATH . "/*/*/app_config.php");
+			//get the list of installed apps from the core and app directories
+				$config_list = glob($_SERVER["DOCUMENT_ROOT"] . PROJECT_PATH . "/*/*/app_{config,menu}.php");
 				$x=0;
 				foreach ($config_list as &$config_path) {
 					include($config_path);
