233ef95a472d8c78bf473ac0147a030857183e70
Mark Crane <markjcrane@gmail.com>
2014-12-27 21:05:56 +0000

    Add logout menu again.

--- app/login/app_menu.php
+++ app/login/app_menu.php
@@ -18,4 +18,22 @@
 	$apps[$x]['menu'][0]['groups'][] = "public";
 	$apps[$x]['menu'][0]['order'] = "99";
 
+	$apps[$x]['menu'][1]['title']['en-us'] = 'Logout';
+	$apps[$x]['menu'][1]['title']['es-mx'] = '';
+	$apps[$x]['menu'][1]['title']['de'] = '';
+	$apps[$x]['menu'][1]['title']['de-ch'] = '';
+	$apps[$x]['menu'][1]['title']['de-at'] = '';
+	$apps[$x]['menu'][1]['title']['fr'] = '';
+	$apps[$x]['menu'][1]['title']['fr-ca'] = '';
+	$apps[$x]['menu'][1]['title']['fr-ch'] = '';
+	$apps[$x]['menu'][1]['title']['pt-pt'] = 'Sair';
+	$apps[$x]['menu'][1]['title']['pt-br'] = '';
+	$apps[$x]['menu'][1]['uuid'] = '0d29e9f4-0c9b-9d8d-cd2d-454899dc9bc4';
+	$apps[$x]['menu'][1]['parent_uuid'] = '02194288-6d56-6d3e-0b1a-d53a2bc10788';
+	$apps[$x]['menu'][1]['category'] = 'internal';
+	$apps[$x]['menu'][1]['path'] = '/logout.php';
+	$apps[$x]['menu'][1]['groups'][] = 'user';
+	$apps[$x]['menu'][1]['groups'][] = 'admin';
+	$apps[$x]['menu'][1]['groups'][] = 'superadmin';
+
 ?>
