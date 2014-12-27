86b55ee887cd6f84b77142fa405906e384cd263d
Mark Crane <markjcrane@gmail.com>
2014-12-27 17:12:42 +0000

    Fix the file extension for app/meetings/app_menu.php

---
+++ app/meetings/app_menu.php
@@ -0,0 +1,12 @@
+<?php
+
+	//$apps[$x]['menu'][0]['title']['en-us'] = "Users";
+	//$apps[$x]['menu'][0]['uuid'] = "8d4920dc-7077-47ab-86c7-cc377ba2a5f5";
+	//$apps[$x]['menu'][0]['parent_uuid'] = "fd29e39c-c936-f5fc-8e2b-611681b266b5";
+	//$apps[$x]['menu'][0]['category'] = "internal";
+	//$apps[$x]['menu'][0]['path'] = "/app/meeting_users/meeting_users.php";
+	//$apps[$x]['menu'][0]['groups'][] = "user";
+	//$apps[$x]['menu'][0]['groups'][] = "admin";
+	//$apps[$x]['menu'][0]['groups'][] = "superadmin";
+
+?>
