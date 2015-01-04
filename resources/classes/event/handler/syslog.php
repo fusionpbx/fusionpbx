cc4009670a32fff34c5649f52d32f7b4dbc0d690
luis daniel lucio quiroz <dlucio@okay.com.mx>
2015-01-04 03:49:10 +0000

    new event handler

---
+++ resources/classes/event/handler/syslog.php
@@ -0,0 +1,36 @@
+include_once('virtual.php');
+
+class Syslog extends Event_Handler{
+
+	protected $ident;
+	protected $option;
+	protected $facility;
+	protected $priority;
+	function __construct($ident='fusionpbx', $option=(LOG_PID | LOG_PERROR), $facility=LOG_LOCAL0, $priority=LOG_INFO){
+		$this->ident = $ident;
+		$this->option = $option;
+		$this->facility = $facility;
+		$this->priority = $priority;
+
+		if ($_SESSION['event']['syslog']['enable'] <> 0){
+			openlog($ident, $option, $facility);
+		}
+	}
+
+	function __destruct(){
+		if ($_SESSION['event']['syslog']['enable'] <> 0){
+			closelog();
+		}
+	} 
+
+	public function log_event($event_type, $params){
+		if ($_SESSION['event']['syslog']['enable'] <> 0){
+			$log = '' ;
+			foreach ($params as $k => $v) {
+				$log .= "[$k]=[$v] ";
+			}
+
+			syslog($priority, $log);
+		}
+	}
+}
---
+++ resources/classes/event/handler/virtual.php
@@ -0,0 +1,4 @@
+abstract class Event_Handler{
+	// Force Extending class to define this method
+	abstract public function log_event($event_type, $params);
+}
