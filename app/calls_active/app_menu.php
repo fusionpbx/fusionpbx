c39447e51017b7ce973b49884bdee2ad26a97e76
Mark Crane <markjcrane@gmail.com>
2015-01-09 20:23:13 +0000

    Remove active extensions from the menu.

--- app/calls_active/app_menu.php
+++ app/calls_active/app_menu.php
@@ -16,22 +16,4 @@
 	$apps[$x]['menu'][0]['path'] = "/app/calls_active/calls_active.php";
 	$apps[$x]['menu'][0]['groups'][] = "superadmin";
 
-	$apps[$x]['menu'][1]['title']['en-us'] = "Active Extensions";
-	$apps[$x]['menu'][1]['title']['es-cl'] = "Extensiones activas";
-	$apps[$x]['menu'][1]['title']['de-de'] = "";
-	$apps[$x]['menu'][1]['title']['de-ch'] = "";
-	$apps[$x]['menu'][1]['title']['de-at'] = "";
-	$apps[$x]['menu'][1]['title']['fr-fr'] = "Extentions actives";
-	$apps[$x]['menu'][1]['title']['fr-ca'] = "";
-	$apps[$x]['menu'][1]['title']['fr-ch'] = "";
-	$apps[$x]['menu'][1]['title']['pt-pt'] = "Extensões Activas";
-	$apps[$x]['menu'][1]['title']['pt-br'] = "";
-	$apps[$x]['menu'][1]['uuid'] = "6dd85c19-cb6b-5cca-bf32-499bbe936f79";
-	$apps[$x]['menu'][1]['parent_uuid'] = "0438b504-8613-7887-c420-c837ffb20cb1";
-	$apps[$x]['menu'][1]['category'] = "internal";
-	$apps[$x]['menu'][1]['path'] = "/app/calls_active/calls_active_extensions.php";
-	//$apps[$x]['menu'][1]['groups'][] = "user";
-	$apps[$x]['menu'][1]['groups'][] = "admin";
-	$apps[$x]['menu'][1]['groups'][] = "superadmin";
-
 ?>
