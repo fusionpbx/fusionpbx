5c279216f1ce1d073c982236398bb203f9ebec06
Mark Crane <markjcrane@gmail.com>
2015-01-22 10:14:45 +0000

    A few minor changes to the sip profile spacing.

--- app/sip_profiles/sip_profile_delete.php
+++ app/sip_profiles/sip_profile_delete.php
@@ -38,62 +38,64 @@ else {
 	$language = new text;
 	$text = $language->get();
 
-if (count($_GET)>0) {
-	$id = check_str($_GET["id"]);
-}
+//get the id
+	if (count($_GET) > 0) {
+		$id = check_str($_GET["id"]);
+	}
 
-if (strlen($id)>0) {
+//delete the records
+	if (strlen($id) > 0) {
 
-	//get the details fo the sip profile
-		$sql = "select * from v_sip_profiles ";
-		$sql .= "where sip_profile_uuid = '$id' ";
-		$prep_statement = $db->prepare(check_sql($sql));
-		$prep_statement->execute();
-		$result = $prep_statement->fetchAll();
-		foreach ($result as &$row) {
-			$sip_profile_name = $row["sip_profile_name"];
-			$sip_profile_hostname = $row["sip_profile_hostname"];
-			$sip_profile_description = $row["sip_profile_description"];
-		}
-		unset ($prep_statement);
-
-	//delete the sip profile settings
-		$sql = "delete from v_sip_profile_settings ";
-		$sql .= "where sip_profile_uuid = '$id' ";
-		$prep_statement = $db->prepare(check_sql($sql));
-		$prep_statement->execute();
-		unset($sql);
-
-	//delete the sip profile
-		$sql = "delete from v_sip_profiles ";
-		$sql .= "where sip_profile_uuid = '$id' ";
-		$prep_statement = $db->prepare(check_sql($sql));
-		$prep_statement->execute();
-		unset($sql);
-
-	//delete the xml sip profile and directory
-		unlink($_SESSION['switch']['conf']['dir']."/sip_profiles/".$sip_profile_name.".xml");
-		unlink($_SESSION['switch']['conf']['dir']."/sip_profiles/".$sip_profile_name);
-
-	//save the sip profile xml
-		save_sip_profile_xml();
-
-	//apply settings reminder
-		$_SESSION["reload_xml"] = true;
-
-	//get the hostname
-		if ($sip_profile_name == nul) {
-			$fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
-			if ($fp) {
-				$switch_cmd = "hostname";
-				$sip_profile_name = event_socket_request($fp, 'api '.$switch_cmd);
+		//get the details of the sip profile
+			$sql = "select * from v_sip_profiles ";
+			$sql .= "where sip_profile_uuid = '$id' ";
+			$prep_statement = $db->prepare(check_sql($sql));
+			$prep_statement->execute();
+			$result = $prep_statement->fetchAll();
+			foreach ($result as &$row) {
+				$sip_profile_name = $row["sip_profile_name"];
+				$sip_profile_hostname = $row["sip_profile_hostname"];
+				$sip_profile_description = $row["sip_profile_description"];
 			}
-		}
+			unset ($prep_statement);
 
-	//clear the cache
-		$cache = new cache;
-		$cache->delete("configuration:sofia.conf:".$sip_profile_name);
-}
+		//delete the sip profile settings
+			$sql = "delete from v_sip_profile_settings ";
+			$sql .= "where sip_profile_uuid = '$id' ";
+			$prep_statement = $db->prepare(check_sql($sql));
+			$prep_statement->execute();
+			unset($sql);
+
+		//delete the sip profile
+			$sql = "delete from v_sip_profiles ";
+			$sql .= "where sip_profile_uuid = '$id' ";
+			$prep_statement = $db->prepare(check_sql($sql));
+			$prep_statement->execute();
+			unset($sql);
+
+		//delete the xml sip profile and directory
+			unlink($_SESSION['switch']['conf']['dir']."/sip_profiles/".$sip_profile_name.".xml");
+			unlink($_SESSION['switch']['conf']['dir']."/sip_profiles/".$sip_profile_name);
+
+		//save the sip profile xml
+			save_sip_profile_xml();
+
+		//apply settings reminder
+			$_SESSION["reload_xml"] = true;
+
+		//get the hostname
+			if ($sip_profile_name == nul) {
+				$fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
+				if ($fp) {
+					$switch_cmd = "hostname";
+					$sip_profile_name = event_socket_request($fp, 'api '.$switch_cmd);
+				}
+			}
+
+		//clear the cache
+			$cache = new cache;
+			$cache->delete("configuration:sofia.conf:".$sip_profile_name);
+	}
 
 //redirect the browser
 	$_SESSION["message"] = $text['message-delete'];
