abb7e561cc2f3e796afb4179260da4c55ca69b16
luis daniel lucio quiroz <dlucio@okay.com.mx>
2015-02-13 19:16:35 +0000

    acl menu fix

--- app/acl/app_menu.php
+++ app/acl/app_menu.php
@@ -4,7 +4,7 @@
 	$apps[$x]['menu'][0]['uuid'] = '0044c532-07ca-477f-aa2d-920ee58e100b';
 	$apps[$x]['menu'][0]['parent_uuid'] = 'fd29e39c-c936-f5fc-8e2b-611681b266b5';
 	$apps[$x]['menu'][0]['category'] = 'internal';
-	$apps[$x]['menu'][0]['path'] = '/app/acl/acl.php';
+	$apps[$x]['menu'][0]['path'] = '/app/acl/acls.php';
 	$apps[$x]['menu'][0]['groups'][] = 'superadmin';
 
 ?>
