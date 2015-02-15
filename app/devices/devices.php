257b98631806be33765a2b1560039374a0b515f7
Nate Jones <git@reliberate.com>
2015-02-15 00:40:39 +0000

    Devices: Add view permission to Key Profiles button.

--- app/devices/devices.php
+++ app/devices/devices.php
@@ -58,7 +58,9 @@ else {
 	echo "		</td>\n";
 	echo "		<td align='right' nowrap='nowrap' valign='top'>\n";
 	echo "			<form method='get' action=''>\n";
-	echo "			<input type='button' class='btn' value='".$text['button-profiles']."' onclick=\"document.location.href='device_profiles.php';\">";
+	if (permission_exists('device_profile_view')) {
+		echo "		<input type='button' class='btn' value='".$text['button-profiles']."' onclick=\"document.location.href='device_profiles.php';\">";
+	}
 	echo "			<input type='text' class='txt' style='width: 150px' name='search' value='$search'>";
 	echo "			<input type='submit' class='btn' name='submit' value='".$text['button-search']."'>";
 	echo "			</form>\n";
