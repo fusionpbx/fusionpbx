a4671d33b60e39616203f492a36da6007070fd87
Nate Jones <git@reliberate.com>
2015-02-24 09:40:45 +0000

    Voicemail: Email notification script updated to support multiple languages.

---
+++ resources/install/scripts/app/voicemail/app_languages.lua
@@ -0,0 +1,19 @@
+text = {};
+
+text['label-download'] = {};
+text['label-download']['en-us'] = "Download";
+text['label-download']['es-cl'] = "Descargar";
+text['label-download']['pt-pt'] = "Baixar";
+text['label-download']['fr-fr'] = "Télécharger";
+
+text['label-listen'] = {};
+text['label-listen']['en-us'] = "Listen";
+text['label-listen']['es-cl'] = "Escuchar";
+text['label-listen']['pt-pt'] = "Ouvir";
+text['label-listen']['fr-fr'] = "écouter";
+
+text['label-attached'] = {};
+text['label-attached']['en-us'] = "Attached";
+text['label-attached']['es-cl'] = "Adjunto";
+text['label-attached']['pt-pt'] = "Ligado";
+text['label-attached']['fr-fr'] = "Attaché";
