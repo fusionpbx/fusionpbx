8750662aca13ce8c632ad3fff6591d5c6dd44b39
luis daniel lucio quiroz <dlucio@okay.com.mx>
2015-02-28 02:32:25 +0000

    you can fix your follow me caller id number/name

--- app/follow_me/app_config.php
+++ app/follow_me/app_config.php
@@ -114,6 +114,12 @@
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en'] = "";
 		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = "follow_me_caller_id_uuid";
+		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = "uuid";
+		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = "text";
+		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = "char(36)";
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en'] = "";
+		$z++;
 
 	//schema details
 		$y = 1; //table array index
