ac19ba116b87bc2baaed692f7a8697d9343daa64
luis daniel lucio quiroz <dlucio@okay.com.mx>
2015-02-28 04:07:15 +0000

    you can fix your forward caller id number/name

--- app/extensions/app_config.php
+++ app/extensions/app_config.php
@@ -317,6 +317,12 @@
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = "description";
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = "forward_caller_id_uuid";
+		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = "uuid";
+		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = "text";
+		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = "char(36)";
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
 
 		$y = 1; //table array index
 		$z = 0; //field array index
