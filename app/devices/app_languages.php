dcbab015ab20c6675eddab242deebc9094b4f1dc
Mark Crane <markjcrane@gmail.com>
2015-03-07 21:59:17 +0000

    Add 'Show All' button text for Devices.

--- app/devices/app_languages.php
+++ app/devices/app_languages.php
@@ -970,8 +970,14 @@
 		$text['description-description']['pt-pt'] = "Introduza uma descrição, caso queira.";
 		$text['description-description']['fr-fr'] = "Entrez une description, si vous le souhaitez.";
 
+		$text['button-show_all']['en-us'] = "Show All";
+		$text['button-show_all']['es-cl'] = "Mostrar Todos";
+		$text['button-show_all']['pt-pt'] = "Mostrar Todos";
+		$text['button-show_all']['fr-fr'] = "Montrer Tout";
+
 		$text['label-domain-name']['en-us'] = "Domain Name";
 		$text['label-domain-name']['es-cl'] = "Nombre de Dominio";
 		$text['label-domain-name']['pt-pt'] = "Nome de Domínio";
 		$text['label-domain-name']['fr-fr'] = "Nom de Domaine";
+
 ?>
