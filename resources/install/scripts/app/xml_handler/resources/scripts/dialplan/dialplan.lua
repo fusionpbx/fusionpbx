4385aac2dda5678f6157c190933274cc835f3ed5
Mark Crane <markjcrane@gmail.com>
2015-03-18 20:51:13 +0000

    Remove trailing spaces or tabs.

--- resources/install/scripts/app/xml_handler/resources/scripts/dialplan/dialplan.lua
+++ resources/install/scripts/app/xml_handler/resources/scripts/dialplan/dialplan.lua
@@ -261,8 +261,8 @@
 									domain_name = domains[domain_uuid];
 									table.insert(xml, [[					<action application="set" data="call_direction=inbound"/>]]);
 									table.insert(xml, [[					<action application="set" data="domain_uuid=]] .. domain_uuid .. [["/>]]);
-									table.insert(xml, [[					<action application="set" data="domain_name=]] .. domain_name .. [["/>]]);			
-									table.insert(xml, [[					<action application="set" data="domain=]] .. domain_name .. [["/>]]);			
+									table.insert(xml, [[					<action application="set" data="domain_name=]] .. domain_name .. [["/>]]);
+									table.insert(xml, [[					<action application="set" data="domain=]] .. domain_name .. [["/>]]);
 									first_action = false;
 								end
 							end
