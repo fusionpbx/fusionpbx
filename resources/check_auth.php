0d5d31f06938849f9564b68ef42ec0a4ac5a7bc8
Mark Crane <markjcrane@gmail.com>
2015-03-24 19:54:59 +0000

    Update the LDAP authentication.

--- resources/check_auth.php
+++ resources/check_auth.php
@@ -107,9 +107,8 @@ require_once "resources/require.php";
 					//ldap_set_option($connect, LDAP_OPT_PROTOCOL_VERSION, 3);
 					$connect = ldap_connect($_SESSION["ldap"]["server_host"]["text"], $_SESSION["ldap"]["server_port"]["numeric"])
 						or die("Could not connect to the LDAP server.");
-					$bind_dn = $_SESSION["ldap"]["user_attribute"]["text"]."=".$_REQUEST["password"].",".$_SESSION["ldap"]["user_dn"]["text"];
-
-					$bind = ldap_bind($connect, $bind_dn, $_SESSION["ldap"]["bind_password"]["text"]);
+					$bind_dn = $_SESSION["ldap"]["user_attribute"]["text"]."=".$username.",".$_SESSION["ldap"]["user_dn"]["text"];
+					$bind = ldap_bind($connect, $bind_dn, $_REQUEST["password"]);
 					if ($bind) {
 						$_SESSION['username'] = $username;
 					}
