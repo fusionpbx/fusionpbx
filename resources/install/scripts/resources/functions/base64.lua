3bac48cebb79c5359e2b1d6605d771d62861b951
Mark Crane <markjcrane@gmail.com>
2015-04-06 06:25:42 +0000

    Add the original base64 class I was using as an alternative if mime is not loaded.

--- resources/install/scripts/resources/functions/base64.lua
+++ resources/install/scripts/resources/functions/base64.lua
@@ -2,12 +2,22 @@ base64 = {}
 
 -- encode a string and return a base64 string
 function base64.encode(s)
-	local mime = require("mime");
-	return (mime.b64(s));
+	if package.loaded["mime"] then
+		local mime = require("mime");
+		return (mime.b64(s));
+	else
+		dofile(scripts_dir.."/resources/functions/base64_alex.lua");
+		return base64.enc(s);
+	end
 end
 
 --decode a base64 string and return a string
 function base64.decode(s)
-	local mime = require("mime");
-	return (mime.unb64(s));
+	if package.loaded["mime"] then
+		local mime = require("mime");
+		return (mime.unb64(s));
+	else
+		dofile(scripts_dir.."/resources/functions/base64_alex.lua");
+		return base64.dec(s);
+	end
 end
