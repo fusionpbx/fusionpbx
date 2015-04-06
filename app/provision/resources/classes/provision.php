d19494f4659fd1b5e3768752b948cebe4ee36546
Mark Crane <markjcrane@gmail.com>
2015-04-06 08:51:12 +0000

    Fix a syntax error with the new code.

--- app/provision/resources/classes/provision.php
+++ app/provision/resources/classes/provision.php
@@ -404,12 +404,12 @@ include "root.php";
 							$lines[$line_number]['register_expires'] = $register_expires;
 							$lines[$line_number]['sip_transport'] = strtolower($sip_transport);
 							$lines[$line_number]['sip_port'] = $sip_port;
-							$lines[$line_number]['server_address'] = $row["server_address"]);
-							$lines[$line_number]['outbound_proxy'] = $row["outbound_proxy"]);
-							$lines[$line_number]['display_name'] = = $row["display_name"]);
-							$lines[$line_number]['auth_id'] = = $row["auth_id"]);
-							$lines[$line_number]['user_id'] = = $row["user_id"]);
-							$lines[$line_number]['password'] = = $row["password"]);
+							$lines[$line_number]['server_address'] = $row["server_address"];
+							$lines[$line_number]['outbound_proxy'] = $row["outbound_proxy"];
+							$lines[$line_number]['display_name'] = = $row["display_name"];
+							$lines[$line_number]['auth_id'] = = $row["auth_id"];
+							$lines[$line_number]['user_id'] = = $row["user_id"];
+							$lines[$line_number]['password'] = = $row["password"];
 
 						//assign the variables
 							$view->assign("server_address_".$line_number, $row["server_address"]);
@@ -495,7 +495,7 @@ include "root.php";
 							$x = $device_key_line;
 
 						//add a simple variable with the index
-							if ($x === 0 or $x === 1) {
+							if ($x === 0 || $x === 1) {
 								$device_key_value = str_replace("\${user_id}", $lines[$x]['user_id'], $device_key_value);
 								$device_key_value = str_replace("\${auth_id}", $lines[$x]['auth_id'], $device_key_value);
 								$device_key_value = str_replace("\${extension}", $lines[$x]['user_id'], $device_key_value);
