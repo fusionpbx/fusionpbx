395fa85cecdab75e79c40c264231898e2e0366ab
Mark Crane <markjcrane@gmail.com>
2015-04-09 15:58:56 +0000

    Fix the problem on calls_active/app_languages.php.

--- app/calls_active/app_languages.php
+++ app/calls_active/app_languages.php
@@ -1,172 +1,185 @@
 <?php
-	$text['check-available-on-demand-status']['en-us'] = "Available (On Demand)";
-	$text['check-available-on-demand-status']['es-cl'] = "Disponible (On Demand)";
-	$text['check-available-on-demand-status']['fr-fr'] = "Disponible (Sur Demande)";
-	$text['check-available-on-demand-status']['pt-pt'] = "Disponível (A pedido)";
 
-	$text['check-available-status']['en-us'] = "Available";
-	$text['check-available-status']['es-cl'] = "Disponible";
-	$text['check-available-status']['fr-fr'] = "Disponible";
-	$text['check-available-status']['pt-pt'] = "Disponível";
+$text['title-2']['en-us'] = "Active Extensions";
+$text['title-2']['es-cl'] = "Extensiones Activas";
+$text['title-2']['fr-fr'] = "Extensions Actives";
+$text['title-2']['pt-pt'] = "Extensões Ativas";
+$text['title-2']['pt-br'] = "Extensões ativas";
+$text['title-2']['pl'] = "Aktywne numery wewnętrzne";
+$text['title-2']['sv-se'] = "Aktiva anknytningar";
 
-	$text['check-do-not-disturb-status']['en-us'] = "Do Not Disturb";
-	$text['check-do-not-disturb-status']['es-cl'] = "No molestar";
-	$text['check-do-not-disturb-status']['fr-fr'] = "Ne Pas Déranger";
-	$text['check-do-not-disturb-status']['pt-pt'] = "Não Perturbar";
+$text['title']['en-us'] = "Active Calls";
+$text['title']['es-cl'] = "Llamadas Activas";
+$text['title']['fr-fr'] = "Communications Actives";
+$text['title']['pt-pt'] = "Chamadas Ativas";
+$text['title']['pt-br'] = "Configurações da Conta";
+$text['title']['pl'] = "Ustawienia konta";
+$text['title']['sv-se'] = "Aktiva samtal";
 
-	$text['check-loggedout-status']['en-us'] = "Logged Out";
-	$text['check-loggedout-status']['es-cl'] = "Desconectado";
-	$text['check-loggedout-status']['fr-fr'] = "Délogué";
-	$text['check-loggedout-status']['pt-pt'] = "Desligado";
+$text['label-transfer']['en-us'] = "Transfer To";
+$text['label-transfer']['es-cl'] = "Transferir a";
+$text['label-transfer']['fr-fr'] = "Transférer à";
+$text['label-transfer']['pt-pt'] = "Transferir Para";
+$text['label-transfer']['pt-br'] = "Tranferência";
+$text['label-transfer']['pl'] = "Transferowanie";
+$text['label-transfer']['sv-se'] = "Överför till";
 
-	$text['check-onbreak-status']['en-us'] = "On Break";
-	$text['check-onbreak-status']['es-cl'] = "En Pausa";
-	$text['check-onbreak-status']['fr-fr'] = "En Pause";
-	$text['check-onbreak-status']['pt-pt'] = "Em Pausa";
+$text['label-time']['en-us'] = "Time";
+$text['label-time']['es-cl'] = "Tiempo";
+$text['label-time']['fr-fr'] = "Temps";
+$text['label-time']['pt-pt'] = "Tempo";
+$text['label-time']['pt-br'] = "Fuso Horário";
+$text['label-time']['pl'] = "Strefa czasowa";
+$text['label-time']['sv-se'] = "Tid";
 
-	$text['confirm-hangup']['en-us'] = "Do you really want to hangup this call?";
-	$text['confirm-hangup']['es-cl'] = "¿Realmente desea terminar esta llamada?";
-	$text['confirm-hangup']['fr-fr'] = "Voulez-vous vraiment raccrocher cette communication?";
-	$text['confirm-hangup']['pt-pt'] = "Deseja realmente terminar esta chamada?";
+$text['label-stop']['en-us'] = "stop record";
+$text['label-stop']['es-cl'] = "terminar grabación";
+$text['label-stop']['fr-fr'] = "arrêter l'enregistrement";
+$text['label-stop']['pt-pt'] = "parar de gravar";
+$text['label-stop']['pt-br'] = "Parar";
+$text['label-stop']['pl'] = "zatrzymaj nagrywanie";
+$text['label-stop']['sv-se'] = "Stoppa inspelning";
 
-	$text['confirm-socket']['en-us'] = "Connection to Event Socket failed.";
-	$text['confirm-socket']['es-cl'] = "Conexión con socket fallida.";
-	$text['confirm-socket']['fr-fr'] = "la Connexion au Event Socket a échoué.";
-	$text['confirm-socket']['pt-pt'] = "A conexão ao Event Socket falhou.";
+$text['label-status']['en-us'] = "Status";
+$text['label-status']['es-cl'] = "Estado";
+$text['label-status']['fr-fr'] = "Etat";
+$text['label-status']['pt-pt'] = "Estado";
+$text['label-status']['pt-br'] = "Estado";
+$text['label-status']['pl'] = "Status";
+$text['label-status']['sv-se'] = "Status";
 
-	$text['description']['en-us'] = "Use this to monitor and interact with the active calls.";
-	$text['description']['es-cl'] = "Utilice este menú para monitorear e interactuar con las llamadas activas.";
-	$text['description']['fr-fr'] = "Superviser et interagir avec les Communications Actives.";
-	$text['description']['pt-pt'] = "Utilize este menu para monitorizar e interagir com as chamadas activas.";
+$text['label-start']['en-us'] = "start record";
+$text['label-start']['es-cl'] = "iniciar grabación";
+$text['label-start']['fr-fr'] = "commencer l'enregistrement";
+$text['label-start']['pt-pt'] = "iniciar a gravação";
+$text['label-start']['pt-br'] = "Iniciar";
+$text['label-start']['pl'] = "rozpocznij nagrywanie";
+$text['label-start']['sv-se'] = "Starta inspelning";
 
-	$text['description-2']['en-us'] = "Use this to view all extensions and monitor and interact with active calls.";
-	$text['description-2']['es-cl'] = "Utilice este menú para ver todas las extensiones, monitorear e interactuar con las llamadas activas.";
-	$text['description-2']['fr-fr'] = "Voir toutes les extensions, superviser et interagir avec les Communications Actives.";
-	$text['description-2']['pt-pt'] = "Utilize este menu para ver todas as extensões, monitorizar e interagir com as chamadas activas.";
+$text['label-secure']['en-us'] = "Secure";
+$text['label-secure']['es-cl'] = "Seguro";
+$text['label-secure']['fr-fr'] = "Securisé";
+$text['label-secure']['pt-pt'] = "Seguro";
+$text['label-secure']['pt-br'] = "Seguro";
+$text['label-secure']['pl'] = "Bezpieczny";
+$text['label-secure']['sv-se'] = "Skydda";
 
-	$text['label-app']['en-us'] = "Application";
-	$text['label-app']['es-cl'] = "Aplicación";
-	$text['label-app']['fr-fr'] = "Application";
-	$text['label-app']['pt-pt'] = "Aplicação";
+$text['label-profile']['en-us'] = "Profile";
+$text['label-profile']['es-cl'] = "Perfil";
+$text['label-profile']['fr-fr'] = "Profile";
+$text['label-profile']['pt-pt'] = "Perfil";
+$text['label-profile']['pt-br'] = "Perfil";
+$text['label-profile']['pl'] = "Profil";
+$text['label-profile']['sv-se'] = "Profil";
 
-	$text['label-cid-name']['en-us'] = "CID Name";
-	$text['label-cid-name']['es-cl'] = "Nombre CID";
-	$text['label-cid-name']['fr-fr'] = "Nom Appelant";
-	$text['label-cid-name']['pt-pt'] = "Nome CID";
+$text['label-park-extension']['en-us'] = "Park";
+$text['label-park-extension']['es-cl'] = "Parquear Extensión";
+$text['label-park-extension']['fr-fr'] = "parquer";
+$text['label-park-extension']['pt-pt'] = "Parquear Extensão";
+$text['label-park-extension']['pt-br'] = "Extensão estacionamento";
+$text['label-park-extension']['pl'] = "Parkowanie";
+$text['label-park-extension']['sv-se'] = "Parkera";
 
-	$text['label-cid-number']['en-us'] = "CID Number";
-	$text['label-cid-number']['es-cl'] = "Número CID";
-	$text['label-cid-number']['fr-fr'] = "Numéro Appelant";
-	$text['label-cid-number']['pt-pt'] = "Número CID";
+$text['label-park']['en-us'] = "park";
+$text['label-park']['es-cl'] = "parquear";
+$text['label-park']['fr-fr'] = "parquer";
+$text['label-park']['pt-pt'] = "parquear";
+$text['label-park']['pt-br'] = "Estacionamento";
+$text['label-park']['pl'] = "parkowanie";
+$text['label-park']['sv-se'] = "Parkera";
 
-	$text['label-codec']['en-us'] = "Read / Write Codec";
-	$text['label-codec']['es-cl'] = "Codec de Lectura / Escritura ";
-	$text['label-codec']['fr-fr'] = "décode/encode";
-	$text['label-codec']['pt-pt'] = "Codec de Leitura / Escrita ";
+$text['label-opt']['en-us'] = "Options";
+$text['label-opt']['es-cl'] = "Opciones";
+$text['label-opt']['fr-fr'] = "Options";
+$text['label-opt']['pt-pt'] = "Opções";
+$text['label-opt']['pt-br'] = "Opções";
+$text['label-opt']['pl'] = "Opcje";
+$text['label-opt']['sv-se'] = "Alternativ";
 
-	$text['label-created']['en-us'] = "Created";
-	$text['label-created']['es-cl'] = "Creado";
-	$text['label-created']['fr-fr'] = "Créé";
-	$text['label-created']['pt-pt'] = "Criado";
+$text['label-number']['en-us'] = "Number";
+$text['label-number']['es-cl'] = "Número";
+$text['label-number']['fr-fr'] = "Numéro";
+$text['label-number']['pt-pt'] = "Numero";
+$text['label-number']['pt-br'] = "Número";
+$text['label-number']['pl'] = "Numer";
+$text['label-number']['sv-se'] = "Nummer";
 
-	$text['label-destination']['en-us'] = "Dest";
-	$text['label-destination']['es-cl'] = "Destino";
-	$text['label-destination']['fr-fr'] = "Dest";
-	$text['label-destination']['pt-pt'] = "Destino";
+$text['label-name']['en-us'] = "Name";
+$text['label-name']['es-cl'] = "Nombre";
+$text['label-name']['fr-fr'] = "Nom";
+$text['label-name']['pt-pt'] = "Nome";
+$text['label-name']['pt-br'] = "Nome";
+$text['label-name']['pl'] = "Nazwa";
+$text['label-name']['sv-se'] = "Namn";
 
-	$text['label-ext']['en-us'] = "Ext";
-	$text['label-ext']['es-cl'] = "Extensión";
-	$text['label-ext']['fr-fr'] = "extension";
-	$text['label-ext']['pt-pt'] = "Extensão";
+$text['label-message']['en-us'] = "Message";
+$text['label-message']['es-cl'] = "Mensaje";
+$text['label-message']['fr-fr'] = "Message";
+$text['label-message']['pt-pt'] = "Mensagem";
+$text['label-message']['pt-br'] = "Mensagem";
+$text['label-message']['pl'] = "Wiadomość";
+$text['label-message']['sv-se'] = "Meddelande";
 
-	$text['label-hangup']['en-us'] = "hangup";
-	$text['label-hangup']['es-cl'] = "terminar";
-	$text['label-hangup']['fr-fr'] = "raccrocher";
-	$text['label-hangup']['pt-pt'] = "terminar";
+$text['label-hangup']['en-us'] = "hangup";
+$text['label-hangup']['es-cl'] = "terminar";
+$text['label-hangup']['fr-fr'] = "raccrocher";
+$text['label-hangup']['pt-pt'] = "terminar";
+$text['label-hangup']['pt-br'] = "Desligar";
+$text['label-hangup']['pl'] = "Zakończ rozmowę";
+$text['label-hangup']['sv-se'] = "Avsluta";
 
-	$text['label-message']['en-us'] = "Message";
-	$text['label-message']['es-cl'] = "Mensaje";
-	$text['label-message']['fr-fr'] = "Message";
-	$text['label-message']['pt-pt'] = "Mensagem";
+$text['label-ext']['en-us'] = "Ext";
+$text['label-ext']['es-cl'] = "Extensión";
+$text['label-ext']['fr-fr'] = "extension";
+$text['label-ext']['pt-pt'] = "Extensão";
+$text['label-ext']['pt-br'] = "Extensão";
+$text['label-ext']['pl'] = "Numer wewnętrzny";
+$text['label-ext']['sv-se'] = "Ank";
 
-	$text['label-name']['en-us'] = "Name";
-	$text['label-name']['es-cl'] = "Nombre";
-	$text['label-name']['fr-fr'] = "Nom";
-	$text['label-name']['pt-pt'] = "Nome";
+$text['label-destination']['en-us'] = "Dest";
+$text['label-destination']['es-cl'] = "Destino";
+$text['label-destination']['fr-fr'] = "Dest";
+$text['label-destination']['pt-pt'] = "Destino";
+$text['label-destination']['pt-br'] = "Número de destino";
+$text['label-destination']['pl'] = "Destynacja";
+$text['label-destination']['sv-se'] = "Dest";
 
-	$text['label-number']['en-us'] = "Number";
-	$text['label-number']['es-cl'] = "Número";
-	$text['label-number']['fr-fr'] = "Numéro";
-	$text['label-number']['pt-pt'] = "Numero";
+$text['label-created']['en-us'] = "Created";
+$text['label-created']['es-cl'] = "Creado";
+$text['label-created']['fr-fr'] = "Créé";
+$text['label-created']['pt-pt'] = "Criado";
+$text['label-created']['pt-br'] = "Criado";
+$text['label-created']['pl'] = "Utworzno";
+$text['label-created']['sv-se'] = "Skapad";
 
-	$text['label-opt']['en-us'] = "Options";
-	$text['label-opt']['es-cl'] = "Opciones";
-	$text['label-opt']['fr-fr'] = "Options";
-	$text['label-opt']['pt-pt'] = "Opções";
+$text['label-codec']['en-us'] = "Read / Write Codec";
+$text['label-codec']['es-cl'] = "Codec de Lectura / Escritura ";
+$text['label-codec']['fr-fr'] = "décode/encode";
+$text['label-codec']['pt-pt'] = "Codec de Leitura / Escrita ";
+$text['label-codec']['pt-br'] = "Codec da leitura / escrita";
+$text['label-codec']['pl'] = "Kodek read/write";
+$text['label-codec']['sv-se'] = "Läs/Skriv Codec";
 
-	$text['label-park']['en-us'] = "park";
-	$text['label-park']['es-cl'] = "parquear";
-	$text['label-park']['fr-fr'] = "parquer";
-	$text['label-park']['pt-pt'] = "parquear";
+$text['label-cid-number']['en-us'] = "CID Number";
+$text['label-cid-number']['es-cl'] = "Número CID";
+$text['label-cid-number']['fr-fr'] = "Numéro Appelant";
+$text['label-cid-number']['pt-pt'] = "Número CID";
+$text['label-cid-number']['pt-br'] = "Número CID";
+$text['label-cid-number']['pl'] = "Prezentacja numeru dzwoniącego";
+$text['label-cid-number']['sv-se'] = "CID, nummerspresentation";
 
-	$text['label-park-extension']['en-us'] = "Park";
-	$text['label-park-extension']['es-cl'] = "Parquear Extensión";
-	$text['label-park-extension']['fr-fr'] = "parquer";
-	$text['label-park-extension']['pt-pt'] = "Parquear Extensão";
+$text['label-cid-name']['en-us'] = "CID Name";
+$text['label-cid-name']['es-cl'] = "Nombre CID";
+$text['label-cid-name']['fr-fr'] = "Nom Appelant";
+$text['label-cid-name']['pt-pt'] = "Nome CID";
+$text['label-cid-name']['pt-br'] = "Nome CID";
+$text['label-cid-name']['pl'] = "Prezentacja nazwy dzwoniącego";
+$text['label-cid-name']['sv-se'] = "CID namnpresentation";
 
-	$text['label-profile']['en-us'] = "Profile";
-	$text['label-profile']['es-cl'] = "Perfil";
-	$text['label-profile']['fr-fr'] = "Profile";
-	$text['label-profile']['pt-pt'] = "Perfil";
-
-	$text['label-secure']['en-us'] = "Secure";
-	$text['label-secure']['es-cl'] = "Seguro";
-	$text['label-secure']['fr-fr'] = "Securisé";
-	$text['label-secure']['pt-pt'] = "Seguro";
-
-	$text['label-start']['en-us'] = "start record";
-	$text['label-start']['es-cl'] = "iniciar grabación";
-	$text['label-start']['fr-fr'] = "commencer l'enregistrement";
-	$text['label-start']['pt-pt'] = "iniciar a gravação";
-
-	$text['label-status']['en-us'] = "Status";
-	$text['label-status']['es-cl'] = "Estado";
-	$text['label-status']['fr-fr'] = "Etat";
-	$text['label-status']['pt-pt'] = "Estado";
-
-	$text['label-stop']['en-us'] = "stop record";
-	$text['label-stop']['es-cl'] = "terminar grabación";
-	$text['label-stop']['fr-fr'] = "arrêter l'enregistrement";
-	$text['label-stop']['pt-pt'] = "parar de gravar";
-
-	$text['label-time']['en-us'] = "Time";
-	$text['label-time']['es-cl'] = "Tiempo";
-	$text['label-time']['fr-fr'] = "Temps";
-	$text['label-time']['pt-pt'] = "Tempo";
-
-//REPEATED
-	$text['label-transfer']['en-us'] = "Transfer";
-	$text['label-transfer']['es-cl'] = "Transferir";
-	$text['label-transfer']['fr-fr'] = "Transfert";
-	$text['label-transfer']['pt-pt'] = "Transferir";
-
-//REPEATED
-	$text['label-transfer']['en-us'] = "Transfer To";
-	$text['label-transfer']['es-cl'] = "Transferir a";
-	$text['label-transfer']['fr-fr'] = "Transférer à";
-	$text['label-transfer']['pt-pt'] = "Transferir Para";
-
-	$text['title']['en-us'] = "Active Calls";
-	$text['title']['es-cl'] = "Llamadas Activas";
-	$text['title']['fr-fr'] = "Communications Actives";
-	$text['title']['pt-pt'] = "Chamadas Ativas";
-
-	$text['title-2']['en-us'] = "Active Extensions";
-	$text['title-2']['es-cl'] = "Extensiones Activas";
-	$text['title-2']['fr-fr'] = "Extensions Actives";
-	$text['title-2']['pt-pt'] = "Extensões Ativas";
-?>
-icação";
+$text['label-app']['en-us'] = "Application";
+$text['label-app']['es-cl'] = "Aplicación";
+$text['label-app']['fr-fr'] = "Application";
+$text['label-app']['pt-pt'] = "Aplicação";
 $text['label-app']['pt-br'] = "Aplicação";
 $text['label-app']['pl'] = "Aplikacja";
 $text['label-app']['sv-se'] = "Ansökan";
