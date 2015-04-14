648cab35bf7b17d2232e8c761913f9a7f8708bb7
Nate Jones <git@reliberate.com>
2015-04-14 18:53:35 +0000

    Content Manager: Fix app_languages (using Translate server version).

--- app/content/app_languages.php
+++ app/content/app_languages.php
@@ -1,232 +1,187 @@
 <?php
-/*
-	FusionPBX
-	Version: MPL 1.1
 
-	The contents of this file are subject to the Mozilla Public License Version
-	1.1 (the "License"); you may not use this file except in compliance with
-	the License. You may obtain a copy of the License at
-	http://www.mozilla.org/MPL/
+$text['title-rss']['en-us'] = "RSS Feed";
+$text['title-rss']['es-cl'] = "Fuentes RSS";
+$text['title-rss']['pt-pt'] = "RSS Feed";
+$text['title-rss']['fr-fr'] = "RSS Feed";
+$text['title-rss']['pt-br'] = "RSS";
+$text['title-rss']['he'] = "עידכוני מידע";
+$text['title-rss']['pl'] = "RSS Feed";
+$text['title-rss']['uk'] = "RSS Подання";
+$text['title-rss']['sv-se'] = "RSS Flöde";
 
-	Software distributed under the License is distributed on an "AS IS" basis,
-	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-	for the specific language governing rights and limitations under the
-	License.
+$text['message-update']['en-us'] = "Update Complete";
+$text['message-update']['es-cl'] = "Actualización Completada";
+$text['message-update']['pt-pt'] = "Actualização Efectuada";
+$text['message-update']['fr-fr'] = "Mis à Jour";
+$text['message-update']['pt-br'] = "Atualização Efetuada";
+$text['message-update']['he'] = "עידכון הושלם";
+$text['message-update']['pl'] = "Zaktualizowano poprawnie";
+$text['message-update']['uk'] = "Оновлення завершено";
+$text['message-update']['sv-se'] = "Uppdatering Klar";
 
-	The Original Code is FusionPBX
+$text['message-item-up']['en-us'] = "Item Moved Up";
+$text['message-item-up']['es-cl'] = "Ítem Ascendido";
+$text['message-item-up']['pt-pt'] = "Item Movido para cima";
+$text['message-item-up']['fr-fr'] = "Objet remonté";
+$text['message-item-up']['pt-br'] = "Mover Item para cima";
+$text['message-item-up']['he'] = "העלה פריט מעלה";
+$text['message-item-up']['pl'] = "Pozycja przeniesiona do góry";
+$text['message-item-up']['uk'] = "Елемент переміщено вгору";
+$text['message-item-up']['sv-se'] = "Raden Flyttades Upp";
 
-	The Initial Developer of the Original Code is
-	Mark J Crane <markjcrane@fusionpbx.com>
-	Portions created by the Initial Developer are Copyright (C) 2008-2012
-	the Initial Developer. All Rights Reserved.
+$text['message-item-down']['en-us'] = "Item Moved Down";
+$text['message-item-down']['es-cl'] = "Ítem Descendido";
+$text['message-item-down']['pt-pt'] = "Item Movido para baixo";
+$text['message-item-down']['fr-fr'] = "Objet descendu";
+$text['message-item-down']['pt-br'] = "Mover Item para baixo";
+$text['message-item-down']['he'] = "הורד פריט מטה";
+$text['message-item-down']['pl'] = "Pozycja przeniesiona w dół";
+$text['message-item-down']['uk'] = "Елемент переміщено вниз";
+$text['message-item-down']['sv-se'] = "Raden Flyttades Ned";
 
-	Contributor(s):
-	Mark J Crane <markjcrane@fusionpbx.com>
-	James Rose <james.o.rose@gmail.com>
-	Philippe Rioual <bhouba@gmail.com>
-*/
-	$text['title-rss']['en-us'] = "RSS Feed";
-	$text['title-rss']['es-cl'] = "Fuentes RSS";
-	$text['title-rss']['pt-pt'] = "RSS Feed";
-	$text['title-rss']['fr-fr'] = "RSS Feed";
+$text['message-error-missing']['en-us'] = "Error missing";
+$text['message-error-missing']['es-cl'] = "Error perdido";
+$text['message-error-missing']['pt-pt'] = "Error missing";
+$text['message-error-missing']['fr-fr'] = "Erreur missing";
+$text['message-error-missing']['pt-br'] = "Erro fatal";
+$text['message-error-missing']['he'] = "שגיאה חסרה";
+$text['message-error-missing']['pl'] = "Błąd braku ";
+$text['message-error-missing']['uk'] = "Помилка відсутня";
+$text['message-error-missing']['sv-se'] = "Fel Saknas";
 
-	$text['description-rss']['en-us'] = "Task List for RSS...";
-	$text['description-rss']['es-cl'] = "Listado de tareas para RSS...";
-	$text['description-rss']['pt-pt'] = "Lista de tarefas para RSS...";
-	$text['description-rss']['fr-fr'] = "Liste de Tâches pour RSS...";
+$text['message-description']['en-us'] = "Please provide a description.";
+$text['message-description']['es-cl'] = "Por favor ingrese una descripción.";
+$text['message-description']['pt-pt'] = "Por favor indique uma descrição.";
+$text['message-description']['fr-fr'] = "Merci d'écrire une description.";
+$text['message-description']['pt-br'] = "Por favor indique uma descrição";
+$text['message-description']['he'] = "אנא הוסף תיאור";
+$text['message-description']['pl'] = "Wprowadź opis";
+$text['message-description']['uk'] = "Будь ласка, надайте опис.";
+$text['message-description']['sv-se'] = "Ange en beskrivning.";
 
-	$text['message-add']['en-us'] = "Add Complete";
-	$text['message-add']['es-cl'] = "Agregar Completado";
-	$text['message-add']['pt-pt'] = "Adição Efectuada";
-	$text['message-add']['fr-fr'] = "ajouté";
+$text['message-delete-done']['en-us'] = "Delete Completed";
+$text['message-delete-done']['es-cl'] = "Eliminar Completado";
+$text['message-delete-done']['pt-pt'] = "Remoção Efectuada";
+$text['message-delete-done']['fr-fr'] = "Supprimé";
+$text['message-delete-done']['pt-br'] = "Remoção efetuada";
+$text['message-delete-done']['he'] = "מחיקה הושלמה";
+$text['message-delete-done']['pl'] = "Usunięto poprawnie";
+$text['message-delete-done']['uk'] = "Видалено";
+$text['message-delete-done']['sv-se'] = "Radera Utförd";
 
-	$text['button-back']['en-us'] = "Back";
-	$text['button-back']['es-cl'] = "Volver";
-	$text['button-back']['pt-pt'] = "Voltar";
-	$text['button-back']['fr-fr'] = "Retour";
+$text['message-confirm-delete']['en-us'] = "Do you really want to delete this?";
+$text['message-confirm-delete']['es-cl'] = "¿Realmente desea eliminar esto?";
+$text['message-confirm-delete']['pt-pt'] = "Deseja realmente remover isto?";
+$text['message-confirm-delete']['fr-fr'] = "Voulez-vous vraiment faire cela?";
+$text['message-confirm-delete']['pt-br'] = "Deseja realmente remover isto?";
+$text['message-confirm-delete']['he'] = "?האם אתה בטוח שברצונך למחוק זאת";
+$text['message-confirm-delete']['pl'] = "Czy na pewno chcesz to usunąć?";
+$text['message-confirm-delete']['uk'] = "Ви дійсно бажаєте це видалити?";
+$text['message-confirm-delete']['sv-se'] = "Vill du verkligen radera detta?";
 
-	$text['button-public']['en-us'] = "public";
-	$text['button-public']['es-cl'] = "público";
-	$text['button-public']['pt-pt'] = "público";
-	$text['button-public']['fr-fr'] = "public";
+$text['message-add']['en-us'] = "Add Complete";
+$text['message-add']['es-cl'] = "Agregar Completado";
+$text['message-add']['pt-pt'] = "Adição Efectuada";
+$text['message-add']['fr-fr'] = "ajouté";
+$text['message-add']['pt-br'] = "Criação efetuada";
+$text['message-add']['he'] = "הוספה הושלמה";
+$text['message-add']['pl'] = "Dodano poprawnie";
+$text['message-add']['uk'] = "Додано";
+$text['message-add']['sv-se'] = "Tillagd";
 
-	$text['label-order']['en-us'] = "Order";
-	$text['label-order']['es-cl'] = "Orden";
-	$text['label-order']['pt-pt'] = "Ordem";
-	$text['label-order']['fr-fr'] = "Ordre";
+$text['label-type']['en-us'] = "Type";
+$text['label-type']['es-cl'] = "Tipo";
+$text['label-type']['pt-pt'] = "Tipo";
+$text['label-type']['fr-fr'] = "Type";
+$text['label-type']['pt-br'] = "Tipo";
+$text['label-type']['he'] = "סוג";
+$text['label-type']['pl'] = "Typ";
+$text['label-type']['uk'] = "Тип";
+$text['label-type']['sv-se'] = "Typ";
 
-	$text['label-content']['en-us'] = "Content";
-	$text['label-content']['es-cl'] = "Contenido";
-	$text['label-content']['pt-pt'] = "Conteúdo";
-	$text['label-content']['fr-fr'] = "Contenu";
+$text['label-title']['en-us'] = "Title";
+$text['label-title']['es-cl'] = "Título";
+$text['label-title']['pt-pt'] = "Título";
+$text['label-title']['fr-fr'] = "Titre";
+$text['label-title']['pt-br'] = "Título";
+$text['label-title']['he'] = "תאר";
+$text['label-title']['pl'] = "Tytuł";
+$text['label-title']['uk'] = "Назва";
+$text['label-title']['sv-se'] = "Titel";
 
-	$text['button-add-title']['en-us'] = "Add";
-	$text['button-add-title']['es-cl'] = "Agregar";
-	$text['button-add-title']['pt-pt'] = "Adicionar";
-	$text['button-add-title']['fr-fr'] = "Ajouter";
+$text['label-template']['en-us'] = "Template";
+$text['label-template']['es-cl'] = "Plantilla";
+$text['label-template']['pt-pt'] = "Template";
+$text['label-template']['fr-fr'] = "Template";
+$text['label-template']['pt-br'] = "Template";
+$text['label-template']['he'] = "תבנית";
+$text['label-template']['pl'] = "Szablon";
+$text['label-template']['uk'] = "Шаблон";
+$text['label-template']['sv-se'] = "Mall";
 
-	$text['message-delete-done']['en-us'] = "Delete Completed";
-	$text['message-delete-done']['es-cl'] = "Eliminar Completado";
-	$text['message-delete-done']['pt-pt'] = "Remoção Efectuada";
-	$text['message-delete-done']['fr-fr'] = "Supprimé";
+$text['label-sub-title']['en-us'] = "Sub Title";
+$text['label-sub-title']['es-cl'] = "Subtítulo";
+$text['label-sub-title']['pt-pt'] = "Sub-Título";
+$text['label-sub-title']['fr-fr'] = "Sous Titre";
+$text['label-sub-title']['pt-br'] = "Sub-Titulo";
+$text['label-sub-title']['he'] = "תת תבנית";
+$text['label-sub-title']['pl'] = "Podtytuł";
+$text['label-sub-title']['uk'] = "Заголовок";
+$text['label-sub-title']['sv-se'] = "Under Titel";
 
-	$text['label-list']['en-us'] = "List";
-	$text['label-list']['es-cl'] = "Lista";
-	$text['label-list']['pt-pt'] = "Lista";
-	$text['label-list']['fr-fr'] = "Liste";
+$text['label-sub-link']['en-us'] = "Sub Link";
+$text['label-sub-link']['es-cl'] = "Sub Enlace";
+$text['label-sub-link']['pt-pt'] = "Sub-Link";
+$text['label-sub-link']['fr-fr'] = "Sub Link";
+$text['label-sub-link']['pt-br'] = "Sub-Link";
+$text['label-sub-link']['he'] = "תת קישור";
+$text['label-sub-link']['pl'] = "Sub Link ";
+$text['label-sub-link']['uk'] = "Посилання";
+$text['label-sub-link']['sv-se'] = "Under Länk";
 
-	$text['label-title']['en-us'] = "Title";
-	$text['label-title']['es-cl'] = "Título";
-	$text['label-title']['pt-pt'] = "Título";
-	$text['label-title']['fr-fr'] = "Titre";
+$text['label-sub-id']['en-us'] = "Sub ID";
+$text['label-sub-id']['es-cl'] = "Sub ID";
+$text['label-sub-id']['pt-pt'] = "Sub-ID";
+$text['label-sub-id']['fr-fr'] = "Sous ID";
+$text['label-sub-id']['pt-br'] = "Sub-ID";
+$text['label-sub-id']['he'] = "תת זיהוי";
+$text['label-sub-id']['pl'] = "Sub ID";
+$text['label-sub-id']['uk'] = "ID";
+$text['label-sub-id']['sv-se'] = "Under ID";
 
-	$text['label-link']['en-us'] = "Link";
-	$text['label-link']['es-cl'] = "Enlace";
-	$text['label-link']['pt-pt'] = "Link";
-	$text['label-link']['fr-fr'] = "Lien";
+$text['label-sub-desc']['en-us'] = "Sub Description";
+$text['label-sub-desc']['es-cl'] = "Sub Descripción";
+$text['label-sub-desc']['pt-pt'] = "Sub-descrição";
+$text['label-sub-desc']['fr-fr'] = "Sub Description";
+$text['label-sub-desc']['pt-br'] = "Sub-Descrição";
+$text['label-sub-desc']['he'] = "תת תיאור";
+$text['label-sub-desc']['pl'] = "Podopis";
+$text['label-sub-desc']['uk'] = "Опис";
+$text['label-sub-desc']['sv-se'] = "Under Beskrivning";
 
-	$text['label-group']['en-us'] = "Group";
-	$text['label-group']['es-cl'] = "Grupo";
-	$text['label-group']['pt-pt'] = "Grupo";
-	$text['label-group']['fr-fr'] = "Groupe";
+$text['label-sub-category']['en-us'] = "Sub Category";
+$text['label-sub-category']['es-cl'] = "Sub Categoría";
+$text['label-sub-category']['pt-pt'] = "Sub-Categoria";
+$text['label-sub-category']['fr-fr'] = "Sous Categorie";
+$text['label-sub-category']['pt-br'] = "Sub-Categoria";
+$text['label-sub-category']['he'] = "תת קטגוריה";
+$text['label-sub-category']['pl'] = "Podkategoria";
+$text['label-sub-category']['uk'] = "Категорія";
+$text['label-sub-category']['sv-se'] = "Under Kategori";
 
-	$text['label-order']['en-us'] = "Order";
-	$text['label-order']['es-cl'] = "Orden";
-	$text['label-order']['pt-pt'] = "Ordem";
-	$text['label-order']['fr-fr'] = "Ordre";
+$text['label-search']['en-us'] = "Search Results";
+$text['label-search']['es-cl'] = "Resultado de la búsqueda";
+$text['label-search']['pt-pt'] = "Resultados da pesquisa";
+$text['label-search']['fr-fr'] = "Resultats de la recherche";
+$text['label-search']['pt-br'] = "Resultados da pequisa";
+$text['label-search']['he'] = "תוצאות חיפוש";
+$text['label-search']['pl'] = "Wyniki wyszukiwania";
+$text['label-search']['uk'] = "Результати пошуку";
+$text['label-search']['sv-se'] = "Sökresultat";
 
-	$text['label-edit']['en-us'] = "Edit";
-	$text['label-edit']['es-cl'] = "Editar";
-	$text['label-edit']['pt-pt'] = "Editar";
-	$text['label-edit']['fr-fr'] = "Editer";
-
-	$text['message-confirm-delete']['en-us'] = "Do you really want to delete this?";
-	$text['message-confirm-delete']['es-cl'] = "¿Realmente desea eliminar esto?";
-	$text['message-confirm-delete']['pt-pt'] = "Deseja realmente remover isto?";
-	$text['message-confirm-delete']['fr-fr'] = "Voulez-vous vraiment faire cela?";
-
-	$text['message-item-down']['en-us'] = "Item Moved Down";
-	$text['message-item-down']['es-cl'] = "Ítem Descendido";
-	$text['message-item-down']['pt-pt'] = "Item Movido para baixo";
-	$text['message-item-down']['fr-fr'] = "Objet descendu";
-
-	$text['message-item-up']['en-us'] = "Item Moved Up";
-	$text['message-item-up']['es-cl'] = "Ítem Ascendido";
-	$text['message-item-up']['pt-pt'] = "Item Movido para cima";
-	$text['message-item-up']['fr-fr'] = "Objet remonté";
-
-	$text['label-search']['en-us'] = "Search Results";
-	$text['label-search']['es-cl'] = "Resultado de la búsqueda";
-	$text['label-search']['pt-pt'] = "Resultados da pesquisa";
-	$text['label-search']['fr-fr'] = "Resultats de la recherche";
-
-	$text['label-id']['en-us'] = "ID";
-	$text['label-id']['es-cl'] = "ID";
-	$text['label-id']['pt-pt'] = "ID";
-	$text['label-id']['fr-fr'] = "ID";
-
-	$text['label-category']['en-us'] = "Category";
-	$text['label-category']['es-cl'] = "Categoría";
-	$text['label-category']['pt-pt'] = "Categoria";
-	$text['label-category']['fr-fr'] = "Categorie";
-
-	$text['label-sub-category']['en-us'] = "Sub Category";
-	$text['label-sub-category']['es-cl'] = "Sub Categoría";
-	$text['label-sub-category']['pt-pt'] = "Sub-Categoria";
-	$text['label-sub-category']['fr-fr'] = "Sous Categorie";
-
-	$text['label-description']['en-us'] = "Description";
-	$text['label-description']['es-cl'] = "Descripción";
-	$text['label-description']['pt-pt'] = "Descrição";
-	$text['label-description']['fr-fr'] = "Description";
-
-	$text['button-search']['en-us'] = "Search";
-	$text['button-search']['es-cl'] = "Buscar";
-	$text['button-search']['pt-pt'] = "Pesquisa";
-	$text['button-search']['fr-fr'] = "recherche";
-
-	$text['label-rss-category']['en-us'] = "RSS Category";
-	$text['label-rss-category']['es-cl'] = "Categoría RSS";
-	$text['label-rss-category']['pt-pt'] = "Categoria RSS";
-	$text['label-rss-category']['fr-fr'] = "RSS Categorie";
-
-	$text['button-update']['en-us'] = "Update";
-	$text['button-update']['es-cl'] = "Actualizar";
-	$text['button-update']['pt-pt'] = "Actualização";
-	$text['button-update']['fr-fr'] = "Mise à Jour";
-
-	$text['message-update']['en-us'] = "Update Complete";
-	$text['message-update']['es-cl'] = "Actualización Completada";
-	$text['message-update']['pt-pt'] = "Actualização Efectuada";
-	$text['message-update']['fr-fr'] = "Mis à Jour";
-
-	$text['label-details']['en-us'] = "Details";
-	$text['label-details']['es-cl'] = "Detalles";
-	$text['label-details']['pt-pt'] = "Detalhes";
-	$text['label-details']['fr-fr'] = "Détails";
-
-	$text['label-template']['en-us'] = "Template";
-	$text['label-template']['es-cl'] = "Plantilla";
-	$text['label-template']['pt-pt'] = "Template";
-	$text['label-template']['fr-fr'] = "Template";
-
-	$text['button-delete']['en-us'] = "Delete";
-	$text['button-delete']['es-cl'] = "Eliminar";
-	$text['button-delete']['pt-pt'] = "Remover";
-	$text['button-delete']['fr-fr'] = "Supprimer";
-
-	$text['label-sub-id']['en-us'] = "Sub ID";
-	$text['label-sub-id']['es-cl'] = "Sub ID";
-	$text['label-sub-id']['pt-pt'] = "Sub-ID";
-	$text['label-sub-id']['fr-fr'] = "Sous ID";
-
-	$text['label-sub-title']['en-us'] = "Sub Title";
-	$text['label-sub-title']['es-cl'] = "Subtítulo";
-	$text['label-sub-title']['pt-pt'] = "Sub-Título";
-	$text['label-sub-title']['fr-fr'] = "Sous Titre";
-
-	$text['label-sub-link']['en-us'] = "Sub Link";
-	$text['label-sub-link']['es-cl'] = "Sub Enlace";
-	$text['label-sub-link']['pt-pt'] = "Sub-Link";
-	$text['label-sub-link']['fr-fr'] = "Sub Link";
-
-	$text['label-sub-desc']['en-us'] = "Sub Description";
-	$text['label-sub-desc']['es-cl'] = "Sub Descripción";
-	$text['label-sub-desc']['pt-pt'] = "Sub-descrição";
-	$text['label-sub-desc']['fr-fr'] = "Sub Description";
-
-	$text['message-error-missing']['en-us'] = "Error missing";
-	$text['message-error-missing']['es-cl'] = "Error perdido";
-	$text['message-error-missing']['pt-pt'] = "Error missing";
-	$text['message-error-missing']['fr-fr'] = "Erreur missing";
-
-	$text['message-description']['en-us'] = "Please provide a description.";
-	$text['message-description']['es-cl'] = "Por favor ingrese una descripción.";
-	$text['message-description']['pt-pt'] = "Por favor indique uma descrição.";
-	$text['message-description']['fr-fr'] = "Merci d'écrire une description.";
-
-	$text['label-content-edit']['en-us'] = "Content";
-	$text['label-content-edit']['es-cl'] = "Contenido";
-	$text['label-content-edit']['pt-pt'] = "Conteúdo";
-	$text['label-content-edit']['fr-fr'] = "Contenu";
-
-	$text['label-type']['en-us'] = "Type";
-	$text['label-type']['es-cl'] = "Tipo";
-	$text['label-type']['pt-pt'] = "Tipo";
-	$text['label-type']['fr-fr'] = "Type";
-
-	$text['label-on-off']['en-us'] = "on/off";
-	$text['label-on-off']['es-cl'] = "on/off";
-	$text['label-on-off']['pt-pt'] = "on/off";
-	$text['label-on-off']['fr-fr'] = "on/off";
-
-	$text['button-save']['en-us'] = "Save";
-	$text['button-save']['es-cl'] = "Guardar";
-	$text['button-save']['pt-pt'] = "Guardar";
-	$text['button-save']['fr-fr'] = "Sauvegarde";
-
-?>
- = "Categoría RSS";
+$text['label-rss-category']['en-us'] = "RSS Category";
+$text['label-rss-category']['es-cl'] = "Categoría RSS";
 $text['label-rss-category']['pt-pt'] = "Categoria RSS";
 $text['label-rss-category']['fr-fr'] = "RSS Categorie";
 $text['label-rss-category']['pt-br'] = "Categoria RSS";
