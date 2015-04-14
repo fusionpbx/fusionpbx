be1f5506d985ec68a242f11dcc1ab0ae688887cf
Nate Jones <git@reliberate.com>
2015-04-14 03:54:01 +0000

    Contacts: Fix app_menu.php, restore menu item.

--- app/contacts/app_menu.php
+++ app/contacts/app_menu.php
@@ -5,23 +5,14 @@ $apps[$x]['menu'][0]['title']['pt-br'] = "Contatos";
 $apps[$x]['menu'][0]['title']['pl'] = "Kontakty";
 $apps[$x]['menu'][0]['title']['uk'] = "Контакти";
 $apps[$x]['menu'][0]['title']['sv-se'] = "Kontakter";
-
-$apps[$x]['menu'][1]['title']['es-cl'] = "Contactos";
-
-$apps[$x]['menu'][2]['title']['fr-fr'] = "Contacts";
-
-$apps[$x]['menu'][3]['title']['pt-pt'] = "Contactos";
-
-$apps[$x]['menu'][4]['uuid'] = "f14e6ab6-6565-d4e6-cbad-a51d2e3e8ec6";
-
-$apps[$x]['menu'][5]['parent_uuid'] = "fd29e39c-c936-f5fc-8e2b-611681b266b5";
-
-$apps[$x]['menu'][6]['category'] = "internal";
-
-$apps[$x]['menu'][7]['path'] = "/app/contacts/contacts.php";
-
-$apps[$x]['menu'][8]['groups'][] = "admin";
-
-$apps[$x]['menu'][9]['groups'][] = "superadmin";
+$apps[$x]['menu'][0]['title']['es-cl'] = "Contactos";
+$apps[$x]['menu'][0]['title']['fr-fr'] = "Contacts";
+$apps[$x]['menu'][0]['title']['pt-pt'] = "Contactos";
+$apps[$x]['menu'][0]['uuid'] = "f14e6ab6-6565-d4e6-cbad-a51d2e3e8ec6";
+$apps[$x]['menu'][0]['parent_uuid'] = "fd29e39c-c936-f5fc-8e2b-611681b266b5";
+$apps[$x]['menu'][0]['category'] = "internal";
+$apps[$x]['menu'][0]['path'] = "/app/contacts/contacts.php";
+$apps[$x]['menu'][0]['groups'][] = "admin";
+$apps[$x]['menu'][0]['groups'][] = "superadmin";
 
 ?>
