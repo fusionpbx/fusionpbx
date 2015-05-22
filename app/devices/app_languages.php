2a8481888f02c6863688658f06c2bc48ff096b47
Mark Crane <markjcrane@gmail.com>
2015-05-22 04:34:31 +0000

    More work on device alternate.

--- app/devices/app_languages.php
+++ app/devices/app_languages.php
@@ -999,14 +999,14 @@ $text['label-device_label']['pl'] = "Etykieta";
 $text['label-device_label']['uk'] = "";
 $text['label-device_label']['sv-se'] = "Etikett";
 
-$text['label-device_alternate']['en-us'] = "Alternate";
-$text['label-device_alternate']['es-cl'] = "Suplente";
-$text['label-device_alternate']['pt-pt'] = "Alternado";
-$text['label-device_alternate']['fr-fr'] = "Alterné";
-$text['label-device_alternate']['pt-br'] = "Alternado";
-$text['label-device_alternate']['pl'] = "Alternatywny";
-$text['label-device_alternate']['uk'] = "заступник";
-$text['label-device_alternate']['sv-se'] = "Alternate";
+$text['label-device_uuid_alternate']['en-us'] = "Alternate";
+$text['label-device_uuid_alternate']['es-cl'] = "Suplente";
+$text['label-device_uuid_alternate']['pt-pt'] = "Alternado";
+$text['label-device_uuid_alternate']['fr-fr'] = "Alterné";
+$text['label-device_uuid_alternate']['pt-br'] = "Alternado";
+$text['label-device_uuid_alternate']['pl'] = "Alternatywny";
+$text['label-device_uuid_alternate']['uk'] = "заступник";
+$text['label-device_uuid_alternate']['sv-se'] = "Alternate";
 
 $text['label-device_key_value']['en-us'] = "Value";
 $text['label-device_key_value']['es-cl'] = "Valor";
