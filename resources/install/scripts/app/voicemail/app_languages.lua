6eb4e7ab6e8a458638bcda9885814968aa4f7b73
Mark Crane <markjcrane@gmail.com>
2015-05-29 02:55:25 +0000

    Change the file from ANSI to UTF-8 no BOM.

--- resources/install/scripts/app/voicemail/app_languages.lua
+++ resources/install/scripts/app/voicemail/app_languages.lua
@@ -4,16 +4,16 @@ text['label-download'] = {};
 text['label-download']['en-us'] = "Download";
 text['label-download']['es-cl'] = "Descargar";
 text['label-download']['pt-pt'] = "Baixar";
-text['label-download']['fr-fr'] = "Télécharger";
+text['label-download']['fr-fr'] = "TÃ©lÃ©charger";
 
 text['label-listen'] = {};
 text['label-listen']['en-us'] = "Listen";
 text['label-listen']['es-cl'] = "Escuchar";
 text['label-listen']['pt-pt'] = "Ouvir";
-text['label-listen']['fr-fr'] = "écouter";
+text['label-listen']['fr-fr'] = "Ã‰couter";
 
 text['label-attached'] = {};
 text['label-attached']['en-us'] = "Attached";
 text['label-attached']['es-cl'] = "Adjunto";
 text['label-attached']['pt-pt'] = "Ligado";
-text['label-attached']['fr-fr'] = "Attaché";
+text['label-attached']['fr-fr'] = "AttachÃ©";
