c5b04b94d96d11e98f4bcedb54738f3678212131
markjcrane <markjcrane@gmail.com>
2015-07-05 23:13:46 -0600

    Add AR eg translation.

--- app/destinations/app_menu.php
+++ app/destinations/app_menu.php
@@ -12,6 +12,7 @@ $apps[$x]['menu'][0]['title']['sv-se'] = "Destinationer";
 $apps[$x]['menu'][0]['title']['uk'] = "Номери";
 $apps[$x]['menu'][0]['title']['ro'] = "Destinații";
 $apps[$x]['menu'][0]['title']['de-at'] = "Kopfnummern";
+$apps[$x]['menu'][0]['title']['ar-eg'] = "جهات الأتصال";
 $apps[$x]['menu'][0]['uuid'] = "fd2a708a-ff03-c707-c19d-5a4194375eba";
 $apps[$x]['menu'][0]['parent_uuid'] = "b94e8bd9-9eb5-e427-9c26-ff7a6c21552a";
 $apps[$x]['menu'][0]['category'] = "internal";
--- app/extensions/app_menu.php
+++ app/extensions/app_menu.php
@@ -12,6 +12,7 @@ $apps[$x]['menu'][0]['title']['uk'] = "Розширення";
 $apps[$x]['menu'][0]['title']['sv-se'] = "Anknytningar";
 $apps[$x]['menu'][0]['title']['ro'] = "Extensii";
 $apps[$x]['menu'][0]['title']['de-at'] = "Nebenstellen";
+$apps[$x]['menu'][0]['title']['ar-eg'] = "الأرقام الداخلية";
 $apps[$x]['menu'][0]['uuid'] = "d3036a99-9a9f-2ad6-a82a-1fe7bebbe2d3";
 $apps[$x]['menu'][0]['parent_uuid'] = "bc96d773-ee57-0cdd-c3ac-2d91aba61b55";
 $apps[$x]['menu'][0]['category'] = "internal";
