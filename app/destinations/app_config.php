0719fe2465575b0f0df01f35715472c9c145bbe4
markjcrane <markjcrane@gmail.com>
2015-08-06 23:21:10 -0500

    Add a missing 'and' in the SQL query.

--- app/destinations/app_config.php
+++ app/destinations/app_config.php
@@ -26,7 +26,7 @@
 		$apps[$x]['destinations'][$y]['label'] = "destinations";
 		$apps[$x]['destinations'][$y]['name'] = "destinations";
 		$apps[$x]['destinations'][$y]['sql'] = "select destination_number as destination, destination_description as description from v_destinations ";
-		$apps[$x]['destinations'][$y]['where'] = "where domain_uuid = '\${domain_uuid}' and destination_type = 'outbound' destination_enabled = 'true' ";
+		$apps[$x]['destinations'][$y]['where'] = "where domain_uuid = '\${domain_uuid}' and destination_type = 'outbound' and destination_enabled = 'true' ";
 		$apps[$x]['destinations'][$y]['order_by'] = "destination_number asc";
 		$apps[$x]['destinations'][$y]['field']['uuid'] = "destination_uuid";
 		$apps[$x]['destinations'][$y]['field']['context'] = "destination_context";
