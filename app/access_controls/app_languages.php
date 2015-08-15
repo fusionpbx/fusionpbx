c80a1b850e5685376e8e7b96a8069d4aebbe94e2
markjcrane <markjcrane@gmail.com>
2015-08-15 23:58:26 +0000

    Add latest translation for Access Controls

--- app/access_controls/app_languages.php
+++ app/access_controls/app_languages.php
@@ -1,245 +1,213 @@
 <?php
 
-	//Access Controls
-		$text['title-access_controls']['en-us'] = 'Access Controls';
-		$text['title-access_controls']['es-cl'] = '';
-		$text['title-access_controls']['pt-pt'] = '';
-		$text['title-access_controls']['fr-fr'] = '';
+$text['title-access_controls']['en-us'] = "Access Controls";
+$text['title-access_controls']['es-cl'] = "";
+$text['title-access_controls']['pt-pt'] = "Controles de Acesso";
+$text['title-access_controls']['fr-fr'] = "Contrôle d'accès";
+$text['title-access_controls']['pt-br'] = "Controles de Acesso";
 
-		$text['title-access_control']['en-us'] = 'Access Control';
-		$text['title-access_control']['es-cl'] = '';
-		$text['title-access_control']['pt-pt'] = '';
-		$text['title-access_control']['fr-fr'] = '';
+$text['title-access_control_nodes']['en-us'] = "Nodes";
+$text['title-access_control_nodes']['es-cl'] = "";
+$text['title-access_control_nodes']['pt-pt'] = " Nodes";
+$text['title-access_control_nodes']['fr-fr'] = " nœuds";
+$text['title-access_control_nodes']['pt-br'] = "Nodes";
 
-		$text['description-access_control']['en-us'] = 'Access control list can allow or deny ranges of IP addresses.';
-		$text['description-access_control']['es-cl'] = '';
-		$text['description-access_control']['pt-pt'] = '';
-		$text['description-access_control']['fr-fr'] = '';
+$text['title-access_control_node']['en-us'] = "Node";
+$text['title-access_control_node']['es-cl'] = "";
+$text['title-access_control_node']['pt-pt'] = "Nó";
+$text['title-access_control_node']['fr-fr'] = " noeud";
+$text['title-access_control_node']['pt-br'] = "Nó";
 
-		$text['label-access_control_name']['en-us'] = 'Name';
-		$text['label-access_control_name']['es-cl'] = '';
-		$text['label-access_control_name']['pt-pt'] = '';
-		$text['label-access_control_name']['fr-fr'] = '';
+$text['title-access_control']['en-us'] = "Access Control";
+$text['title-access_control']['es-cl'] = "";
+$text['title-access_control']['pt-pt'] = "Controle de acesso";
+$text['title-access_control']['fr-fr'] = "Contrôle d'accès";
+$text['title-access_control']['pt-br'] = "Controle de acesso";
 
-		$text['description-access_control_name']['en-us'] = 'Enter the name.';
-		$text['description-access_control_name']['es-cl'] = '';
-		$text['description-access_control_name']['pt-pt'] = '';
-		$text['description-access_control_name']['fr-fr'] = '';
+$text['message-update']['en-us'] = "Update Completed";
+$text['message-update']['es-cl'] = "";
+$text['message-update']['pt-pt'] = "Actualização Efectuada";
+$text['message-update']['fr-fr'] = "Mis à jour";
+$text['message-update']['pt-br'] = "Atualização Efetuada";
 
-		$text['label-access_control_default']['en-us'] = 'Default';
-		$text['label-access_control_default']['es-cl'] = '';
-		$text['label-access_control_default']['pt-pt'] = '';
-		$text['label-access_control_default']['fr-fr'] = '';
+$text['message-required']['en-us'] = "Please provide: ";
+$text['message-required']['es-cl'] = "";
+$text['message-required']['pt-pt'] = "Por favor indique:";
+$text['message-required']['fr-fr'] = "Merci d'indiquer:";
+$text['message-required']['pt-br'] = "Por favor Indique:";
 
-		$text['description-access_control_default']['en-us'] = 'Select the default type.';
-		$text['description-access_control_default']['es-cl'] = '';
-		$text['description-access_control_default']['pt-pt'] = '';
-		$text['description-access_control_default']['fr-fr'] = '';
+$text['message-delete']['en-us'] = "Delete Completed";
+$text['message-delete']['es-cl'] = "";
+$text['message-delete']['pt-pt'] = "Remoção Efectuada";
+$text['message-delete']['fr-fr'] = "Supprimé";
+$text['message-delete']['pt-br'] = "Remoção Efetuada";
 
-		$text['label-access_control_description']['en-us'] = 'Description';
-		$text['label-access_control_description']['es-cl'] = '';
-		$text['label-access_control_description']['pt-pt'] = '';
-		$text['label-access_control_description']['fr-fr'] = '';
+$text['message-add']['en-us'] = "Add Completed";
+$text['message-add']['es-cl'] = "";
+$text['message-add']['pt-pt'] = "Adição Efectuada";
+$text['message-add']['fr-fr'] = "Ajouté";
+$text['message-add']['pt-br'] = "Criação Efetuada";
 
-		$text['description-access_control_description']['en-us'] = 'Enter the description';
-		$text['description-access_control_description']['es-cl'] = '';
-		$text['description-access_control_description']['pt-pt'] = '';
-		$text['description-access_control_description']['fr-fr'] = '';
+$text['label-true']['en-us'] = "true";
+$text['label-true']['es-cl'] = "";
+$text['label-true']['pt-pt'] = "Sim";
+$text['label-true']['fr-fr'] = "Oui";
+$text['label-true']['pt-br'] = "Sim";
 
-		$text['label-allow']['en-us'] = 'allow';
-		$text['label-allow']['es-cl'] = '';
-		$text['label-allow']['pt-pt'] = '';
-		$text['label-allow']['fr-fr'] = '';
+$text['label-node_type']['en-us'] = "Type";
+$text['label-node_type']['es-cl'] = "";
+$text['label-node_type']['pt-pt'] = "Tipo";
+$text['label-node_type']['fr-fr'] = "Type";
+$text['label-node_type']['pt-br'] = "Tipo";
 
-		$text['label-deny']['en-us'] = 'deny';
-		$text['label-deny']['es-cl'] = '';
-		$text['label-deny']['pt-pt'] = '';
-		$text['label-deny']['fr-fr'] = '';
+$text['label-node_domain']['en-us'] = "Domain";
+$text['label-node_domain']['es-cl'] = "";
+$text['label-node_domain']['pt-pt'] = "Domínio";
+$text['label-node_domain']['fr-fr'] = "Domaine";
+$text['label-node_domain']['pt-br'] = "Domínio";
 
-		$text['label-true']['en-us'] = 'true';
-		$text['label-true']['es-cl'] = '';
-		$text['label-true']['pt-pt'] = '';
-		$text['label-true']['fr-fr'] = '';
+$text['label-node_description']['en-us'] = "Description";
+$text['label-node_description']['es-cl'] = "";
+$text['label-node_description']['pt-pt'] = "Descrição";
+$text['label-node_description']['fr-fr'] = "Description";
+$text['label-node_description']['pt-br'] = "Descrição";
 
-		$text['label-false']['en-us'] = 'false';
-		$text['label-false']['es-cl'] = 'falso';
-		$text['label-false']['pt-pt'] = 'falso';
-		$text['label-false']['fr-fr'] = 'falso';
+$text['label-node_cidr']['en-us'] = "CIDR";
+$text['label-node_cidr']['es-cl'] = "";
+$text['label-node_cidr']['pt-pt'] = "CIDR";
+$text['label-node_cidr']['fr-fr'] = "CIDR";
+$text['label-node_cidr']['pt-br'] = "CIDR";
 
-		$text['button-add']['en-us'] = 'Add';
-		$text['button-add']['es-cl'] = '';
-		$text['button-add']['pt-pt'] = '';
-		$text['button-add']['fr-fr'] = '';
+$text['label-false']['en-us'] = "false";
+$text['label-false']['es-cl'] = "falso";
+$text['label-false']['pt-pt'] = "falso";
+$text['label-false']['fr-fr'] = "falso";
+$text['label-false']['pt-br'] = "Não";
 
-		$text['button-edit']['en-us'] = 'Edit';
-		$text['button-edit']['es-cl'] = '';
-		$text['button-edit']['pt-pt'] = '';
-		$text['button-edit']['fr-fr'] = '';
+$text['label-deny']['en-us'] = "deny";
+$text['label-deny']['es-cl'] = "";
+$text['label-deny']['pt-pt'] = "negar";
+$text['label-deny']['fr-fr'] = " nier";
+$text['label-deny']['pt-br'] = "Negar";
 
-		$text['button-delete']['en-us'] = 'Delete';
-		$text['button-delete']['es-cl'] = '';
-		$text['button-delete']['pt-pt'] = '';
-		$text['button-delete']['fr-fr'] = '';
+$text['label-allow']['en-us'] = "allow";
+$text['label-allow']['es-cl'] = "";
+$text['label-allow']['pt-pt'] = "permitir";
+$text['label-allow']['fr-fr'] = " permettre";
+$text['label-allow']['pt-br'] = "permitir";
 
-		$text['button-save']['en-us'] = 'Save';
-		$text['button-save']['es-cl'] = '';
-		$text['button-save']['pt-pt'] = 'Guardar';
-		$text['button-save']['fr-fr'] = '';
+$text['label-access_control_name']['en-us'] = "Name";
+$text['label-access_control_name']['es-cl'] = "";
+$text['label-access_control_name']['pt-pt'] = "Nome";
+$text['label-access_control_name']['fr-fr'] = "Nom";
+$text['label-access_control_name']['pt-br'] = "Nome";
 
-		$text['button-view']['en-us'] = 'View';
-		$text['button-view']['es-cl'] = '';
-		$text['button-view']['pt-pt'] = '';
-		$text['button-view']['fr-fr'] = '';
+$text['label-access_control_description']['en-us'] = "Description";
+$text['label-access_control_description']['es-cl'] = "";
+$text['label-access_control_description']['pt-pt'] = "Descrição";
+$text['label-access_control_description']['fr-fr'] = "Description";
+$text['label-access_control_description']['pt-br'] = "Descrição";
 
-		$text['button-back']['en-us'] = 'Back';
-		$text['button-back']['es-cl'] = '';
-		$text['button-back']['pt-pt'] = 'Voltar';
-		$text['button-back']['fr-fr'] = '';
+$text['label-access_control_default']['en-us'] = "Default";
+$text['label-access_control_default']['es-cl'] = "";
+$text['label-access_control_default']['pt-pt'] = "Padrão";
+$text['label-access_control_default']['fr-fr'] = "Par défaut";
+$text['label-access_control_default']['pt-br'] = "Padrão";
 
-		$text['confirm-delete']['en-us'] = 'Do you really want to delete this?';
-		$text['confirm-delete']['es-cl'] = '';
-		$text['confirm-delete']['pt-pt'] = '';
-		$text['confirm-delete']['fr-fr'] = '';
+$text['description-node_type']['en-us'] = "Select the type.";
+$text['description-node_type']['es-cl'] = "";
+$text['description-node_type']['pt-pt'] = "Selecione o tipo.";
+$text['description-node_type']['fr-fr'] = " Sélectionnez le type";
+$text['description-node_type']['pt-br'] = "Selecione o tipo.";
 
-		$text['message-add']['en-us'] = 'Add Completed';
-		$text['message-add']['es-cl'] = '';
-		$text['message-add']['pt-pt'] = '';
-		$text['message-add']['fr-fr'] = '';
+$text['description-node_domain']['en-us'] = "Enter the domain name.";
+$text['description-node_domain']['es-cl'] = "";
+$text['description-node_domain']['pt-pt'] = "Digite o nome de domínio.";
+$text['description-node_domain']['fr-fr'] = "Entrez le nom de domaine.";
+$text['description-node_domain']['pt-br'] = "Digite o nome de domínio.";
 
-		$text['message-update']['en-us'] = 'Update Completed';
-		$text['message-update']['es-cl'] = '';
-		$text['message-update']['pt-pt'] = '';
-		$text['message-update']['fr-fr'] = '';
+$text['description-node_description']['en-us'] = "Enter the description.";
+$text['description-node_description']['es-cl'] = "";
+$text['description-node_description']['pt-pt'] = " Digite a descrição.";
+$text['description-node_description']['fr-fr'] = "Entrez la description.";
+$text['description-node_description']['pt-br'] = "Digite a descrição.";
 
-		$text['message-delete']['en-us'] = 'Delete Completed';
-		$text['message-delete']['es-cl'] = '';
-		$text['message-delete']['pt-pt'] = '';
-		$text['message-delete']['fr-fr'] = '';
+$text['description-node_cidr']['en-us'] = "Enter the IP CIDR range.";
+$text['description-node_cidr']['es-cl'] = "";
+$text['description-node_cidr']['pt-pt'] = "Digite o intervalo IP CIDR.";
+$text['description-node_cidr']['fr-fr'] = "Entrez la plage IP CIDR.";
+$text['description-node_cidr']['pt-br'] = "Digite o intervalo IP CIDR.";
 
-		$text['message-required']['en-us'] = 'Please provide: ';
-		$text['message-required']['es-cl'] = '';
-		$text['message-required']['pt-pt'] = '';
-		$text['message-required']['fr-fr'] = '';
+$text['description-access_control_node']['en-us'] = "";
+$text['description-access_control_node']['es-cl'] = "";
+$text['description-access_control_node']['pt-pt'] = "";
+$text['description-access_control_node']['fr-fr'] = "";
+$text['description-access_control_node']['pt-br'] = "";
 
-//Nodes
-		$text['title-access_control_nodes']['en-us'] = 'Nodes';
-		$text['title-access_control_nodes']['es-cl'] = '';
-		$text['title-access_control_nodes']['pt-pt'] = '';
-		$text['title-access_control_nodes']['fr-fr'] = '';
+$text['description-access_control_name']['en-us'] = "Enter the name.";
+$text['description-access_control_name']['es-cl'] = "";
+$text['description-access_control_name']['pt-pt'] = "Digite o nome.";
+$text['description-access_control_name']['fr-fr'] = "Entrez le nom.";
+$text['description-access_control_name']['pt-br'] = "Digite o nome.";
 
-		$text['title-access_control_node']['en-us'] = 'Node';
-		$text['title-access_control_node']['es-cl'] = '';
-		$text['title-access_control_node']['pt-pt'] = '';
-		$text['title-access_control_node']['fr-fr'] = '';
+$text['description-access_control_description']['en-us'] = "Enter the description";
+$text['description-access_control_description']['es-cl'] = "";
+$text['description-access_control_description']['pt-pt'] = "Digite a descrição";
+$text['description-access_control_description']['fr-fr'] = "Entrez la description.";
+$text['description-access_control_description']['pt-br'] = "Digite a descrição";
 
-		$text['description-access_control_node']['en-us'] = '';
-		$text['description-access_control_node']['es-cl'] = '';
-		$text['description-access_control_node']['pt-pt'] = '';
-		$text['description-access_control_node']['fr-fr'] = '';
+$text['description-access_control_default']['en-us'] = "Select the default type.";
+$text['description-access_control_default']['es-cl'] = "";
+$text['description-access_control_default']['pt-pt'] = "Selecione o tipo de padrão.";
+$text['description-access_control_default']['fr-fr'] = "Sélectionnez le type de défaut";
+$text['description-access_control_default']['pt-br'] = "Selecione o tipo de padrão.";
 
-		$text['label-node_type']['en-us'] = 'Type';
-		$text['label-node_type']['es-cl'] = '';
-		$text['label-node_type']['pt-pt'] = '';
-		$text['label-node_type']['fr-fr'] = '';
+$text['description-access_control']['en-us'] = "Access control list can allow or deny ranges of IP addresses.";
+$text['description-access_control']['es-cl'] = "";
+$text['description-access_control']['pt-pt'] = "Lista de controle de acesso pode permitir ou negar intervalos de endereços IP.";
+$text['description-access_control']['fr-fr'] = " Liste de contrôle d'accès peut autoriser ou refuser des plages d'adresses IP.";
+$text['description-access_control']['pt-br'] = "Lista de controle de acesso pode permitir ou negar intervalos de endereços IP.";
 
-		$text['description-node_type']['en-us'] = 'Select the type.';
-		$text['description-node_type']['es-cl'] = '';
-		$text['description-node_type']['pt-pt'] = '';
-		$text['description-node_type']['fr-fr'] = '';
+$text['confirm-delete']['en-us'] = "Do you really want to delete this?";
+$text['confirm-delete']['es-cl'] = "";
+$text['confirm-delete']['pt-pt'] = "Deseja realmente remover isto?";
+$text['confirm-delete']['fr-fr'] = "Voulez-vous vraiment supprimer cela?";
+$text['confirm-delete']['pt-br'] = "Deseja realmente remover isto?";
 
-		$text['label-node_cidr']['en-us'] = 'CIDR';
-		$text['label-node_cidr']['es-cl'] = '';
-		$text['label-node_cidr']['pt-pt'] = '';
-		$text['label-node_cidr']['fr-fr'] = '';
+$text['button-view']['en-us'] = "View";
+$text['button-view']['es-cl'] = "";
+$text['button-view']['pt-pt'] = "Vista";
+$text['button-view']['fr-fr'] = "Voir";
+$text['button-view']['pt-br'] = "Visualizar";
 
-		$text['description-node_cidr']['en-us'] = 'Enter the IP CIDR range.';
-		$text['description-node_cidr']['es-cl'] = '';
-		$text['description-node_cidr']['pt-pt'] = '';
-		$text['description-node_cidr']['fr-fr'] = '';
+$text['button-save']['en-us'] = "Save";
+$text['button-save']['es-cl'] = "";
+$text['button-save']['pt-pt'] = "Guardar";
+$text['button-save']['fr-fr'] = "Sauvegarder";
+$text['button-save']['pt-br'] = "Salvar";
 
-		$text['label-node_domain']['en-us'] = 'Domain';
-		$text['label-node_domain']['es-cl'] = '';
-		$text['label-node_domain']['pt-pt'] = '';
-		$text['label-node_domain']['fr-fr'] = '';
+$text['button-edit']['en-us'] = "Edit";
+$text['button-edit']['es-cl'] = "";
+$text['button-edit']['pt-pt'] = "Editar";
+$text['button-edit']['fr-fr'] = "Editer";
+$text['button-edit']['pt-br'] = "Editar";
 
-		$text['description-node_domain']['en-us'] = 'Enter the domain name.';
-		$text['description-node_domain']['es-cl'] = '';
-		$text['description-node_domain']['pt-pt'] = '';
-		$text['description-node_domain']['fr-fr'] = '';
+$text['button-delete']['en-us'] = "Delete";
+$text['button-delete']['es-cl'] = "";
+$text['button-delete']['pt-pt'] = "Remover";
+$text['button-delete']['fr-fr'] = "Supprimer";
+$text['button-delete']['pt-br'] = "Remover";
 
-		$text['label-node_description']['en-us'] = 'Description';
-		$text['label-node_description']['es-cl'] = '';
-		$text['label-node_description']['pt-pt'] = '';
-		$text['label-node_description']['fr-fr'] = '';
+$text['button-back']['en-us'] = "Back";
+$text['button-back']['es-cl'] = "";
+$text['button-back']['pt-pt'] = "Voltar";
+$text['button-back']['fr-fr'] = "Retour";
+$text['button-back']['pt-br'] = "Voltar";
 
-		$text['description-node_description']['en-us'] = 'Enter the description.';
-		$text['description-node_description']['es-cl'] = '';
-		$text['description-node_description']['pt-pt'] = '';
-		$text['description-node_description']['fr-fr'] = '';
-
-		$text['label-true']['en-us'] = 'true';
-		$text['label-true']['es-cl'] = '';
-		$text['label-true']['pt-pt'] = '';
-		$text['label-true']['fr-fr'] = '';
-
-		$text['label-false']['en-us'] = 'false';
-		$text['label-false']['es-cl'] = 'falso';
-		$text['label-false']['pt-pt'] = 'falso';
-		$text['label-false']['fr-fr'] = 'falso';
-
-		$text['button-add']['en-us'] = 'Add';
-		$text['button-add']['es-cl'] = '';
-		$text['button-add']['pt-pt'] = '';
-		$text['button-add']['fr-fr'] = '';
-
-		$text['button-edit']['en-us'] = 'Edit';
-		$text['button-edit']['es-cl'] = '';
-		$text['button-edit']['pt-pt'] = '';
-		$text['button-edit']['fr-fr'] = '';
-
-		$text['button-delete']['en-us'] = 'Delete';
-		$text['button-delete']['es-cl'] = '';
-		$text['button-delete']['pt-pt'] = '';
-		$text['button-delete']['fr-fr'] = '';
-
-		$text['button-save']['en-us'] = 'Save';
-		$text['button-save']['es-cl'] = '';
-		$text['button-save']['pt-pt'] = 'Guardar';
-		$text['button-save']['fr-fr'] = '';
-
-		$text['button-view']['en-us'] = 'View';
-		$text['button-view']['es-cl'] = '';
-		$text['button-view']['pt-pt'] = '';
-		$text['button-view']['fr-fr'] = '';
-
-		$text['button-back']['en-us'] = 'Back';
-		$text['button-back']['es-cl'] = '';
-		$text['button-back']['pt-pt'] = 'Voltar';
-		$text['button-back']['fr-fr'] = '';
-
-		$text['confirm-delete']['en-us'] = 'Do you really want to delete this?';
-		$text['confirm-delete']['es-cl'] = '';
-		$text['confirm-delete']['pt-pt'] = '';
-		$text['confirm-delete']['fr-fr'] = '';
-
-		$text['message-add']['en-us'] = 'Add Completed';
-		$text['message-add']['es-cl'] = '';
-		$text['message-add']['pt-pt'] = '';
-		$text['message-add']['fr-fr'] = '';
-
-		$text['message-update']['en-us'] = 'Update Completed';
-		$text['message-update']['es-cl'] = '';
-		$text['message-update']['pt-pt'] = '';
-		$text['message-update']['fr-fr'] = '';
-
-		$text['message-delete']['en-us'] = 'Delete Completed';
-		$text['message-delete']['es-cl'] = '';
-		$text['message-delete']['pt-pt'] = '';
-		$text['message-delete']['fr-fr'] = '';
-
-		$text['message-required']['en-us'] = 'Please provide: ';
-		$text['message-required']['es-cl'] = '';
-		$text['message-required']['pt-pt'] = '';
-		$text['message-required']['fr-fr'] = '';
+$text['button-add']['en-us'] = "Add";
+$text['button-add']['es-cl'] = "";
+$text['button-add']['pt-pt'] = "Adicionar";
+$text['button-add']['fr-fr'] = "Ajouter";
+$text['button-add']['pt-br'] = "Adicionar";
 
 ?>
