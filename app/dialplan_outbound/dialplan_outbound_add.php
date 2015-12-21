5e08c98840f18405ac20084e12925123cc603c04
markjcrane <markjcrane@gmail.com>
2015-12-21 03:02:52 -0700

    Reduce the size of the SIP header by removing the sip_h_X-Tag if its needed the administrator can add it.

--- app/dialplan_outbound/dialplan_outbound_add.php
+++ app/dialplan_outbound/dialplan_outbound_add.php
@@ -401,13 +401,6 @@ else {
 							$dialplan_detail_group = '0';
 							dialplan_detail_add($_SESSION['domain_uuid'], $dialplan_uuid, $dialplan_detail_tag, $dialplan_detail_order, $dialplan_detail_group, $dialplan_detail_type, $dialplan_detail_data);
 						}
-
-						$dialplan_detail_tag = 'action'; //condition, action, antiaction
-						$dialplan_detail_type = 'set';
-						$dialplan_detail_data = 'sip_h_X-Tag=';
-						$dialplan_detail_order = '015';
-						$dialplan_detail_group = '0';
-						dialplan_detail_add($_SESSION['domain_uuid'], $dialplan_uuid, $dialplan_detail_tag, $dialplan_detail_order, $dialplan_detail_group, $dialplan_detail_type, $dialplan_detail_data);
 					}
 
 					$dialplan_detail_tag = 'action'; //condition, action, antiaction
