88e6d0599cfdc57281deec4479434868804d004f
markjcrane <markjcrane@gmail.com>
2016-02-22 14:28:45 -0700

    Add round brackets around the condition destination_number for inbound routes generated by destinations.

--- app/destinations/destination_edit.php
+++ app/destinations/destination_edit.php
@@ -221,7 +221,7 @@ if (count($_POST) > 0 && strlen($_POST["persistformvar"]) == 0) {
 							$dialplan["dialplan_details"][$y]["domain_uuid"] = $domain_uuid;
 							$dialplan["dialplan_details"][$y]["dialplan_detail_tag"] = "condition";
 							$dialplan["dialplan_details"][$y]["dialplan_detail_type"] = "destination_number";
-							$dialplan["dialplan_details"][$y]["dialplan_detail_data"] = $regex_destination_number;
+							$dialplan["dialplan_details"][$y]["dialplan_detail_data"] = "(".$regex_destination_number.")";
 							$dialplan["dialplan_details"][$y]["dialplan_detail_order"] = $dialplan_detail_order;
 							$y++;
 
