64dcd97d23beed1774ddc9b3e60f9e18ebca7418
dis45927 <mjbemail2000-github@yahoo.co.uk>
2016-03-15 10:30:33 +0000

    Fix bug preventing mysql database creation

--- core/install/resources/classes/install_fusionpbx.php
+++ core/install/resources/classes/install_fusionpbx.php
@@ -426,10 +426,10 @@ include "root.php";
 					}
 
 				//create the table, user and set the permissions only if the db_create_username was provided
-					if ($this->global_settings->db_create()) {
+					if ($this->global_settings->db_create_username()) {
 						$this->write_progress("\tCreating database");
 						try {
-							$this->dbh = new PDO($connect_string, $this->global_settings->db_create_username(), db_create_password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+							$this->dbh = new PDO($connect_string, $this->global_settings->db_create_username(), $this->global_settings->db_create_password(), array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 							$this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 							$this->dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
 						}
