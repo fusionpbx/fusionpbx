4ce2bb5c8f37c547d6383d4cee0d360a9a5d97f9
FusionPBX <markjcrane@gmail.com>
2016-03-25 01:00:43 -0600

    Update acl.conf.lua
    Change expire acl.conf to acl_conf

--- resources/install/scripts/app/xml_handler/resources/scripts/configuration/acl.conf.lua
+++ resources/install/scripts/app/xml_handler/resources/scripts/configuration/acl.conf.lua
@@ -103,7 +103,7 @@
 			dbh:release();
 
 		--set the cache
-			result = trim(api:execute("memcache", "set configuration:acl.conf '"..XML_STRING:gsub("'", "&#39;").."' "..expire["acl.conf"]));
+			result = trim(api:execute("memcache", "set configuration:acl.conf '"..XML_STRING:gsub("'", "&#39;").."' "..expire["acl_conf"]));
 
 		--send the xml to the console
 			if (debug["xml_string"]) then
