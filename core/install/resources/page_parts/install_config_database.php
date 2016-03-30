b9f2e358afd025116eb37efe7e280f3b93c5c55c
Alexey Melnichuk <mimir@newmail.ru>
2016-03-30 15:05:25 +0300

    Fix. Install with create new database for PgSQL/MySQL
    * MySQL/PgSQL ignore error while create new user because user may already exists.
    * MySQL on database page use same checkbox like one for PgSQL
    * MySQL/PgSQL if create user empty try use db user.(may be better use root/postgres without pass)
    * PgSQL fix `GRANT ALL ON DATABASE dbname` instead of `GRANT ALL ON dbname`
    * MySQL fix connect string build
    * MySQL fix use correct password to connect

--- core/install/resources/page_parts/install_config_database.php
+++ core/install/resources/page_parts/install_config_database.php
@@ -142,15 +142,9 @@
 		echo "		Create Database Options\n";
 		echo "</td>\n";
 		echo "<td class='vtable' align='left'>\n";
-		echo "	<label class='radio'><input type='radio' name='create_db_option' value='none'";
-		if($db_create_option=='none') { echo " checked='checked'"; }
-		echo "/>Do not create database</label>\n";
-		echo "	<label class='radio'><input type='radio' name='create_db_option' value='same'";
-		if($db_create_option=='same') { echo " checked='checked'"; }
-		echo "/>Create database using above username/password</label>\n";
-		echo "	<label class='radio'><input type='radio' name='create_db_option' value='user'";
-		if($db_create_option=='user') { echo " checked='checked'"; }
-		echo "/>Create database using below username/password</label>\n";
+		if($db_create=='1') { $checked = "checked='checked'"; } else { $checked = ''; }
+		echo "	<input type='checkbox' name='db_create' value='1' $checked />&nbsp;";
+		echo "Create the database\n";
 		echo "<br />\n";
 		echo "Choose whether to create the database\n";
 		echo "</td>\n";
