3b73e0c1d1c0094643782ecd69b6bb8c70a64c97
FusionPBX <markjcrane@gmail.com>
2016-04-22 13:43:02 -0600

    Update user_dashboard.php
    Add an inline if statement to the user dashboard.

--- core/user_settings/user_dashboard.php
+++ core/user_settings/user_dashboard.php
@@ -957,7 +957,7 @@
 
 			//pbx version
 				$hud[$n]['html'] .= "<tr class='tr_link_void'>\n";
-				$hud[$n]['html'] .= "<td valign='top' class='".$row_style[$c]." hud_text'>FusionPBX</td>\n";
+				$hud[$n]['html'] .= "<td valign='top' class='".$row_style[$c]." hud_text'>".(isset($_SESSION['theme']['title']['text'])?$_SESSION['theme']['title']['text']:'FusionPBX')."</td>\n";
 				$hud[$n]['html'] .= "<td valign='top' class='".$row_style[$c]." hud_text' style='text-align: right;'>".software_version()."</td>\n";
 				$hud[$n]['html'] .= "</tr>\n";
 				$c = ($c) ? 0 : 1;
