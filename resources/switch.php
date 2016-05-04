d8549206705d0b5e4b50268a534749e0b0be5276
Rony Shtamler <rony.shtamler@gmail.com>
2016-05-04 11:01:26 +0200

    Add exec-set to vars.xml

--- resources/switch.php
+++ resources/switch.php
@@ -524,13 +524,15 @@ function save_var_xml() {
 					$xml .= "<!-- ".base64_decode($row['var_description'])." -->\n";
 				}
 			}
-			if (strlen($row['var_hostname']) == 0 && $row['var_cat'] =='Exec-Set') {
-				$xml .= "<X-PRE-PROCESS cmd=\"exec-set\" data=\"".$row['var_name']."=".$row['var_value']."\"/>\n";
-			} elseif (strlen($row['var_hostname']) == 0) {
-				$xml .= "<X-PRE-PROCESS cmd=\"set\" data=\"".$row['var_name']."=".$row['var_value']."\"/>\n";
-			} elseif ($row['var_hostname'] == $hostname) {
-				$xml .= "<X-PRE-PROCESS cmd=\"set\" data=\"".$row['var_name']."=".$row['var_value']."\"/>\n";
-			}
+		$varcmd = 'set';
+		if ($row['var_cat'] == 'Exec-Set') {
+				$varcmd = 'exec-set';
+		}
+		if (strlen($row['var_hostname']) == 0) {
+			$xml .= "<X-PRE-PROCESS cmd=\"".$varcmd."\" data=\"".$row['var_name']."=".$row['var_value']."\"/>\n";
+		} elseif ($row['var_hostname'] == $hostname) {
+			$xml .= "<X-PRE-PROCESS cmd=\"".$varcmd."\" data=\"".$row['var_name']."=".$row['var_value']."\"/>\n";
+		}
 		}
 		$prev_var_cat = $row['var_cat'];
 	}
