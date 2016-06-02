ad1ae9541568b772eb2372673cd8acb2115d2f58
reliberate <git@reliberate.com>
2016-06-01 22:04:00 -0600

    Theme: Additional settings to control domain name (selector), menu toggle icon, and logout icon color and hover colors separately. Adminer: Added ability to login automatically.

--- app/adminer/index.php
+++ app/adminer/index.php
@@ -31,29 +31,34 @@ function adminer_object() {
 			// custom name in title and heading
 			return 'Adminer';
 		}
-		/*
+
 		function permanentLogin() {
 			// key used for permanent login
-			return "7bebc76d8680196752c6b961ef13c360";
+			if ($_SESSION['adminer']['auto_login']['boolean'] == 'true') {
+				return "7bebc76d8680196752c6b961ef13c360";
+			}
 		}
 
 		function credentials() {
-			global $db_host, $db_username, $db_password;
 			// server, username and password for connecting to database
-			return array($db_host.':'.$db_port, $db_username, $db_password);
+			if ($_SESSION['adminer']['auto_login']['boolean'] == 'true') {
+				global $db_host, $db_username, $db_password;
+				return array($db_host.':'.$db_port, $db_username, $db_password);
+			}
 		}
 
 		function database() {
-			global $db_name;
 			// database name, will be escaped by Adminer
-			return $db_name;
+			if ($_SESSION['adminer']['auto_login']['boolean'] == 'true') {
+				global $db_name;
+				return $db_name;
+			}
 		}
 
 		function login($login, $password) {
 			// validate user submitted credentials
-			return ($login == 'admin' && $password == '');
+			return ($_SESSION['adminer']['auto_login']['boolean'] == 'true') ? true : false;
 		}
-		*/
 
     }
 
