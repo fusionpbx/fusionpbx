5e74f6cd92fdf29e7a9978bcefdaed031df50040
markjcrane <markjcrane@gmail.com>
2016-06-22 13:49:54 -0600

    Add app/pin_numbers.

---
+++ app/pin_numbers/pin_number_edit.php
@@ -0,0 +1,240 @@
+<?php
+/*
+	FusionPBX
+	Version: MPL 1.1
+
+	The contents of this file are subject to the Mozilla Public License Version
+	1.1 (the "License"); you may not use this file except in compliance with
+	the License. You may obtain a copy of the License at
+	http://www.mozilla.org/MPL/
+
+	Software distributed under the License is distributed on an "AS IS" basis,
+	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	for the specific language governing rights and limitations under the
+	License.
+
+	The Original Code is FusionPBX
+
+	The Initial Developer of the Original Code is
+	Mark J Crane <markjcrane@fusionpbx.com>
+	Portions created by the Initial Developer are Copyright (C) 2016
+	the Initial Developer. All Rights Reserved.
+
+	Contributor(s):
+	Mark J Crane <markjcrane@fusionpbx.com>
+*/
+
+//includes
+	require_once "root.php";
+	require_once "resources/require.php";
+
+//check permissions
+	require_once "resources/check_auth.php";
+	if (permission_exists('pin_number_add') || permission_exists('pin_number_edit')) {
+		//access granted
+	}
+	else {
+		echo "access denied";
+		exit;
+	}
+
+//add multi-lingual support
+	$language = new text;
+	$text = $language->get();
+
+//action add or update
+	if (isset($_REQUEST["id"])) {
+		$action = "update";
+		$pin_number_uuid = check_str($_REQUEST["id"]);
+	}
+	else {
+		$action = "add";
+	}
+
+//get http post variables and set them to php variables
+	if (count($_POST)>0) {
+		$pin_number = check_str($_POST["pin_number"]);
+		$accountcode = check_str($_POST["accountcode"]);
+		$enabled = check_str($_POST["enabled"]);
+		$description = check_str($_POST["description"]);
+	}
+
+if (count($_POST)>0 && strlen($_POST["persistformvar"]) == 0) {
+
+	$msg = '';
+	if ($action == "update") {
+		$pin_number_uuid = check_str($_POST["pin_number_uuid"]);
+	}
+
+	//check for all required data
+		if (strlen($pin_number) == 0) { $msg .= $text['message-required']." ".$text['label-pin_number']."<br>\n"; }
+		//if (strlen($accountcode) == 0) { $msg .= $text['message-required']." ".$text['label-accountcode']."<br>\n"; }
+		if (strlen($enabled) == 0) { $msg .= $text['message-required']." ".$text['label-enabled']."<br>\n"; }
+		//if (strlen($description) == 0) { $msg .= $text['message-required']." ".$text['label-description']."<br>\n"; }
+		if (strlen($msg) > 0 && strlen($_POST["persistformvar"]) == 0) {
+			require_once "resources/header.php";
+			require_once "resources/persist_form_var.php";
+			echo "<div align='center'>\n";
+			echo "<table><tr><td>\n";
+			echo $msg."<br />";
+			echo "</td></tr></table>\n";
+			persistformvar($_POST);
+			echo "</div>\n";
+			require_once "resources/footer.php";
+			return;
+		}
+
+	//add or update the database
+		if ($_POST["persistformvar"] != "true") {
+			if ($action == "add" && permission_exists('pin_number_add')) {
+				$sql = "insert into v_pin_numbers ";
+				$sql .= "(";
+				$sql .= "domain_uuid, ";
+				$sql .= "pin_number_uuid, ";
+				$sql .= "pin_number, ";
+				$sql .= "accountcode, ";
+				$sql .= "enabled, ";
+				$sql .= "description ";
+				$sql .= ")";
+				$sql .= "values ";
+				$sql .= "(";
+				$sql .= "'$domain_uuid', ";
+				$sql .= "'".uuid()."', ";
+				$sql .= "'$pin_number', ";
+				$sql .= "'$accountcode', ";
+				$sql .= "'$enabled', ";
+				$sql .= "'$description' ";
+				$sql .= ")";
+				$db->exec(check_sql($sql));
+				unset($sql);
+
+				$_SESSION["message"] = $text['message-add'];
+				header("Location: pin_numbers.php");
+				return;
+
+			} //if ($action == "add")
+
+			if ($action == "update" && permission_exists('pin_number_edit')) {
+				$sql = "update v_pin_numbers set ";
+				$sql .= "pin_number = '$pin_number', ";
+				$sql .= "accountcode = '$accountcode', ";
+				$sql .= "enabled = '$enabled', ";
+				$sql .= "description = '$description' ";
+				$sql .= "where pin_number_uuid = '$pin_number_uuid'";
+				$sql .= "and domain_uuid = '$domain_uuid' ";
+				$db->exec(check_sql($sql));
+				unset($sql);
+
+				$_SESSION["message"] = $text['message-update'];
+				header("Location: pin_numbers.php");
+				return;
+
+			} //if ($action == "update")
+		} //if ($_POST["persistformvar"] != "true")
+} //(count($_POST)>0 && strlen($_POST["persistformvar"]) == 0)
+
+//pre-populate the form
+	if (count($_GET) > 0 && $_POST["persistformvar"] != "true") {
+		$pin_number_uuid = check_str($_GET["id"]);
+		$sql = "select * from v_pin_numbers ";
+		$sql .= "where domain_uuid = '$domain_uuid' ";
+		$sql .= "and pin_number_uuid = '$pin_number_uuid' ";
+		$prep_statement = $db->prepare(check_sql($sql));
+		$prep_statement->execute();
+		$result = $prep_statement->fetchAll(PDO::FETCH_NAMED);
+		foreach ($result as &$row) {
+			$pin_number = $row["pin_number"];
+			$accountcode = $row["accountcode"];
+			$enabled = $row["enabled"];
+			$description = $row["description"];
+		}
+		unset ($prep_statement);
+	}
+
+//show the header
+	require_once "resources/header.php";
+
+//show the content
+	echo "<form name='frm' id='frm' method='post' action=''>\n";
+	echo "<table width='100%'  border='0' cellpadding='0' cellspacing='0'>\n";
+	echo "<tr>\n";
+	echo "<td align='left' width='30%' nowrap='nowrap' valign='top'><b>".$text['title-pin_number']."</b><br><br></td>\n";
+	echo "<td width='70%' align='right' valign='top'>\n";
+	echo "	<input type='button' class='btn' name='' alt='".$text['button-back']."' onclick=\"window.location='pin_numbers.php'\" value='".$text['button-back']."'>";
+	echo "	<input type='submit' name='submit' class='btn' value='".$text['button-save']."'>";
+	echo "</td>\n";
+	echo "</tr>\n";
+
+	echo "<tr>\n";
+	echo "<td class='vncellreq' valign='top' align='left' nowrap='nowrap'>\n";
+	echo "	".$text['label-pin_number']."\n";
+	echo "</td>\n";
+	echo "<td class='vtable' align='left'>\n";
+	echo "	<input class='formfld' type='text' name='pin_number' maxlength='255' value=\"$pin_number\">\n";
+	echo "<br />\n";
+	echo $text['description-pin_number']."\n";
+	echo "</td>\n";
+	echo "</tr>\n";
+
+	echo "<tr>\n";
+	echo "<td class='vncell' valign='top' align='left' nowrap='nowrap'>\n";
+	echo "	".$text['label-accountcode']."\n";
+	echo "</td>\n";
+	echo "<td class='vtable' align='left'>\n";
+	echo "	<input class='formfld' type='text' name='accountcode' maxlength='255' value=\"$accountcode\">\n";
+	echo "<br />\n";
+	echo $text['description-accountcode']."\n";
+	echo "</td>\n";
+	echo "</tr>\n";
+
+	echo "<tr>\n";
+	echo "<td class='vncellreq' valign='top' align='left' nowrap='nowrap'>\n";
+	echo "	".$text['label-enabled']."\n";
+	echo "</td>\n";
+	echo "<td class='vtable' align='left'>\n";
+	echo "	<select class='formfld' name='enabled'>\n";
+	echo "	<option value=''></option>\n";
+	if ($enabled == "true") {
+		echo "	<option value='true' selected='selected'>".$text['label-true']."</option>\n";
+	}
+	else {
+		echo "	<option value='true'>".$text['label-true']."</option>\n";
+	}
+	if ($enabled == "false") {
+		echo "	<option value='false' selected='selected'>".$text['label-false']."</option>\n";
+	}
+	else {
+		echo "	<option value='false'>".$text['label-false']."</option>\n";
+	}
+	echo "	</select>\n";
+	echo "<br />\n";
+	echo $text['description-enabled']."\n";
+	echo "</td>\n";
+	echo "</tr>\n";
+
+	echo "<tr>\n";
+	echo "<td class='vncell' valign='top' align='left' nowrap='nowrap'>\n";
+	echo "	".$text['label-description']."\n";
+	echo "</td>\n";
+	echo "<td class='vtable' align='left'>\n";
+	echo "	<input class='formfld' type='text' name='description' maxlength='255' value=\"$description\">\n";
+	echo "<br />\n";
+	echo $text['description-description']."\n";
+	echo "</td>\n";
+	echo "</tr>\n";
+	echo "	<tr>\n";
+	echo "		<td colspan='2' align='right'>\n";
+	if ($action == "update") {
+		echo "				<input type='hidden' name='pin_number_uuid' value='$pin_number_uuid'>\n";
+	}
+	echo "				<input type='submit' name='submit' class='btn' value='".$text['button-save']."'>\n";
+	echo "		</td>\n";
+	echo "	</tr>";
+	echo "</table>";
+	echo "</form>";
+	echo "<br /><br />";
+
+//include the footer
+	require_once "resources/footer.php";
+
+?>
