23fb7118a649a7e380d4ddbebb60799be330e9bc
FusionPBX <markjcrane@gmail.com>
2016-06-23 16:42:12 -0600

    Update local_stream.conf.lua
    Music on hold set the default timer name to 'soft'.

--- resources/install/scripts/app/xml_handler/resources/scripts/configuration/local_stream.conf.lua
+++ resources/install/scripts/app/xml_handler/resources/scripts/configuration/local_stream.conf.lua
@@ -32,7 +32,7 @@
 			chime_list = "";
 			for k,v in pairs(chime_array) do
 				f = explode("/", v);
-				if (file_exists(sounds_dir.."/en/us/callie/"..f[1].."/8000/"..f[2])) then
+				if (f[1] ~= nil and f[2] ~= nil and file_exists(sounds_dir.."/en/us/callie/"..f[1].."/8000/"..f[2])) then
 					chime_list = chime_list .. sounds_dir.."/en/us/callie/"..v;
 				else
 					chime_list = chime_list .. v;
@@ -40,13 +40,20 @@
 			end
 		end
 
+		--set the default timer name to soft
+		if (row.music_on_hold_timer_name == nil) then
+			timer_name = "soft";
+		else
+			timer_name = row.music_on_hold_timer_name;
+		end
+		
 		--build the xml ]]..row.music_on_hold_name..[["
 		table.insert(xml, [[	<directory name="]]..row.music_on_hold_name..[[" path="]]..music_on_hold_path..[[">]]);
 		table.insert(xml, [[			<param name="rate" value="]]..row.music_on_hold_rate..[["/>]]);
 		table.insert(xml, [[			<param name="shuffle" value="]]..row.music_on_hold_shuffle..[["/>]]);
 		table.insert(xml, [[			<param name="channels" value="1"/>]]);
 		table.insert(xml, [[			<param name="interval" value="20"/>]]);
-		table.insert(xml, [[			<param name="timer-name" value="]]..row.music_on_hold_timer_name..[["/>]]);
+		table.insert(xml, [[			<param name="timer-name" value="]]..timer_name..[["/>]]);
 		if (chime_list ~= nil) then
 			table.insert(xml, [[			<param name="chime-list" value="]]..chime_list..[["/>]]);
 		end
