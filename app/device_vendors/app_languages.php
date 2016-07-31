eb2541d92bdca7332787ecb04fd5e9fbe73b9e87
markjcrane <markjcrane@gmail.com>
2016-07-31 16:22:10 -0600

    Add app/device_vendors.

---
+++ app/device_vendors/app_languages.php
@@ -0,0 +1,224 @@
+<?php
+
+	//Device Vendors
+		$text['title-device_vendors']['en-us'] = 'Device Vendors';
+		$text['title-device_vendors']['es-cl'] = '';
+		$text['title-device_vendors']['pt-pt'] = '';
+		$text['title-device_vendors']['fr-fr'] = '';
+
+		$text['title-device_vendor']['en-us'] = 'Device Vendor';
+		$text['title-device_vendor']['es-cl'] = '';
+		$text['title-device_vendor']['pt-pt'] = '';
+		$text['title-device_vendor']['fr-fr'] = '';
+
+		$text['title_description-device_vendor']['en-us'] = 'Defines the list of vendors used with provisioning devices.';
+		$text['title_description-device_vendor']['es-cl'] = '';
+		$text['title_description-device_vendor']['pt-pt'] = '';
+		$text['title_description-device_vendor']['fr-fr'] = '';
+
+		$text['label-name']['en-us'] = 'Name';
+		$text['label-name']['es-cl'] = '';
+		$text['label-name']['pt-pt'] = '';
+		$text['label-name']['fr-fr'] = '';
+
+		$text['description-name']['en-us'] = 'Enter the name.';
+		$text['description-name']['es-cl'] = '';
+		$text['description-name']['pt-pt'] = '';
+		$text['description-name']['fr-fr'] = '';
+
+		$text['label-enabled']['en-us'] = 'Enabled';
+		$text['label-enabled']['es-cl'] = '';
+		$text['label-enabled']['pt-pt'] = '';
+		$text['label-enabled']['fr-fr'] = '';
+
+		$text['description-enabled']['en-us'] = 'Set the status of the vendor.';
+		$text['description-enabled']['es-cl'] = '';
+		$text['description-enabled']['pt-pt'] = '';
+		$text['description-enabled']['fr-fr'] = '';
+
+		$text['label-description']['en-us'] = 'Description';
+		$text['label-description']['es-cl'] = '';
+		$text['label-description']['pt-pt'] = '';
+		$text['label-description']['fr-fr'] = '';
+
+		$text['description-description']['en-us'] = 'Enter the description.';
+		$text['description-description']['es-cl'] = '';
+		$text['description-description']['pt-pt'] = '';
+		$text['description-description']['fr-fr'] = '';
+
+		$text['label-true']['en-us'] = 'true';
+		$text['label-true']['es-cl'] = '';
+		$text['label-true']['pt-pt'] = '';
+		$text['label-true']['fr-fr'] = '';
+
+		$text['label-false']['en-us'] = 'false';
+		$text['label-false']['es-cl'] = 'falso';
+		$text['label-false']['pt-pt'] = 'falso';
+		$text['label-false']['fr-fr'] = 'falso';
+
+		$text['button-add']['en-us'] = 'Add';
+		$text['button-add']['es-cl'] = '';
+		$text['button-add']['pt-pt'] = '';
+		$text['button-add']['fr-fr'] = '';
+
+		$text['button-edit']['en-us'] = 'Edit';
+		$text['button-edit']['es-cl'] = '';
+		$text['button-edit']['pt-pt'] = '';
+		$text['button-edit']['fr-fr'] = '';
+
+		$text['button-delete']['en-us'] = 'Delete';
+		$text['button-delete']['es-cl'] = '';
+		$text['button-delete']['pt-pt'] = '';
+		$text['button-delete']['fr-fr'] = '';
+
+		$text['button-save']['en-us'] = 'Save';
+		$text['button-save']['es-cl'] = '';
+		$text['button-save']['pt-pt'] = 'Guardar';
+		$text['button-save']['fr-fr'] = '';
+
+		$text['button-view']['en-us'] = 'View';
+		$text['button-view']['es-cl'] = '';
+		$text['button-view']['pt-pt'] = '';
+		$text['button-view']['fr-fr'] = '';
+
+		$text['button-back']['en-us'] = 'Back';
+		$text['button-back']['es-cl'] = '';
+		$text['button-back']['pt-pt'] = 'Voltar';
+		$text['button-back']['fr-fr'] = '';
+
+		$text['confirm-delete']['en-us'] = 'Do you really want to delete this?';
+		$text['confirm-delete']['es-cl'] = '';
+		$text['confirm-delete']['pt-pt'] = '';
+		$text['confirm-delete']['fr-fr'] = '';
+
+		$text['message-add']['en-us'] = 'Add Completed';
+		$text['message-add']['es-cl'] = '';
+		$text['message-add']['pt-pt'] = '';
+		$text['message-add']['fr-fr'] = '';
+
+		$text['message-update']['en-us'] = 'Update Completed';
+		$text['message-update']['es-cl'] = '';
+		$text['message-update']['pt-pt'] = '';
+		$text['message-update']['fr-fr'] = '';
+
+		$text['message-delete']['en-us'] = 'Delete Completed';
+		$text['message-delete']['es-cl'] = '';
+		$text['message-delete']['pt-pt'] = '';
+		$text['message-delete']['fr-fr'] = '';
+
+		$text['message-required']['en-us'] = 'Please provide: ';
+		$text['message-required']['es-cl'] = '';
+		$text['message-required']['pt-pt'] = '';
+		$text['message-required']['fr-fr'] = '';
+
+	//Device Vendors
+		$text['title-device_vendors']['en-us'] = 'Device Vendors';
+		$text['title-device_vendors']['es-cl'] = '';
+		$text['title-device_vendors']['pt-pt'] = '';
+		$text['title-device_vendors']['fr-fr'] = '';
+
+		$text['title-device_vendor']['en-us'] = 'Device Vendor';
+		$text['title-device_vendor']['es-cl'] = '';
+		$text['title-device_vendor']['pt-pt'] = '';
+		$text['title-device_vendor']['fr-fr'] = '';
+
+		$text['title_description-device_vendor']['en-us'] = 'Defines the list of vendors used with provisioning devices.';
+		$text['title_description-device_vendor']['es-cl'] = '';
+		$text['title_description-device_vendor']['pt-pt'] = '';
+		$text['title_description-device_vendor']['fr-fr'] = '';
+
+		$text['label-name']['en-us'] = 'Name';
+		$text['label-name']['es-cl'] = '';
+		$text['label-name']['pt-pt'] = '';
+		$text['label-name']['fr-fr'] = '';
+
+		$text['description-name']['en-us'] = 'Enter the name.';
+		$text['description-name']['es-cl'] = '';
+		$text['description-name']['pt-pt'] = '';
+		$text['description-name']['fr-fr'] = '';
+
+		$text['label-enabled']['en-us'] = 'Enabled';
+		$text['label-enabled']['es-cl'] = '';
+		$text['label-enabled']['pt-pt'] = '';
+		$text['label-enabled']['fr-fr'] = '';
+
+		$text['description-enabled']['en-us'] = 'Set the status of the vendor.';
+		$text['description-enabled']['es-cl'] = '';
+		$text['description-enabled']['pt-pt'] = '';
+		$text['description-enabled']['fr-fr'] = '';
+
+		$text['label-description']['en-us'] = 'Description';
+		$text['label-description']['es-cl'] = '';
+		$text['label-description']['pt-pt'] = '';
+		$text['label-description']['fr-fr'] = '';
+
+		$text['description-description']['en-us'] = 'Enter the description.';
+		$text['description-description']['es-cl'] = '';
+		$text['description-description']['pt-pt'] = '';
+		$text['description-description']['fr-fr'] = '';
+
+		$text['label-true']['en-us'] = 'true';
+		$text['label-true']['es-cl'] = '';
+		$text['label-true']['pt-pt'] = '';
+		$text['label-true']['fr-fr'] = '';
+
+		$text['label-false']['en-us'] = 'false';
+		$text['label-false']['es-cl'] = 'falso';
+		$text['label-false']['pt-pt'] = 'falso';
+		$text['label-false']['fr-fr'] = 'falso';
+
+		$text['button-add']['en-us'] = 'Add';
+		$text['button-add']['es-cl'] = '';
+		$text['button-add']['pt-pt'] = '';
+		$text['button-add']['fr-fr'] = '';
+
+		$text['button-edit']['en-us'] = 'Edit';
+		$text['button-edit']['es-cl'] = '';
+		$text['button-edit']['pt-pt'] = '';
+		$text['button-edit']['fr-fr'] = '';
+
+		$text['button-delete']['en-us'] = 'Delete';
+		$text['button-delete']['es-cl'] = '';
+		$text['button-delete']['pt-pt'] = '';
+		$text['button-delete']['fr-fr'] = '';
+
+		$text['button-save']['en-us'] = 'Save';
+		$text['button-save']['es-cl'] = '';
+		$text['button-save']['pt-pt'] = 'Guardar';
+		$text['button-save']['fr-fr'] = '';
+
+		$text['button-view']['en-us'] = 'View';
+		$text['button-view']['es-cl'] = '';
+		$text['button-view']['pt-pt'] = '';
+		$text['button-view']['fr-fr'] = '';
+
+		$text['button-back']['en-us'] = 'Back';
+		$text['button-back']['es-cl'] = '';
+		$text['button-back']['pt-pt'] = 'Voltar';
+		$text['button-back']['fr-fr'] = '';
+
+		$text['confirm-delete']['en-us'] = 'Do you really want to delete this?';
+		$text['confirm-delete']['es-cl'] = '';
+		$text['confirm-delete']['pt-pt'] = '';
+		$text['confirm-delete']['fr-fr'] = '';
+
+		$text['message-add']['en-us'] = 'Add Completed';
+		$text['message-add']['es-cl'] = '';
+		$text['message-add']['pt-pt'] = '';
+		$text['message-add']['fr-fr'] = '';
+
+		$text['message-update']['en-us'] = 'Update Completed';
+		$text['message-update']['es-cl'] = '';
+		$text['message-update']['pt-pt'] = '';
+		$text['message-update']['fr-fr'] = '';
+
+		$text['message-delete']['en-us'] = 'Delete Completed';
+		$text['message-delete']['es-cl'] = '';
+		$text['message-delete']['pt-pt'] = '';
+		$text['message-delete']['fr-fr'] = '';
+
+		$text['message-required']['en-us'] = 'Please provide: ';
+		$text['message-required']['es-cl'] = '';
+		$text['message-required']['pt-pt'] = '';
+		$text['message-required']['fr-fr'] = '';
+?>
