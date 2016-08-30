2636bfacc7cf2e60c49bb492f7b68651343e9efc
minotaur01 <minotaur01@hotmail.com>
2016-08-30 19:32:39 -0400

    TFTP Service v1.0.8 (#1881)
    * TFTP Service 1.0.8
    
    Added a check to pervent script execution from a webserver.
    
    * TFTP Service 1.0.9
    
    Renamed fileslocation setting to file_path and fixed default settings
    bug by modifing the sql statement.

--- app/tftp/app_defaults.php
+++ app/tftp/app_defaults.php
@@ -45,7 +45,7 @@
 			$array[$x]['default_setting_description'] = 'the port for the TFTP service to listen for connection on';
 			$x++;
 			$array[$x]['default_setting_category'] = 'provision';
-			$array[$x]['default_setting_subcategory'] = 'tftp_service_fileslocation';
+			$array[$x]['default_setting_subcategory'] = 'tftp_service_file_path';
 			$array[$x]['default_setting_name'] = 'numeric';
 			$array[$x]['default_setting_value'] = '/tmp';
 			$array[$x]['default_setting_enabled'] = 'true';
@@ -53,7 +53,7 @@
     
     //get an array of the default settings
 			$sql = "SELECT * FROM v_default_settings ";
-			$sql .= "WHERE default_setting_category = 'provision' AND default_setting_subcategory = 'tftp_service_%'";
+			$sql .= "WHERE default_setting_category = 'provision' AND default_setting_subcategory LIKE 'tftp_service_%'";
 			$prep_statement = $db->prepare($sql);
 			$prep_statement->execute();
 			$default_settings = $prep_statement->fetchAll(PDO::FETCH_NAMED);
