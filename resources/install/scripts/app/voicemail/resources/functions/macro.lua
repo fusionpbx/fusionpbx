db38b8dba30f7d7faf96c7fe7512ecda1150b6f3
FusionPBX <markjcrane@gmail.com>
2016-09-17 18:20:58 -0600

    Update macro.lua

--- resources/install/scripts/app/voicemail/resources/functions/macro.lua
+++ resources/install/scripts/app/voicemail/resources/functions/macro.lua
@@ -41,7 +41,12 @@
 				if (name == "person_not_available_record_message") then
 					table.insert(actions, {app="streamFile",data="voicemail/vm-person.wav"});
 					--pronounce the voicemail_id
-					table.insert(actions, {app="say.number.iterated",data=voicemail_id});
+					if (session:getVariable("voicemail_alternate_greet_id")) then
+						local voicemail_greet_id = session:getVariable("voicemail_alternate_greet_id");
+					else
+						local voicemail_greet_id = voicemail_id;
+					end
+					table.insert(actions, {app="say.number.iterated",data=voicemail_greet_id});
 					table.insert(actions, {app="streamFile",data="voicemail/vm-not_available.wav"});
 				end
 			--record your message at the tone press any key or stop talking to end the recording
