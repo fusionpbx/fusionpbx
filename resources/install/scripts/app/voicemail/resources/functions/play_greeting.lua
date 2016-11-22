d627f76eb479340d051dcf7d0898bec09704a1d1
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-11-22 21:12:10 +0300

    Add. Use params in vm/play_greeting.lua (#2131)

--- resources/install/scripts/app/voicemail/resources/functions/play_greeting.lua
+++ resources/install/scripts/app/voicemail/resources/functions/play_greeting.lua
@@ -51,15 +51,17 @@
 							if (storage_type == "base64") then
 								local dbh = Database.new('system', 'base64/read')
 
-								sql = [[SELECT * FROM v_voicemail_greetings
-									WHERE domain_uuid = ']] .. domain_uuid ..[['
-									AND voicemail_id = ']].. voicemail_id.. [['
-									AND greeting_id = ']].. greeting_id.. [[' ]];
+								local sql = [[SELECT * FROM v_voicemail_greetings
+									WHERE domain_uuid = :domain_uuid
+									AND voicemail_id = :voicemail_id
+									AND greeting_id = :greeting_id ]];
+								local params = {domain_uuid = domain_uuid, voicemail_id = voicemail_id,
+									greeting_id = greeting_id};
 								if (debug["sql"]) then
-									freeswitch.consoleLog("notice", "[voicemail] SQL: " .. sql .. "\n");
+									freeswitch.consoleLog("notice", "[voicemail] SQL: " .. sql .. "; params:" .. json.encode(params) .. "\n");
 								end
 								local saved
-								status = dbh:query(sql, function(row)
+								dbh:query(sql, params, function(row)
 									--set the voicemail message path
 										mkdir(voicemail_dir.."/"..voicemail_id);
 										greeting_location = voicemail_dir.."/"..voicemail_id.."/greeting_"..greeting_id..".wav"; --vm_message_ext;
