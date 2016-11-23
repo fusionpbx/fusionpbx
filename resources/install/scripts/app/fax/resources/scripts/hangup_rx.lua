c0f0d81d01c4bfb6b701ccf3eb15fd76edb71b0d
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-11-23 12:02:06 +0300

    Fix. Use correct database class in hangup_rx.lua (#2155)

--- resources/install/scripts/app/fax/resources/scripts/hangup_rx.lua
+++ resources/install/scripts/app/fax/resources/scripts/hangup_rx.lua
@@ -32,7 +32,7 @@
 	require "resources.functions.config";
 
 --connect to the database
-	local Database = require "resources.functions.database_handle";
+	local Database = require "resources.functions.database";
 	dbh = Database.new('system');
 
 --include json library
