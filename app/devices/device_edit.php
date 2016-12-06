33e51a811cd0f18312a66046fad734121efa8b59
FusionPBX <markjcrane@gmail.com>
2016-12-06 11:25:08 -0700

    Update device_edit.php

--- app/devices/device_edit.php
+++ app/devices/device_edit.php
@@ -316,7 +316,9 @@
 
 				//write the provision files
 					if (strlen($_SESSION['provision']['path']['text']) > 0) {
-						require_once "app/provision/provision_write.php";
+						$prov = new provision;
+						$prov->domain_uuid = $domain_uuid;
+						$response = $prov->write();
 					}
 
 				//set the message
