f68487d7bda36af8ad238fd580b67b525581b871
FusionPBX <markjcrane@gmail.com>
2017-01-10 00:33:24 -0700

    Update app_defaults.php

--- app/conference_controls/app_defaults.php
+++ app/conference_controls/app_defaults.php
@@ -49,7 +49,7 @@
 						$xml_string = file_get_contents($xml_file);
 					}
 					elseif (file_exists($xml_file_alt)) {
-						$xml_string = file_get_contents(xml_file_alt);
+						$xml_string = file_get_contents($xml_file_alt);
 					}
 					$xml_object = simplexml_load_string($xml_string);
 					$json = json_encode($xml_object);
