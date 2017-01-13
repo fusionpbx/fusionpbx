efd77c36be2a9753eafc915397b0a72c4fb9f790
FusionPBX <markjcrane@gmail.com>
2017-01-12 23:04:41 -0700

    Update app_defaults.php

--- app/adminer/app_defaults.php
+++ app/adminer/app_defaults.php
@@ -27,39 +27,6 @@
 //proccess this only one time
 if ($domains_processed == 1) {
 
-	//define array of settings
-		$x = 0;
-		$array[$x]['default_setting_category'] = 'adminer';
-		$array[$x]['default_setting_subcategory'] = 'auto_login';
-		$array[$x]['default_setting_name'] = 'boolean';
-		$array[$x]['default_setting_value'] = 'true';
-		$array[$x]['default_setting_enabled'] = 'false';
-		$array[$x]['default_setting_description'] = 'Set whether to auto-login to Adminer, or require a username and password.';
-		$x++;
-
-	//iterate and add each, if necessary
-		foreach ($array as $index => $default_settings) {
-			//add the default setting
-			$sql = "select count(*) as num_rows from v_default_settings ";
-			$sql .= "where default_setting_category = '".$default_settings['default_setting_category']."' ";
-			$sql .= "and default_setting_subcategory = '".$default_settings['default_setting_subcategory']."' ";
-			$sql .= "and default_setting_name = '".$default_settings['default_setting_name']."' ";
-			$prep_statement = $db->prepare($sql);
-			if ($prep_statement) {
-				$prep_statement->execute();
-				$row = $prep_statement->fetch(PDO::FETCH_ASSOC);
-				unset($prep_statement);
-				if ($row['num_rows'] == 0) {
-					$orm = new orm;
-					$orm->name('default_settings');
-					$orm->save($array[$index]);
-					$message = $orm->message;
-					//print_r($message);
-				}
-				unset($row);
-			}
-		}
-
 }
 
 ?>
