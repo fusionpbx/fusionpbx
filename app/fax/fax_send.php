7b4fa99857c883cfcc804062b252250abbe132d4
FusionPBX <markjcrane@gmail.com>
2017-01-22 22:36:53 -0700

    Update fax_send.php
    Prevent the fax cover page from exceeding 1 page.

--- app/fax/fax_send.php
+++ app/fax/fax_send.php
@@ -542,7 +542,7 @@ function fax_split_dtmf(&$fax_number, &$fax_dtmf){
 				$pdf -> SetTopMargin(0.6);
 				$pdf -> SetFont($pdf_font, "", 12);
 				$pdf -> SetXY($x + 0.75, $y + 3.65);
-				$pdf -> MultiCell(7, 5.75, $fax_message, 0, 'L', false);
+				$pdf -> MultiCell(7, 5.40, $fax_message, 0, 'L', false);
 			}
 
 			$pages = $pdf -> getNumPages();
