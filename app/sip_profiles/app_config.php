bca48e7f5711a337c0a3510aa39d883ac73eb05a
FusionPBX <markjcrane@gmail.com>
2017-01-24 02:02:02 -0700

    Update app_config.php
    Fix the spacing

--- app/sip_profiles/app_config.php
+++ app/sip_profiles/app_config.php
@@ -59,18 +59,18 @@
 		$apps[$x]['permissions'][$y]['name'] = "sip_profile_setting_delete";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
 		$y++;
-                $apps[$x]['permissions'][$y]['name'] = 'sip_profile_domain_view';
-                $apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
-                $y++;
-                $apps[$x]['permissions'][$y]['name'] = 'sip_profile_domain_add';
-                $apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
-                $y++;
-                $apps[$x]['permissions'][$y]['name'] = 'sip_profile_domain_edit';
-                $apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
-                $y++;
-                $apps[$x]['permissions'][$y]['name'] = 'sip_profile_domain_delete';
-                $apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
-                $y++;
+		$apps[$x]['permissions'][$y]['name'] = 'sip_profile_domain_view';
+		$apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
+		$y++;
+		$apps[$x]['permissions'][$y]['name'] = 'sip_profile_domain_add';
+		$apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
+		$y++;
+		$apps[$x]['permissions'][$y]['name'] = 'sip_profile_domain_edit';
+		$apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
+		$y++;
+		$apps[$x]['permissions'][$y]['name'] = 'sip_profile_domain_delete';
+		$apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
+		$y++;
 
 	//schema details
 		$y = 0; //table array index
@@ -100,35 +100,35 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "Enter the description.";
 		$z++;
 
-                $y = 1; //table array index
-                $z = 0; //field array index
-                $apps[$x]['db'][$y]['table'] = 'v_sip_profile_domains';
-                $apps[$x]['db'][$y]['fields'][$z]['name'] = 'sip_profile_domain_uuid';
-                $apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
-                $apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
-                $apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
-                $apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'primary';
-                $z++;
-                $apps[$x]['db'][$y]['fields'][$z]['name'] = 'sip_profile_uuid';
-                $apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
-                $apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
-                $apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
-                $apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
-                $apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_sip_profile';
-                $apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'sip_profile_uuid';
-                $z++;
-                $apps[$x]['db'][$y]['fields'][$z]['name'] = 'sip_profile_domain_name';
-                $apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
-                $apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = 'Enter the name.';
-                $z++;
-                $apps[$x]['db'][$y]['fields'][$z]['name'] = 'sip_profile_domain_alias';
-                $apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
-                $apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = 'Enable or disable the alias.';
-                $z++;
-                $apps[$x]['db'][$y]['fields'][$z]['name'] = 'sofia_profile_domain_parse';
-                $apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
-                $apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = 'Enable or disable the parsing.';
-                $z++;
+		$y = 1; //table array index
+		$z = 0; //field array index
+		$apps[$x]['db'][$y]['table'] = 'v_sip_profile_domains';
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'sip_profile_domain_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'primary';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'sip_profile_uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_sip_profile';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'sip_profile_uuid';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'sip_profile_domain_name';
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = 'Enter the name.';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'sip_profile_domain_alias';
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = 'Enable or disable the alias.';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'sofia_profile_domain_parse';
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = 'Enable or disable the parsing.';
+		$z++;
 
 		$y = 2; //table array index
 		$z = 0; //field array index
