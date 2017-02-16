49c7f98a22489eb170ecab5169074f53e072855e
FusionPBX <markjcrane@gmail.com>
2017-02-15 23:38:36 -0700

    Update app_config.php

--- app/vars/app_config.php
+++ app/vars/app_config.php
@@ -21,7 +21,7 @@
 		$apps[$x]['description']['pt-br'] = "";
 
 	//permission details
-		$y = 0;
+		$y=0;
 		$apps[$x]['permissions'][$y]['name'] = "var_view";
 		$apps[$x]['permissions'][$y]['menu']['uuid'] = "7a4e9ec5-24b9-7200-89b8-d70bf8afdd8f";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
@@ -36,7 +36,10 @@
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
 
 	//schema details
-		$apps[$x]['db'][$y]['table'] = "v_vars";
+		$y=0;
+		$apps[$x]['db'][$y]['table']['name'] = "v_vars";
+		$apps[$x]['db'][$y]['table']['parent'] = "";
+		$z=0;
 		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = "id";
 		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = "var_id";
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = "serial";
@@ -52,15 +55,6 @@
 		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = "primary";
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
 		$z++;
-		//$apps[$x]['db'][$y]['fields'][$z]['name'] = "domain_uuid";
-		//$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = "uuid";
-		//$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = "text";
-		//$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = "char(36)";
-		//$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = "foreign";
-		//$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = "v_domains";
-		//$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = "domain_uuid";
-		//$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
-		//$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = "v_id";
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
