0018e767f287bd849b93242bc7ca2e2de5fa5de2
FusionPBX <markjcrane@gmail.com>
2017-02-16 17:12:34 -0700

    Update contact_import.php

--- app/contacts/contact_import.php
+++ app/contacts/contact_import.php
@@ -72,7 +72,7 @@
 	}
 
 //copy the csv file
-	if ($_POST['submit'] == "Upload" && is_uploaded_file($_FILES['ulfile']['tmp_name']) && permission_exists('contact_upload')) {
+	if ( is_uploaded_file($_FILES['ulfile']['tmp_name']) && permission_exists('contact_upload')) {
 		if (check_str($_POST['type']) == 'csv') {
 			move_uploaded_file($_FILES['ulfile']['tmp_name'], $_SESSION['server']['temp']['dir'].'/'.$_FILES['ulfile']['name']);
 			$save_msg = "Uploaded file to ".$_SESSION['server']['temp']['dir']."/". htmlentities($_FILES['ulfile']['name']);
