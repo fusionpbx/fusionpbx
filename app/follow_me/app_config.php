8404838b986bc8503f8c2401e595211f1f46a326
FusionPBX <markjcrane@gmail.com>
2017-03-02 13:51:52 -0700

    Update app_config.php

--- app/follow_me/app_config.php
+++ app/follow_me/app_config.php
@@ -127,6 +127,9 @@
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = "uuid";
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = "char(36)";
+		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = "foreign";
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = "v_destinations";
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = "destination_uuid";
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en'] = "";
 		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = "follow_me_ignore_busy";
