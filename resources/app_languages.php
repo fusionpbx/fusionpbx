515ee3705671591d73d9fac2d357c716aa7d667d
FusionPBX <markjcrane@gmail.com>
2017-03-16 00:58:19 -0600

    Update app_languages.php

--- resources/app_languages.php
+++ resources/app_languages.php
@@ -823,34 +823,6 @@ $text['label-ringback']['he'] = "לצלצל בחזרה";
 $text['label-ringback']['ru-ru'] = "Перезвонить";
 $text['label-ringback']['it-it'] = "Richiamare";
 
-$text['label-tone']['en-us'] = "Tone";
-$text['label-tone']['es-cl'] = "Tono";
-$text['label-tone']['pt-pt'] = "Tom";
-$text['label-tone']['pt-br'] = "Tom";
-$text['label-tone']['pl'] = "Ton";
-$text['label-tone']['fr-fr'] = "Ton";
-$text['label-tone']['nl-nl'] = "Toon";
-$text['label-tone']['sv-se'] = "Tona";
-$text['label-tone']['uk'] = "Тон";
-$text['label-tone']['de-at'] = "Ton";
-$text['label-tone']['he'] = "טוֹן";
-$text['label-tone']['ru-ru'] = "Тон";
-$text['label-tone']['it-it'] = "Tono";
-
-$text['label-tones']['en-us'] = "Tones";
-$text['label-tones']['es-cl'] = "Tono";
-$text['label-tones']['pt-pt'] = "Tom";
-$text['label-tones']['pt-br'] = "Tom";
-$text['label-tones']['pl'] = "Ton";
-$text['label-tones']['fr-fr'] = "Ton";
-$text['label-tones']['nl-nl'] = "Toon";
-$text['label-tones']['sv-se'] = "Tona";
-$text['label-tones']['uk'] = "Тон";
-$text['label-tones']['de-at'] = "Ton";
-$text['label-tones']['he'] = "טוֹן";
-$text['label-tones']['ru-ru'] = "Тон";
-$text['label-tones']['it-it'] = "Tono";
-
 $text['label-music_on_hold']['en-us'] = "Music on Hold";
 $text['label-music_on_hold']['es-cl'] = "Musica en Espera";
 $text['label-music_on_hold']['pt-pt'] = "Música em Espera";
