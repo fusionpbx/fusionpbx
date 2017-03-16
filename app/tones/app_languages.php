57de69e804cd8fb66af4e2e7070734e63296f996
FusionPBX <markjcrane@gmail.com>
2017-03-16 00:46:25 -0600

    Create app_languages.php

---
+++ app/tones/app_languages.php
@@ -0,0 +1,17 @@
+<?php
+
+$text['title-tones']['en-us'] = "Tones";
+$text['title-tones']['es-cl'] = "Tono";
+$text['title-tones']['pt-pt'] = "Tom";
+$text['title-tones']['pt-br'] = "Tom";
+$text['title-tones']['pl'] = "Ton";
+$text['title-tones']['fr-fr'] = "Ton";
+$text['title-tones']['nl-nl'] = "Toon";
+$text['title-tones']['sv-se'] = "Tona";
+$text['title-tones']['uk'] = "Тон";
+$text['title-tones']['de-at'] = "Ton";
+$text['title-tones']['he'] = "טוֹן";
+$text['title-tones']['ru-ru'] = "Тон";
+$text['title-tones']['it-it'] = "Tono";
+
+?>
