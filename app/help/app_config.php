4fb40490eb7a9278cb0556825b49c7be20c6f5be
mafoo <github@mafoo.org>
2017-03-23 12:09:19 +0000

    Add help link to menu for admin and superadmin
    Fix for $mod_li and $mod_a_li not being blanked/reset foreach menu
    parent item rendered, causing the first menu with sub items to force sub
    item rendering for all remaining menu items

---
+++ app/help/app_config.php
@@ -0,0 +1,12 @@
+<?php
+
+	$apps[$x]['name'] = "Help";
+	$apps[$x]['uuid'] = "9e0e39d0-4b5f-44ef-afc6-648ae121ab15";
+	$apps[$x]['category'] = "Menu";
+	$apps[$x]['subcategory'] = "";
+	$apps[$x]['version'] = "1";
+	$apps[$x]['license'] = "Mozilla Public License 1.1";
+	$apps[$x]['url'] = "http://www.fusionpbx.com";
+	$apps[$x]['description']['en-us'] = "Add help menu items";
+
+?>
