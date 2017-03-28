4d27e579c812ef160011d5d4e8f9daabf15668bd
Luis Daniel Lucio Quiroz <dlucio@okay.com.mx>
2017-03-27 22:52:24 -0400

    Same rationale as 4.0 (#2454)
    =

--- app/destinations/destination_edit.php
+++ app/destinations/destination_edit.php
@@ -457,7 +457,7 @@
 					if ($action == "add") {
 						$_SESSION["message"] = $text['message-add'];
 						// billing
-						if (file_exists($_SERVER["PROJECT_ROOT"]."/app/billing/app_config.php")){
+						if ((file_exists($_SERVER["PROJECT_ROOT"]."/app/billing/app_config.php")) && (permission_exists('billing_edit'))){
 							$db2 = new database;
 							$db2->sql = "select currency, billing_uuid, balance from v_billings where type_value='$destination_accountcode'";
 							$db2->result = $db2->execute();
@@ -798,8 +798,7 @@
 	echo "</tr>\n";
 
 	// billing
-	if (file_exists($_SERVER["PROJECT_ROOT"]."/app/billing/app_config.php")){
-		echo "<tr id='tr_sell'>\n";
+	if ((file_exists($_SERVER["PROJECT_ROOT"]."/app/billing/app_config.php")) && (permission_exists('billing_edit'))){		echo "<tr id='tr_sell'>\n";
 		echo "<td class='vncell' valign='top' align='left' nowrap='nowrap'>\n";
 		echo "  ".$text['label-monthly_price']."\n";
 		echo "</td>\n";
