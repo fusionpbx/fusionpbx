3123a8f02ea2e0f80c6dc9eb10dd2b16076a70b7
FusionPBX <markjcrane@gmail.com>
2017-04-14 22:13:18 -0600

    Update switch.php

--- resources/switch.php
+++ resources/switch.php
@@ -17,7 +17,7 @@
 
 	The Initial Developer of the Original Code is
 	Mark J Crane <markjcrane@fusionpbx.com>
-	Portions created by the Initial Developer are Copyright (C) 2008-2015
+	Portions created by the Initial Developer are Copyright (C) 2008-2016
 	the Initial Developer. All Rights Reserved.
 
 	Contributor(s):
@@ -25,8 +25,10 @@
 	Luis Daniel Lucio Quiroz <dlucio@okay.com.mx>
 	Riccardo Granchi <riccardo.granchi@nems.it>
 */
-require_once "root.php";
-require_once "resources/require.php";
+
+//includes
+	require_once "root.php";
+	require_once "resources/require.php";
 
 //get the event socket information
 	if (file_exists($_SERVER["PROJECT_ROOT"]."/app/settings/app_config.php")) {
@@ -755,7 +757,7 @@ function save_dialplan_xml() {
 						unset ($prep_statement_2, $sql);
 
 						//create a new array that is sorted into groups and put the tags in order conditions, actions, anti-actions
-							$details = '';
+							$details = array();
 							$previous_tag = '';
 							$details[$group]['condition_count'] = '';
 							//conditions
