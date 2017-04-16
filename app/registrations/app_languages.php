c2b23c08ece954abc9012ac44e6ade91c033404c
vkrt-11 <kraftv@mail.ru>
2017-04-16 11:07:38 +0500

    /app/registrations/app_languages.php adding locales ru-ru

--- app/registrations/app_languages.php
+++ app/registrations/app_languages.php
@@ -9,6 +9,7 @@ $text['label-user']['pl'] = "Użytkownik";
 $text['label-user']['sv-se'] = "Användare";
 $text['label-user']['uk'] = "Користувач";
 $text['label-user']['de-at'] = "Benutzer";
+$text['label-user']['ru-ru'] = "Пользователь";
 
 $text['label-status']['en-us'] = "Status";
 $text['label-status']['es-cl'] = "Estado";
@@ -19,6 +20,7 @@ $text['label-status']['pl'] = "Status";
 $text['label-status']['sv-se'] = "Status";
 $text['label-status']['uk'] = "Статус";
 $text['label-status']['de-at'] = "Status";
+$text['label-status']['ru-ru'] = "Статус";
 
 $text['label-response']['en-us'] = "Response:";
 $text['label-response']['es-cl'] = "Respuesta:";
@@ -29,6 +31,7 @@ $text['label-response']['pl'] = "Odpowiedź";
 $text['label-response']['sv-se'] = "Respons:";
 $text['label-response']['uk'] = "Відповідь:";
 $text['label-response']['de-at'] = "Antwort:";
+$text['label-response']['ru-ru'] = "Отклик:";
 
 $text['label-refresh_pause']['en-us'] = "Pause Refresh";
 $text['label-refresh_pause']['es-cl'] = "Actualizar Pausa";
@@ -39,6 +42,7 @@ $text['label-refresh_pause']['pl'] = "Pauzuj odÅ›wieÅ¼anie";
 $text['label-refresh_pause']['sv-se'] = "Paus Uppdatera";
 $text['label-refresh_pause']['uk'] = "пауза Оновити";
 $text['label-refresh_pause']['de-at'] = "Seite neu laden deaktivieren";
+$text['label-refresh_pause']['ru-ru'] = "Приостановить обновление";
 
 $text['label-refresh_enable']['en-us'] = "Enable Refresh";
 $text['label-refresh_enable']['es-cl'] = "Activar Actualizar";
@@ -49,6 +53,7 @@ $text['label-refresh_enable']['pl'] = "WÅ‚Ä…cz odÅ›wieÅ¼anie";
 $text['label-refresh_enable']['sv-se'] = "Aktivera Uppdatera";
 $text['label-refresh_enable']['uk'] = "включити Оновити";
 $text['label-refresh_enable']['de-at'] = "Seite neu laden aktivieren";
+$text['label-refresh_enable']['ru-ru'] = "Включить обновление";
 
 $text['label-port']['en-us'] = "Port";
 $text['label-port']['es-cl'] = "Puerto";
@@ -59,6 +64,7 @@ $text['label-port']['pl'] = "Port";
 $text['label-port']['sv-se'] = "Port";
 $text['label-port']['uk'] = "Порт";
 $text['label-port']['de-at'] = "Port";
+$text['label-port']['ru-ru'] = "Порт";
 
 $text['label-ping']['en-us'] = "Ping";
 $text['label-ping']['es-cl'] = "Silbido";
@@ -69,6 +75,7 @@ $text['label-ping']['pl'] = "Świst";
 $text['label-ping']['sv-se'] = "Ping";
 $text['label-ping']['uk'] = "пінг";
 $text['label-ping']['de-at'] = "Klingeln";
+$text['label-ping']['ru-ru'] = "Ping";
 
 $text['label-message']['en-us'] = "Invalid Profile!";
 $text['label-message']['es-cl'] = "&iexcl;Perfil Inválido!";
@@ -79,6 +86,7 @@ $text['label-message']['pl'] = "Wiadomość";
 $text['label-message']['sv-se'] = "Ogiltig Profil!";
 $text['label-message']['uk'] = "Хибний профіль!";
 $text['label-message']['de-at'] = "Ungültiges Profil!";
+$text['label-message']['ru-ru'] = "Недопустимый профиль!";
 
 $text['label-lan_ip']['en-us'] = "LAN IP";
 $text['label-lan_ip']['es-cl'] = "LAN IP";
@@ -89,6 +97,7 @@ $text['label-lan_ip']['pl'] = "LAN IP";
 $text['label-lan_ip']['sv-se'] = "LAN IP";
 $text['label-lan_ip']['uk'] = "LAN IP";
 $text['label-lan_ip']['de-at'] = "LAN IP";
+$text['label-lan_ip']['ru-ru'] = "LAN IP";
 
 $text['label-ip']['en-us'] = "IP";
 $text['label-ip']['es-cl'] = "IP";
@@ -99,6 +108,7 @@ $text['label-ip']['pl'] = "IP";
 $text['label-ip']['sv-se'] = "IP";
 $text['label-ip']['uk'] = "IP";
 $text['label-ip']['de-at'] = "IP";
+$text['label-ip']['ru-ru'] = "IP-адрес";
 
 $text['label-interval']['en-us'] = "Interval (Sec)";
 $text['label-interval']['es-cl'] = "Intervalo";
@@ -109,6 +119,7 @@ $text['label-interval']['pl'] = "Interwał (sek)";
 $text['label-interval']['sv-se'] = "Intervall (Sek)";
 $text['label-interval']['uk'] = "Інтервал (с.)";
 $text['label-interval']['de-at'] = "Intervall (Sek)";
+$text['label-interval']['ru-ru'] = "Интервал (сек)";
 
 $text['label-hostname']['en-us'] = "Hostname";
 $text['label-hostname']['es-cl'] = "Hostname";
@@ -119,6 +130,7 @@ $text['label-hostname']['pl'] = "Nazwa hosta";
 $text['label-hostname']['sv-se'] = "Hostname";
 $text['label-hostname']['uk'] = "Назва хоста";
 $text['label-hostname']['de-at'] = "Hostname";
+$text['label-hostname']['ru-ru'] = "Имя хоста";
 
 $text['label-event']['en-us'] = "Event:";
 $text['label-event']['es-cl'] = "Evento:";
@@ -129,6 +141,7 @@ $text['label-event']['pl'] = "Zdarzenie";
 $text['label-event']['sv-se'] = "Event:";
 $text['label-event']['uk'] = "Подія:";
 $text['label-event']['de-at'] = "Event:";
+$text['label-event']['ru-ru'] = "Событие:";
 
 $text['label-domain']['en-us'] = "Domain";
 $text['label-domain']['es-cl'] = "Dominio";
@@ -139,6 +152,7 @@ $text['label-domain']['pl'] = "Domena";
 $text['label-domain']['sv-se'] = "Domän";
 $text['label-domain']['uk'] = "Домен";
 $text['label-domain']['de-at'] = "Domain";
+$text['label-domain']['ru-ru'] = "Домен";
 
 $text['label-agent']['en-us'] = "Agent";
 $text['label-agent']['es-cl'] = "Agente";
@@ -149,6 +163,7 @@ $text['label-agent']['pl'] = "Agent";
 $text['label-agent']['sv-se'] = "Agent";
 $text['label-agent']['uk'] = "Оператор";
 $text['label-agent']['de-at'] = "Agent";
+$text['label-agent']['ru-ru'] = "Оператор";
 
 $text['label-contact']['en-us'] = "Contact";
 $text['label-contact']['es-cl'] = "Contacto";
@@ -159,6 +174,7 @@ $text['label-contact']['pl'] = "Kontakt";
 $text['label-contact']['sv-se'] = "Kontakta";
 $text['label-contact']['uk'] = "Контакт";
 $text['label-contact']['de-at'] = "Kontakt";
+$text['label-contact']['ru-ru'] = "Контакт";
 
 $text['header-registrations']['en-us'] = "Registrations";
 $text['header-registrations']['es-cl'] = "Inscripciones";
@@ -169,6 +185,7 @@ $text['header-registrations']['pl'] = "Rejestracje";
 $text['header-registrations']['sv-se'] = "Registreringar";
 $text['header-registrations']['uk'] = "Реєстрації";
 $text['header-registrations']['de-at'] = "Registrierungen";
+$text['header-registrations']['ru-ru'] = "Регистрация";
 
 $text['error-event-socket']['en-us'] = "Connection to Event Socket failed.";
 $text['error-event-socket']['es-cl'] = "Conexión a Socket de eventos fallida.";
@@ -179,6 +196,7 @@ $text['error-event-socket']['pl'] = "Połączenie do “Event Socket” nie powi
 $text['error-event-socket']['sv-se'] = "Kontakten till 'Event Socket' misslyckades.";
 $text['error-event-socket']['uk'] = "Помилка підключення до Event Socket";
 $text['error-event-socket']['de-at'] = "Verbindung zum Event Socket fehlgeschlagen.";
+$text['error-event-socket']['ru-ru'] = "Подключение к сокету событий не удалось.";
 
 $text['button-unregister']['en-us'] = "Unregister";
 $text['button-unregister']['es-cl'] = "";
@@ -189,6 +207,7 @@ $text['button-unregister']['pl'] = "Wyrejestruj";
 $text['button-unregister']['sv-se'] = "Avregistrera";
 $text['button-unregister']['uk'] = "Розреєструвати";
 $text['button-unregister']['de-at'] = "Unregister";
+$text['button-unregister']['ru-ru'] = "Отменить регистрацию";
 
 $text['button-reboot']['en-us'] = "Reboot";
 $text['button-reboot']['es-cl'] = "Reiniciar";
@@ -199,6 +218,7 @@ $text['button-reboot']['pl'] = "Zrestartuj";
 $text['button-reboot']['sv-se'] = "Starta Om";
 $text['button-reboot']['uk'] = "Перезавантажити";
 $text['button-reboot']['de-at'] = "Neu starten";
+$text['button-reboot']['ru-ru'] = "Перезагрузить";
 
 $text['button-provision']['en-us'] = "Provision";
 $text['button-provision']['es-cl'] = "";
@@ -209,5 +229,6 @@ $text['button-provision']['pl'] = "Konfigutuj";
 $text['button-provision']['sv-se'] = "Provisionera";
 $text['button-provision']['uk'] = "Provision ";
 $text['button-provision']['de-at'] = "Bereitstellen";
+$text['button-provision']['ru-ru'] = "Обеспечение";
 
 ?>
