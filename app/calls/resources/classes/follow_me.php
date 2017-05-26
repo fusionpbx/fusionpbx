6c802f40840ca2dff9b1b67d6064cf1c4c719bb5
Luis Daniel Lucio Quiroz <dlucio@okay.com.mx>
2017-05-26 18:07:33 -0400

    Fixes CID (#2587)
    * Fix the CID
    
    Although you already had somehow fixed the caller id here. you need to put origination and effective. For some reason, not all carriers get the CID from the same field in the SIP payload. origination_* and effective_* affects different parts of the SIP invite.
    
    * Fixes CID
    
    Fix the CID
    Although you already had somehow fixed the caller id here. you need to put origination and effective. For some reason, not all carriers get the CID from the same field in the SIP payload. origination_* and effective_* affects different parts of the SIP invite.
    
    I also fix he dialstring when using follow_me_fix, by adding a dummy variable at the start of the square brace [
    
    * Update call_forward.php
    
    * Update follow_me.php

--- app/calls/resources/classes/follow_me.php
+++ app/calls/resources/classes/follow_me.php
@@ -378,29 +378,37 @@ include "root.php";
 					}
 					else {
 						$presence_id = extension_presence_id($this->extension, $this->number_alias);
-						$dial_string .= "[";
+						$dial_string .= "[anyvar=";
 
 						//set the caller id
 						if ($_SESSION['cdr']['follow_me_fix']['boolean'] == "true") {
 							if (strlen($this->outbound_caller_id_name) > 0) {
 								$dial_string .= ",origination_caller_id_name=".$this->cid_name_prefix.$this->outbound_caller_id_name;
+								$dial_string .= ",effective_caller_id_name=".$this->cid_name_prefix.$this->outbound_caller_id_name;
 							}
 							if (strlen($this->outbound_caller_id_number) > 0) {
 								$dial_string .= ",origination_caller_id_number=".$this->cid_number_prefix.$this->outbound_caller_id_number;
+								$dial_string .= ",effective_caller_id_number=".$this->cid_number_prefix.$this->outbound_caller_id_number;
 							}
 						}
 						else {
 							if (strlen($caller_id_number) > 0) {
 								//set the caller id if it is set
-								if (strlen($caller_id_name) > 0) { $dial_string .= ",origination_caller_id_name=".$this->cid_name_prefix.$caller_id_name; }
+								if (strlen($caller_id_name) > 0) { 
+									$dial_string .= ",origination_caller_id_name=".$this->cid_name_prefix.$caller_id_name; 
+									$dial_string .= ",effective_caller_id_name=".$this->cid_name_prefix.$caller_id_name; 
+								}
 								$dial_string .= ",origination_caller_id_number=".$this->cid_number_prefix.$caller_id_number;
+								$dial_string .= ",effective_caller_id_number=".$this->cid_number_prefix.$caller_id_number;
 							}
 							else {
 								//set the outbound caller id number if the caller id number is a user
-								$dial_string .= ",\${cond(\${user_exists id \${caller_id_number} \${domain_name}} == true ? origination_caller_id_name=".$this->cid_name_prefix."\${outbound_caller_id_name},origination_caller_id_number=".$this->cid_number_prefix."\${outbound_caller_id_number} : )}";
+								$dial_string .=',origination_caller_id_number=${cond(${from_user_exists} == true ? ${outbound_caller_id_number} : ${origination_caller_id_number})}';
+								$dial_string .=',effective_caller_id_number=${cond(${from_user_exists} == true ? ${outbound_caller_id_number} : ${effective_caller_id_number})}';
+								$dial_string .=',origination_caller_id_name=${cond(${from_user_exists} == true ? ${outbound_caller_id_name} : ${origination_caller_id_name})}';
+								$dial_string .=',effective_caller_id_name=${cond(${from_user_exists} == true ? ${outbound_caller_id_name} : ${effective_caller_id_name})}';						}
 							}
 						}
-
 						$dial_string .= ",presence_id=".$presence_id."@".$_SESSION['domain_name'];
 						if ($row["follow_me_prompt"] == "1") {
 							$dial_string .= ",group_confirm_key=exec,group_confirm_file=lua confirm.lua,confirm=true,";
