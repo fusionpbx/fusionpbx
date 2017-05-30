bfe03611f63828077ea18740aadec73cfd94dde4
FusionPBX <markjcrane@gmail.com>
2017-05-30 09:53:16 -0600

    Update dialplans.php

--- app/dialplan/dialplans.php
+++ app/dialplan/dialplans.php
@@ -133,13 +133,13 @@
 	$sql .= "where (domain_uuid = '$domain_uuid' or domain_uuid is null) ";
 	if (strlen($app_uuid) == 0) {
 		//hide inbound routes
-			$sql .= "and (app_uuid <> 'c03b422e-13a8-bd1b-e42b-b6b9b4d27ce4' or dialplan_context = 'public') ";
+			$sql .= "and app_uuid <> 'c03b422e-13a8-bd1b-e42b-b6b9b4d27ce4' ";
 		//hide outbound routes
 			$sql .= "and app_uuid <> '8c914ec3-9fc0-8ab5-4cda-6c9288bdc9a3' ";
 	}
 	else {
 		if ($app_uuid == 'c03b422e-13a8-bd1b-e42b-b6b9b4d27ce4') {
-			$sql .= "and (app_uuid = '".$app_uuid."'  or dialplan_context = 'public') ";
+			$sql .= "and (app_uuid = '".$app_uuid."' or dialplan_context = 'public') ";
 		}
 		else {
 			$sql .= "and app_uuid = '".$app_uuid."' ";
