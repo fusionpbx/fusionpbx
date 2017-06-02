6296765c303da4b56f746bd5e37983013d7b8e84
Mafoo <github@mafoo.org>
2017-06-02 18:33:20 +0100

    BugFix [master] - Convert all menus to 2-2 format
    convert all app_menu.php files to have all placeholders and languages and use 2-2 format for languages
    update app_defaults to convert any items found using legacy values in user_setings, domain_settings and default_settings

--- core/user_settings/app_defaults.php
+++ core/user_settings/app_defaults.php
@@ -26,16 +26,12 @@
 
 //proccess this only one time
 	if ($domains_processed == 1) {
-		//set domains with enabled status of empty or null to true
-			$sql = "update v_domains set domain_enabled = 'true' where domain_enabled = '' or domain_enabled is null";
-			$db->exec(check_sql($sql));
-			unset($sql);
-		//update any domains set to legacy languages
+		//update any users set to legacy languages
 			$language = new text;
 			foreach ($language->legacy_map as $language_code => $legacy_code) {
 				if(strlen($legacy_code) == 5)
 					continue;
-				$sql = "update v_domain_settings set domain_setting_value = '$language_code' where domain_setting_value = '$legacy_code' and deafult_setting_name = 'code' and domain_setting_dubcategory = 'language' and domain_setting_category = 'domain'";
+				$sql = "update v_user_settings set user_setting_value = '$language_code' where user_setting_value = '$legacy_code' and deafult_setting_name = 'code' and user_setting_dubcategory = 'language' and user_setting_category = 'domain'";
 				$db->exec(check_sql($sql));
 				unset($sql);
 			}
