d11d29a4a7fff59731cee703a80add8e199bcd43
Mafoo <github@mafoo.org>
2017-06-12 13:31:58 +0100

    Enhance-master-translation autofill
    Autofill all language files for pt de and es

--- app/tones/app_languages.php
+++ app/tones/app_languages.php
@@ -1,14 +1,14 @@
 <?php
-#This file was last reorganized on 2nd of June 2017 04:38:28 PM UTC
+#This file was last reorganized on 12th of June 2017 10:57:41 AM UTC
 
 $text['title-tones']['en-us'] = "Tones";
 $text['title-tones']['ar-eg'] = "";
 $text['title-tones']['de-at'] = "Töne";
-$text['title-tones']['de-ch'] = "";
+$text['title-tones']['de-ch'] = "Töne"; //copied from de-de
 $text['title-tones']['de-de'] = "Töne";
 $text['title-tones']['es-cl'] = "Tono";
-$text['title-tones']['es-mx'] = "";
-$text['title-tones']['fr-ca'] = "";
+$text['title-tones']['es-mx'] = "Tono"; //copied from es-cl
+$text['title-tones']['fr-ca'] = "Ton"; //copied from fr-fr
 $text['title-tones']['fr-fr'] = "Ton";
 $text['title-tones']['he-il'] = "טוֹן";
 $text['title-tones']['it-it'] = "Tono";
@@ -24,11 +24,11 @@ $text['title-tones']['uk-ua'] = "Тон";
 $text['label-tone']['en-us'] = "Tone";
 $text['label-tone']['ar-eg'] = "";
 $text['label-tone']['de-at'] = "Ton";
-$text['label-tone']['de-ch'] = "";
+$text['label-tone']['de-ch'] = "Ton"; //copied from de-de
 $text['label-tone']['de-de'] = "Ton";
 $text['label-tone']['es-cl'] = "Tono";
-$text['label-tone']['es-mx'] = "";
-$text['label-tone']['fr-ca'] = "";
+$text['label-tone']['es-mx'] = "Tono"; //copied from es-cl
+$text['label-tone']['fr-ca'] = "Ton"; //copied from fr-fr
 $text['label-tone']['fr-fr'] = "Ton";
 $text['label-tone']['he-il'] = "טוֹן";
 $text['label-tone']['it-it'] = "Tono";
@@ -44,11 +44,11 @@ $text['label-tone']['uk-ua'] = "Тон";
 $text['label-tones']['en-us'] = "Tones";
 $text['label-tones']['ar-eg'] = "";
 $text['label-tones']['de-at'] = "Töne";
-$text['label-tones']['de-ch'] = "";
+$text['label-tones']['de-ch'] = "Töne"; //copied from de-de
 $text['label-tones']['de-de'] = "Töne";
 $text['label-tones']['es-cl'] = "Tono";
-$text['label-tones']['es-mx'] = "";
-$text['label-tones']['fr-ca'] = "";
+$text['label-tones']['es-mx'] = "Tono"; //copied from es-cl
+$text['label-tones']['fr-ca'] = "Ton"; //copied from fr-fr
 $text['label-tones']['fr-fr'] = "Ton";
 $text['label-tones']['he-il'] = "טוֹן";
 $text['label-tones']['it-it'] = "Tono";
