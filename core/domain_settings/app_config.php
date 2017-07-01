4b85fbbc84754dd2db108876a23c8429065cb3fe
FusionPBX <markjcrane@gmail.com>
2017-06-30 18:32:39 -0600

    Update app_config.php

--- core/domain_settings/app_config.php
+++ core/domain_settings/app_config.php
@@ -30,7 +30,6 @@
 		$y++;
 		$apps[$x]['permissions'][$y]['name'] = "domain_edit";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
-		$apps[$x]['permissions'][$y]['groups'][] = "admin";
 		$y++;
 		$apps[$x]['permissions'][$y]['name'] = "domain_delete";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
@@ -40,19 +39,15 @@
 		$y++;
 		$apps[$x]['permissions'][$y]['name'] = "domain_setting_view";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
-		$apps[$x]['permissions'][$y]['groups'][] = "admin";
 		$y++;
 		$apps[$x]['permissions'][$y]['name'] = "domain_setting_add";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
-		$apps[$x]['permissions'][$y]['groups'][] = "admin";
 		$y++;
 		$apps[$x]['permissions'][$y]['name'] = "domain_setting_edit";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
-		$apps[$x]['permissions'][$y]['groups'][] = "admin";
 		$y++;
 		$apps[$x]['permissions'][$y]['name'] = "domain_setting_delete";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
-		$apps[$x]['permissions'][$y]['groups'][] = "admin";
 		$y++;
 		$apps[$x]['permissions'][$y]['name'] = "domain_setting_category_edit";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
