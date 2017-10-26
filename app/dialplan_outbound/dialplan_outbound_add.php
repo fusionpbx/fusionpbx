2f056d44ab079b6a8220a49c4ab5206aa7bc5582
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2017-10-26 19:13:53 +0300

    Fix. Export call direction to B-Leg.
    It is needed to use with b-leg filtering.

--- app/dialplan_outbound/dialplan_outbound_add.php
+++ app/dialplan_outbound/dialplan_outbound_add.php
@@ -416,7 +416,7 @@
 						$array['dialplans'][$x]['dialplan_details'][$y]['domain_uuid'] = $_SESSION['domain_uuid'];
 						$array['dialplans'][$x]['dialplan_details'][$y]['dialplan_uuid'] = $dialplan_uuid;
 						$array['dialplans'][$x]['dialplan_details'][$y]['dialplan_detail_tag'] = 'action';
-						$array['dialplans'][$x]['dialplan_details'][$y]['dialplan_detail_type'] = 'set';
+						$array['dialplans'][$x]['dialplan_details'][$y]['dialplan_detail_type'] = 'export';
 						$array['dialplans'][$x]['dialplan_details'][$y]['dialplan_detail_data'] = 'call_direction=outbound';
 						$array['dialplans'][$x]['dialplan_details'][$y]['dialplan_detail_order'] = $y * 10;
 						$array['dialplans'][$x]['dialplan_details'][$y]['dialplan_detail_group'] = '0';
