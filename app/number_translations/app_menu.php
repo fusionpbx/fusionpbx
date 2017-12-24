05965f0e85f9e256e0dc1d2e957db8d2dc231401
FusionPBX <markjcrane@gmail.com>
2017-12-23 23:17:24 -0700

    Update app_menu.php

--- app/number_translations/app_menu.php
+++ app/number_translations/app_menu.php
@@ -1,7 +1,7 @@
 <?php
 
 	$y=0;
-	$apps[$x]['menu'][$y]['title']['en-us'] = "Number Translation";
+	$apps[$x]['menu'][$y]['title']['en-us'] = "Number Translations";
 	$apps[$x]['menu'][$y]['title']['ar-eg'] = "عدد الترجمة"; //Google translate
 	$apps[$x]['menu'][$y]['title']['de-at'] = "Nummer Übersetzung";
 	$apps[$x]['menu'][$y]['title']['de-ch'] = "Nummer Übersetzung";
