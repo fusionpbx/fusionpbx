f9e0ddf9b346b1dd338e737591367fd0304e1af4
Len <Len-PGH@users.noreply.github.com>
2018-02-26 15:15:36 -0500

    Update app_config.php

--- app/htek/app_config.php
+++ app/htek/app_config.php
@@ -34,7 +34,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "18";
 		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
-		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Time zone";		
+		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Time zone 18=EST 14=CST 6=PST 9,10=MST";		
 		$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "25c97d75-1e20-42ae-bb38-a6eb79e5795d";
 		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
@@ -42,15 +42,15 @@
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "numeric";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "1";
 		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
-		$apps[$x]['default_settings'][$y]['default_setting_description'] = "DST off 0";
+		$apps[$x]['default_settings'][$y]['default_setting_description'] = "DST off=0 on=1 auto=2";
 		$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "6570d02b-5dbe-42c6-8bb3-b0eaf34d555e";
 		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "htek_date_display_format";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "numeric";
-		$apps[$x]['default_settings'][$y]['default_setting_value'] = "2";
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = "1";
 		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
-		$apps[$x]['default_settings'][$y]['default_setting_description'] = "value 0-13 Date Format";
+		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Year-Month-Day=0  Month-Day-Year=1  Day-Month-Year=2";
 		$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "d46268fc-622d-4ee2-8f7a-00dfb98f90b5";
 		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
