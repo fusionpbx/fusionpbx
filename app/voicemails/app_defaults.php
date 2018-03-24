9b05f798c7e144e2dc72fb7e6c747dd41d7ddb3f
FusionPBX <markjcrane@gmail.com>
2018-03-24 15:32:55 -0600

    Update app_defaults.php

--- app/voicemails/app_defaults.php
+++ app/voicemails/app_defaults.php
@@ -119,8 +119,8 @@
 					$array['email_templates'][$x]['template_body'] .= "Received \${message_date}\n";
 					$array['email_templates'][$x]['template_body'] .= "Length \${message_duration}\n";
 					$array['email_templates'][$x]['template_body'] .= "Message \${message}\n";
--					$array['email_templates'][$x]['template_body'] .= "\n";
--					$array['email_templates'][$x]['template_body'] .= "\${message_text}\n";
+					$array['email_templates'][$x]['template_body'] .= "\n";
+					$array['email_templates'][$x]['template_body'] .= "\${message_text}\n";
 					$array['email_templates'][$x]['template_enabled'] = 'true';
 					$array['email_templates'][$x]['template_description'] = '';
 					$x++;
