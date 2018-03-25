c2d0767377d038f07247135b195946afccaec273
FusionPBX <markjcrane@gmail.com>
2018-03-25 10:11:25 -0600

    Update app_config.php

--- app/vars/app_config.php
+++ app/vars/app_config.php
@@ -68,6 +68,10 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = "true";
 		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = "var_command";
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
+		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = "var_name";
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
