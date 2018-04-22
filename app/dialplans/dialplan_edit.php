78ddfe6045ed8ff989507980801f9b90d4571873
Robert Stevens <rstevens011@users.noreply.github.com>
2018-04-21 23:30:31 -0700

    Adding sip_h_Diversion channel variable to list of conditions in dialplan editor. (#3032)

--- app/dialplans/dialplan_edit.php
+++ app/dialplans/dialplan_edit.php
@@ -673,6 +673,7 @@
 								echo "		<option value='\${sip_to_user}'>\${sip_to_user}</option>\n";
 								echo "		<option value='\${sip_to_host}'>\${sip_to_host}</option>\n";
 								echo "		<option value='\${toll_allow}'>\${toll_allow}</option>\n";
+								echo "		<option value='\${sip_h_Diversion}'>\$sip_h_Diversion}</option>\n";
 								echo "	</optgroup>\n";
 							//}
 							//if (strlen($dialplan_detail_tag) == 0 || $dialplan_detail_tag == "action" || $dialplan_detail_tag == "anti-action") {
