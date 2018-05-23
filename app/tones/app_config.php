e5aa0c12fb30dbe9025f53344aca1114d574b70e
FusionPBX <markjcrane@gmail.com>
2018-05-23 15:59:57 -0700

    Update app_config.php

--- app/tones/app_config.php
+++ app/tones/app_config.php
@@ -3,7 +3,7 @@
 	//application details
 		$apps[$x]['name'] = "Tones";
 		$apps[$x]['uuid'] = "38ab9f01-bcd2-4726-a9ff-9af8ed9e396a";
-		$apps[$x]['category'] = "Switch";;
+		$apps[$x]['category'] = "Switch";
 		$apps[$x]['subcategory'] = "";
 		$apps[$x]['version'] = "1.0";
 		$apps[$x]['license'] = "Mozilla Public License 1.1";
