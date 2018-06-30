1a3c65f33e2b6e2fe60bf915013961ce3256090f
FusionPBX <markjcrane@gmail.com>
2018-06-29 20:09:22 -0600

    Update persist_form.php

--- resources/persist_form.php
+++ resources/persist_form.php
@@ -27,15 +27,15 @@
 function persistform($form_array) {
 	// Remember Form Input Values
 	if (is_array($form_array)) {
-		$content .= "<form method='post' action='".$_SERVER["HTTP_REFERER"]."' target='_self'>\n";
+		$content .= "<form method='post' action='".escape($_SERVER["HTTP_REFERER"])."' target='_self'>\n";
 		foreach ($form_array as $key => $val) {
 			if ($key == "XID" || $key == "ACT" || $key == "RET") continue;
 			if ($key != "persistform") { //clears the persistform value
 			    $content .= "<input type='hidden' name='".escape($key)."' value='".escape($val)."' />\n";
 			}
 		}
-		$content .= "<input type='hidden' name='persistform' value='1' />\n"; //sets persistform to yes
-		$content .= "<input class='btn' type='submit' value='Back' />\n";
+		$content .= "	<input type='hidden' name='persistform' value='1' />\n"; //sets persistform to yes
+		$content .= "	<input class='btn' type='submit' value='Back' />\n";
 		$content .= "</form>\n";
 	}
 	return $content;
