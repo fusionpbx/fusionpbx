7ab30d8f7e88cd3c83b7a3f68922a751ef96240f
FusionPBX <markjcrane@gmail.com>
2018-06-30 17:32:08 -0600

    Update ivr_menus.php

--- app/ivr_menus/ivr_menus.php
+++ app/ivr_menus/ivr_menus.php
@@ -100,7 +100,7 @@
 //prepare to page the results
 	$rows_per_page = ($_SESSION['domain']['paging']['numeric'] != '') ? $_SESSION['domain']['paging']['numeric'] : 50;
 	$param = "";
-	$page = $_GET['page'];
+	$page = escape($_GET['page']);
 	if (strlen($page) == 0) { $page = 0; $_GET['page'] = 0; }
 	list($paging_controls, $rows_per_page, $var3) = paging($num_rows, $param, $rows_per_page);
 	$offset = $rows_per_page * $page;
