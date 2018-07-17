71d152cb6b61916ac064f88bf52daf24743afbcb
konradSC <konradSC@users.noreply.github.com>
2018-07-17 00:31:14 -0400

    List does not display with escape (#3242)
    List does not display with escape

--- core/users/user_edit.php
+++ core/users/user_edit.php
@@ -709,7 +709,7 @@ if (count($_POST) > 0 && $_POST["persistform"] != "1") {
 				if ($row['contact_name_family'] != '') { $contact_name[] = $row['contact_name_family']; }
 				if ($row['contact_name_given'] != '') { $contact_name[] = $row['contact_name_given']; }
 				if ($row['contact_name_family'] == '' && $row['contact_name_family'] == '' && $row['contact_nickname'] != '') { $contact_name[] = $row['contact_nickname']; }
-				echo "<option value='".escape($row['contact_uuid'])."' ".(($row['contact_uuid'] == $contact_uuid) ? "selected='selected'" : null).">".implode(', ', escape($contact_name))."</option>\n";
+				echo "<option value='".$row['contact_uuid']."' ".(($row['contact_uuid'] == $contact_uuid) ? "selected='selected'" : null).">".implode(', ', $contact_name)."</option>\n";
 		}
 		unset($sql, $result, $row_count);
 		echo "</select>\n";
