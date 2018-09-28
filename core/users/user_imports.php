ccb33f21906581a56685dfc0726611066318eb4d
FusionPBX <markjcrane@gmail.com>
2018-09-28 12:51:01 -0600

    Update user_imports.php

--- core/users/user_imports.php
+++ core/users/user_imports.php
@@ -143,7 +143,7 @@
 
 			echo "	<tr>\n";
 			echo "	<td valign='top' align='left' nowrap='nowrap'>\n";
-			echo "		<b>".$text['header-users']."</b><br />\n";
+			echo "		<b>".$text['header-import']."</b><br />\n";
 			echo "	</td>\n";
 			echo "	<td valign='top' align='right'>\n";
 			echo "		<input type='button' class='btn' name='' alt='".$text['button-back']."' onclick=\"window.location='users.php'\" value='".$text['button-back']."'>\n";
