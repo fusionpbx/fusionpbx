eed2e3d6495b5331e3d1a19e859b7112b1f6d9a3
FusionPBX <markjcrane@gmail.com>
2018-09-28 10:46:03 -0600

    Update user_imports.php

--- core/users/user_imports.php
+++ core/users/user_imports.php
@@ -367,7 +367,7 @@
 					}
 
 				//send the redirect header
-					header("Location: ../../core/users/users.php");
+					header("Location: users.php");
 					return;
 			}
 	}
@@ -383,7 +383,7 @@
 	echo "		".$text['description-import']."\n";
 	echo "	</td>\n";
 	echo "	<td valign='top' width='70%' align='right'>\n";
-	echo "		<input type='button' class='btn' name='' alt='".$text['button-back']."' onclick=\"window.location='../../core/users/users.php?".$_GET["query_string"]."'\" value='".$text['button-back']."'>\n";
+	echo "		<input type='button' class='btn' name='' alt='".$text['button-back']."' onclick=\"window.location='users.php?".$_GET["query_string"]."'\" value='".$text['button-back']."'>\n";
 	//echo "		<input name='submit' type='submit' class='btn' id='import' value=\"".$text['button-import']."\">\n";
 	echo "	</td>\n";
 	echo "	</tr>\n";
