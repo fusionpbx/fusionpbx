24e5909f5524eafeac0030aa50e6f79f12c45e54
FusionPBX <markjcrane@gmail.com>
2018-10-17 22:01:13 -0600

    Update app_config.php

--- app/xml_cdr/app_config.php
+++ app/xml_cdr/app_config.php
@@ -164,7 +164,8 @@
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
 		$apps[$x]['db'][$y]['fields'][$z]['deprecated'] = "true";
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = "uuid";
+		$apps[$x]['db'][$y]['fields'][$z]['name']['text'] = "xml_cdr_uuid";
+		$apps[$x]['db'][$y]['fields'][$z]['name']['deprecated'] = "uuid";
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = "uuid";
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = "char(36)";
