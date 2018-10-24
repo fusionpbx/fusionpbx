7dbee814b429c98eb25b8e6084389ded12be5dd2
FusionPBX <markjcrane@gmail.com>
2018-10-23 23:12:24 -0600

    Update device_edit.php

--- app/devices/device_edit.php
+++ app/devices/device_edit.php
@@ -807,22 +807,24 @@
 		$template_dir = $device->get_template_dir();
 		echo "	<select id='device_template' name='device_template' class='formfld'>\n";
 		echo "		<option value=''></option>\n";
-		foreach($device_vendors as $row) {
-			echo "		<optgroup label='".escape($row["name"])."'>\n";
-			$templates = scandir($template_dir.'/'.$row["name"]);
-			foreach($templates as $dir) {
-				if ($file != "." && $dir != ".." && $dir[0] != '.') {
-					if (is_dir($template_dir . '/' . $row["name"] .'/'. $dir)) {
-						if ($device_template == $row["name"]."/".$dir) {
-							echo "			<option value='".escape($row["name"])."/".escape($dir)."' selected='selected'>".escape($row["name"])."/".escape($dir)."</option>\n";
-						}
-						else {
-							echo "			<option value='".escape($row["name"])."/".escape($dir)."'>".$row["name"]."/".escape($dir)."</option>\n";
+		if (is_dir($template_dir) && is_array($device_vendors)) {
+			foreach($device_vendors as $row) {
+				echo "		<optgroup label='".escape($row["name"])."'>\n";
+				$templates = scandir($template_dir.'/'.$row["name"]);
+				foreach($templates as $dir) {
+					if ($file != "." && $dir != ".." && $dir[0] != '.') {
+						if (is_dir($template_dir . '/' . $row["name"] .'/'. $dir)) {
+							if ($device_template == $row["name"]."/".$dir) {
+								echo "			<option value='".escape($row["name"])."/".escape($dir)."' selected='selected'>".escape($row["name"])."/".escape($dir)."</option>\n";
+							}
+							else {
+								echo "			<option value='".escape($row["name"])."/".escape($dir)."'>".$row["name"]."/".escape($dir)."</option>\n";
+							}
 						}
 					}
 				}
+				echo "		</optgroup>\n";
 			}
-			echo "		</optgroup>\n";
 		}
 		echo "	</select>\n";
 		echo "	<br />\n";
