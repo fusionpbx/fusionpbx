9c6b85bc35dff6da0584f435ad2f3f234717a2cf
FusionPBX <markjcrane@gmail.com>
2018-11-03 11:23:30 -0600

    Update app_config.php

--- app/ivr_menus/app_config.php
+++ app/ivr_menus/app_config.php
@@ -93,6 +93,9 @@
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
 		$apps[$x]['permissions'][$y]['groups'][] = "admin";
 		$y++;
+		$apps[$x]['permissions'][$y]['name'] = "ivr_menu_context";
+		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
+		$y++;
 		$apps[$x]['permissions'][$y]['name'] = "ivr_menu_domain";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
 
