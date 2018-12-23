21c0c3492e5cff87849f62aa9f4b2027d8c93d4b
FusionPBX <markjcrane@gmail.com>
2018-12-23 12:07:24 -0700

    Update index.lua

--- resources/install/scripts/app/ring_groups/index.lua
+++ resources/install/scripts/app/ring_groups/index.lua
@@ -880,7 +880,7 @@
 									session:execute(ring_group_timeout_app, ring_group_timeout_data);
 								end
 							--check and report missed call
-								missed()
+								missed();
 						end
 					else
 						if (ring_group_timeout_app ~= nil) then
