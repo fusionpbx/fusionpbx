6cb5264b1cda605f5a044730384c08c0f71fe065
FusionPBX <markjcrane@gmail.com>
2019-01-19 11:39:04 -0700

    Update follow_me.php

--- app/calls/resources/classes/follow_me.php
+++ app/calls/resources/classes/follow_me.php
@@ -332,8 +332,7 @@ include "root.php";
 						}
 						//toll allow
 						if ($this->toll_allow != '') {
-							$this->toll_allow = str_replace(",", "\,", $this->toll_allow);
-							$variables[] = "toll_allow=''".$this->toll_allow."''";
+							$variables[] = "toll_allow=''".str_replace(",", "\,", $this->toll_allow)."''";
 						}
 
 						$variables[] = "instant_ringback=true";
@@ -396,8 +395,7 @@ include "root.php";
 
 						//toll allow
 						if ($this->toll_allow != '') {
-							$this->toll_allow = str_replace(",", "\,", $this->toll_allow);
-							$variables[] = "toll_allow=''".$this->toll_allow."''";
+							$variables[] = "toll_allow=''".str_replace(",", "\,", $this->toll_allow)."''";
 						}
 
 						if ($this->follow_me_ignore_busy != 'true') {
