01bbccb70619193c5249ba09832b5fe2cdd0cebd
FusionPBX <markjcrane@gmail.com>
2019-02-05 18:15:54 -0700

    Update phrase_detail_delete.php

--- app/phrases/phrase_detail_delete.php
+++ app/phrases/phrase_detail_delete.php
@@ -58,12 +58,9 @@ else {
 //save the xml to the file system if the phrase directory is set
 	save_phrases_xml();
 
-//delete the phrase from memcache
-	$fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
-	if ($fp) {
-		$switch_cmd .= "memcache delete languages:".$phrase_language;
-		$switch_result = event_socket_request($fp, 'api '.$switch_cmd);
-	}
+//clear the cache
+	$cache = new cache;
+	$cache->delete("languages:".$phrase_language);
 
 //redirect the user
 	message::add($text['message-delete']);
