79e42aafef41244d1a8d3f18f8bce7e3b3e7c55e
FusionPBX <markjcrane@gmail.com>
2019-03-16 04:18:21 -0600

    Update app_config.php

--- app/xml_cdr/app_config.php
+++ app/xml_cdr/app_config.php
@@ -421,6 +421,12 @@
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "Save the last application data.";
 		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = "missed_call";
+		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = "boolean";
+		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = "text";
+		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = "text";
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
+		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = "cc_side";
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "Queue side is either member or agent";
