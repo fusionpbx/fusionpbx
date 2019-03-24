0f8b1a9ed2041d47c65d8ecdebea1efe1f4c7c8d
FusionPBX <markjcrane@gmail.com>
2019-03-24 08:04:45 -0600

    Update app_config.php

--- app/ring_groups/app_config.php
+++ app/ring_groups/app_config.php
@@ -254,6 +254,8 @@
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = "char(36)";
 		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = "foreign";
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = "v_dialplans";
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = "dialplan_uuid";
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
 		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = "ring_group_forward_toll_allow";
