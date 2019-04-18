9fe2924511f2c204b68dfa8d31b9dffa60fb8226
FusionPBX <markjcrane@gmail.com>
2019-04-18 09:56:08 -0600

    Update email_log_delete.php

--- app/email_logs/email_log_delete.php
+++ app/email_logs/email_log_delete.php
@@ -48,7 +48,7 @@
 	if ($email_uuid != '') {
 		$sql = "delete from v_email_logs ";
 		$sql .= "where email_uuid = '".$email_log_uuid."' ";
-		if (permission_exists('emails_all') && $_REQUEST['showall'] == 'true') {
+		if (permission_exists('email_log_all') && $_REQUEST['showall'] == 'true') {
 			$sql .= "";
 		} else {
 			$sql .= "and domain_uuid = '".$domain_uuid."' ";
