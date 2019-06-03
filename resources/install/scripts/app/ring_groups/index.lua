f4ffba34e878b6d9e078e208736f6f4bc9e328be
FusionPBX <markjcrane@gmail.com>
2019-06-03 08:24:47 -0600

    Update index.lua

--- resources/install/scripts/app/ring_groups/index.lua
+++ resources/install/scripts/app/ring_groups/index.lua
@@ -227,8 +227,13 @@
 	record_path = recordings_dir .. "/" .. domain_name .. "/archive/" .. os.date("%Y/%b/%d");
 	record_path = record_path:gsub("\\", "/");
 
---set the recording file
-	record_name = uuid .. "." .. record_ext;
+--set the recording file name
+	if (session:ready()) then
+		record_name = session:getVariable("record_name");
+		if (not record_name) then
+			record_name = uuid .. "." .. record_ext;
+		end
+	end
 
 ---set the call_timeout to a higher value to prevent the early timeout of the ring group
 	if (session:ready()) then
