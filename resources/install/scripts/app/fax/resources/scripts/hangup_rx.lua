57f6309744c1de171da2fc2860ea10136cd8ca7c
FusionPBX <markjcrane@gmail.com>
2019-06-11 02:58:52 -0600

    Update hangup_rx.lua

--- resources/install/scripts/app/fax/resources/scripts/hangup_rx.lua
+++ resources/install/scripts/app/fax/resources/scripts/hangup_rx.lua
@@ -58,7 +58,6 @@
 	end
 
 -- escape shell arguments to prevent command injection
-
         local function shell_esc(x)
                 return ("'"..x:gsub('\\', '\\\\')
                        :gsub('\'', '\\\'').."'")
