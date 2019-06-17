5e767f13343c55f25699716485d5b57ebf98b604
FusionPBX <markjcrane@gmail.com>
2019-06-17 02:03:41 -0600

    Update call_center_active.php

--- app/call_center_active/call_center_active.php
+++ app/call_center_active/call_center_active.php
@@ -113,7 +113,7 @@
 		else {// code for IE6, IE5
 			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 		}
-		xmlhttp.open("GET",url,false);
+		xmlhttp.open("GET", url, false);
 		xmlhttp.send(null);
 		//document.getElementById('cmd_response').innerHTML=xmlhttp.responseText;
 	}
