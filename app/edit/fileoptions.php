6c1fc088c9dc9a85eb7dbb06eb1d39f959fe1ce2
FusionPBX <markjcrane@gmail.com>
2019-07-09 15:00:06 -0600

    Update fileoptions.php

--- app/edit/fileoptions.php
+++ app/edit/fileoptions.php
@@ -17,7 +17,7 @@
 
 	The Initial Developer of the Original Code is
 	Mark J Crane <markjcrane@fusionpbx.com>
-	Portions created by the Initial Developer are Copyright (C) 2008-2016
+	Portions created by the Initial Developer are Copyright (C) 2008-2019
 	the Initial Developer. All Rights Reserved.
 
 	Contributor(s):
@@ -56,39 +56,35 @@
 
 //show the content
 	echo "<table cellpadding='0' cellspacing='0' border='0' style='height: 100%; width: 100%;'>\n";
-	echo "<tr>";
-	echo "<td colspan='2'>";
-	
-	echo "<table border='0'>";
-	echo "<form method='post' name='frm' action=''>";
-	echo "<tr><td align='right'>".$text['label-path']."</td><td width='95%'><input type='text' name='folder' id='folder' style=\"width: 100%;\" value=''></td><tr>\n";
-	echo "<tr><td align='right'>".$text['label-file']."</td><td width='95%' style=\"width: 60%;\"><input type='text' name='filename' id='filename' style=\"width: 100%;\" value=''></div></td></tr>\n";
-	echo "</form>";
+	echo "	<tr>";
+	echo "		<td colspan='2'>";
+	echo "			<table border='0'>";
+	echo "				<form method='post' name='frm' action=''>";
+	echo "				<tr><td align='right'>".$text['label-path']."</td><td width='95%'><input type='text' name='folder' id='folder' style=\"width: 100%;\" value=''></td><tr>\n";
+	echo "				<tr><td align='right'>".$text['label-file']."</td><td width='95%' style=\"width: 60%;\"><input type='text' name='filename' id='filename' style=\"width: 100%;\" value=''></div></td></tr>\n";
+	echo "				</form>";
+	echo "			</table>";
+	echo "		</td>\n";
+	echo "	</tr>\n";
+	echo "	<tr>\n";
+	echo "		<td valign='top' width='200' nowrap>";
+	echo "			<iframe src='fileoptionslist.php' style='border: solid 1px #ccc; height: calc(100% - 3px); width: calc(100% - 4px);'></iframe>";
+	echo "		</td>\n";
+	echo "		<td valign='top' style='height: 100%; padding: 0;'>";
+	echo "			<table cellpadding='0' cellspacing='0' border='0' width='100%' height='100%'>";
+	echo "				<tr><td><input type='button' class='btn' onclick=\"window.location='filenew.php?folder='+document.getElementById('folder').value;\" value='".$text['button-add-file']."'></td></tr>\n";
+	echo "				<tr><td><input type='button' class='btn' onclick=\"window.location='foldernew.php?folder='+document.getElementById('folder').value;\" value='".$text['button-add-dir']."'></td></tr>\n";
+	echo "				<tr><td><input type='button' class='btn' onclick=\"window.location='filerename.php?folder='+document.getElementById('folder').value+'&filename='+document.getElementById('filename').value;\" value='".$text['button-rename-file']."'></td></tr>\n";
+	echo "				<tr><td><input type='button' class='btn' onclick=\"window.location='filedelete.php?folder='+document.getElementById('folder').value+'&file='+document.getElementById('filename').value;\" value='".$text['button-del-file']."'></td></tr>\n";
+	echo "				<tr><td><input type='button' class='btn' onclick=\"window.location='folderdelete.php?folder='+document.getElementById('folder').value;\" value='".$text['button-del-dir']."'></td></tr>\n";
+	echo "				<tr><td height='100%'>&nbsp;</td></tr>\n";
+	echo "				<tr><td><input type='button' class='btn' onclick='opener.location.reload(); self.close();' value='".$text['button-close']."'></td></tr>\n";
+	echo "			</table>";
+	echo "		</td>\n";
+	echo "	</tr>\n";
 	echo "</table>";
-	
-	echo "</td>\n";
-	echo "</tr>\n";
-	
-	echo "<tr>\n";
-	echo "<td valign='top' width='200' nowrap>";
-	echo "  <iframe src='fileoptionslist.php' style='border: solid 1px #ccc; height: calc(100% - 3px); width: calc(100% - 4px);'></iframe>";
-	echo "</td>\n";
-	echo "<td valign='top' style='height: 100%; padding: 0;'>";
-	
-	echo "<table cellpadding='0' cellspacing='0' border='0' width='100%' height='100%'>";
-	echo "  <tr><td><input type='button' class='btn' onclick=\"window.location='filenew.php?folder='+document.getElementById('folder').value;\" value='".$text['button-add-file']."'></td></tr>\n";
-	echo "  <tr><td><input type='button' class='btn' onclick=\"window.location='foldernew.php?folder='+document.getElementById('folder').value;\" value='".$text['button-add-dir']."'></td></tr>\n";
-	echo "  <tr><td><input type='button' class='btn' onclick=\"window.location='filerename.php?folder='+document.getElementById('folder').value+'&filename='+document.getElementById('filename').value;\" value='".$text['button-rename-file']."'></td></tr>\n";
-	echo "  <tr><td><input type='button' class='btn' onclick=\"if (confirm('".$text['message-delete-file']."')){ window.location='filedelete.php?folder='+document.getElementById('folder').value+'&file='+document.getElementById('filename').value; }\" value='".$text['button-del-file']."'></td></tr>\n";
-	echo "  <tr><td><input type='button' class='btn' onclick=\"if (confirm('".$text['message-delete-folder']."')){ window.location='folderdelete.php?folder='+document.getElementById('folder').value; }\" value='".$text['button-del-dir']."'></td></tr>\n";
-	echo "  <tr><td height='100%'>&nbsp;</td></tr>\n";
-	echo "  <tr><td><input type='button' class='btn' onclick='opener.location.reload(); self.close();' value='".$text['button-close']."'></td></tr>\n";
-	echo "</table>";
-	
-	echo "</td>\n";
-	echo "</tr>\n";
-	echo "</table>";
-	
+
+//include the footer
 	require_once "footer.php";
 
 ?>
