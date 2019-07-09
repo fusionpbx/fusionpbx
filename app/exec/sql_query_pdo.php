f9fec33a81671d1cdd1c5e2c74846f72ed0fb8dd
FusionPBX <markjcrane@gmail.com>
2019-07-09 12:14:21 -0600

    Update sql_query_pdo.php

--- app/exec/sql_query_pdo.php
+++ app/exec/sql_query_pdo.php
@@ -23,16 +23,20 @@
  Contributor(s):
  Mark J Crane <markjcrane@fusionpbx.com>
  */
-require_once "root.php";
-require_once "resources/require.php";
-require_once "resources/check_auth.php";
-if (if_group("admin") || if_group("superadmin")) {
-	//access granted
-}
-else {
-	echo "access denied";
-	exit;
-}
+
+//includes
+	require_once "root.php";
+	require_once "resources/require.php";
+	require_once "resources/check_auth.php";
+
+//check permissions
+	if (if_group("admin") || if_group("superadmin")) {
+		//access granted
+	}
+	else {
+		echo "access denied";
+		exit;
+	}
 
  //set the default values
 	if (isset($db_file_path) > 0) {
