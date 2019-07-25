4a2025a73a8bfab1d810629efbda48cea65eb2a0
FusionPBX <markjcrane@gmail.com>
2019-07-25 09:37:03 -0600

    Update app_defaults.php

--- core/upgrade/app_defaults.php
+++ core/upgrade/app_defaults.php
@@ -28,36 +28,37 @@
 if ($domains_processed == 1) {
 
 	//update the software table
-		$sql = "select count(*) as num_rows from v_software ";
-		$prep_statement = $db->prepare($sql);
-		if ($prep_statement) {
-			$prep_statement->execute();
-			$row = $prep_statement->fetch(PDO::FETCH_ASSOC);
-			if ($row['num_rows'] == 0) {
-				$sql = "insert into v_software ";
-				$sql .= "(";
-				$sql .= "software_uuid, ";
-				$sql .= "software_name, ";
-				$sql .= "software_url, ";
-				$sql .= "software_version ";
-				$sql .= ")";
-				$sql .= "values ";
-				$sql .= "(";
-				$sql .= "'".uuid()."', ";
-				$sql .= "'FusionPBX', ";
-				$sql .= "'www.fusionpbx.com', ";
-				$sql .= "'".software_version()."' ";
-				$sql .= ")";
-				$db->exec(check_sql($sql));
-				unset($sql);
-			}
-			else {
-				$sql = "update v_software ";
-				$sql .= "set software_version = '".software_version()."' ";
-				$db->exec(check_sql($sql));
-				unset($sql);
-			}
-			unset($prep_statement, $row);
+		$sql = "select count(*) from v_software ";
+		$database = new database;
+		$num_rows = $database->execute($sql, null, 'column');
+		if ($row['num_rows'] == 0) {
+			$sql = "insert into v_software ";
+			$sql .= "(";
+			$sql .= "software_uuid, ";
+			$sql .= "software_name, ";
+			$sql .= "software_url, ";
+			$sql .= "software_version ";
+			$sql .= ")";
+			$sql .= "values ";
+			$sql .= "(";
+			$sql .= ":software_uuid, ";
+			$sql .= "'FusionPBX', ";
+			$sql .= "'www.fusionpbx.com', ";
+			$sql .= ":software_version ";
+			$sql .= ")";
+			$parameters['software_uuid'] = uuid();
+			$parameters['software_version'] = software_version();
+			$database = new database;
+			$database->execute($sql, $parameters);
+			unset($sql, $parameters);
+		}
+		else {
+			$sql = "update v_software ";
+			$sql .= "set software_version = :software_version ";
+			$parameters['software_version'] = software_version();
+			$database = new database;
+			$database->execute($sql, $parameters);
+			unset($sql, $parameters);
 		}
 
 	//ensure the login message is set, if new message exists
@@ -65,72 +66,71 @@ if ($domains_processed == 1) {
 		$sql .= "where default_setting_category = 'login' ";
 		$sql .= "and default_setting_subcategory = 'message' ";
 		$sql .= "and default_setting_name = 'text' ";
-		$prep_statement = $db->prepare($sql);
-		if ($prep_statement) {
-			$prep_statement->execute();
-			$row = $prep_statement->fetch(PDO::FETCH_ASSOC);
-			unset($prep_statement);
-			if ($row['num_rows'] == 0) {
+		$database = new database;
+		$num_rows = $database->execute($sql, null, 'column');
+		if ($num_rows == 0) {
 
-				// insert message
-				$sql = "insert into v_default_settings ";
-				$sql .= "(";
-				$sql .= "default_setting_uuid, ";
-				$sql .= "default_setting_category, ";
-				$sql .= "default_setting_subcategory, ";
-				$sql .= "default_setting_name, ";
-				$sql .= "default_setting_value, ";
-				$sql .= "default_setting_enabled, ";
-				$sql .= "default_setting_description ";
-				$sql .= ")";
-				$sql .= "values ";
-				$sql .= "(";
-				$sql .= "'e2bff94b-2c68-45ee-9141-d4cdb437c644', ";
-				$sql .= "'login', ";
-				$sql .= "'message', ";
-				$sql .= "'text', ";
-				$sql .= "'".$text['login-message_text']."', ";
-				$sql .= "'true', ";
-				$sql .= "'' ";
-				$sql .= ")";
-				$db->exec(check_sql($sql));
-				unset($sql);
+			// insert message
+			$sql = "insert into v_default_settings ";
+			$sql .= "(";
+			$sql .= "default_setting_uuid, ";
+			$sql .= "default_setting_category, ";
+			$sql .= "default_setting_subcategory, ";
+			$sql .= "default_setting_name, ";
+			$sql .= "default_setting_value, ";
+			$sql .= "default_setting_enabled, ";
+			$sql .= "default_setting_description ";
+			$sql .= ")";
+			$sql .= "values ";
+			$sql .= "(";
+			$sql .= "'e2bff94b-2c68-45ee-9141-d4cdb437c644', ";
+			$sql .= "'login', ";
+			$sql .= "'message', ";
+			$sql .= "'text', ";
+			$sql .= "':default_setting_value, ";
+			$sql .= "'true', ";
+			$sql .= "'' ";
+			$sql .= ")";
+			$parameters['default_setting_value'] = $text['login-message_text'];
+			$database = new database;
+			$database->execute($sql, $parameters);
+			unset($sql, $parameters);
 
-			}
-			else {
-
-				// get current message value
-				$sql = "select default_setting_uuid, default_setting_value ";
-				$sql .= "from v_default_settings ";
-				$sql .= "where default_setting_category = 'login' ";
-				$sql .= "and default_setting_subcategory = 'message' ";
-				$sql .= "and default_setting_name = 'text' ";
-				$prep_statement = $db->prepare($sql);
-				if ($prep_statement) {
-					$prep_statement->execute();
-					$result = $prep_statement->fetchAll(PDO::FETCH_NAMED);
-					if (count($result) > 0) {
-						foreach($result as $row) {
-							$current_default_setting_uuid = $row["default_setting_uuid"];
-							$current_default_setting_value = $row["default_setting_value"];
-							break;
-						}
-
-						// compare to message in language file, update and enable if different
-						$new_default_setting_value = str_replace("''", "'", $text['login-message_text']);
-						if ($current_default_setting_value != $new_default_setting_value) {
-							$sql = "update v_default_settings set ";
-							$sql .= "default_setting_value = '".$text['login-message_text']."', ";
-							$sql .= "default_setting_enabled = 'true' ";
-							$sql .= "where default_setting_uuid = '".$current_default_setting_uuid."' ";
-							$db->exec(check_sql($sql));
-							unset($sql);
-						}
-					}
-					unset($prep_statement, $result);
-				}
-			}
 		}
+		else {
+
+			// get current message value
+			$sql = "select default_setting_uuid, default_setting_value ";
+			$sql .= "from v_default_settings ";
+			$sql .= "where default_setting_category = 'login' ";
+			$sql .= "and default_setting_subcategory = 'message' ";
+			$sql .= "and default_setting_name = 'text' ";
+			$database = new database;
+			$result = $database->execute($sql, null, 'all');
+			if (is_array($result) && count($result) > 0) {
+					foreach($result as $row) {
+						$current_default_setting_uuid = $row["default_setting_uuid"];
+						$current_default_setting_value = $row["default_setting_value"];
+						break;
+					}
+
+					// compare to message in language file, update and enable if different
+					$new_default_setting_value = str_replace("''", "'", $text['login-message_text']);
+					if ($current_default_setting_value != $new_default_setting_value) {
+						$sql = "update v_default_settings set ";
+						$sql .= "default_setting_value = :default_setting_value, ";
+						$sql .= "default_setting_enabled = 'true' ";
+						$sql .= "where default_setting_uuid = :default_setting_uuid ";
+						$parameters['default_setting_value'] = $text['login-message_text'];
+						$parameters['default_setting_uuid'] = $current_default_setting_uuid;
+						$database = new database;
+						$database->execute($sql, $parameters);
+						unset($sql, $parameters);
+					}
+				}
+				unset($prep_statement, $result);
+		}
+
 }
 
 ?>
