00bac027cc0f0c0d19741fed8f0f7af20f925a85
FusionPBX <markjcrane@gmail.com>
2019-08-09 19:59:01 -0600

    Update stream_delete.php

--- app/streams/stream_delete.php
+++ app/streams/stream_delete.php
@@ -24,6 +24,16 @@
 //includes
 	require_once "root.php";
 	require_once "resources/require.php";
+	require_once "resources/check_auth.php";
+
+//check permissions
+	if (permission_exists('stream_delete')) {
+		//access granted
+	}
+	else {
+		echo "access denied";
+		exit;
+	}
 
 //add multi-lingual support
 	$language = new text;
@@ -33,7 +43,7 @@
 	messages::add($text['message-delete']);
 
 //delete the data
-	if (isset($_GET["id"]) && is_uuid($_GET["id"]) && permission_exists('stream_delete')) {
+	if (isset($_GET["id"]) && is_uuid($_GET["id"])) {
 
 		//get the id
 			$id = check_str($_GET["id"]);
