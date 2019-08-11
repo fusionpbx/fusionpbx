6e7f42715569263a17ae2556fb9752df8a87c946
benwiesner1 <42681729+benwiesner1@users.noreply.github.com>
2019-08-11 13:57:28 -0700

    Update app_menu.php
    seems more logical to move this menu item to the Status category

--- app/errors/app_menu.php
+++ app/errors/app_menu.php
@@ -20,7 +20,7 @@
 	$apps[$x]['menu'][$y]['title']['sv-se'] = "";
 	$apps[$x]['menu'][$y]['title']['uk-ua'] = "";
 	$apps[$x]['menu'][$y]['uuid'] = "ccdddbd1-40d2-46ca-a86f-4ed93eba4942";
-	$apps[$x]['menu'][$y]['parent_uuid'] = "594d99c5-6128-9c88-ca35-4b33392cec0f";
+	$apps[$x]['menu'][$y]['parent_uuid'] = "0438b504-8613-7887-c420-c837ffb20cb1";
 	$apps[$x]['menu'][$y]['category'] = "internal";
 	$apps[$x]['menu'][$y]['path'] = "/app/errors/errors.php";
 	$apps[$x]['menu'][$y]['groups'][] = "superadmin";
