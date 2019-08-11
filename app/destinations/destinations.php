704ce69438476c9de6d773a3620210e2b29b5b3e
FusionPBX <markjcrane@gmail.com>
2019-08-11 08:47:04 -0600

    Update destinations.php

--- app/destinations/destinations.php
+++ app/destinations/destinations.php
@@ -265,7 +265,7 @@
 			echo "	</td>\n";
 			if ($_GET['show'] == "all" && permission_exists('destination_all')) {
 				if (strlen($_SESSION['domains'][$row['domain_uuid']]['domain_name']) > 0) {
-					$domain = escape($_SESSION['domains'][$row['domain_uuid']]['domain_name']);
+					$domain = $_SESSION['domains'][$row['domain_uuid']]['domain_name'];
 				}
 				else {
 					$domain = $text['label-global'];
