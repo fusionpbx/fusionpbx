14ac4ab07cf9b006fe46fadd76f585759029f628
Nate <github@najo.com>
2019-08-12 04:34:48 -0600

    Phrases: Database class integration.

--- app/phrases/phrase_detail_delete.php
+++ app/phrases/phrase_detail_delete.php
@@ -43,29 +43,44 @@
 	$text = $language->get();
 
 //get values
-	$phrase_detail_uuid = check_str($_GET["pdid"]);
-	$phrase_uuid = check_str($_GET["pid"]);
-	$phrase_language = check_str($_GET["lang"]);
+	$phrase_detail_uuid = $_GET["pdid"];
+	$phrase_uuid = $_GET["pid"];
+	$phrase_language = $_GET["lang"];
 
 //delete the detail entry
-	if ($phrase_detail_uuid != '' && $phrase_uuid != '') {
-		$sql = "delete from v_phrase_details ";
-		$sql .= " where phrase_detail_uuid = '".$phrase_detail_uuid."'";
-		$sql .= " and phrase_uuid = '".$phrase_uuid."' ";
-		$sql .= " and domain_uuid = '".$domain_uuid."' ";
-		$db->exec(check_sql($sql));
-		unset($sql);
+	if (is_uuid($phrase_detail_uuid) && is_uuid($phrase_uuid)) {
+		//build array
+			$array['phrase_details'][0]['phrase_detail_uuid'] = $phrase_detail_uuid;
+			$array['phrase_details'][0]['phrase_uuid'] = $phrase_uuid;
+			$array['phrase_details'][0]['domain_uuid'] = $domain_uuid;
+
+		//grant temporary permissions
+			$p = new permissions;
+			$p->add('phrase_detail_delete', 'temp');
+
+		//execute delete
+			$database = new database;
+			$database->app_name = 'phrases';
+			$database->app_uuid = '5c6f597c-9b78-11e4-89d3-123b93f75cba';
+			$database->delete($array);
+			unset($array);
+
+		//revoke temporary permissions
+			$p->delete('phrase_detail_delete', 'temp');
+
+		//save the xml to the file system if the phrase directory is set
+			save_phrases_xml();
+
+		//clear the cache
+			$cache = new cache;
+			$cache->delete("languages:".$phrase_language);
+
+		//set message
+			message::add($text['message-delete']);
 	}
 
-//save the xml to the file system if the phrase directory is set
-	save_phrases_xml();
-
-//clear the cache
-	$cache = new cache;
-	$cache->delete("languages:".$phrase_language);
-
 //redirect the user
-	message::add($text['message-delete']);
 	header('Location: phrase_edit.php?id='.$phrase_uuid);
+	exit;
 
 ?>
