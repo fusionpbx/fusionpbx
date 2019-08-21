daf69086ab7e86c51d17ca3d86289704d8b3c88f
FusionPBX <markjcrane@gmail.com>
2019-08-21 02:10:13 -0600

    Update clip_update.php

--- app/edit/clip_update.php
+++ app/edit/clip_update.php
@@ -30,7 +30,7 @@
 	require_once "resources/check_auth.php";
 
 //check permissions
-	if (permission_exists('script_editor_save')) {
+	if (permission_exists('clip_edit')) {
 		//access granted
 	}
 	else {
