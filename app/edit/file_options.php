f4baf081c89f9b5a4ea0f55ebff56fb1c924ff06
FusionPBX <markjcrane@gmail.com>
2019-08-21 02:11:30 -0600

    Update file_options.php

--- app/edit/file_options.php
+++ app/edit/file_options.php
@@ -31,7 +31,7 @@
 	require_once "resources/check_auth.php";
 
 //check permissions
-	if (permission_exists('script_editor_view')) {
+	if (permission_exists('edit_save')) {
 		//access granted
 	}
 	else {
