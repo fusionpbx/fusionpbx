25f3ff205136238f01dfa8a79af86d75df03031e
FusionPBX <markjcrane@gmail.com>
2019-09-16 08:00:30 -0600

    Update xml_cdr.php

--- app/xml_cdr/resources/classes/xml_cdr.php
+++ app/xml_cdr/resources/classes/xml_cdr.php
@@ -8,7 +8,7 @@
 	the License. You may obtain a copy of the License at
 	http://www.mozilla.org/MPL/
 
-	Software distributed under the License is distributed on an "as IS" basis,
+	Software distributed under the License is distributed on an "AS IS" basis,
 	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 	for the specific language governing rights and limitations under the
 	License.
