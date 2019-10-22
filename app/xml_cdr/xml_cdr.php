362d7cd786326dacf1f306529d884afb567fd65e
FusionPBX <markjcrane@gmail.com>
2019-10-22 10:06:24 -0600

    Update xml_cdr.php

--- app/xml_cdr/xml_cdr.php
+++ app/xml_cdr/xml_cdr.php
@@ -812,11 +812,11 @@
 						$content .= "	<td class='no-wrap hide-sm-dn'>".ucwords(escape($call_result))."</td>\n";
 					}
 				//action icon
-					if (permission_exists('xml_cdr_details')) {
-						$content .= "	<td class='action-button'>";
-						$content .= button::create(['type'=>'button','title'=>$text['button-view'],'icon'=>$_SESSION['theme']['button_icon_view'],'link'=>$list_row_url]);
-						$content .= "	</td>\n";
-					}
+					//if (permission_exists('xml_cdr_details')) {
+					//	$content .= "	<td class='action-button'>";
+					//	$content .= button::create(['type'=>'button','title'=>$text['button-view'],'icon'=>$_SESSION['theme']['button_icon_view'],'link'=>$list_row_url]);
+					//	$content .= "	</td>\n";
+					//}
 					$content .= "</tr>\n";
 
 				//show the leg b only to those with the permission
