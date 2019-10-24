bf3d3fbcebcdddc7ddf719603f3a52ffc5240c41
FusionPBX <markjcrane@gmail.com>
2019-10-24 03:04:34 -0600

    Update exec.php

--- app/basic_operator_panel/exec.php
+++ app/basic_operator_panel/exec.php
@@ -2,7 +2,7 @@
 /* $Id$ */
 /*
 	v_exec.php
-	Copyright (C) 2008 Mark J Crane
+	Copyright (C) 2008-2019 Mark J Crane
 	All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without
