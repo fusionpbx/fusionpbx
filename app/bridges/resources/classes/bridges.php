b27410c6004dd8937abe0407face66ba233afdc2
Nate <github@najo.com>
2019-10-29 20:18:51 -0600

    Follow Me, Call Forward, DND: List view updates.

--- app/bridges/resources/classes/bridges.php
+++ app/bridges/resources/classes/bridges.php
@@ -69,7 +69,7 @@ if (!class_exists('bridges')) {
 					if (is_array($records) && @sizeof($records) != 0) {
 
 						//build the delete array
-							foreach($records as $x => $record) {
+							foreach ($records as $x => $record) {
 								if ($record['checked'] == 'true' && is_uuid($record['uuid'])) {
 									$array[$this->table][$x][$this->uuid_prefix.'uuid'] = $record['uuid'];
 									$array[$this->table][$x]['domain_uuid'] = $_SESSION['domain_uuid'];
