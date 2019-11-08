3446b0d1db182c4c31aae88ed695c6d6d7c58fe4
FusionPBX <markjcrane@gmail.com>
2019-11-07 23:18:59 -0700

    Update software.php

--- core/software/resources/classes/software.php
+++ core/software/resources/classes/software.php
@@ -11,14 +11,14 @@ if (!class_exists('software')) {
 		/**
 		 * version
 		 */
-		public function version() {
+		public static function version() {
 			return '4.5.10';
 		}
 
 		/**
 		 * numeric_version
 		 */
-		public function numeric_version() {
+		public static function numeric_version() {
 			$v = explode('.', $this->version());
 			$n = ($v[0] * 10000 + $v[1] * 100 + $v[2]);
 			return $n;
