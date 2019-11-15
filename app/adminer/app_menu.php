e4a95effaf41ffa3b0954103336f527f97e3c766
noci2012 <github@noci.work>
2019-11-15 17:29:08 +0100

    Add nl-nl items (#4862)

--- app/adminer/app_menu.php
+++ app/adminer/app_menu.php
@@ -12,7 +12,7 @@
 	$apps[$x]['menu'][$y]['title']['fr-fr'] = "Admin BDD";
 	$apps[$x]['menu'][$y]['title']['he-il'] = "";
 	$apps[$x]['menu'][$y]['title']['it-it'] = "Adminer";
-	$apps[$x]['menu'][$y]['title']['nl-nl'] = "";
+	$apps[$x]['menu'][$y]['title']['nl-nl'] = "Adminer";
 	$apps[$x]['menu'][$y]['title']['pl-pl'] = "";
 	$apps[$x]['menu'][$y]['title']['pt-br'] = "Adminer";
 	$apps[$x]['menu'][$y]['title']['pt-pt'] = "Administrador";
