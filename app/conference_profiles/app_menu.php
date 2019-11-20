ab9baf5c6a20f3877c70d1f997667b6623ef3f3f
noci2012 <noci2012@github.com>
2019-11-20 21:22:18 +0100

    Add nl-nl items (conference_profiles)

--- app/conference_profiles/app_menu.php
+++ app/conference_profiles/app_menu.php
@@ -12,7 +12,7 @@
 	$apps[$x]['menu'][$y]['title']['fr-fr'] = "";
 	$apps[$x]['menu'][$y]['title']['he-il'] = "";
 	$apps[$x]['menu'][$y]['title']['it-it'] = "Profili Conferenza";
-	$apps[$x]['menu'][$y]['title']['nl-nl'] = "";
+	$apps[$x]['menu'][$y]['title']['nl-nl'] = "Conferentie profielen";
 	$apps[$x]['menu'][$y]['title']['pl-pl'] = "";
 	$apps[$x]['menu'][$y]['title']['pt-br'] = "";
 	$apps[$x]['menu'][$y]['title']['pt-pt'] = "";
