de51ef00a447f029772fac0ef27e5beb2993a5df
noci2012 <noci2012@github.com>
2019-11-23 21:01:12 +0100

    nl-nl items (users)

--- core/users/app_config.php
+++ core/users/app_config.php
@@ -19,7 +19,7 @@
 		$apps[$x]['description']['fr-fr'] = "Ajouter, Editer, Supprimer et Chercher des Utilisateurs";
 		$apps[$x]['description']['he-il'] = "";
 		$apps[$x]['description']['it-it'] = "";
-		$apps[$x]['description']['nl-nl'] = "";
+		$apps[$x]['description']['nl-nl'] = "Gebruikers toevoegen, bewerken, verwijderen en zoeken.";
 		$apps[$x]['description']['pl-pl'] = "";
 		$apps[$x]['description']['pt-br'] = "";
 		$apps[$x]['description']['pt-pt'] = "Adicionar, editar, apagar e pesquisa pelos utilizadores.";
--- core/users/app_menu.php
+++ core/users/app_menu.php
@@ -36,7 +36,7 @@
 	$apps[$x]['menu'][$y]['title']['fr-fr'] = "Configuration du Compte";
 	$apps[$x]['menu'][$y]['title']['he-il'] = "הגדרת חשבון";
 	$apps[$x]['menu'][$y]['title']['it-it'] = "Impostazioni Utente";
-	$apps[$x]['menu'][$y]['title']['nl-nl'] = "";
+	$apps[$x]['menu'][$y]['title']['nl-nl'] = "Account instellingen";
 	$apps[$x]['menu'][$y]['title']['pl-pl'] = "Ustawienia konta";
 	$apps[$x]['menu'][$y]['title']['pt-br'] = "Configurações da conta";
 	$apps[$x]['menu'][$y]['title']['pt-pt'] = "Configurações da Conta";
