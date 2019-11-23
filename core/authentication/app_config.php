dde2a48e8ebfad7ae91c3d7d58926d867af8699f
noci2012 <noci2012@github.com>
2019-11-23 17:11:37 +0100

    nl-nl items (authentication)

--- core/authentication/app_config.php
+++ core/authentication/app_config.php
@@ -19,7 +19,7 @@
 		$apps[$x]['description']['fr-fr'] = "";
 		$apps[$x]['description']['he-il'] = "";
 		$apps[$x]['description']['it-it'] = "";
-		$apps[$x]['description']['nl-nl'] = "";
+		$apps[$x]['description']['nl-nl'] = "Voorziet in een autenticatie netwerk met extra plug-ins om te controleren of een gebruiker geautorisserd is";
 		$apps[$x]['description']['pl-pl'] = "";
 		$apps[$x]['description']['pt-br'] = "";
 		$apps[$x]['description']['pt-pt'] = "";
