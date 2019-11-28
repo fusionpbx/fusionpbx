21e7e3137559a71c8a6e73411ffc9685c6da41b7
fneto1977 <47830093+fneto1977@users.noreply.github.com>
2019-11-28 16:17:57 -0300

    app_config - edit

--- app/edit/app_config.php
+++ app/edit/app_config.php
@@ -21,7 +21,7 @@
 		$apps[$x]['description']['it-it'] = "";
 		$apps[$x]['description']['nl-nl'] = "Script bewerker kan worden gebruik om lua, javascript of andere scripts aan te passen.";
 		$apps[$x]['description']['pl-pl'] = "";
-		$apps[$x]['description']['pt-br'] = "";
+		$apps[$x]['description']['pt-br'] = "Editor de script pode ser usado para editar lua, javascript ou outros scripts.";
 		$apps[$x]['description']['pt-pt'] = "Editor de script pode ser usado para editar lua, javascript ou outros scripts.";
 		$apps[$x]['description']['ro-ro'] = "";
 		$apps[$x]['description']['ru-ru'] = "";
