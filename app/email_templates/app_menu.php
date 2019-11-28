b85e1d8d29fb5e473de85c46aabf59f339eee34d
fneto1977 <47830093+fneto1977@users.noreply.github.com>
2019-11-28 16:13:31 -0300

    app_menu - email

--- app/email_templates/app_menu.php
+++ app/email_templates/app_menu.php
@@ -8,6 +8,7 @@ $apps[$x]['menu'][0]['title']['pl'] = '';
 $apps[$x]['menu'][0]['title']['sv-se'] = '';
 $apps[$x]['menu'][0]['title']['uk'] = '';
 $apps[$x]['menu'][0]['title']['de-at'] = '';
+$apps[$x]['menu'][0]['title']['pt-br'] = 'Modelos de Email';
 $apps[$x]['menu'][0]['uuid'] = '5e548de3-ff1a-44e4-a2e6-f3917dac85a3';
 $apps[$x]['menu'][0]['parent_uuid'] = '594d99c5-6128-9c88-ca35-4b33392cec0f';
 $apps[$x]['menu'][0]['category'] = 'internal';
