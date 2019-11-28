7c212f72f69b87477574e4df49b5b966d4f9289d
fneto1977 <47830093+fneto1977@users.noreply.github.com>
2019-11-28 16:49:14 -0300

    app_config - errors

--- app/errors/app_config.php
+++ app/errors/app_config.php
@@ -21,7 +21,7 @@
 		$apps[$x]['description']['it-it'] = "";
 		$apps[$x]['description']['nl-nl'] = "Toon web server fouten.";
 		$apps[$x]['description']['pl-pl'] = "";
-		$apps[$x]['description']['pt-br'] = "";
+		$apps[$x]['description']['pt-br'] = "Mostra os erros do Web Server";
 		$apps[$x]['description']['pt-pt'] = "";
 		$apps[$x]['description']['ro-ro'] = "";
 		$apps[$x]['description']['ru-ru'] = "";
