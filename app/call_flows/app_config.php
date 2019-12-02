23dd969453fead1ea0a5edffd34eaf30aaa5cf24
fneto1977 <47830093+fneto1977@users.noreply.github.com>
2019-12-02 18:48:27 -0300

    call flow - app_config

--- app/call_flows/app_config.php
+++ app/call_flows/app_config.php
@@ -21,7 +21,7 @@
 		$apps[$x]['description']['it-it'] = "";
 		$apps[$x]['description']['nl-nl'] = "Directe oproepen tussen twee bestemmingen via een feature code.";
 		$apps[$x]['description']['pl-pl'] = "";
-		$apps[$x]['description']['pt-br'] = "";
+		$apps[$x]['description']['pt-br'] = "Direciona chamadas entre dois destinos através de um código de funcionalidade.";
 		$apps[$x]['description']['pt-pt'] = "";
 		$apps[$x]['description']['ro-ro'] = "";
 		$apps[$x]['description']['ru-ru'] = "";
