64ee1051d771c5aee774d42f93e7bbeadc157617
fneto1977 <47830093+fneto1977@users.noreply.github.com>
2019-12-02 17:49:36 -0300

    contact - app_config

--- app/contacts/app_config.php
+++ app/contacts/app_config.php
@@ -21,7 +21,7 @@
 		$apps[$x]['description']['it-it'] = "";
 		$apps[$x]['description']['nl-nl'] = "Kontacten";
 		$apps[$x]['description']['pl-pl'] = "";
-		$apps[$x]['description']['pt-br'] = "";
+		$apps[$x]['description']['pt-br'] = "Fornece um local para armazenar informações de contato para indivíduos e organizações.";
 		$apps[$x]['description']['pt-pt'] = "Fornece um local para armazenar informações de contato para indivíduos e organizações.";
 		$apps[$x]['description']['ro-ro'] = "";
 		$apps[$x]['description']['ru-ru'] = "";
