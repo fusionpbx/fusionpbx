71630ecd68123d70b49701512e2afbc7592acaa9
fneto1977 <47830093+fneto1977@users.noreply.github.com>
2019-12-02 17:36:04 -0300

    conf pro - app_menu

--- app/conference_profiles/app_menu.php
+++ app/conference_profiles/app_menu.php
@@ -14,7 +14,7 @@
 	$apps[$x]['menu'][$y]['title']['it-it'] = "Profili Conferenza";
 	$apps[$x]['menu'][$y]['title']['nl-nl'] = "Conferentie profielen";
 	$apps[$x]['menu'][$y]['title']['pl-pl'] = "";
-	$apps[$x]['menu'][$y]['title']['pt-br'] = "";
+	$apps[$x]['menu'][$y]['title']['pt-br'] = "Perfis de Conferências";
 	$apps[$x]['menu'][$y]['title']['pt-pt'] = "";
 	$apps[$x]['menu'][$y]['title']['ro-ro'] = "";
 	$apps[$x]['menu'][$y]['title']['ru-ru'] = 'Конференции Профили';
