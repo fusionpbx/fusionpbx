59c2766756d14e0ef1ce48a5fec73e06f940056a
fneto1977 <47830093+fneto1977@users.noreply.github.com>
2019-12-02 23:14:33 -0300

    Update app_config.php

--- app/ivr_menus/app_config.php
+++ app/ivr_menus/app_config.php
@@ -21,7 +21,7 @@
 		$apps[$x]['description']['it-it'] = "";
 		$apps[$x]['description']['nl-nl'] = "Het IVR menu speelt een vooraf opgenomen tekst af die de beller opties om uit te kiezen ggeft. Elke optie krijgt een specifieke bestemming. De bestemming kan een toestel, antwoorapparaat, IVR menu, belgroep etc. zijn.";
 		$apps[$x]['description']['pl-pl'] = "";
-		$apps[$x]['description']['pt-br'] = "a URA toca uma gravação ou uma frase pré-definida, que são apresentados ao chamador na forma de opções para escolher. Cada opção tem um destino correspondente. Os destinos podem ser ramais, correio de voz, outros URAs, grupos, ramais de fax, etc.";
+		$apps[$x]['description']['pt-br'] = "A URA toca uma gravação ou uma frase pré-definida, que são apresentados ao chamador na forma de opções para escolher. Cada opção tem um destino correspondente. Os destinos podem ser ramais, correio de voz, outros URAs, grupos, ramais de fax, etc.";
 		$apps[$x]['description']['pt-pt'] = "O menu IVR toca uma gravação ou uma frase pré-definidos, que são apresentados ao chamador na forma de opções para escolher. Cada opção tem um destino correspondente. Os destinos podem ser extensões, correio de voz, outros IVRs, grupos, extensões de fax, etc.";
 		$apps[$x]['description']['ro-ro'] = "";
 		$apps[$x]['description']['ru-ru'] = "";
