fc88bf007aa5ea7b045692f682c8f5b81870b4fe
fneto1977 <47830093+fneto1977@users.noreply.github.com>
2019-12-03 01:05:28 -0300

    Update app_config.php

--- core/authentication/app_config.php
+++ core/authentication/app_config.php
@@ -21,7 +21,7 @@
 		$apps[$x]['description']['it-it'] = "";
 		$apps[$x]['description']['nl-nl'] = "";
 		$apps[$x]['description']['pl-pl'] = "";
-		$apps[$x]['description']['pt-br'] = "";
+		$apps[$x]['description']['pt-br'] = "Prove um framework de autenticação com plugins para verificar se o usuário é autorizado a entrar no sistema.";
 		$apps[$x]['description']['pt-pt'] = "";
 		$apps[$x]['description']['ro-ro'] = "";
 		$apps[$x]['description']['ru-ru'] = "Предоставляет платформу проверки подлинности с плагинами для проверки авторизации пользователя.";
