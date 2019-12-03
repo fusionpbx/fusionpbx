556ae67bd294454f974d59be4e282516a6578651
fneto1977 <47830093+fneto1977@users.noreply.github.com>
2019-12-03 01:21:03 -0300

    noti - app_config

--- core/notifications/app_config.php
+++ core/notifications/app_config.php
@@ -20,7 +20,7 @@
 		$apps[$x]['description']['it-it'] = "";
 		$apps[$x]['description']['nl-nl'] = "";
 		$apps[$x]['description']['pl-pl'] = "";
-		$apps[$x]['description']['pt-br'] = "";
+		$apps[$x]['description']['pt-br'] = "Configure as preferências de notificações.";
 		$apps[$x]['description']['pt-pt'] = "";
 		$apps[$x]['description']['ro-ro'] = "";
 		$apps[$x]['description']['ru-ru'] = "Настройка параметров уведомлений";
