bcc72b3e1acd660ecefefac70ec44a0699429672
fneto1977 <47830093+fneto1977@users.noreply.github.com>
2019-12-03 00:46:14 -0300

    pin - app_config

--- app/pin_numbers/app_config.php
+++ app/pin_numbers/app_config.php
@@ -21,7 +21,7 @@
 		$apps[$x]['description']['it-it'] = "";
 		$apps[$x]['description']['nl-nl'] = "Beheer PIN codes en account codes.";
 		$apps[$x]['description']['pl-pl'] = "";
-		$apps[$x]['description']['pt-br'] = "";
+		$apps[$x]['description']['pt-br'] = "Gerencia os números PIN e códigos de conta.";
 		$apps[$x]['description']['pt-pt'] = "";
 		$apps[$x]['description']['ro-ro'] = "";
 		$apps[$x]['description']['ru-ru'] = "";
