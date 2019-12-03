fe7c24dc7c0cc33dcc7ce70b3c0b3de4fce38eb1
fneto1977 <47830093+fneto1977@users.noreply.github.com>
2019-12-02 23:28:30 -0300

    moh - app_config

--- app/music_on_hold/app_config.php
+++ app/music_on_hold/app_config.php
@@ -21,7 +21,7 @@
 		$apps[$x]['description']['it-it'] = "";
 		$apps[$x]['description']['nl-nl'] = "Voeg toe, verwijder of speel wachtmuziek bestanden.";
 		$apps[$x]['description']['pl-pl'] = "";
-		$apps[$x]['description']['pt-br'] = "";
+		$apps[$x]['description']['pt-br'] = "Adicionar, excluir ou reproduzir música de espera em arquivos.";
 		$apps[$x]['description']['pt-pt'] = "Adicionar, excluir ou reproduzir música em arquivos de espera.";
 		$apps[$x]['description']['ro-ro'] = "";
 		$apps[$x]['description']['ru-ru'] = "";
