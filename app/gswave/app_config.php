ea7e54ca6e662c44182fa5d86d015fc05d4eee9e
fneto1977 <47830093+fneto1977@users.noreply.github.com>
2019-12-02 22:57:22 -0300

    gswave - app_config

--- app/gswave/app_config.php
+++ app/gswave/app_config.php
@@ -21,7 +21,7 @@
 		$apps[$x]['description']['it-it'] = "";
 		$apps[$x]['description']['nl-nl'] = "";
 		$apps[$x]['description']['pl-pl'] = "";
-		$apps[$x]['description']['pt-br'] = "";
+		$apps[$x]['description']['pt-br'] = "Geração de QR Code para configuração do soft fone Grandstream Wave";
 		$apps[$x]['description']['pt-pt'] = "";
 		$apps[$x]['description']['ro-ro'] = "";
 		$apps[$x]['description']['ru-ru'] = "";
