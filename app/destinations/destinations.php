45fd6cd195dab8d8274abe41cc4672c3774e17c7
Nate <github@najo.com>
2020-01-06 10:24:49 -0700

    Destinations: Add window title.

--- app/destinations/destinations.php
+++ app/destinations/destinations.php
@@ -164,6 +164,7 @@
 	$token = $object->create($_SERVER['PHP_SELF']);
 
 //include the header
+	$document['title'] = $text['title-destinations'];
 	require_once "resources/header.php";
 
 //show the content
