d151f2c11fc1812c88d4a6a501fefcc7e6ff8645
Nate <github@najo.com>
2020-01-06 11:14:01 -0700

    Conference Centers: Add window titles.

--- app/conference_centers/conference_centers.php
+++ app/conference_centers/conference_centers.php
@@ -125,6 +125,7 @@
 	$token = $object->create($_SERVER['PHP_SELF']);
 
 //include the header
+	$document['title'] = $text['title-conference_centers'];
 	require_once "resources/header.php";
 
 //show the content
