a8501b35f0606456bcdf27c1aa0b5b66e8b3be76
Nate <github@najo.com>
2020-01-06 11:00:41 -0700

    Call Recordings: Add window title.

--- app/call_recordings/call_recordings.php
+++ app/call_recordings/call_recordings.php
@@ -121,6 +121,7 @@
 	$token = $object->create($_SERVER['PHP_SELF']);
 
 //include the header
+	$document['title'] = $text['title-call_recordings'];
 	require_once "resources/header.php";
 
 //show the content
