c7cc354036b79bdc43e7bf13d129435ffc10437f
Nate <github@najo.com>
2020-01-06 11:25:38 -0700

    Conferences: Add window titles.

--- app/conferences/conferences.php
+++ app/conferences/conferences.php
@@ -134,6 +134,7 @@
 	$token = $object->create($_SERVER['PHP_SELF']);
 
 //include the header
+	$document['title'] = $text['title-conferences'];
 	require_once "resources/header.php";
 
 //show the content
