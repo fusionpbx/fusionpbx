05f63ce960c0b0c020dee3676c2ee19f9b74105c
chansizzle <14916599+chansizzle@users.noreply.github.com>
2020-01-09 10:51:16 -0700

    Update app_config.php

--- app/destinations/app_config.php
+++ app/destinations/app_config.php
@@ -230,7 +230,6 @@
 		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = "destination_enabled";
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
-		$apps[$x]['db'][$y]['fields'][$z]['search'] = 'true';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
 		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = "destination_description";
