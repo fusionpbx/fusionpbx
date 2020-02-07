9c41941010de70ba2719139f9f6d8dad3d2f05e4
FusionPBX <markjcrane@gmail.com>
2020-02-07 12:31:33 -0700

    Update dialplan_add.php

--- app/dialplans/dialplan_add.php
+++ app/dialplans/dialplan_add.php
@@ -43,6 +43,9 @@
 	$language = new text;
 	$text = $language->get();
 
+//initialize the destinations object
+	$destination = new destinations;
+
 //set the variables
 	if (count($_POST) > 0) {
 		$dialplan_name = $_POST["dialplan_name"];
@@ -155,8 +158,10 @@
 			$array['dialplan_details'][2]['dialplan_uuid'] = $dialplan_uuid;
 			$array['dialplan_details'][2]['dialplan_detail_uuid'] = $dialplan_detail_uuid;
 			$array['dialplan_details'][2]['dialplan_detail_tag'] = 'action';
-			$array['dialplan_details'][2]['dialplan_detail_type'] = $action_application_1;
-			$array['dialplan_details'][2]['dialplan_detail_data'] = $action_data_1;
+			if ($destination->valid($action_application_1.':'.$action_data_1)) {
+				$array['dialplan_details'][2]['dialplan_detail_type'] = $action_application_1;
+				$array['dialplan_details'][2]['dialplan_detail_data'] = $action_data_1;
+			}
 			$array['dialplan_details'][2]['dialplan_detail_order'] = '3';
 	
 		//add action 2
@@ -166,8 +171,10 @@
 				$array['dialplan_details'][3]['dialplan_uuid'] = $dialplan_uuid;
 				$array['dialplan_details'][3]['dialplan_detail_uuid'] = $dialplan_detail_uuid;
 				$array['dialplan_details'][3]['dialplan_detail_tag'] = 'action';
-				$array['dialplan_details'][3]['dialplan_detail_type'] = $action_application_2;
-				$array['dialplan_details'][3]['dialplan_detail_data'] = $action_data_2;
+				if ($destination->valid($action_application_2.':'.$action_data_2)) {
+					$array['dialplan_details'][3]['dialplan_detail_type'] = $action_application_2;
+					$array['dialplan_details'][3]['dialplan_detail_data'] = $action_data_2;
+				}
 				$array['dialplan_details'][3]['dialplan_detail_order'] = '4';
 			}
 	
@@ -190,9 +197,6 @@
 			header("Location: ".PROJECT_PATH."/app/dialplans/dialplans.php");
 			exit;
 	}
-
-//initialize the destinations object
-	$destination = new destinations;
 
 //javascript type on change
 	?><script type="text/javascript">
