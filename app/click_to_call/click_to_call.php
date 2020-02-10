1d0a10cc2d46a952dac93e729a65442790666bcd
Luis Daniel Lucio Quiroz <dlucio@okay.com.mx>
2020-02-10 17:51:05 -0500

    Update click_to_call.php

--- app/click_to_call/click_to_call.php
+++ app/click_to_call/click_to_call.php
@@ -176,7 +176,7 @@
 			}
 			else {
 				//local extension (source) > external number (destination)
-				if (strlen($src) < 7 && strlen($dest_cid_number) == 0) {
+				if (user_exists($src)&& strlen($dest_cid_number) == 0) {
 					//retrieve outbound caller id from the (source) extension
 					$sql = "select outbound_caller_id_name, outbound_caller_id_number from v_extensions where domain_uuid = '".$_SESSION['domain_uuid']."' and extension = '".$src."' ";
 					$prep_statement = $db->prepare(check_sql($sql));
