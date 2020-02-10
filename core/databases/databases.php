9a2ab8a426e49bc5675c608422d735014196cf98
Nate <github@najo.com>
2020-02-10 07:56:15 -0700

    Databases: List view and button updates.

--- core/databases/databases.php
+++ core/databases/databases.php
@@ -17,131 +17,172 @@
 
 	The Initial Developer of the Original Code is
 	Mark J Crane <markjcrane@fusionpbx.com>
-	Portions created by the Initial Developer are Copyright (C) 2008-2012
+	Portions created by the Initial Developer are Copyright (C) 2008-2020
 	the Initial Developer. All Rights Reserved.
 
 	Contributor(s):
 	Mark J Crane <markjcrane@fusionpbx.com>
 */
-require_once "root.php";
-require_once "resources/require.php";
-require_once "resources/check_auth.php";
-if (if_group("superadmin")) {
-	//access granted
-}
-else {
-	echo "access denied";
-	exit;
-}
+
+//includes
+	require_once "root.php";
+	require_once "resources/require.php";
+	require_once "resources/check_auth.php";
+	require_once "resources/paging.php";
+
+//check permissions
+	if (permission_exists('database_view')) {
+		//access granted
+	}
+	else {
+		echo "access denied";
+		exit;
+	}
 
 //add multi-lingual support
 	$language = new text;
 	$text = $language->get();
 
-//includes and title
-	require_once "resources/header.php";
-	$document['title'] = $text['title-databases'];
-	require_once "resources/paging.php";
+//get the http post data
+	if (is_array($_POST['databases'])) {
+		$action = $_POST['action'];
+		$databases = $_POST['databases'];
+	}
+
+//process the http post data by action
+	if ($action != '' && is_array($databases) && @sizeof($databases) != 0) {
+		switch ($action) {
+			case 'copy':
+				if (permission_exists('database_add')) {
+					$obj = new databases;
+					$obj->copy($databases);
+				}
+				break;
+			case 'delete':
+				if (permission_exists('database_delete')) {
+					$obj = new databases;
+					$obj->delete($databases);
+				}
+				break;
+		}
+
+		header('Location: databases.php');
+		exit;
+	}
 
 //get variables used to control the order
 	$order_by = $_GET["order_by"];
 	$order = $_GET["order"];
 
+//prepare to page the results
+	$sql = "select count(*) from v_databases ";
+	$database = new database;
+	$num_rows = $database->select($sql, null, 'column');
+
+//prepare to page the results
+	$rows_per_page = ($_SESSION['domain']['paging']['numeric'] != '') ? $_SESSION['domain']['paging']['numeric'] : 50;
+	$param = "";
+	$page = is_numeric($_GET['page']) ? $_GET['page'] : 0;
+	list($paging_controls, $rows_per_page) = paging($num_rows, $param, $rows_per_page);
+	$offset = $rows_per_page * $page;
+
+//get the  list
+	$sql = str_replace('count(*)', '*', $sql);
+	$sql .= order_by($order_by, $order);
+	$sql .= limit_offset($rows_per_page, $offset);
+	$database = new database;
+	$databases = $database->select($sql, null, 'all');
+	unset($sql);
+
+//create token
+	$object = new token;
+	$token = $object->create($_SERVER['PHP_SELF']);
+
+//include the header
+	$document['title'] = $text['title-databases'];
+	require_once "resources/header.php";
+
 //show the content
-	echo "<table width='100%' cellpadding='0' cellspacing='0' border='0'>\n";
-	echo "	<tr>\n";
-	echo "		<td width='50%' align=\"left\" nowrap=\"nowrap\"><b>".$text['header-databases']."</b></td>\n";
-	echo "		<td width='50%' align=\"right\">&nbsp;</td>\n";
-	echo "	</tr>\n";
-	echo "	<tr>\n";
-	echo "		<td align=\"left\" colspan='2'>\n";
-	echo "			".$text['description-databases']."<br /><br />\n";
-	echo "		</td>\n";
-	echo "	</tr>\n";
-	echo "</table>\n";
+	echo "<div class='action_bar' id='action_bar'>\n";
+	echo "	<div class='heading'><b>".$text['header-databases']."</b></div>\n";
+	echo "	<div class='actions'>\n";
+	if (permission_exists('database_add')) {
+		echo button::create(['type'=>'button','label'=>$text['button-add'],'icon'=>$_SESSION['theme']['button_icon_add'],'link'=>'database_edit.php']);
+	}
+	if (permission_exists('database_add') && $databases) {
+		echo button::create(['type'=>'button','label'=>$text['button-copy'],'icon'=>$_SESSION['theme']['button_icon_copy'],'onclick'=>"if (confirm('".$text['confirm-copy']."')) { list_action_set('copy'); list_form_submit('form_list'); } else { this.blur(); return false; }"]);
+	}
+	if (permission_exists('database_delete') && $databases) {
+		echo button::create(['type'=>'button','label'=>$text['button-delete'],'icon'=>$_SESSION['theme']['button_icon_delete'],'onclick'=>"if (confirm('".$text['confirm-delete']."')) { list_action_set('delete'); list_form_submit('form_list'); } else { this.blur(); return false; }"]);
+	}
+	echo "	</div>\n";
+	echo "	<div style='clear: both;'></div>\n";
+	echo "</div>\n";
 
-	//prepare to page the results
-		$sql = "select count(*) from v_databases ";
-		$sql .= order_by($order_by, $order);
-		$database = new database;
-		$num_rows = $database->select($sql, null, 'column');
-		unset($sql);
+	echo $text['description-databases']."\n";
+	echo "<br /><br />\n";
 
-	//prepare to page the results
-		$rows_per_page = ($_SESSION['domain']['paging']['numeric'] != '') ? $_SESSION['domain']['paging']['numeric'] : 50;
-		$param = "";
-		$page = $_GET['page'];
-		if (strlen($page) == 0) { $page = 0; $_GET['page'] = 0; }
-		list($paging_controls, $rows_per_page, $var_3) = paging($num_rows, $param, $rows_per_page);
-		$offset = $rows_per_page * $page;
+	echo "<form id='form_list' method='post'>\n";
+	echo "<input type='hidden' id='action' name='action' value=''>\n";
 
-	//get the  list
-		$sql = "select * from v_databases ";
-		$sql .= order_by($order_by, $order);
-		$sql .= limit_offset($rows_per_page, $offset);
-		$database = new database;
-		$result = $database->select($sql, null, 'all');
-		unset($sql);
-
-	$c = 0;
-	$row_style["0"] = "row_style0";
-	$row_style["1"] = "row_style1";
-
-	echo "<table class='tr_hover' width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
-
-	echo "<tr>\n";
+	echo "<table class='list'>\n";
+	echo "<tr class='list-header'>\n";
+	if (permission_exists('database_add') || permission_exists('database_delete')) {
+		echo "	<th class='checkbox'>\n";
+		echo "		<input type='checkbox' id='checkbox_all' name='checkbox_all' onclick='list_all_toggle();' ".($databases ?: "style='visibility: hidden;'").">\n";
+		echo "	</th>\n";
+	}
 	echo th_order_by('database_driver', $text['label-driver'], $order_by, $order);
 	echo th_order_by('database_type', $text['label-type'], $order_by, $order);
 	echo th_order_by('database_host', $text['label-host'], $order_by, $order);
-	//echo th_order_by('database_port', $text['label-port'], $order_by, $order);
 	echo th_order_by('database_name', $text['label-name'], $order_by, $order);
-	//echo th_order_by('database_username', $text['label-username'], $order_by, $order);
-	//echo th_order_by('database_path', $text['label-path'], $order_by, $order);
-	echo th_order_by('database_description', $text['label-description'], $order_by, $order);
-	echo "<td class='list_control_icons'>";
-	echo 	"<a href='database_edit.php' alt='".$text['button-add']."'>$v_link_label_add</a>";
-	echo "</td>\n";
+	echo th_order_by('database_description', $text['label-description'], $order_by, $order, null, "class='hide-sm-dn'");
+	if (permission_exists('database_edit') && $_SESSION['theme']['list_row_edit_button']['boolean'] == 'true') {
+		echo "	<td class='action-button'>&nbsp;</td>\n";
+	}
 	echo "</tr>\n";
 
-	if (is_array($result) && sizeof($result) != 0) {
-		foreach($result as $row) {
-			$tr_link = "href='database_edit.php?id=".$row['database_uuid']."'";
-			echo "<tr ".$tr_link.">\n";
-			echo "	<td valign='top' class='".$row_style[$c]."'>".escape($row['database_driver'])."&nbsp;</td>\n";
-			echo "	<td valign='top' class='".$row_style[$c]."'>".escape($row['database_type'])."&nbsp;</td>\n";
-			echo "	<td valign='top' class='".$row_style[$c]."'>".escape($row['database_host'])."&nbsp;</td>\n";
-			//echo "	<td valign='top' class='".$row_style[$c]."'>".escape($row['database_port'])."&nbsp;</td>\n";
-			echo "	<td valign='top' class='".$row_style[$c]."'><a href='database_edit.php?id=".$row['database_uuid']."'>".escape($row['database_name'])."</a>&nbsp;</td>\n";
-			//echo "	<td valign='top' class='".$row_style[$c]."'>".escape($row['database_username'])."&nbsp;</td>\n";
-			//echo "	<td valign='top' class='".$row_style[$c]."'>".escape($row['database_path'])."&nbsp;</td>\n";
-			echo "	<td valign='top' class='row_stylebg'>".escape($row['database_description'])."&nbsp;</td>\n";
-			echo "	<td class='list_control_icons'>";
-			echo 		"<a href='database_edit.php?id=".escape($row['database_uuid'])."' alt='".$text['button-edit']."'>$v_link_label_edit</a>";
-			echo 		"<a href='database_delete.php?id=".escape($row['database_uuid'])."' alt='".$text['button-delete']."' onclick=\"return confirm('".$text['confirm-delete']."')\">$v_link_label_delete</a>";
+	if (is_array($databases) && @sizeof($databases) != 0) {
+		$x = 0;
+		foreach ($databases as $row) {
+			$list_row_url = "database_edit.php?id=".urlencode($row['database_uuid']);
+			echo "<tr class='list-row' href='".$list_row_url."'>\n";
+			if (permission_exists('database_add') || permission_exists('database_delete')) {
+				echo "	<td class='checkbox'>\n";
+				echo "		<input type='checkbox' name='databases[$x][checked]' id='checkbox_".$x."' value='true' onclick=\"if (!this.checked) { document.getElementById('checkbox_all').checked = false; }\">\n";
+				echo "		<input type='hidden' name='databases[$x][uuid]' value='".escape($row['database_uuid'])."' />\n";
+				echo "	</td>\n";
+			}
+			echo "	<td>".escape($row['database_driver'])."&nbsp;</td>\n";
+			echo "	<td>".escape($row['database_type'])."&nbsp;</td>\n";
+			echo "	<td>".escape($row['database_host'])."&nbsp;</td>\n";
+			echo "	<td>";
+			if (permission_exists('database_edit')) {
+				echo "<a href='".$list_row_url."'>".escape($row['database_name'])."</a>";
+			}
+			else {
+				echo escape($row['database_name']);
+			}
 			echo "	</td>\n";
+			echo "	<td class='description overflow hide-sm-dn'>".escape($row['database_description'])."&nbsp;</td>\n";
+			if (permission_exists('database_edit') && $_SESSION['theme']['list_row_edit_button']['boolean'] == 'true') {
+				echo "	<td class='action-button'>\n";
+				echo button::create(['type'=>'button','title'=>$text['button-edit'],'icon'=>$_SESSION['theme']['button_icon_edit'],'link'=>$list_row_url]);
+				echo "	</td>\n";
+			}
 			echo "</tr>\n";
-			if ($c==0) { $c=1; } else { $c=0; }
-		} //end foreach
-		unset($result);
-	} //end if results
+			$x++;
+		}
+		unset($databases);
+	}
 
-	echo "<tr>\n";
-	echo "<td colspan='10' align='left'>\n";
-	echo "	<table width='100%' cellpadding='0' cellspacing='0'>\n";
-	echo "	<tr>\n";
-	echo "		<td width='33.3%' nowrap>&nbsp;</td>\n";
-	echo "		<td width='33.3%' align='center' nowrap>$paging_controls</td>\n";
-	echo "		<td class='list_control_icons'>";
-	echo 			"<a href='database_edit.php' alt='".$text['button-add']."'>$v_link_label_add</a>";
-	echo "		</td>\n";
-	echo "	</tr>\n";
- 	echo "	</table>\n";
-	echo "</td>\n";
-	echo "</tr>\n";
-
-	echo "</table>";
-	echo "<br><br>";
+	echo "</table>\n";
+	echo "<br />\n";
+	echo "<div align='center'>".$paging_controls."</div>\n";
+	echo "<input type='hidden' name='".$token['name']."' value='".$token['hash']."'>\n";
+	echo "</form>\n";
 
 //include the footer
 	require_once "resources/footer.php";
+
 ?>
