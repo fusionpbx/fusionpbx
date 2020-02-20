7e6fbcbf1da9aeecece10f47d897edd774fd37f8
Nate <github@najo.com>
2020-02-19 19:54:10 -0700

    Destinations (Class): Adjust select() method so defaults to top of options when no option selected.

--- app/destinations/resources/classes/destinations.php
+++ app/destinations/resources/classes/destinations.php
@@ -323,7 +323,9 @@ if (!class_exists('destinations')) {
 				$destination_label = str_replace("menu-exec-app", "", $destination_label);
 				$destination_label = str_replace("transfer", "", $destination_label);
 				$destination_label = str_replace("XML ".$_SESSION['domain_name'], "", $destination_label);
-				$response .= "			<option value='".escape($destination_value)."' selected='selected'>".trim($destination_label)."</option>\n";
+				if ($destination_value != '' || $destination_label != '') {
+					$response .= "			<option value='".escape($destination_value)."' selected='selected'>".trim($destination_label)."</option>\n";
+				}
 			}
 			$response .= "	</select>\n";
 			if (if_group("superadmin")) {
