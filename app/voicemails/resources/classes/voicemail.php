4cd703c9575f6bffa25b11096b1c61bf362c04e8
Nate <github@najo.com>
2020-03-30 20:08:45 -0600

    Safari Audio Support: Call Recordings, MOH, VM Greetings & Messages and CDR audio files.

--- app/voicemails/resources/classes/voicemail.php
+++ app/voicemails/resources/classes/voicemail.php
@@ -725,9 +725,6 @@
 			//change the message status
 				$this->message_saved();
 
-			//clear the cache
-				session_cache_limiter('public');
-
 			//set source folder path
 				$path = $_SESSION['switch']['voicemail']['dir'].'/default/'.$_SESSION['domain_name'].'/'.$this->voicemail_id;
 
@@ -778,32 +775,37 @@
 					$file_path = $path.'/msg_'.$this->voicemail_message_uuid.'.mp3';
 				}
 				if ($file_path != '') {
+					//content-range
+					if (isset($_SERVER['HTTP_RANGE']) && $this->type != 'bin')  {
+						$this->range_download($file_path);
+					}
+
 					$fd = fopen($file_path, "rb");
 					if ($this->type == 'bin') {
 						header("Content-Type: application/force-download");
 						header("Content-Type: application/octet-stream");
 						header("Content-Type: application/download");
 						header("Content-Description: File Transfer");
-						$file_ext = substr($file_path, -3);
-						if ($file_ext == "wav") {
-							header('Content-Disposition: attachment; filename="msg_'.$this->voicemail_message_uuid.'.wav"');
-						}
-						if ($file_ext == "mp3") {
-							header('Content-Disposition: attachment; filename="msg_'.$this->voicemail_message_uuid.'.mp3"');
+						$file_ext = pathinfo($file_path, PATHINFO_EXTENSION);
+						switch ($file_ext) {
+							case "wav" : header('Content-Disposition: attachment; filename="msg_'.$this->voicemail_message_uuid.'.wav"'); break;
+							case "mp3" : header('Content-Disposition: attachment; filename="msg_'.$this->voicemail_message_uuid.'.mp3"'); break;
+							case "ogg" : header('Content-Disposition: attachment; filename="msg_'.$this->voicemail_message_uuid.'.ogg"'); break;
 						}
 					}
 					else {
-						$file_ext = substr($file_path, -3);
-						if ($file_ext == "wav") {
-							header("Content-Type: audio/wav");
-						}
-						if ($file_ext == "mp3") {
-							header("Content-Type: audio/mpeg");
+						$file_ext = pathinfo($file_path, PATHINFO_EXTENSION);
+						switch ($file_ext) {
+							case "wav" : header("Content-Type: audio/x-wav"); break;
+							case "mp3" : header("Content-Type: audio/mpeg"); break;
+							case "ogg" : header("Content-Type: audio/ogg"); break;
 						}
 					}
 					header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 					header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // date in the past
-					header("Content-Length: " . filesize($file_path));
+					if ($this->type == 'bin') {
+						header("Content-Length: ".filesize($file_path));
+					}
 					ob_end_clean();
 					fpassthru($fd);
 				}
@@ -814,6 +816,100 @@
 				}
 
 		}
+
+		/*
+		 * range download method (helps safari play audio sources)
+		 */
+		private function range_download($file) {
+			$fp = @fopen($file, 'rb');
+
+			$size   = filesize($file); // File size
+			$length = $size;           // Content length
+			$start  = 0;               // Start byte
+			$end    = $size - 1;       // End byte
+			// Now that we've gotten so far without errors we send the accept range header
+			/* At the moment we only support single ranges.
+			* Multiple ranges requires some more work to ensure it works correctly
+			* and comply with the spesifications: http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
+			*
+			* Multirange support annouces itself with:
+			* header('Accept-Ranges: bytes');
+			*
+			* Multirange content must be sent with multipart/byteranges mediatype,
+			* (mediatype = mimetype)
+			* as well as a boundry header to indicate the various chunks of data.
+			*/
+			header("Accept-Ranges: 0-$length");
+			// header('Accept-Ranges: bytes');
+			// multipart/byteranges
+			// http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
+			if (isset($_SERVER['HTTP_RANGE'])) {
+
+				$c_start = $start;
+				$c_end   = $end;
+				// Extract the range string
+				list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+				// Make sure the client hasn't sent us a multibyte range
+				if (strpos($range, ',') !== false) {
+					// (?) Shoud this be issued here, or should the first
+					// range be used? Or should the header be ignored and
+					// we output the whole content?
+					header('HTTP/1.1 416 Requested Range Not Satisfiable');
+					header("Content-Range: bytes $start-$end/$size");
+					// (?) Echo some info to the client?
+					exit;
+				}
+				// If the range starts with an '-' we start from the beginning
+				// If not, we forward the file pointer
+				// And make sure to get the end byte if spesified
+				if ($range0 == '-') {
+					// The n-number of the last bytes is requested
+					$c_start = $size - substr($range, 1);
+				}
+				else {
+					$range  = explode('-', $range);
+					$c_start = $range[0];
+					$c_end   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+				}
+				/* Check the range and make sure it's treated according to the specs.
+				* http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
+				*/
+				// End bytes can not be larger than $end.
+				$c_end = ($c_end > $end) ? $end : $c_end;
+				// Validate the requested range and return an error if it's not correct.
+				if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
+
+					header('HTTP/1.1 416 Requested Range Not Satisfiable');
+					header("Content-Range: bytes $start-$end/$size");
+					// (?) Echo some info to the client?
+					exit;
+				}
+				$start  = $c_start;
+				$end    = $c_end;
+				$length = $end - $start + 1; // Calculate new content length
+				fseek($fp, $start);
+				header('HTTP/1.1 206 Partial Content');
+			}
+			// Notify the client the byte range we'll be outputting
+			header("Content-Range: bytes $start-$end/$size");
+			header("Content-Length: $length");
+
+			// Start buffered download
+			$buffer = 1024 * 8;
+			while(!feof($fp) && ($p = ftell($fp)) <= $end) {
+				if ($p + $buffer > $end) {
+					// In case we're only outputtin a chunk, make sure we don't
+					// read past the length
+					$buffer = $end - $p + 1;
+				}
+				set_time_limit(0); // Reset time limit for big files
+				echo fread($fp, $buffer);
+				flush(); // Free up memory. Otherwise large files will trigger PHP's memory limit.
+			}
+
+			fclose($fp);
+		}
+
 
 	}
 
