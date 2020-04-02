448c245d399026091f069a7a1710539363361576
agree <37550360+greenbea@users.noreply.github.com>
2020-04-02 19:23:25 -0400

    Update app_config.php

--- app/call_flows/app_config.php
+++ app/call_flows/app_config.php
@@ -167,6 +167,11 @@
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "Enter the alernate application data.";
 		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = "call_flow_enabled";
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
+		$apps[$x]['db'][$y]['fields'][$z]['search'] = 'true';
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "Select whether to enable or disable the call flow";
+		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = "call_flow_description";
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['search'] = 'true';
