c6ceb884f6782c61e370db1d871054bf0d3f1bf7
FusionPBX <markjcrane@gmail.com>
2020-05-25 17:51:12 -0600

    Set the dial_string default to true.

--- app/extensions/app_config.php
+++ app/extensions/app_config.php
@@ -215,7 +215,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "dial_string";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "{sip_invite_domain=\${domain_name},leg_timeout=\${call_timeout},presence_id=\${dialed_user}@\${dialed_domain}}\${sofia_contact(*/\${dialed_user}@\${dialed_domain})}";
-		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "false";
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "";
 		$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "3eeb3757-f7bb-437a-9021-8ccf3f27c98b";
