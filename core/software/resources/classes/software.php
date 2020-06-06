f4b70e6ba92c4e93d024f6ab07ab31d8a1b5473a
Luis Daniel Lucio Quiroz <dlucio@okay.com.mx>
2020-06-06 11:11:35 -0400

    $this is a syntax error when using static classes (#5264)

--- core/software/resources/classes/software.php
+++ core/software/resources/classes/software.php
@@ -19,7 +19,7 @@ if (!class_exists('software')) {
 		 * numeric_version
 		 */
 		public static function numeric_version() {
-			$v = explode('.', $this->version());
+			$v = explode('.', software::version());
 			$n = ($v[0] * 10000 + $v[1] * 100 + $v[2]);
 			return $n;
 		}
