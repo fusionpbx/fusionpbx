6aabdea1446a2c839cfb0cd0a10624b251f15c41
FusionPBX <markjcrane@gmail.com>
2020-06-19 08:41:50 -0600

    Remove a line that was accidently added to the new ivr_menu_uuid field.

--- app/ivr_menus/app_config.php
+++ app/ivr_menus/app_config.php
@@ -207,7 +207,6 @@
 		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = "uuid";
 		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = "char(36)";
-		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = "primary";
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "";
 		$z++;
 		$apps[$x]['db'][$y]['fields'][$z]['name'] = "ivr_menu_language";
