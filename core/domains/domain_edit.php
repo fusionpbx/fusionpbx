616f1aa58f85665e1a6672ac0163577a2071911b
FusionPBX <markjcrane@gmail.com>
2020-07-10 09:45:54 -0600

    Cast domain_enabled to text

--- core/domains/domain_edit.php
+++ core/domains/domain_edit.php
@@ -528,7 +528,8 @@
 
 //pre-populate the form (admin won't have domain_add permissions, but domain_uuid will already be set above)
 	if ((count($_GET) > 0 || (!permission_exists('domain_add') && $domain_uuid != '')) && $_POST["persistformvar"] != "true") {
-		$sql = "select * from v_domains ";
+		$sql = "select domain_uuid, domain_name, cast(domain_enabled as text), domain_description ";
+		$sql .= "from v_domains ";
 		$sql .= "where domain_uuid = :domain_uuid ";
 		$parameters['domain_uuid'] = $domain_uuid;
 		$database = new database;
