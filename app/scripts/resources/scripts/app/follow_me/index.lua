59bd5a9e5447df7b92a4e73748a6174d0394d280
agree <37550360+greenbea@users.noreply.github.com>
2020-07-12 17:47:54 -0400

    Add domain_uuid variable (#5344)

--- app/scripts/resources/scripts/app/follow_me/index.lua
+++ app/scripts/resources/scripts/app/follow_me/index.lua
@@ -384,7 +384,7 @@
 					end
 
 				--set the destination dial string
-					dial_string = "[ignore_early_media=true,toll_allow=".. toll_allow ..",".. caller_id ..",sip_invite_domain="..domain_name..",call_direction="..call_direction..","..group_confirm..","..timeout_name.."="..destination_timeout..","..delay_name.."="..destination_delay.."]"..route_bridge
+					dial_string = "[ignore_early_media=true,toll_allow=".. toll_allow ..",".. caller_id ..",sip_invite_domain="..domain_name..",domain_uuid="..domain_uuid..",call_direction="..call_direction..","..group_confirm..","..timeout_name.."="..destination_timeout..","..delay_name.."="..destination_delay.."]"..route_bridge
 			end
 
 		--add a delimiter between destinations
