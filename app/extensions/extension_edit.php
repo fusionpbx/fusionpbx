dc2f9ee731c60fb03cd94fce6f88644e7fd286ef
FusionPBX <markjcrane@gmail.com>
2020-08-05 10:55:42 -0600

    change button-call_routing to button-call_forward

--- app/extensions/extension_edit.php
+++ app/extensions/extension_edit.php
@@ -881,7 +881,7 @@
 			unset($button_margin);
 		}
 		if (permission_exists('follow_me') || permission_exists('call_forward') || permission_exists('do_not_disturb')) {
-			echo button::create(['type'=>'button','label'=>$text['button-call_routing'],'icon'=>'project-diagram','style'=>$button_margin,'link'=>'../calls/call_edit.php?id='.urlencode($extension_uuid)]);
+			echo button::create(['type'=>'button','label'=>$text['button-call_forward'],'icon'=>'project-diagram','style'=>$button_margin,'link'=>'../calls/call_edit.php?id='.urlencode($extension_uuid)]);
 			unset($button_margin);
 		}
 		if (permission_exists('extension_copy')) {
