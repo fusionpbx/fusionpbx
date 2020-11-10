e5cf6bde5136fb456d60fde95f1d4135f4c2883b
FusionPBX <markjcrane@gmail.com>
2020-11-09 22:03:33 -0700

    Update mwi_notify.lua
    Add 0 as default values for new and saved messages.

--- app/scripts/resources/scripts/app/voicemail/resources/scripts/mwi_notify.lua
+++ app/scripts/resources/scripts/app/voicemail/resources/scripts/mwi_notify.lua
@@ -2,8 +2,8 @@
 --get the argv values
 	voicemail_id = argv[1];
 	domain_name = argv[2];
-	new_messages = argv[3];
-	saved_messages = argv[4];
+	new_messages = argv[3] or '0';
+	saved_messages = argv[4] or '0';
 
 --include the lua script
 	require "resources.functions.config";
@@ -19,3 +19,4 @@
 
 --send the message waiting event
 	mwi_notify(voicemail_id..'@'..domain_name, domain_name, new_messages, saved_messages);
+
