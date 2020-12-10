1687ebb3aebdbe453d8040cdba3837742c52f73a
fusionate <nate@fusionpbx.com>
2020-12-10 16:58:50 -0700

    Destinations: Correct permission.

--- app/destinations/resources/destinations.php
+++ app/destinations/resources/destinations.php
@@ -6,7 +6,7 @@
 	require_once "resources/check_auth.php";
 
 //check permissions
-	if (permission_exists('extension_view')) {
+	if (permission_exists('destination_view')) {
 		//access granted
 	}
 	else {
