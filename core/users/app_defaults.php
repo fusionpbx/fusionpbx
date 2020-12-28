7ba8b3c20ebb05e49895de6ceb1a8f8257ca8825
Luis Daniel Lucio Quiroz <dlucio@okay.com.mx>
2020-12-28 18:41:11 -0500

    my db compatibility work

--- core/users/app_defaults.php
+++ core/users/app_defaults.php
@@ -41,7 +41,12 @@ if ($domains_processed == 1) {
 		}
 		$sql .= "	( \n";
 		$sql .= "		select \n";
-		$sql .= "		string_agg(g.group_name, ', ') \n";
+		if ($db_type == 'pgsql'){
+			$sql .= "               string_agg(g.group_name, ', ') \n";
+		}
+		else{
+			$sql .= "               group_concat(g.group_name) \n";
+		}
 		$sql .= "		from \n";
 		$sql .= "		v_user_groups as ug, \n";
 		$sql .= "		v_groups as g \n";
