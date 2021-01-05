f1e87b4e7e4fabc99e863251b1e80db601875ec7
FusionPBX <markjcrane@gmail.com>
2021-01-04 18:31:44 -0700

    add alternative array_key_first for older verisons of PHP

--- resources/functions.php
+++ resources/functions.php
@@ -2194,4 +2194,14 @@ function number_pad($number,$n) {
 		}
 	}
 
+//add alternative array_key_first for older verisons of PHP
+	if (!function_exists('array_key_first')) {
+	    function array_key_first(array $arr) {
+		foreach($arr as $key => $unused) {
+		    return $key;
+		}
+		return NULL;
+	    }
+	}
+
 ?>
