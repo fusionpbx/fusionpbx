15e7807e9000f57e1130f4b4c50204f5cd2188cc
chansizzle <14916599+chansizzle@users.noreply.github.com>
2021-05-12 15:48:33 -0600

    create destinations export

--- app/destinations/app_config.php
+++ app/destinations/app_config.php
@@ -90,6 +90,9 @@
 		$apps[$x]['permissions'][$y]['name'] = "destination_import";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
 		$y++;
+		$apps[$x]['permissions'][$y]['name'] = "destination_export";
+		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
+		$y++;
 		$apps[$x]['permissions'][$y]['name'] = "destination_upload";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
 		$y++;
