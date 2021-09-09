960575bbfca7f4e65e0b0c15ba6a3de7712ab5cd
FusionPBX <markjcrane@gmail.com>
2021-09-08 22:39:23 -0600

    Add fax_subject, fax_message, and fax_footer to the user group.

--- app/fax/app_config.php
+++ app/fax/app_config.php
@@ -183,14 +183,17 @@
 		$apps[$x]['permissions'][$y]['name'] = "fax_subject";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
 		$apps[$x]['permissions'][$y]['groups'][] = "admin";
+		$apps[$x]['permissions'][$y]['groups'][] = "user";
 		$y++;
 		$apps[$x]['permissions'][$y]['name'] = "fax_message";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
 		$apps[$x]['permissions'][$y]['groups'][] = "admin";
+		$apps[$x]['permissions'][$y]['groups'][] = "user";
 		$y++;
 		$apps[$x]['permissions'][$y]['name'] = "fax_footer";
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
 		$apps[$x]['permissions'][$y]['groups'][] = "admin";
+		$apps[$x]['permissions'][$y]['groups'][] = "user";
 
 	//default settings
 		$y=0;
