48b4d90754b6af9c8c966d4880a6e7f7b236595f
markjcrane <markjcrane@gmail.com>
2021-11-09 19:53:39 -0700

    Update order for Device Keys and Call Center Agents

--- app/call_centers/resources/dashboard/config.php
+++ app/call_centers/resources/dashboard/config.php
@@ -3,8 +3,8 @@
 $array['dashboard'][$x]['dashboard_uuid'] = '9083305a-ebb4-4b67-bb9b-8c09cf030261';
 $array['dashboard'][$x]['dashboard_name'] = 'Call Center Agents';
 $array['dashboard'][$x]['dashboard_path'] = 'app/call_centers/resources/dashboard/call_center_agents.php';
-$array['dashboard'][$x]['dashboard_order'] = '160';
-$array['dashboard'][$x]['dashboard_enabled'] = 'true';
+$array['dashboard'][$x]['dashboard_order'] = '180';
+$array['dashboard'][$x]['dashboard_enabled'] = 'false';
 $array['dashboard'][$x]['dashboard_description'] = 'Status for agent in a call center.';
 $y = 0;
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = '22d8937c-a9b3-4458-b34c-0ebeda15ffad';
