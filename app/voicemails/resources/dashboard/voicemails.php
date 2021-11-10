aa9eac06849ed02ccb96d9fa53acb26e95969b1f
AlexC <40072887+alexdcrane@users.noreply.github.com>
2021-11-09 23:19:37 -0700

    Update voicemails.php (#6115)

--- app/voicemails/resources/dashboard/voicemails.php
+++ app/voicemails/resources/dashboard/voicemails.php
@@ -22,116 +22,120 @@
 	$theme_image_path = $_SERVER["DOCUMENT_ROOT"]."/themes/".$_SESSION['domain']['template']['name']."/images/"; 
 
 //voicemail
-	//if (is_array($selected_blocks) && in_array('voicemail', $selected_blocks) && permission_exists('voicemail_message_view') && file_exists($_SERVER["DOCUMENT_ROOT"].PROJECT_PATH."/app/voicemails/")) {
-		//required class
-			require_once "app/voicemails/resources/classes/voicemail.php";
-		//get the voicemail
-			$vm = new voicemail;
-			$vm->db = $db;
-			$vm->domain_uuid = $_SESSION['domain_uuid'];
-			$vm->order_by = $order_by;
-			$vm->order = $order;
-			$voicemails = $vm->messages();
-		//sum total and new
-			$messages['total'] = 0;
-			$messages['new'] = 0;
-			if (sizeof($voicemails) > 0) {
-				foreach($voicemails as $field) {
-					$messages[$field['voicemail_uuid']]['ext'] = $field['voicemail_id'];
-					$messages[$field['voicemail_uuid']]['total'] = 0;
-					$messages[$field['voicemail_uuid']]['new'] = 0;
-					foreach($field['messages'] as &$row) {
-						if ($row['message_status'] == '') {
-							$messages[$field['voicemail_uuid']]['new']++;
-							$messages['new']++;
-						}
-						$messages[$field['voicemail_uuid']]['total']++;
-						$messages['total']++;
-					}
+	echo "<div class='hud_box'>\n";
+
+//required class
+	require_once "app/voicemails/resources/classes/voicemail.php";
+
+//get the voicemail
+	$vm = new voicemail;
+	$vm->db = $db;
+	$vm->domain_uuid = $_SESSION['domain_uuid'];
+	$vm->order_by = $order_by;
+	$vm->order = $order;
+	$voicemails = $vm->messages();
+
+//sum total and new
+	$messages['total'] = 0;
+	$messages['new'] = 0;
+	if (sizeof($voicemails) > 0) {
+		foreach($voicemails as $field) {
+			$messages[$field['voicemail_uuid']]['ext'] = $field['voicemail_id'];
+			$messages[$field['voicemail_uuid']]['total'] = 0;
+			$messages[$field['voicemail_uuid']]['new'] = 0;
+			foreach($field['messages'] as &$row) {
+				if ($row['message_status'] == '') {
+					$messages[$field['voicemail_uuid']]['new']++;
+					$messages['new']++;
 				}
+				$messages[$field['voicemail_uuid']]['total']++;
+				$messages['total']++;
 			}
+		}
+	}
 
-		//add doughnut chart
-			echo "
-				<div style='display: flex; flex-wrap: wrap; justify-content: center; padding-bottom:10px;'>
-					<div style='width: 175px; height: 175px;'><canvas id='new_messages_chart'></canvas></div>
-				</div>
+//add doughnut chart
+	echo "
+		<div style='display: flex; flex-wrap: wrap; justify-content: center; padding-bottom:10px;'>
+			<div style='width: 175px; height: 175px;'><canvas id='new_messages_chart'></canvas></div>
+		</div>
 
-				<script>
-					var new_messages_bgc = ['#ff9933', '#d4d4d4'];
+		<script>
+			var new_messages_bgc = ['#ff9933', '#d4d4d4'];
 
-					const new_messages_data = {
-						datasets: [{
-							data:[".$messages['new'].", 0.00001],
-							borderColor: 'rgba(0,0,0,0)',
-							backgroundColor: [new_messages_bgc[0], new_messages_bgc[1]],
-							cutout: chart_cutout
-						}]
-					};
+			const new_messages_data = {
+				datasets: [{
+					data:[".$messages['new'].", 0.00001],
+					borderColor: 'rgba(0,0,0,0)',
+					backgroundColor: [new_messages_bgc[0], new_messages_bgc[1]],
+					cutout: chart_cutout
+				}]
+			};
 
-					const new_messages_config = {
-						type: 'doughnut',
-						data: new_messages_data,
-						options: {
-							responsive: true,
-							maintainAspectRatio: false,
-							plugins: {
-								chart_counter: {
-									chart_text: ".$messages['new'].",
-								},
-								legend: {
-									display: false
-								},
-								title: {
-									display: true,
-									text: '".$text['label-new_messages']."',
-									fontFamily: chart_font_family
-								}
-							}
+			const new_messages_config = {
+				type: 'doughnut',
+				data: new_messages_data,
+				options: {
+					responsive: true,
+					maintainAspectRatio: false,
+					plugins: {
+						chart_counter: {
+							chart_text: ".$messages['new'].",
 						},
-						plugins: [chart_counter],
-					};
-
-					const new_messages_chart = new Chart(
-						document.getElementById('new_messages_chart'),
-						new_messages_config
-					);
-				</script>
-			";
-
-			echo "<div class='hud_details hud_box' id='hud_voicemail_details'>";
-			if (sizeof($voicemails) > 0) {
-				echo "<table class='tr_hover' cellpadding='2' cellspacing='0' border='0' width='100%'>";
-				echo "<tr>";
-				echo "	<th class='hud_heading' width='50%'>".$text['label-voicemail']."</th>";
-				echo "	<th class='hud_heading' style='text-align: center;' width='50%'>".$text['label-new']."</th>";
-				echo "	<th class='hud_heading' style='text-align: center;'>".$text['label-total']."</th>";
-				echo "</tr>";
-
-				$c = 0;
-				$row_style["0"] = "row_style0";
-				$row_style["1"] = "row_style1";
-
-				foreach ($messages as $voicemail_uuid => $row) {
-					if (is_uuid($voicemail_uuid)) {
-						$tr_link = "href='".PROJECT_PATH."/app/voicemails/voicemail_messages.php?id=".(permission_exists('voicemail_view') ? $voicemail_uuid : $row['ext'])."'";
-						echo "<tr ".$tr_link." style='cursor: pointer;'>";
-						echo "	<td class='".$row_style[$c]." hud_text'><a href='".PROJECT_PATH."/app/voicemails/voicemail_messages.php?id=".(permission_exists('voicemail_view') ? $voicemail_uuid : $row['ext'])."'>".$row['ext']."</a></td>";
-						echo "	<td class='".$row_style[$c]." hud_text' style='text-align: center;'>".$row['new']."</td>";
-						echo "	<td class='".$row_style[$c]." hud_text' style='text-align: center;'>".$row['total']."</td>";
-						echo "</tr>";
-						$c = ($c) ? 0 : 1;
+						legend: {
+							display: false
+						},
+						title: {
+							display: true,
+							text: '".$text['label-new_messages']."',
+							fontFamily: chart_font_family
+						}
 					}
-				}
+				},
+				plugins: [chart_counter],
+			};
 
-				echo "</table>";
+			const new_messages_chart = new Chart(
+				document.getElementById('new_messages_chart'),
+				new_messages_config
+			);
+		</script>
+	";
+
+	echo "<div class='hud_details hud_box' id='hud_voicemail_details'>";
+	if (sizeof($voicemails) > 0) {
+		echo "<table class='tr_hover' cellpadding='2' cellspacing='0' border='0' width='100%'>";
+		echo "<tr>";
+		echo "	<th class='hud_heading' width='50%'>".$text['label-voicemail']."</th>";
+		echo "	<th class='hud_heading' style='text-align: center;' width='50%'>".$text['label-new']."</th>";
+		echo "	<th class='hud_heading' style='text-align: center;'>".$text['label-total']."</th>";
+		echo "</tr>";
+
+		$c = 0;
+		$row_style["0"] = "row_style0";
+		$row_style["1"] = "row_style1";
+
+		foreach ($messages as $voicemail_uuid => $row) {
+			if (is_uuid($voicemail_uuid)) {
+				$tr_link = "href='".PROJECT_PATH."/app/voicemails/voicemail_messages.php?id=".(permission_exists('voicemail_view') ? $voicemail_uuid : $row['ext'])."'";
+				echo "<tr ".$tr_link." style='cursor: pointer;'>";
+				echo "	<td class='".$row_style[$c]." hud_text'><a href='".PROJECT_PATH."/app/voicemails/voicemail_messages.php?id=".(permission_exists('voicemail_view') ? $voicemail_uuid : $row['ext'])."'>".$row['ext']."</a></td>";
+				echo "	<td class='".$row_style[$c]." hud_text' style='text-align: center;'>".$row['new']."</td>";
+				echo "	<td class='".$row_style[$c]." hud_text' style='text-align: center;'>".$row['total']."</td>";
+				echo "</tr>";
+				$c = ($c) ? 0 : 1;
 			}
-			else {
-				echo "<br />".$text['label-no_voicemail_assigned'];
-			}
-			echo "</div>";
-			$n++;
-	//}
-	echo "			<span class='hud_expander' onclick=\"$('#hud_voicemail_details').slideToggle('fast');\"><span class='fas fa-ellipsis-h'></span></span>";
+		}
+
+		echo "</table>";
+	}
+	else {
+		echo "<br />".$text['label-no_voicemail_assigned'];
+	}
+	echo "</div>";
+	$n++;
+	
+	echo "<span class='hud_expander' onclick=\"$('#hud_voicemail_details').slideToggle('fast');\"><span class='fas fa-ellipsis-h'></span></span>";
+	echo "</div>\n";
 
 ?>
