5131e2695dc2b406d8db2fdd65a94bfef509f6c9
FusionPBX <markjcrane@gmail.com>
2021-11-16 13:11:59 -0700

    Make sure the file name exists and is not empty.

--- app/recordings/resources/classes/switch_recordings.php
+++ app/recordings/resources/classes/switch_recordings.php
@@ -145,7 +145,7 @@ if (!class_exists('switch_recordings')) {
 								//delete recording files
 									if (is_array($filenames) && @sizeof($filenames) != 0) {
 										foreach ($filenames as $filename) {
-											if (file_exists($_SESSION['switch']['recordings']['dir']."/".$_SESSION['domain_name']."/".$filename)) {
+											if (isset($filename) && strlen($filename) > 0 && file_exists($_SESSION['switch']['recordings']['dir']."/".$_SESSION['domain_name']."/".$filename)) {
 												@unlink($_SESSION['switch']['recordings']['dir']."/".$_SESSION['domain_name']."/".$filename);
 											}
 										}
