3f8ce4c550a552a02e845580e410f000467b3d69
nemerald-voip <58834187+nemerald-voip@users.noreply.github.com>
2021-11-18 18:10:42 -0800

    initial upload

---
+++ app/redirection_service/app_menu.php
@@ -0,0 +1,31 @@
+<?php
+
+	$y=0;
+	$apps[$x]['menu'][$y]['title']['en-us'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['en-gb'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['ar-eg'] = "";
+	$apps[$x]['menu'][$y]['title']['de-at'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['de-ch'] = "";
+	$apps[$x]['menu'][$y]['title']['de-de'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['es-cl'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['es-mx'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['fr-ca'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['fr-fr'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['he-il'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['it-it'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['nl-nl'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['pl-pl'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['pt-br'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['pt-pt'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['ro-ro'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['ru-ru'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['sv-se'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['title']['uk-ua'] = "Redirection Service";
+	$apps[$x]['menu'][$y]['uuid'] = "3cebfc8e-0ca3-410f-b3e2-78862426cd80";
+	$apps[$x]['menu'][$y]['parent_uuid'] = "424453ef-606a-4e33-8bfd-8f94754f61c8";
+	$apps[$x]['menu'][$y]['category'] = "internal";
+	$apps[$x]['menu'][$y]['path'] = "/app/redirection_service/redirection_service.php";
+	$apps[$x]['menu'][$y]['groups'][] = "admin";
+	$apps[$x]['menu'][$y]['groups'][] = "superadmin";
+
+?>
