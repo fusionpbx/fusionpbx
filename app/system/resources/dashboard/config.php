97376518acf5885ac87e8f1f9393e369f129aa5f
FusionPBX <markjcrane@gmail.com>
2021-11-23 18:45:01 -0700

    Update config.php

--- app/system/resources/dashboard/config.php
+++ app/system/resources/dashboard/config.php
@@ -11,8 +11,8 @@ $y = 0;
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = '625971b9-4892-4948-97e6-8bb8d409f4e1';
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'c9b1f67a-d78a-4299-a16e-594a4b7cd246';
 $array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'superadmin';
-
 $x++;
+
 $array['dashboard'][$x]['dashboard_uuid'] = 'beade936-846b-4f02-986c-a2de6fa762c2';
 $array['dashboard'][$x]['dashboard_name'] = 'System CPU Status';
 $array['dashboard'][$x]['dashboard_path'] = 'app/system/resources/dashboard/system_cpu_status.php';
@@ -23,6 +23,7 @@ $y = 0;
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = '41befc82-c65a-499e-9e29-513a3dd7974a';
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'beade936-846b-4f02-986c-a2de6fa762c2';
 $array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'superadmin';
+$x++;
 
 $array['dashboard'][$x]['dashboard_uuid'] = 'bb76f7fc-669f-41dd-a636-6ddaee5deae1';
 $array['dashboard'][$x]['dashboard_name'] = 'System Counts';
@@ -38,4 +39,5 @@ $y++;
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = '127382ae-0dd1-45eb-869c-56a708220de5';
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'bb76f7fc-669f-41dd-a636-6ddaee5deae1';
 $array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'admin';
+
 ?>
