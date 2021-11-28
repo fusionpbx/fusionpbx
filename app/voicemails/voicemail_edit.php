0073ea230336b35979935e9b85da8e70e70ae777
Joseph A Nadiv <ynadiv@corpit.xyz>
2021-11-28 10:25:37 -0500

    Add option to use name as standard voicemail greeting
    This commit makes the initial logic changes in the LUA
    scripts to support using the recorded_name as the
    greeting for VM.
    
    We are not addressing the prompts to allow the user
    to select this option from within the phone tree.

--- app/voicemails/voicemail_edit.php
+++ app/voicemails/voicemail_edit.php
@@ -520,6 +520,8 @@
 	echo "<td class='vtable' align='left'>\n";
 	echo "	<select class='formfld' name='greeting_id'>\n";
 	echo "		<option value=''>".$text['label-default']."</option>\n";
+	$selected = ($greeting_id == 11) ? "selected" : null;
+	echo "		<option value='11' ".escape($selected).">".$text['label-name']."</option>\n";
 	if (is_array($greetings) && @sizeof($greetings) != 0) {
 		foreach ($greetings as $greeting) {
 			$selected = ($greeting['greeting_id'] == $greeting_id) ? 'selected' : null;
