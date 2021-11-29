1b91db3971285e7d53bdd0d3501bb7e5edf0e134
FusionPBX <markjcrane@gmail.com>
2021-11-28 22:07:00 -0700

    Change conference_email to conference_email_address.

--- app/conferences/app_config.php
+++ app/conferences/app_config.php
@@ -132,7 +132,7 @@
 		$apps[$x]['db'][$y]['fields'][$z]['search'] = 'true';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "Optional pin number to secure access to the conference.";
 		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = "conference_email";
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = "conference_email_address";
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['search'] = 'true';
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "Optional email address for the conference.";
