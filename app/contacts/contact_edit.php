57d38fdb8d40a0ede2a3194ebb50a05beec47cc2
AlexC <40072887+alexdcrane@users.noreply.github.com>
2021-12-13 10:47:03 -0700

    Update contact_edit.php

--- app/contacts/contact_edit.php
+++ app/contacts/contact_edit.php
@@ -2045,7 +2045,7 @@ if (permission_exists('contact_url_view')) {
 		foreach($contact_url_labels as $label) {
 			$url_label_options[] = "<option value='".$label."' ".(($label == $row['url_label']) ? "selected='selected'" : null).">".$label."</option>";
 		}
-		$url_label_found = (in_array($url_label, $_SESSION["contact"]["url_label"])) ? true : false;
+		$url_label_found = (in_array($url_label, $contact_url_labels)) ? true : false;
 
 		echo "				<select class='formfld' ".((!$url_label_found && $url_label != '') ? "style='display: none;'" : "style='width: auto;'")." name='contact_urls[$x][url_label]' id='url_label' onchange=\"getElementById('url_label_custom').value='';\">\n";
 		echo "					<option value=''></option>\n";
