24ea779817d402c1b23e963cea1aadc7f1bd48e3
FusionPBX <markjcrane@gmail.com>
2021-12-20 11:26:40 -0700

    Match the yealink default

--- app/yealink/app_config.php
+++ app/yealink/app_config.php
@@ -827,7 +827,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "yealink_dialplan_dialnow_delay";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
-		$apps[$x]['default_settings'][$y]['default_setting_value'] = "2";
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = "1";
 		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "false";
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Time in second till dialnow activates.";
 		$y++;
