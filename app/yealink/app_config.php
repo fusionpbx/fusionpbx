2d1bdedeb20244227a7f4135b8a6651c29e88b5f
smeyernyc <smeyer@kotter.net>
2022-01-03 18:38:28 -0500

    Update app_config.php

--- app/yealink/app_config.php
+++ app/yealink/app_config.php
@@ -406,7 +406,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "T46U(T43U,T46U,T41U,T48U,T42U)-108.86.0.20.rom";
 		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Filename of the T46U firmware ROM";
-    $y++;
+	    	$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "6b70cfeb-2124-42dc-ba8b-6cd77da3fa5f";
 		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "yealink_firmware_t53w";
