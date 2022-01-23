0815dc524d7ad98d7223d99e3a1b8f127bb4871e
nemerald-voip <root@ip-172-26-4-139.us-west-2.compute.internal>
2022-01-23 17:58:34 +0000

    added laravel files

--- index.php
+++ index.php
@@ -1,84 +1,55 @@
 <?php
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
 /*
-	FusionPBX
-	Version: MPL 1.1
-
-	The contents of this file are subject to the Mozilla Public License Version
-	1.1 (the "License"); you may not use this file except in compliance with
-	the License. You may obtain a copy of the License at
-	http://www.mozilla.org/MPL/
-
-	Software distributed under the License is distributed on an "AS IS" basis,
-	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-	for the specific language governing rights and limitations under the
-	License.
-
-	The Original Code is FusionPBX
-
-	The Initial Developer of the Original Code is
-	Mark J Crane <markjcrane@fusionpbx.com>
-	Portions created by the Initial Developer are Copyright (C) 2008-2021
-	the Initial Developer. All Rights Reserved.
-
-	Contributor(s):
-	Mark J. Crane <markjcrane@fusionpbx.com>
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
 */
 
-//include root
-	include "root.php";
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-//start the session
-	ini_set("session.cookie_httponly", True);
-	if (!isset($_SESSION)) { session_start(); }
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
 
-//if config.php file does not exist then redirect to the install page
-	if (file_exists($_SERVER["PROJECT_ROOT"]."/resources/config.php")) {
-		//do nothing
-	} elseif (file_exists($_SERVER["PROJECT_ROOT"]."/resources/config.php")) {
-		//original directory
-	} elseif (file_exists($_SERVER["PROJECT_ROOT"]."/includes/config.php")) {
-		//move config.php from the includes to resources directory.
-		rename($_SERVER["PROJECT_ROOT"]."/includes/config.php", $_SERVER["PROJECT_ROOT"]."/resources/config.php");
-	} elseif (file_exists("/etc/fusionpbx/config.php")) {
-		//linux
-	} elseif (file_exists("/usr/local/etc/fusionpbx/config.php")) {
-		//bsd
-	} else {
-		header("Location: ".PROJECT_PATH."/core/install/install.php");
-		exit;
-	}
+require __DIR__.'/../vendor/autoload.php';
 
-//if not logged in, clear the session variables
-	//if (strlen($_SESSION["username"]) == 0) {
-	//	session_unset();
-	//	session_destroy();
-	//}
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
 
-//adds multiple includes
-	require_once "resources/require.php";
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-//if logged in, redirect to login destination
-	if (isset($_SESSION["username"])) {
-		if (isset($_SESSION['login']['destination']['url'])) {
-			header("Location: ".$_SESSION['login']['destination']['url']);
-		} elseif (file_exists($_SERVER["PROJECT_ROOT"]."/core/dashboard/app_config.php")) {
-			header("Location: ".PROJECT_PATH."/core/dashboard/");
-		}
-		else {
-			require_once "resources/header.php";
-			require_once "resources/footer.php";
-		}
-	}
-	else {
-		//use custom index, if present, otherwise use custom login, if present, otherwise use default login
-		if (file_exists($_SERVER["PROJECT_ROOT"]."/themes/".$_SESSION['domain']['template']['name']."/index.php")) {
-			require_once "themes/".$_SESSION['domain']['template']['name']."/index.php";
-		} else if (file_exists($_SERVER["PROJECT_ROOT"]."/themes/".$_SESSION['domain']['template']['name']."/login.php")) {
-			require_once "themes/".$_SESSION['domain']['template']['name']."/login.php";
-		}
-		else {
-			require_once "resources/login.php";
-		}
-	}
+$kernel = $app->make(Kernel::class);
 
-?>
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
