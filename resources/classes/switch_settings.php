6d5b05f4a3453b1473944f193769d170cbdbe68a
markjcrane <markjcrane@gmail.com>
2022-01-30 13:08:49 -0700

    change the switch phrases to switch languages

--- resources/classes/switch_settings.php
+++ resources/classes/switch_settings.php
@@ -156,7 +156,7 @@ if (!class_exists('switch_settings')) {
 				$array[$x]['default_setting_description'] = '';
 				$x++;
 				$array[$x]['default_setting_category'] = 'switch';
-				$array[$x]['default_setting_subcategory'] = 'phrases';
+				$array[$x]['default_setting_subcategory'] = 'languages';
 				$array[$x]['default_setting_name'] = 'dir';
 				$array[$x]['default_setting_value'] = $vars['conf_dir'].'/languages';
 				$array[$x]['default_setting_enabled'] = 'true';
