2f68324fe22e3dfa3f403d16110f53d7212254cf
fusionate <nate@fusionpbx.com>
2022-02-11 20:48:28 -0700

    Theme: Side Menu - Setting to control submenu close behavior.

--- themes/default/template.php
+++ themes/default/template.php
@@ -213,9 +213,15 @@
 					$('#sub_arrow_'+item_id).toggleClass(['fa-{/literal}{$settings.theme.menu_side_item_main_sub_icon_contract}{literal}','fa-{/literal}{$settings.theme.menu_side_item_main_sub_icon_expand}{literal}']);
 					$('.sub_arrows').not('#sub_arrow_'+item_id).removeClass('fa-{/literal}{$settings.theme.menu_side_item_main_sub_icon_contract}{literal}').addClass('fa-{/literal}{$settings.theme.menu_side_item_main_sub_icon_expand}{literal}');
 					$('#sub_'+item_id).slideToggle(180, function() {
-						if (!$(this).is(':hidden')) {
-							$('.menu_side_sub').not($(this)).slideUp(180);
-						}
+						{/literal}
+						{if $settings.theme.menu_side_item_main_sub_close != 'manual'}
+							{literal}
+							if (!$(this).is(':hidden')) {
+								$('.menu_side_sub').not($(this)).slideUp(180);
+							}
+							{/literal}
+						{/if}
+						{literal}
 					});
 				}
 
