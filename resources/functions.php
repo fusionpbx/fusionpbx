db8c63985e0ed9fdd10c4925c120303d597dd719
FusionPBX <markjcrane@gmail.com>
2022-03-22 14:07:22 -0600

    Temporarily exclude the email_queue function transcribe.

--- resources/functions.php
+++ resources/functions.php
@@ -2285,7 +2285,10 @@ function number_pad($number,$n) {
 //include additional functions
 	$functions = glob("{".$_SERVER["DOCUMENT_ROOT"].PROJECT_PATH."/resources/functions/*.php,".$_SERVER["DOCUMENT_ROOT"].PROJECT_PATH."/*/*/resources/functions/*.php}", GLOB_BRACE);
 	foreach($functions as $function) {
-		require($function);
+		$path = pathinfo($function);
+		if ($path['filename'] != 'transcribe') {
+			require($function);
+		}
 	}
 
 ?>
