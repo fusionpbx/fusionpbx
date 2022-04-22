133c51bfcfa47aa85271cdca6e25a07cec91e41f
markjcrane <markjcrane@gmail.com>
2022-04-22 13:00:38 -0600

    Add the email_queue.

---
+++ app/email_queue/app_menu.php
@@ -0,0 +1,19 @@
+<?php
+
+$apps[$x]['menu'][0]['title']['en-us'] = 'Email Queue';
+$apps[$x]['menu'][0]['title']['es-cl'] = '';
+$apps[$x]['menu'][0]['title']['fr-fr'] = '';
+$apps[$x]['menu'][0]['title']['fr-ca'] = '';
+$apps[$x]['menu'][0]['title']['pl'] = '';
+$apps[$x]['menu'][0]['title']['sv-se'] = '';
+$apps[$x]['menu'][0]['title']['uk'] = '';
+$apps[$x]['menu'][0]['title']['de-at'] = '';
+$apps[$x]['menu'][0]['uuid'] = 'dfa6a878-e2b0-4051-acaf-f6ffc13b9abf';
+$apps[$x]['menu'][0]['parent_uuid'] = '594d99c5-6128-9c88-ca35-4b33392cec0f';
+$apps[$x]['menu'][0]['category'] = 'internal';
+$apps[$x]['menu'][0]['path'] = '/app/email_queue/email_queue.php';
+$apps[$x]['menu'][0]['groups'][] = 'superadmin';
+//$apps[$x]['menu'][0]['groups'][] = 'admin';
+//$apps[$x]['menu'][0]['groups'][] = 'user';
+
+?>
