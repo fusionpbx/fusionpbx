4adcdddf41bbc878c290e337e08f7983c56fd269
AlexC <40072887+alexdcrane@users.noreply.github.com>
2022-04-25 10:25:04 -0600

    Add notify date column to Fax Queue (#6349)
    * Add notify date column to Fax Queue
    
    * Update app_languages.php
    
    * Update fax_queue_edit.php
    
    * Update app_languages.php

--- app/fax_queue/app_languages.php
+++ app/fax_queue/app_languages.php
@@ -481,6 +481,26 @@
 		$text['label-fax_retry_date']['sv-se'] = '';
 		$text['label-fax_retry_date']['uk-ua'] = '';
 
+		$text['label-fax_notify_date']['en-us'] = 'Notify Date';
+		$text['label-fax_notify_date']['ar-eg'] = '';
+		$text['label-fax_notify_date']['de-at'] = '';
+		$text['label-fax_notify_date']['de-ch'] = '';
+		$text['label-fax_notify_date']['de-de'] = '';
+		$text['label-fax_notify_date']['es-cl'] = '';
+		$text['label-fax_notify_date']['es-mx'] = '';
+		$text['label-fax_notify_date']['fr-ca'] = '';
+		$text['label-fax_notify_date']['fr-fr'] = '';
+		$text['label-fax_notify_date']['he-il'] = '';
+		$text['label-fax_notify_date']['it-it'] = '';
+		$text['label-fax_notify_date']['nl-nl'] = '';
+		$text['label-fax_notify_date']['pl-pl'] = '';
+		$text['label-fax_notify_date']['pt-br'] = '';
+		$text['label-fax_notify_date']['pt-pt'] = '';
+		$text['label-fax_notify_date']['ro-ro'] = '';
+		$text['label-fax_notify_date']['ru-ru'] = '';
+		$text['label-fax_notify_date']['sv-se'] = '';
+		$text['label-fax_notify_date']['uk-ua'] = '';
+
 		$text['description-fax_retry_date']['en-us'] = 'Date the fax was retried';
 		$text['description-fax_retry_date']['ar-eg'] = '';
 		$text['description-fax_retry_date']['de-at'] = '';
@@ -500,6 +520,26 @@
 		$text['description-fax_retry_date']['ru-ru'] = '';
 		$text['description-fax_retry_date']['sv-se'] = '';
 		$text['description-fax_retry_date']['uk-ua'] = '';
+
+		$text['description-fax_notify_date']['en-us'] = 'Date the fax was notified';
+		$text['description-fax_notify_date']['ar-eg'] = '';
+		$text['description-fax_notify_date']['de-at'] = '';
+		$text['description-fax_notify_date']['de-ch'] = '';
+		$text['description-fax_notify_date']['de-de'] = '';
+		$text['description-fax_notify_date']['es-cl'] = '';
+		$text['description-fax_notify_date']['es-mx'] = '';
+		$text['description-fax_notify_date']['fr-ca'] = '';
+		$text['description-fax_notify_date']['fr-fr'] = '';
+		$text['description-fax_notify_date']['he-il'] = '';
+		$text['description-fax_notify_date']['it-it'] = '';
+		$text['description-fax_notify_date']['nl-nl'] = '';
+		$text['description-fax_notify_date']['pl-pl'] = '';
+		$text['description-fax_notify_date']['pt-br'] = '';
+		$text['description-fax_notify_date']['pt-pt'] = '';
+		$text['description-fax_notify_date']['ro-ro'] = '';
+		$text['description-fax_notify_date']['ru-ru'] = '';
+		$text['description-fax_notify_date']['sv-se'] = '';
+		$text['description-fax_notify_date']['uk-ua'] = '';
 
 		$text['label-fax_retry_count']['en-us'] = 'Retry Count';
 		$text['label-fax_retry_count']['ar-eg'] = '';
