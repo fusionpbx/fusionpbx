4c9a0d174ce20d33468c530279596bb2e7e0cf33
nemerald-voip <info@nemerald.com>
2022-05-17 14:26:23 -0700

    fixed login issue

--- resources/check_auth.php
+++ resources/check_auth.php
@@ -43,7 +43,8 @@
 	if (!isset($_SESSION['template_content'])) { $_SESSION["template_content"] = null; }
 
 // check if the user has been authenticated in Laravel or here
-	if (isset($_SESSION['LARAVEL_UN']) || strlen($_SESSION['username']) != 0){
+	if ((isset($_SESSION['LARAVEL_UN']) || strlen($_SESSION['username']) != 0) &&
+	isset($_COOKIE[$_SESSION['cookie_name']])){
 		$_REQUEST["username"] = $_SESSION['LARAVEL_UN'];
 		$_REQUEST["password"] = $_SESSION['LARAVEL_PW'];
 		$_SESSION['login']['destination']['url'] = $_SESSION['redirect_url'];
