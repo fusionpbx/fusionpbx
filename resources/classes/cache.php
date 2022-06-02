9b2cf5220a0e1a4dbe3724e7c606a4786dd0737f
FusionPBX <markjcrane@gmail.com>
2022-06-01 22:28:12 -0600

    When the cache is deleted log details to the syslog server.

--- resources/classes/cache.php
+++ resources/classes/cache.php
@@ -106,6 +106,11 @@ class cache {
 	 */
 	public function delete($key) {
 
+		//debug information
+			openlog("fusionpbx", LOG_PID | LOG_PERROR, LOG_LOCAL0);
+			syslog(LOG_WARNING, "debug: cache: [key: ".$key.", script: ".$_SERVER['SCRIPT_NAME'].", line: ".__line__."]");
+			closelog();
+
 		//cache method memcache 
 			if ($_SESSION['cache']['method']['text'] == "memcache") {
 				//connect to event socket
@@ -166,6 +171,12 @@ class cache {
 	 * Delete the entire cache
 	 */
 	public function flush() {
+
+		//debug information
+			openlog("fusionpbx", LOG_PID | LOG_PERROR, LOG_LOCAL0);
+			syslog(LOG_WARNING, "debug: cache: [flush: all, script: ".$_SERVER['SCRIPT_NAME'].", line: ".__line__."]");
+			closelog();
+
 		//cache method memcache 
 			if ($_SESSION['cache']['method']['text'] == "memcache") {
 				// connect to event socket
