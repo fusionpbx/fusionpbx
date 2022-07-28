39d6652d9620c6a1f812c57d726f3a2595a7f8bf
FusionPBX <markjcrane@gmail.com>
2022-07-28 09:29:28 -0600

    Create app_menu.php

---
+++ app/event_guard/app_menu.php
@@ -0,0 +1,17 @@
+<?php
+
+$apps[$x]['menu'][0]['title']['en-us'] = 'Event Guard';
+$apps[$x]['menu'][0]['title']['es-cl'] = '';
+$apps[$x]['menu'][0]['title']['fr-fr'] = '';
+$apps[$x]['menu'][0]['title']['fr-ca'] = '';
+$apps[$x]['menu'][0]['title']['pl'] = '';
+$apps[$x]['menu'][0]['title']['sv-se'] = '';
+$apps[$x]['menu'][0]['title']['uk'] = '';
+$apps[$x]['menu'][0]['title']['de-at'] = '';
+$apps[$x]['menu'][0]['uuid'] = 'e80a81ae-7894-4003-9c0a-6df56c8148eb';
+$apps[$x]['menu'][0]['parent_uuid'] = '0438b504-8613-7887-c420-c837ffb20cb1';
+$apps[$x]['menu'][0]['category'] = 'internal';
+$apps[$x]['menu'][0]['path'] = '/app/event_guard/event_guard_logs.php';
+$apps[$x]['menu'][0]['groups'][] = 'superadmin';
+
+?>
