a189bc4991e47de8ea454c0b867f1b48a614b87b
FusionPBX <markjcrane@gmail.com>
2022-07-29 10:28:22 -0600

    Add proxy to the gateway list.

--- app/gateways/gateways.php
+++ app/gateways/gateways.php
@@ -246,6 +246,7 @@
 		echo th_order_by('domain_name', $text['label-domain'], $order_by, $order, $param);
 	}
 	echo th_order_by('gateway', $text['label-gateway'], $order_by, $order);
+	echo "<th class='hide-sm-dn'>".$text['label-proxy']."</th>\n";
 	echo th_order_by('context', $text['label-context'], $order_by, $order);
 	if ($fp) {
 		echo "<th class='hide-sm-dn'>".$text['label-status']."</th>\n";
@@ -293,6 +294,7 @@
 				echo escape($row['gateway']);
 			}
 			echo "	</td>\n";
+			echo "	<td>".escape($row["proxy"])."</td>\n";
 			echo "	<td>".escape($row["context"])."</td>\n";
 			if ($fp) {
 				if ($row["enabled"] == "true") {
