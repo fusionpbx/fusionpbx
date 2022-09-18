0ca73f28dacd83d68a8ed2cffec467c49201e1c1
FusionPBX <markjcrane@gmail.com>
2022-09-18 02:10:28 -0600

    Update event_guard_logs.php

--- app/event_guard/event_guard_logs.php
+++ app/event_guard/event_guard_logs.php
@@ -1,6 +1,8 @@
 <?php
 /*
+	BSD-2-Clause License
 	Copyright (C) 2022 Mark J Crane <markjcrane@fusionpbx.com>
+
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
 	1. Redistributions of source code must retain the above copyright notice,
@@ -8,6 +10,7 @@
 	2. Redistributions in binary form must reproduce the above copyright
 	   notice, this list of conditions and the following disclaimer in the
 	   documentation and/or other materials provided with the distribution.
+
 	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
 	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
 	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
