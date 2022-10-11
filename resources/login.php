a34d2c818e71ee48d741949b7edd66df54f08c59
FusionPBX <markjcrane@gmail.com>
2022-10-10 19:37:47 -0600

    Minor change from spaces to tabs

--- resources/login.php
+++ resources/login.php
@@ -25,11 +25,11 @@
 */
 
 //set the include path
-        $conf = glob("{/usr/local/etc,/etc}/fusionpbx/config.conf", GLOB_BRACE);
-        set_include_path(parse_ini_file($conf[0])['document.root']);
+	$conf = glob("{/usr/local/etc,/etc}/fusionpbx/config.conf", GLOB_BRACE);
+	set_include_path(parse_ini_file($conf[0])['document.root']);
 
 //includes files
-        require_once "resources/require.php";
+	require_once "resources/require.php";
 
 //add multi-lingual support
 	$language = new text;
