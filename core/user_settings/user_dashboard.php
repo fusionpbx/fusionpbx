e3b782e7cb7b2f08c97862cbe8e28144a6c27bc2
FusionPBX <markjcrane@gmail.com>
2022-10-10 19:39:27 -0600

    Update user_dashboard.php

--- core/user_settings/user_dashboard.php
+++ core/user_settings/user_dashboard.php
@@ -31,20 +31,6 @@
 //includes files
 	require_once "resources/require.php";
 
-//if config.php file does not exist then redirect to the install page
-	if (file_exists($_SERVER["PROJECT_ROOT"]."/resources/config.php")) {
-		//do nothing
-	} elseif (file_exists($_SERVER["PROJECT_ROOT"]."/resources/config.php")) {
-		//original directory
-	} elseif (file_exists("/etc/fusionpbx/config.php")){
-		//linux
-	} elseif (file_exists("/usr/local/etc/fusionpbx/config.php")){
-		//bsd
-	} else {
-		header("Location: ".PROJECT_PATH."/core/install/install.php");
-		exit;
-	}
-
 //additional includes
 	require_once "resources/check_auth.php";
 
