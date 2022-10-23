20fa20b20034a010520a101d0c3fb94ee720aa0c
FusionPBX <markjcrane@gmail.com>
2022-10-22 23:52:01 -0600

    Re-organize the inlcudes.

--- app/email_queue/resources/jobs/email_queue.php
+++ app/email_queue/resources/jobs/email_queue.php
@@ -5,14 +5,16 @@
 		//set the include path
 		$conf = glob("{/usr/local/etc,/etc}/fusionpbx/config.conf", GLOB_BRACE);
 		set_include_path(parse_ini_file($conf[0])['document.root']);
-
-		//includes files
-		require_once "resources/require.php";
-		require_once "resources/pdo.php";
 	}
 	else {
 		exit;
 	}
+
+//includes files
+	require_once "resources/require.php";
+	require_once "resources/pdo.php";
+	include "resources/classes/permissions.php";
+	require $document_root."/app/email_queue/resources/functions/transcribe.php";
 
 //increase limits
 	set_time_limit(0);
@@ -33,10 +35,6 @@
 	if (isset($_GET['debug'])) {
 		$debug = $_GET['debug'];
 	}
-
-//includes
-	include "resources/classes/permissions.php";
-	require $document_root."/app/email_queue/resources/functions/transcribe.php";
 
 //define the process id file
 	$pid_file = "/var/run/fusionpbx/".basename( $argv[0], ".php") .".pid";
