7767c7dc332e1f2e6fe1e479b2a8c3082dfa5456
FusionPBX <markjcrane@gmail.com>
2022-12-02 17:39:02 -0700

    Disable yealink_provision_url by default.
    - When this is disable it will use the domain of the tenant.
    - If the domain of the tenant is not resolvable then yealink_provision_url is needed.
    - One reason to disable it is the domain_name has to be manually updated to the correct domain.

--- app/yealink/app_config.php
+++ app/yealink/app_config.php
@@ -36,7 +36,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "yealink_provision_url";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = 'https://{$domain_name}/app/provision/';
-		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "false";
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Provisioning URL for Yealink";
 		$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "fc2fa8cd-b14e-48e3-99bd-7c01c9d6208d";
