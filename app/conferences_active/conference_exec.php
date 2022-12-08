b7147022cc35ee9f6c2a8b281f3480e94fb01923
FusionPBX <markjcrane@gmail.com>
2022-12-07 21:52:34 -0700

    include document root and config.conf

--- app/conferences_active/conference_exec.php
+++ app/conferences_active/conference_exec.php
@@ -36,6 +36,10 @@
 
 */
 
+//set the include path
+	$conf = glob("{/usr/local/etc,/etc}/fusionpbx/config.conf", GLOB_BRACE);
+	set_include_path(parse_ini_file($conf[0])['document.root']);
+
 //includes
 	require_once "resources/require.php";
 	require_once "resources/check_auth.php";
