4b526360226b97b30d31b26f467b5e4434bacd09
fusionate <nate@fusionpbx.com>
2023-01-17 00:06:01 +0000

    Log Viewer: Update copyright date.

--- app/log_viewer/log_viewer.php
+++ app/log_viewer/log_viewer.php
@@ -17,7 +17,7 @@
 
 	The Initial Developer of the Original Code is
 	Mark J Crane <markjcrane@fusionpbx.com>
-	Portions created by the Initial Developer are Copyright (C) 2008-2021
+	Portions created by the Initial Developer are Copyright (C) 2008-2023
 	the Initial Developer. All Rights Reserved.
 
 	Contributor(s):
