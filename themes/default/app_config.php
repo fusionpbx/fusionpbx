e2d577ef8dd7dc35f2ea321de1e7a8cee2e2149c
fusionate <nate@fusionpbx.com>
2023-01-28 23:06:36 +0000

    Theme: Fix icon reference for Domain button.

--- themes/default/app_config.php
+++ themes/default/app_config.php
@@ -2115,7 +2115,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_category'] = "theme";
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "button_icon_domain";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
-		$apps[$x]['default_settings'][$y]['default_setting_value'] = "fas vector-square";
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = "fas fa-vector-square";
 		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "";
 		$y++;
