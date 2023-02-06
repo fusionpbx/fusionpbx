9a09ca2c6ca8556d39f23b6f16982ccd189345df
fusionate <nate@fusionpbx.com>
2023-02-06 23:24:49 +0000

    Voicemail - Edit: Renenable Listen Link option in select, like Extension edit page.

--- app/extensions/extension_edit.php
+++ app/extensions/extension_edit.php
@@ -1744,7 +1744,7 @@
 			echo "</td>\n";
 			echo "<td class='vtable' align='left'>\n";
 			echo "    <select class='formfld' name='voicemail_file' id='voicemail_file' onchange=\"if (this.selectedIndex != 2) { document.getElementById('voicemail_local_after_email').selectedIndex = 0; }\">\n";
-			echo "    	<option value='' ".(($voicemail_file == "listen") ? "selected='selected'" : null).">".$text['option-voicemail_file_listen']."</option>\n";
+			echo "    	<option value=''>".$text['option-voicemail_file_listen']."</option>\n";
 			echo "    	<option value='link' ".(($voicemail_file == "link") ? "selected='selected'" : null).">".$text['option-voicemail_file_link']."</option>\n";
 			echo "    	<option value='attach' ".(($voicemail_file == "attach") ? "selected='selected'" : null).">".$text['option-voicemail_file_attach']."</option>\n";
 			echo "    </select>\n";
