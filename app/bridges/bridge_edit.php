b80bf704028b222d618ddd416fd36d4327ed3649
Alex <40072887+alexdcrane@users.noreply.github.com>
2023-02-14 10:31:57 -0700

    fixed enable on bridge_edit.php

--- app/bridges/bridge_edit.php
+++ app/bridges/bridge_edit.php
@@ -226,8 +226,8 @@
 	}
 	else {
 		echo "	<select class='formfld' id='bridge_enabled' name='bridge_enabled'>\n";
-		echo "		<option value='false'>".$text['option-false']."</option>\n";
 		echo "		<option value='true' ".($bridge_enabled == 'true' ? "selected='selected'" : null).">".$text['option-true']."</option>\n";
+		echo "		<option value='false' ".($bridge_enabled == 'false' ? "selected='selected'" : null).">".$text['option-false']."</option>\n";
 		echo "	</select>\n";
 	}
 	echo "<br />\n";
