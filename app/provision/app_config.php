5fe06f76c1499e66e053004b9255ee02abe9f8a1
FusionPBX <markjcrane@gmail.com>
2023-02-16 11:50:03 -0700

    Update app_config.php

--- app/provision/app_config.php
+++ app/provision/app_config.php
@@ -91,7 +91,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "admin_name";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
-		$apps[$x]['default_settings'][$y]['default_setting_value'] = "";
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = "admin";
 		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "false";
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "";
 		$y++;
