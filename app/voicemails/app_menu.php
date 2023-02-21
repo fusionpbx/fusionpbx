dc872f090cbdf4c1b72d303ea387412129b539a3
FusionPBX <markjcrane@gmail.com>
2023-02-21 15:47:31 -0700

    Add user voicemail to menu

--- app/voicemails/app_menu.php
+++ app/voicemails/app_menu.php
@@ -26,5 +26,30 @@
 	$apps[$x]['menu'][$y]['path'] = "/app/voicemails/voicemails.php";
 	$apps[$x]['menu'][$y]['groups'][] = "superadmin";
 	$apps[$x]['menu'][$y]['groups'][] = "admin";
+	$y++;
+	$apps[$x]['menu'][$y]['title']['en-us'] = "Voicemail";
+	$apps[$x]['menu'][$y]['title']['en-gb'] = "Voicemail";
+	$apps[$x]['menu'][$y]['title']['ar-eg'] = "";
+	$apps[$x]['menu'][$y]['title']['de-at'] = "Sprachnachrichten";
+	$apps[$x]['menu'][$y]['title']['de-de'] = "Sprachnachrichten";
+	$apps[$x]['menu'][$y]['title']['es-cl'] = "";
+	$apps[$x]['menu'][$y]['title']['es-mx'] = "";
+	$apps[$x]['menu'][$y]['title']['fr-ca'] = "";
+	$apps[$x]['menu'][$y]['title']['fr-fr'] = "Messagerie Vocale";
+	$apps[$x]['menu'][$y]['title']['he-il'] = "תא קולי";
+	$apps[$x]['menu'][$y]['title']['it-it'] = "Caselle Vocali";
+	$apps[$x]['menu'][$y]['title']['nl-nl'] = "Antwoordapparaat";
+	$apps[$x]['menu'][$y]['title']['pl-pl'] = "Poczta głosowa";
+	$apps[$x]['menu'][$y]['title']['pt-br'] = "Correio de voz";
+	$apps[$x]['menu'][$y]['title']['pt-pt'] = "";
+	$apps[$x]['menu'][$y]['title']['ro-ro'] = "";
+	$apps[$x]['menu'][$y]['title']['ru-ru'] = "Голосовая почта";
+	$apps[$x]['menu'][$y]['title']['sv-se'] = "Röstbrevlåda";
+	$apps[$x]['menu'][$y]['title']['uk-ua'] = "Голосова пошта";
+	$apps[$x]['menu'][$y]['uuid'] = "b8277aba-8697-41e2-aeae-48979b4d4d24";
+	$apps[$x]['menu'][$y]['parent_uuid'] = "fd29e39c-c936-f5fc-8e2b-611681b266b5";
+	$apps[$x]['menu'][$y]['category'] = "internal";
+	$apps[$x]['menu'][$y]['path'] = "/app/voicemails/voicemail_messages.php";
+	$apps[$x]['menu'][$y]['groups'][] = "user";
 
 ?>
