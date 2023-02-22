0242944019199f6200ce96d1377dffa11f4f5cb3
FusionPBX <markjcrane@gmail.com>
2023-02-21 20:45:54 -0700

    Add key to the name

--- app/devices/device_profile_edit.php
+++ app/devices/device_profile_edit.php
@@ -309,11 +309,11 @@
 
 //add an empty row
 	if (!is_array($device_profile_keys) || count($device_profile_keys) == 0) {
-		$rows = $_SESSION['devices']['profile_add_rows']['numeric'];
+		$rows = $_SESSION['devices']['profile_key_add_rows']['numeric'];
 		$id = 0;
 	}
 	if (is_array($device_profile_keys) && count($device_profile_keys) > 0) {
-		$rows = $_SESSION['devices']['profile_edit_rows']['numeric'];
+		$rows = $_SESSION['devices']['profile_key_edit_rows']['numeric'];
 		$id = count($device_profile_keys)+1;
 	}
 	for ($x = 0; $x < $rows; $x++) {
