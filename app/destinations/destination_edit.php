3247357c5dfa935306ac63cd54c8b9e45debacb5
FusionPBX <markjcrane@gmail.com>
2023-03-22 10:01:43 -0600

    Remove action == add

--- app/destinations/destination_edit.php
+++ app/destinations/destination_edit.php
@@ -174,8 +174,8 @@
 		//check for all required data
 			$msg = '';
 			if (strlen($destination_type) == 0) { $msg .= $text['message-required']." ".$text['label-destination_type']."<br>\n"; }
-			if (strlen($destination_country_code) == 0 && permission_exists('destination_prefix') && $action == 'add') { $msg .= $text['message-required']." ".$text['label-destination_country_code']."<br>\n"; }
-			if (strlen($destination_number) == 0 && $action == 'add') { $msg .= $text['message-required']." ".$text['label-destination_number']."<br>\n"; }
+			if (strlen($destination_prefix) == 0 && permission_exists('destination_prefix')) { $msg .= $text['message-required']." ".$text['label-destination_country_code']."<br>\n"; }
+			if (strlen($destination_number) == 0) { $msg .= $text['message-required']." ".$text['label-destination_number']."<br>\n"; }
 			if (strlen($destination_context) == 0) { $msg .= $text['message-required']." ".$text['label-destination_context']."<br>\n"; }
 			if (strlen($destination_enabled) == 0) { $msg .= $text['message-required']." ".$text['label-destination_enabled']."<br>\n"; }
 
