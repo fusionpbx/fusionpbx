21c013651b836e00a768171a93a92ccb765e8187
FusionPBX <markjcrane@gmail.com>
2023-04-14 22:33:32 -0600

    Add a comment for the xml library

--- app/scripts/resources/scripts/app/xml_handler/resources/scripts/directory/action/group_call.lua
+++ app/scripts/resources/scripts/app/xml_handler/resources/scripts/directory/action/group_call.lua
@@ -24,6 +24,7 @@
 --	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 --	POSSIBILITY OF SUCH DAMAGE.
 
+--include xml library
 	local Xml = require "resources.functions.xml";
 
 --get the cache
