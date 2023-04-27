2e181978f273e2dbb36ccb4043e0c0a32fb96e25
Tim Fry <tim@voipstratus.com>
2023-04-27 00:01:39 -0300

    php 8.1 fixes

--- app/call_centers/call_center_agents.php
+++ app/call_centers/call_center_agents.php
@@ -74,7 +74,7 @@
 
 //add the search term
 	$search = strtolower($_GET["search"]);
-	if (strlen($search) > 0) {
+	if (!empty($search)) {
 		$sql_search = " (";
 		$sql_search .= "lower(agent_name) like :search ";
 		$sql_search .= "or lower(agent_id) like :search ";
@@ -102,13 +102,13 @@
 		$param .= "&show=all";
 	}
 	$page = $_GET['page'];
-	if (strlen($page) == 0) { $page = 0; $_GET['page'] = 0; }
+	if (empty($page)) { $page = 0; $_GET['page'] = 0; }
 	list($paging_controls, $rows_per_page) = paging($num_rows, $param, $rows_per_page);
 	list($paging_controls_mini, $rows_per_page) = paging($num_rows, $param, $rows_per_page, true);
 	$offset = $rows_per_page * $page;
 
 //get the list
-	$sql = str_replace('count(*)', '*', $sql);
+	$sql = str_replace('count(*)', '*', $sql ?? '');
 	$sql .= order_by($order_by, $order, 'agent_name', 'asc');
 	$sql .= limit_offset($rows_per_page, $offset);
 	$database = new database;
