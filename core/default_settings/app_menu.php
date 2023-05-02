bc4ed0c8c103acd2be6c0399893c4cd6fb80d1d5
FusionPBX <markjcrane@gmail.com>
2023-05-01 20:51:40 -0600

    Add additional languages

--- core/default_settings/app_menu.php
+++ core/default_settings/app_menu.php
@@ -3,14 +3,14 @@
 	$y=0;
 	$apps[$x]['menu'][$y]['title']['en-us'] = "Default Settings";
 	$apps[$x]['menu'][$y]['title']['en-gb'] = "Default Settings";
-	$apps[$x]['menu'][$y]['title']['ar-eg'] = "";
+	$apps[$x]['menu'][$y]['title']['ar-eg'] = "Default Settings";
 	$apps[$x]['menu'][$y]['title']['de-at'] = "Standardeinstellungen";
 	$apps[$x]['menu'][$y]['title']['de-de'] = "Standardeinstellungen";
 	$apps[$x]['menu'][$y]['title']['es-cl'] = "Conf. Predeterminada";
-	$apps[$x]['menu'][$y]['title']['es-mx'] = "";
-	$apps[$x]['menu'][$y]['title']['fr-ca'] = "";
+	$apps[$x]['menu'][$y]['title']['es-mx'] = "Ajustes predeterminados";
+	$apps[$x]['menu'][$y]['title']['fr-ca'] = "Paramètres par défaut";
 	$apps[$x]['menu'][$y]['title']['fr-fr'] = "Confs par Défaut";
-	$apps[$x]['menu'][$y]['title']['he-il'] = "";
+	$apps[$x]['menu'][$y]['title']['he-il'] = "הגדרות Default Settings";
 	$apps[$x]['menu'][$y]['title']['it-it'] = "Parametri di Default";
 	$apps[$x]['menu'][$y]['title']['nl-nl'] = "Standaard instellingen";
 	$apps[$x]['menu'][$y]['title']['pl-pl'] = "Ustawienia domyślne";
@@ -25,5 +25,6 @@
 	$apps[$x]['menu'][$y]['category'] = "internal";
 	$apps[$x]['menu'][$y]['path'] = "/core/default_settings/default_settings.php";
 	$apps[$x]['menu'][$y]['groups'][] = "superadmin";
+	$y++;
 
 ?>
