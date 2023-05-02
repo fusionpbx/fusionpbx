bc36fdb3cd3fe5f76f0d55a4a90ba6e5d5357d88
FusionPBX <markjcrane@gmail.com>
2023-05-01 21:02:16 -0600

    Add additional translations

--- core/notifications/app_menu.php
+++ core/notifications/app_menu.php
@@ -3,20 +3,20 @@
 	$y = 0;
 	$apps[$x]['menu'][$y]['title']['en-us'] = "Notifications";
 	$apps[$x]['menu'][$y]['title']['en-gb'] = "Notifications";
-	$apps[$x]['menu'][$y]['title']['ar-eg'] = "";
+	$apps[$x]['menu'][$y]['title']['ar-eg'] = "الإخطارات";
 	$apps[$x]['menu'][$y]['title']['de-at'] = "Benachrichtigungen";
 	$apps[$x]['menu'][$y]['title']['de-de'] = "Benachrichtigungen";
 	$apps[$x]['menu'][$y]['title']['el-gr'] = "Γνωστοποιήσεις";
 	$apps[$x]['menu'][$y]['title']['es-cl'] = "Notificaciones";
-	$apps[$x]['menu'][$y]['title']['es-mx'] = "";
+	$apps[$x]['menu'][$y]['title']['es-mx'] = "Notificaciones";
 	$apps[$x]['menu'][$y]['title']['fr-ca'] = "Notifications";
 	$apps[$x]['menu'][$y]['title']['fr-fr'] = "Notifications";
-	$apps[$x]['menu'][$y]['title']['he-il'] = "";
+	$apps[$x]['menu'][$y]['title']['he-il'] = "Notifications";
 	$apps[$x]['menu'][$y]['title']['it-it'] = "Notifiche";
-	$apps[$x]['menu'][$y]['title']['nl-nl'] = "";
+	$apps[$x]['menu'][$y]['title']['nl-nl'] = "Verklaringen";
 	$apps[$x]['menu'][$y]['title']['pl-pl'] = "Powiadomienia";
-	$apps[$x]['menu'][$y]['title']['pt-br'] = "";
-	$apps[$x]['menu'][$y]['title']['pt-pt'] = "";
+	$apps[$x]['menu'][$y]['title']['pt-br'] = "Notificações";
+	$apps[$x]['menu'][$y]['title']['pt-pt'] = "Notificações";
 	$apps[$x]['menu'][$y]['title']['ro-ro'] = "";
 	$apps[$x]['menu'][$y]['title']['ru-ru'] = "Уведомления";
 	$apps[$x]['menu'][$y]['title']['sv-se'] = "Meddelanden";
@@ -25,6 +25,6 @@
 	$apps[$x]['menu'][$y]['parent_uuid'] = "594d99c5-6128-9c88-ca35-4b33392cec0f";
 	$apps[$x]['menu'][$y]['category'] = "internal";
 	$apps[$x]['menu'][$y]['path'] = "/core/notifications/notification_edit.php";
-	//$apps[$x]['menu'][$y]['groups'][] = "superadmin";
+	$y++;
 
 ?>
