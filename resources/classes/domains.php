f8c0589248d3c199e6d5dffe418c62c11d37743a
Tim Fry <tim.fry@hotmail.com>
2023-05-03 09:18:00 -0300

    remove unused variable db_host

--- resources/classes/domains.php
+++ resources/classes/domains.php
@@ -604,7 +604,6 @@ if (!class_exists('domains')) {
 				$config = new config;
 				$config_path = $config->find();
 				$config->get();
-				$db_host = $config->db_host;
 				$db_path = $config->db_path;
 				$db_port = $config->db_port;
 
