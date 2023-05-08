7fdb4a2eeb6c9b3c885944a3f99aff7c8008167a
Alex <40072887+alexdcrane@users.noreply.github.com>
2023-05-08 15:12:41 -0600

    Update app_menu.php

--- app/dialplan_inbound/app_menu.php
+++ app/dialplan_inbound/app_menu.php
@@ -17,13 +17,13 @@
 	$apps[$x]['menu'][$y]['title']['pl-pl'] = "Trasy przychodzące";
 	$apps[$x]['menu'][$y]['title']['pt-br'] = "Rotas de Entrada";
 	$apps[$x]['menu'][$y]['title']['pt-pt'] = "Rotas de Entrada";
-	$apps[$x]['menu'][$y]['title']['ro-ro'] = "";
+	$apps[$x]['menu'][$y]['title']['ro-ro'] = "Rute de intrare";
 	$apps[$x]['menu'][$y]['title']['ru-ru'] = "Маршрутизация входящих";
 	$apps[$x]['menu'][$y]['title']['sv-se'] = "Inbound Routes";
 	$apps[$x]['menu'][$y]['title']['uk-ua'] = "Вхідні маршрути";
-	$apps[$x]['menu'][$y]['title']['zh-cn'] = "A. 限制路线";
+	$apps[$x]['menu'][$y]['title']['zh-cn'] = "入境航线";
 	$apps[$x]['menu'][$y]['title']['ja-jp'] = "インバウンドルート";
-	$apps[$x]['menu'][$y]['title']['ko-kr'] = "Inbound 노선";
+	$apps[$x]['menu'][$y]['title']['ko-kr'] = "인바운드 경로";
 	$apps[$x]['menu'][$y]['uuid'] = "b64b2bbf-f99b-b568-13dc-32170515a687";
 	$apps[$x]['menu'][$y]['parent_uuid'] = "b94e8bd9-9eb5-e427-9c26-ff7a6c21552a";
 	$apps[$x]['menu'][$y]['category'] = "internal";
