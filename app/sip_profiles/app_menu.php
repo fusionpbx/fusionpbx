a5e96f3a5cba0e8298142f5f2200d319db6ff818
Alex <40072887+alexdcrane@users.noreply.github.com>
2023-05-09 09:27:18 -0600

    Update app_menu.php

--- app/sip_profiles/app_menu.php
+++ app/sip_profiles/app_menu.php
@@ -3,8 +3,9 @@
 	$y=0;
 	$apps[$x]['menu'][$y]['title']['en-us'] = "SIP Profiles";
 	$apps[$x]['menu'][$y]['title']['en-gb'] = "SIP Profiles";
-	$apps[$x]['menu'][$y]['title']['ar-eg'] = "SIP Profiles";
+	$apps[$x]['menu'][$y]['title']['ar-eg'] = "ملفات تعريف SIP";
 	$apps[$x]['menu'][$y]['title']['de-at'] = "SIP Profile";
+	$apps[$x]['menu'][$y]['title']['de-ch'] = "SIP Profile";
 	$apps[$x]['menu'][$y]['title']['de-de'] = "SIP Profile";
 	$apps[$x]['menu'][$y]['title']['es-cl'] = "Perfiles SIP";
 	$apps[$x]['menu'][$y]['title']['es-mx'] = "Perfiles SIP";
@@ -16,12 +17,12 @@
 	$apps[$x]['menu'][$y]['title']['pl-pl'] = "Profile SIP";
 	$apps[$x]['menu'][$y]['title']['pt-br'] = "Perfis SIP";
 	$apps[$x]['menu'][$y]['title']['pt-pt'] = "Perfis SIP";
-	$apps[$x]['menu'][$y]['title']['ro-ro'] = "";
+	$apps[$x]['menu'][$y]['title']['ro-ro'] = "Profiluri SIP";
 	$apps[$x]['menu'][$y]['title']['ru-ru'] = "Профили SIP";
 	$apps[$x]['menu'][$y]['title']['sv-se'] = "SIP Profiler";
 	$apps[$x]['menu'][$y]['title']['uk-ua'] = "SIP профілі";
-	$apps[$x]['menu'][$y]['title']['zh-cn'] = "SIP Profiles";
-	$apps[$x]['menu'][$y]['title']['ja-jp'] = "SIPプロフィール";
+	$apps[$x]['menu'][$y]['title']['zh-cn'] = "SIP 配置文件";
+	$apps[$x]['menu'][$y]['title']['ja-jp'] = "SIP プロファイル";
 	$apps[$x]['menu'][$y]['title']['ko-kr'] = "SIP 프로필";
 	$apps[$x]['menu'][$y]['uuid'] = "47014b1d-13ad-921c-313d-ca42c0424b37";
 	$apps[$x]['menu'][$y]['parent_uuid'] = "594d99c5-6128-9c88-ca35-4b33392cec0f";
