c2f0138f27ff50351218bc0ad58829404e3a82ac
Alex <40072887+alexdcrane@users.noreply.github.com>
2023-05-09 09:07:04 -0600

    Update app_menu.php

--- app/ring_groups/app_menu.php
+++ app/ring_groups/app_menu.php
@@ -5,6 +5,7 @@
 	$apps[$x]['menu'][$y]['title']['en-gb'] = "Ring Groups";
 	$apps[$x]['menu'][$y]['title']['ar-eg'] = "مجموعات حصص الإعاشة";
 	$apps[$x]['menu'][$y]['title']['de-at'] = "Rufgruppen";
+	$apps[$x]['menu'][$y]['title']['de-ch'] = "Rufgruppen";
 	$apps[$x]['menu'][$y]['title']['de-de'] = "Rufgruppen";
 	$apps[$x]['menu'][$y]['title']['es-cl'] = "Grupo de llamados";
 	$apps[$x]['menu'][$y]['title']['es-mx'] = "Ring Groups";
@@ -16,11 +17,11 @@
 	$apps[$x]['menu'][$y]['title']['pl-pl'] = "Grupy odbiorców rozmów";
 	$apps[$x]['menu'][$y]['title']['pt-br'] = "Grupos de chamada";
 	$apps[$x]['menu'][$y]['title']['pt-pt'] = "Grupos de Ring";
-	$apps[$x]['menu'][$y]['title']['ro-ro'] = "";
+	$apps[$x]['menu'][$y]['title']['ro-ro'] = "Grupuri de apeluri";
 	$apps[$x]['menu'][$y]['title']['ru-ru'] = "Группы вызовов";
 	$apps[$x]['menu'][$y]['title']['sv-se'] = "Ringgrupper";
 	$apps[$x]['menu'][$y]['title']['uk-ua'] = "Групи";
-	$apps[$x]['menu'][$y]['title']['zh-cn'] = "区域集团";
+	$apps[$x]['menu'][$y]['title']['zh-cn'] = "振铃组";
 	$apps[$x]['menu'][$y]['title']['ja-jp'] = "リンググループ";
 	$apps[$x]['menu'][$y]['title']['ko-kr'] = "링 그룹";
 	$apps[$x]['menu'][$y]['uuid'] = "b30f085f-3ec6-2819-7e62-53dfba5cb8d5";
