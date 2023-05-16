8149fcbcc30004b37d8bdcf51b805a67336b1788
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2023-05-16 12:38:09 -0400

    Update index.lua

--- app/scripts/resources/scripts/app/call_block/index.lua
+++ app/scripts/resources/scripts/app/call_block/index.lua
@@ -101,8 +101,8 @@
 				sql = sql .. "	(\n";
 				sql = sql .. "		call_block_name = :call_block_name \n";
 				sql = sql .. "		and ( \n";
-				sql = sql .. " 			CONCAT('+', call_block_country_code, call_block_number) = :call_block_number \n";
-				sql = sql .. " 			or CONCAT(call_block_country_code, call_block_number) = :call_block_number \n";
+				sql = sql .. "			'+' || call_block_country_code || call_block_number = :call_block_number \n";
+				sql = sql .. "			or call_block_country_code || call_block_number = :call_block_number \n";
 				sql = sql .. "			or call_block_number = :call_block_number \n";
 				sql = sql .. "		) \n";
 				sql = sql .. "	) \n";
