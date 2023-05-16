f2eecaebbcc8c93640d1511689ae8cd4fcade2ca
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2023-05-16 15:58:37 -0400

    Update switch_settings.php

--- resources/classes/switch_settings.php
+++ resources/classes/switch_settings.php
@@ -194,6 +194,7 @@ if (!class_exists('switch_settings')) {
 				$array[$x]['default_setting_enabled'] = 'true';
 				$array[$x]['default_setting_description'] = '';
 				$x++;
+				$array2 = $array;
 
 			//get an array of the default settings
 				$sql = "select * from v_default_settings ";
@@ -249,10 +250,10 @@ if (!class_exists('switch_settings')) {
 
 			//set the default settings
 				if (is_array($array)) {
-					foreach ($array as $row) {
+					foreach ($array2 as $row) {
 						if (!isset($_SESSION['switch'][$row['default_setting_subcategory']])) {
 							if ($row['default_setting_enabled'] != "false") {
-								$_SESSION['switch'][$row['default_setting_subcategory']] = $row['default_setting_value'];
+								$_SESSION['switch'][$row['default_setting_subcategory']][$row['default_setting_name']] = $row['default_setting_value'];
 							}
 						}
 					}
