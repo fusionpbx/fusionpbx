886c609a44242cc84b323c4358521532d5ef6c4a
Finn <finn@acceleratenetworks.com>
2023-05-19 16:33:12 -0700

    add a robots meta tag to reduce chances of private fusionpbx instances appearing in public Google search results (#6710)

--- themes/default/template.php
+++ themes/default/template.php
@@ -13,6 +13,7 @@
 <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
 <meta http-equiv='X-UA-Compatible' content='IE=edge'>
 <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+<meta name="robots" content="noindex, nofollow, noarchive" />
 
 {*//external css files *}
 	<link rel='stylesheet' type='text/css' href='{$project_path}/resources/bootstrap/css/bootstrap.min.css.php'>
