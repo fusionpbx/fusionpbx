4b5c56fb74fb449f38350dbc14a45da946301e5b
Tim Fry <tim.fry@hotmail.com>
2023-05-19 21:54:15 -0300

    use magic constant __DIR__

--- core/user_settings/user_dashboard.php
+++ core/user_settings/user_dashboard.php
@@ -1,15 +1,11 @@
 <?php
 
-//set the include path
-	$conf = glob("{/usr/local/etc,/etc}/fusionpbx/config.conf", GLOB_BRACE);
-	set_include_path(parse_ini_file($conf[0])['document.root']);
-
 //start the session
 	ini_set("session.cookie_httponly", True);
 	if (!isset($_SESSION)) { session_start(); }
 
 //includes files
-	require_once "resources/require.php";
+    require_once dirname(dirname(__DIR__)) . "/resources/require.php";
 	require_once "resources/check_auth.php";
 
 //add the header
