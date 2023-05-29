3a6d3792db99e9b5efde98bed0cb9fe732e3449a
fusionate <nate@fusionpbx.com>
2023-05-29 19:48:47 +0000

    Registrations [Class]: Updates for PHP 8.1

--- app/registrations/resources/classes/registrations.php
+++ app/registrations/resources/classes/registrations.php
@@ -56,6 +56,10 @@ if (!class_exists('registrations')) {
 		 */
 		public function get($profile = 'all') {
 
+			//add multi-lingual support
+				$language = new text;
+				$text = $language->get();
+
 			//initialize the id used in the registrations array
 				$id = 0;
 
