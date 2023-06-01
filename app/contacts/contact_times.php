0cb2f2ea5ac372fc0bcb313519fbe0b704381d7c
Alex <40072887+alexdcrane@users.noreply.github.com>
2023-06-01 11:28:56 -0600

    Update contact_times.php

--- app/contacts/contact_times.php
+++ app/contacts/contact_times.php
@@ -59,7 +59,7 @@
 	$parameters['domain_uuid'] = $domain_uuid;
 	$parameters['contact_uuid'] = $contact_uuid ?? '';
 	$database = new database;
-	$contact_times = $database->select($sql, $parameters ?? null, 'all');
+	$contact_times = $database->select($sql, $parameters, 'all');
 	unset($sql, $parameters);
 
 //show if exists
