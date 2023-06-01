48a03cbcf06d93d22f23aace36c4b49ea16648c6
Alex <40072887+alexdcrane@users.noreply.github.com>
2023-06-01 11:17:57 -0600

    Update contact_attachments.php

--- app/contacts/contact_attachments.php
+++ app/contacts/contact_attachments.php
@@ -52,7 +52,7 @@
 	$parameters['domain_uuid'] = $domain_uuid;
 	$parameters['contact_uuid'] = $contact_uuid ?? '';
 	$database = new database;
-	$contact_attachments = $database->select($sql, $parameters ?? null, 'all');
+	$contact_attachments = $database->select($sql, $parameters, 'all');
 	unset($sql, $parameters);
 
 //show if exists
