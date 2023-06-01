e977ab543867119b7ae94a786ecb6e61796115c6
Alex <40072887+alexdcrane@users.noreply.github.com>
2023-06-01 11:28:05 -0600

    Update contact_settings.php

--- app/contacts/contact_settings.php
+++ app/contacts/contact_settings.php
@@ -56,7 +56,7 @@
 	$parameters['domain_uuid'] = $domain_uuid;
 	$parameters['contact_uuid'] = $contact_uuid ?? '';
 	$database = new database;
-	$contact_settings = $database->select($sql, $parameters ?? null, 'all');
+	$contact_settings = $database->select($sql, $parameters, 'all');
 	unset($sql, $parameters);
 
 //show if exists
