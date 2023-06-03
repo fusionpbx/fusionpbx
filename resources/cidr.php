bc43f696caa8e092117f38ff6ecd034896b2332f
FusionPBX <markjcrane@gmail.com>
2023-06-03 10:19:42 -0600

    Bug fixed unset the cidr variable

--- resources/cidr.php
+++ resources/cidr.php
@@ -27,11 +27,14 @@
 //check the domain cidr range 
 	if (isset($_SESSION['domain']["cidr"]) && !defined('STDIN')) {
 		$found = false;
-		foreach($_SESSION['domain']["cidr"] as $cidr) {
-			if (check_cidr($cidr, $_SERVER['REMOTE_ADDR'])) {
-				$found = true;
-				break;
+		if (!empty($_SESSION['domain']["cidr"])) {
+			foreach($_SESSION['domain']["cidr"] as $cidr) {
+				if (check_cidr($cidr, $_SERVER['REMOTE_ADDR'])) {
+					$found = true;
+					break;
+				}
 			}
+			unset($cidr);
 		}
 		if (!$found) {
 			echo "access denied";
