65f5d0815bd611a83cfac8c9e0e0045a31b541f9
FusionPBX <markjcrane@gmail.com>
2023-06-15 11:15:04 -0600

    Update index.php

--- secure/index.php
+++ secure/index.php
@@ -27,7 +27,7 @@
 //block directory browsing and send the user to the main index
 
 //includes files
-    require_once dirname(__DIR__) . "/resources/require.php";
+	require_once dirname(__DIR__) . "/resources/require.php";
 
 //set a redirect
 	header("Location: /core/dashboard/index.php");
