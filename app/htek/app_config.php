9eaf041fd71ab5cb24901de0541aa97c1d531220
Tim Fry <tim.fry@hotmail.com>
2023-06-20 16:21:15 -0300

    split vendor to its own default settings category

--- app/htek/app_config.php
+++ app/htek/app_config.php
@@ -31,7 +31,7 @@
 	//default settings
 		$y=0;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "eebecb32-7c99-4d4c-bf05-6f9ce35da6ea";
-		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "htek_provision";
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "htek_time_zone";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "18";
@@ -39,7 +39,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Time zone 18=EST 14=CST 6=PST 9,10=MST";		
 		$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "25c97d75-1e20-42ae-bb38-a6eb79e5795d";
-		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "htek_provision";
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "htek_dst";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "numeric";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "1";
@@ -47,7 +47,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "DST off=0 on=1 auto=2";
 		$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "6570d02b-5dbe-42c6-8bb3-b0eaf34d555e";
-		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "htek_provision";
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "htek_date_display_format";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "numeric";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "1";
@@ -55,7 +55,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Year-Month-Day=0  Month-Day-Year=1  Day-Month-Year=2";
 		$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "d46268fc-622d-4ee2-8f7a-00dfb98f90b5";
-		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "htek_provision";
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "htek_time_format";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "numeric";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "1";
@@ -63,7 +63,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "1=12hr 0=24hr";
 		$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "58cb27b9-558d-42dd-a3c6-1f4c12690ba6";
-		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "htek_provision";
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "htek_account1_ring_tone";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "numeric";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "1";
@@ -71,7 +71,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Select ring tone number for account 1";
 		$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "e79dddf6-86ce-495b-a0f9-b6a8a3cae648";
-		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "htek_provision";
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "htek_screentimeout";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "numeric";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "1";
@@ -79,7 +79,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Select timeout for phone screen";
 		$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "7b6d11d4-675d-46e2-9dc1-27043478d33a";
-		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "htek_provision";
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "htek_pcport_mode";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "numeric";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "1";
