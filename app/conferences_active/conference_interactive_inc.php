c393b27bf393f98b8dcd1d1a078b0e28e10b2f47
FusionPBX <markjcrane@gmail.com>
2023-06-27 10:15:05 -0600

    Bug fixed for conference active

--- app/conferences_active/conference_interactive_inc.php
+++ app/conferences_active/conference_interactive_inc.php
@@ -43,15 +43,19 @@
 	$text = $language->get();
 
 //get the http get or post and set it as php variables
-	if (is_numeric($_REQUEST["c"])) {
+	if (!empty($_REQUEST["c"]) && is_numeric($_REQUEST["c"])) {
 		$conference_id = $_REQUEST["c"];
 	}
-	elseif (is_uuid($_REQUEST["c"])) {
+	elseif (!empty($_REQUEST["c"]) && is_uuid($_REQUEST["c"])) {
 		$conference_id = $_REQUEST["c"];
+	}
+	else {
+		//exit if the conference id is invalid
+		exit;
 	}
 
 //replace the space with underscore
-	$conference_name = !empty($conference_id).'@'.$_SESSION['domain_name'];
+	$conference_name = $conference_id.'@'.$_SESSION['domain_name'];
 
 //create the conference list command
 	$switch_cmd = "conference '".$conference_name."' xml_list";
