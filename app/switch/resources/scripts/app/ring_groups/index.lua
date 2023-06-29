e5ca998ef877b6746c9a77530c1c603732d2ad0c
FusionPBX <markjcrane@gmail.com>
2023-06-29 14:13:32 -0600

    Ad ring group destination delay with the follow me delay

--- app/switch/resources/scripts/app/ring_groups/index.lua
+++ app/switch/resources/scripts/app/ring_groups/index.lua
@@ -642,8 +642,7 @@
 								destinations[new_key]['ring_group_ringback'] = row.ring_group_ringback;
 								destinations[new_key]['domain_name'] = field.domain_name;
 								destinations[new_key]['destination_number'] = field.destination_number;
-								--destinations[new_key]['destination_delay'] = field.destination_delay + row.destination_delay;
-								destinations[new_key]['destination_delay'] = field.destination_delay;
+								destinations[new_key]['destination_delay'] = tonumber(field.destination_delay) + tonumber(row.destination_delay);
 								destinations[new_key]['destination_timeout'] = new_destination_timeout;
 								destinations[new_key]['destination_prompt'] = field.destination_prompt;
 								destinations[new_key]['group_confirm_key'] = row.group_confirm_key;
