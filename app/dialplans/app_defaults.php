15732df5ae18bd680dc47d5f98c3a81b2a51f6f4
jacobbuscher-bt <111894981+jacobbuscher-bt@users.noreply.github.com>
2023-08-02 12:25:45 -0600

    Finalize changing origination_callee_id_name from destination_number to caller_destination in domain_variables and variables dialplan. (#6782)
    * Add the / character to the ALLOWED_CHARS variable to fix bug with front slashes being stripped. Previously was turning /var/lib/freeswitch into varlibfreeswitch
    
    * Update app_defaults.php
    
    Updating app defaults for Dialplan -> Domain Variables to set origination_callee_id_name to match the caller_destination instead of the destination number so that if you call another tenant in the same Fusion instance without is_local enabled the caller_destination shows instead of the tenant's called extension.
    
    * Update app_defaults.php
    
    Instead of removing the export all together we need to update it from destination_number to caller_destination.
    
    * Update 020_domain-variables.xml
    
    Set origination callee ID to caller_destination so internal calls don't show "Outbound Call"
    
    ---------
    
    Authored-by: Jacob Buscher <jacob@bfitec.com>

--- app/dialplans/app_defaults.php
+++ app/dialplans/app_defaults.php
@@ -132,18 +132,18 @@
 		$database = new database;
 		$num_rows = $database->select($sql, null, 'column');
 		if ($num_rows > 0) {
-			$sql = "delete from v_dialplan_details \n";
-			$sql .= "where dialplan_uuid in (select dialplan_uuid from v_dialplans where dialplan_name = 'domain-variables' or dialplan_name = 'variables') \n";
-			$sql .= "and dialplan_detail_data like 'origination_callee_id_name%'; \n";
-			$database->execute($sql);
+                        $sql = "update v_dialplan_details set dialplan_detail_data = 'origination_callee_id_name=\${caller_destination}', update_date = now() \n";
+                        $sql .= "where dialplan_uuid in (select dialplan_uuid from v_dialplans where dialplan_name = 'domain-variables' or dialplan_name = 'variables') \n";
+                        $sql .= "and dialplan_detail_data = 'origination_callee_id_name=\${destination_number}'; \n";
+                        $database->execute($sql);
 
-			$sql = "update v_dialplans set dialplan_xml = REPLACE(dialplan_xml, '<action application=\"export\" data=\"origination_callee_id_name=\${caller_destination}\"/>', ''), update_date = now() \n";
-			$sql .= "where dialplan_uuid in (select dialplan_uuid from v_dialplans where dialplan_name = 'domain-variables' or dialplan_name = 'variables'); \n";
-			$database->execute($sql);
+                        $sql = "update v_dialplans set dialplan_xml = REPLACE(dialplan_xml, '<action application=\"export\" data=\"origination_callee_id_name=\${destination_number}\"/>', '<action application=\"export\" data=\"origination_callee_id_name=\${caller_destination}\"/>'), update_date = now() \n";
+                        $sql .= "where dialplan_uuid in (select dialplan_uuid from v_dialplans where dialplan_name = 'domain-variables' or dialplan_name = 'variables'); \n";
+                        $database->execute($sql);
 
-			$sql = "update v_dialplans set dialplan_xml = REPLACE(dialplan_xml, '<action application=\"export\" data=\"origination_callee_id_name=\${caller_destination}\" inline=\"true\"/>', ''), update_date = now() \n";
-			$sql .= "where dialplan_uuid in (select dialplan_uuid from v_dialplans where dialplan_name = 'domain-variables' or dialplan_name = 'variables'); \n";
-			$database->execute($sql);
+                        $sql = "update v_dialplans set dialplan_xml = REPLACE(dialplan_xml, '<action application=\"export\" data=\"origination_callee_id_name=\${destination_number}\" inline=\"true\"/>', '<action application=\"export\" data=\"origination_callee_id_name=\${caller_destination}\" inline=\"true\"/>'), update_date = now() \n";
+                        $sql .= "where dialplan_uuid in (select dialplan_uuid from v_dialplans where dialplan_name = 'domain-variables' or dialplan_name = 'variables'); \n";
+                        $database->execute($sql);
 		}
 		unset($sql, $num_rows);
 	}
