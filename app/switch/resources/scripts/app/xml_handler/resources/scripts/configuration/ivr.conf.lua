cc0cf96b762f3f1f8d0ad3b4e4abe2ca9884c39d
FusionPBX <markjcrane@gmail.com>
2023-10-23 14:25:04 -0600

    IVR Menu: allow sending the ${uuid} variable

--- app/switch/resources/scripts/app/xml_handler/resources/scripts/configuration/ivr.conf.lua
+++ app/switch/resources/scripts/app/xml_handler/resources/scripts/configuration/ivr.conf.lua
@@ -287,6 +287,7 @@
 						if (#ivr_menu_option_action > 0) then
 							ivr_menu_option_param = xml.sanitize(ivr_menu_option_param);
 							ivr_menu_option_param = string.gsub(ivr_menu_option_param, "{accountcode}", "${accountcode}");
+							ivr_menu_option_param = string.gsub(ivr_menu_option_param, "{uuid}", "${uuid}");
 
 							xml:append([[					<entry action="]] .. xml.sanitize(ivr_menu_option_action) .. [[" digits="]] .. ivr_menu_option_digits .. [[" param="]] .. ivr_menu_option_param .. [[" description="]] .. xml.sanitize(ivr_menu_option_description) .. [["/>]]);
 							if (tonumber(ivr_menu_option_digits) and #ivr_menu_option_digits >= tonumber(direct_dial_digits_min)) then
