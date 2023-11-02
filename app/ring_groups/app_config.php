f978515f6958b7c1483fc5bf3415320c4ece8295
K-Flagman <105959875+k-flagman@users.noreply.github.com>
2023-11-02 13:18:12 -0700

    Update app_config.php
    Enable or disable the feature of add range of extensions.

--- app/ring_groups/app_config.php
+++ app/ring_groups/app_config.php
@@ -214,6 +214,14 @@
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "0";
 		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "";
+		$y++;
+		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "88297698-339d-4971-8019-3f7095ec1f36";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "ring_groups_range_extensions_add";
+		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "enabled";
+		$apps[$x]['default_settings'][$y]['default_setting_name'] = "boolean";
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = "true";
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "false";
+		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Enable or disable the feature of add range of extensions.";
 
 	//schema details
 		$y=0;
