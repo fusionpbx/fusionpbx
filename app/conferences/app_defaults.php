0d124f47efaf8190411183bbd3cb86865f18b827
FusionPBX <markjcrane@gmail.com>
2023-11-29 13:48:47 -0700

    Add default value for conference_context

--- app/conferences/app_defaults.php
+++ app/conferences/app_defaults.php
@@ -2,9 +2,21 @@
 
 if ($domains_processed == 1) {
 
+	//replace - with an @ symbol
 	$sql = "update v_dialplan_details ";
 	$sql .= "set dialplan_detail_data = replace(dialplan_detail_data, '-','@') ";
 	$sql .= "where dialplan_detail_type = 'conference' and dialplan_detail_data like '%-%';";
+	$database = new database;
+	$database->execute($sql);
+	unset($sql);
+
+	//add the domain_name as the context
+	$sql = "UPDATE v_conferences as c ";
+	$sql .= "SET conference_context = ( ";
+	$sql .= "	SELECT domain_name FROM v_domains as d ";
+	$sql .= "	WHERE d.domain_uuid = c.domain_uuid ";
+	$sql .= ") ";
+	$sql .= "WHERE conference_context is null; ";
 	$database = new database;
 	$database->execute($sql);
 	unset($sql);
