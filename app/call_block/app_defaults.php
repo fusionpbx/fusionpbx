2ef5aa1c99eded1e14000efbe2fb993c0f66ab58
FusionPBX <markjcrane@gmail.com>
2023-12-13 16:43:53 -0700

    Adding time zone handling for call block

--- app/call_block/app_defaults.php
+++ app/call_block/app_defaults.php
@@ -31,7 +31,8 @@ if ($domains_processed == 1) {
 		$database->execute("DROP VIEW view_call_block;", null);
 		$sql = "CREATE VIEW view_call_block AS ( \n";
 		$sql .= "	select c.domain_uuid, call_block_uuid, c.call_block_direction, c.extension_uuid, c.call_block_name, c.call_block_country_code, \n";
-		$sql .= "	c.call_block_number, e.extension, e.number_alias, c.call_block_count, c.call_block_app, c.call_block_data, c.date_added, c.call_block_enabled, c.call_block_description \n";
+		$sql .= "	c.call_block_number, e.extension, e.number_alias, c.call_block_count, c.call_block_app, c.call_block_data, c.date_added, \n";
+		$sql .= "	c.call_block_enabled, c.call_block_description, c.insert_date, c.insert_user, c.update_date, c.update_user \n";
 		$sql .= "	from v_call_block as c \n";
 		$sql .= " left join v_extensions as e \n";
 		$sql .= "	on c.extension_uuid = e.extension_uuid \n";
@@ -49,3 +50,4 @@ if ($domains_processed == 1) {
 }
 
 ?>
+
