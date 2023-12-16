26d549334e477f3d1d6832a53c7791f9aba86ba9
K-Flagman <105959875+k-flagman@users.noreply.github.com>
2023-12-16 10:15:22 -0800

    Device address in extensions list (#6811)
    * Update app_config.php
    
    adding permissions for showing MAC address in the extension list
    
    * Update extensions.php
    
    Adding a new column with MAC address info if it exists for the extension.
    
    * Renamed the permission to access of extension address
    
    * Fixed fields where we used deprecated "device mac address"
    
    Also added support for "show all" button (load device list for current domain or get all devices)

--- app/extensions/app_config.php
+++ app/extensions/app_config.php
@@ -237,6 +237,11 @@
 		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
 		$apps[$x]['permissions'][$y]['groups'][] = "admin";
 		$apps[$x]['permissions'][$y]['groups'][] = "user";
+		$y++;
+		$apps[$x]['permissions'][$y]['name'] = "extension_address";
+		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
+		$apps[$x]['permissions'][$y]['groups'][] = "admin";
+		$apps[$x]['permissions'][$y]['groups'][] = "user"; 
 
 	//default settings
 		$y=0;
