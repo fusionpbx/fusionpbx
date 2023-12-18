28a2f0da5bbbd629645aecab7ac38fbb6f6c6ede
FusionPBX <markjcrane@gmail.com>
2023-12-18 10:33:47 -0700

    Add new setting call_center record_name

--- app/call_centers/app_config.php
+++ app/call_centers/app_config.php
@@ -210,6 +210,14 @@
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "hold_music";
 		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Pass through hold_music variable.";
+		$y++;
+		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "f7ecaf4b-8228-4f15-9df1-c694a8259aab";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "call_center";
+		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "record_name";
+		$apps[$x]['default_settings'][$y]['default_setting_name'] = "array";
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = "\${sip_from_user}-\${sip_to_user}-\${strftime(%Y)}\${strftime(%b)}\${strftime(%d)}.\${record_ext}";
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "false";
+		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Custom name for call recording. Options: \${record_ext}, \${sip_from_user}, \${sip_to_user}, \${caller_id_number}, \${uuid}";
 
 	//cache details
 		$apps[$x]['cache']['key'] = "dialplan.\${domain_name}";
