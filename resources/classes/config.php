00bc80d1df6892c880b373a21d8c7f2767a36369
Tim Fry <tim.fry@hotmail.com>
2024-02-27 19:58:53 -0400

    initial commit of config class

--- resources/classes/config.php
+++ resources/classes/config.php
@@ -1,155 +1,314 @@
 <?php
 
 /**
- * config
- *
- * @method get config.php
- * @method find find the path to the config.php file
- * @method exists determin if the the config.php file exists
+ * config class loads configuration from the filesystem
  */
 class config {
 
-	/**
-	 * database variables and config path
-	 */
-	public $db_type;
-	public $db_name;
-	public $db_username;
-	public $db_password;
-	public $db_sslmode;
-	public $db_host;
-	public $db_path;
-	public $db_port;
-	public $db_secure;
-	public $db_cert_authority;
-	public $config_path;
+	// Full path and filename of config.conf
+	private $file;
+
+	// The internal array that holds the configuration in the config.conf file
+	private $configuration;
+
+	// Reports if a config.php file is found then this will be set to true
+	private $deprecated;
+
+	// Reports if the configuration is found in the same directory as the framework then this is set to true
+	private $within_framework;
 
 	/**
-	 * Called when the object is created
+	 * Loads the framework configuration file
 	 */
 	public function __construct() {
-		//place holder
-	}
 
-	/**
-	 * Determine whether the config file exists
-	 * @var string $db_type - type of database
-	 * @var string $db_name - name of the database
-	 * @var string $db_username - username to access the database
-	 * @var string $db_password - password to access the database
-	 * @var string $db_host - hostname of the database server
-	 * @var string $db_path - path of the database file
-	 * @var string $db_port - network port to connect to the database
-	 * @var bool $db_secure - whether or not to connect with SSL
-	 * @var string $db_cert_authority - location of certificate authority
-	 */
-	public function get() {
-		//find the config_path
-		$config_path = $this->find();
+		//initialize object variables to empty values
+		$this->configuration = [];
+		$this->deprecated = false;
+		$this->file = '';
 
-		//add the document root to the include path
-		$conf = parse_ini_file($config_path);
-		set_include_path($conf['document.root']);
+		//locate the conf file
+		$this->find();
 
 		//check if the config file exists
-		$config_exists = file_exists($config_path) ? true : false;
-		
-		//set the server variables and define project path constant
-		$_SERVER["DOCUMENT_ROOT"] = $conf['document.root'];
-		$_SERVER["PROJECT_ROOT"] = $conf['document.root'];
-		$_SERVER["PROJECT_PATH"]  = $conf['project.path'];
-		if (isset($conf['project.path'])) {
-			$_SERVER["PROJECT_ROOT"] = $conf['document.root'].'/'.$conf['project.path'];
-			if (!defined('PROJECT_ROOT')) { define("PROJECT_ROOT", $conf['document.root'].'/'.$conf['project.path']); }
-			if (!defined('PROJECT_PATH')) { define("PROJECT_PATH", $conf['project.path']); }
-		}
-		else {
-			if (!defined('PROJECT_ROOT')) { define("PROJECT_ROOT", $conf['document.root']); }
-			if (!defined('PROJECT_PATH')) { define("PROJECT_PATH", ''); }
+		if (!$this->exists()) {
+			//unable to load config.conf so throw an exception
+			throw new Exception("Unable to find config path");
 		}
 
-		//add the database settings
-		$this->db_type = $conf['database.0.type'];
-		$this->db_name = $conf['database.0.name'];
-		$this->db_username = $conf['database.0.username'];
-		$this->db_password = $conf['database.0.password'];
-		$this->db_sslmode = $conf['database.0.sslmode'] ?? '';
-		$this->db_secure = $conf['database.0.secure'] ?? '';
-		$this->db_cert_authority = $conf['database.0.db_cert_authority'] ?? '';
-		$this->db_host = $conf['database.0.host'];
-		$this->db_path = $conf['database.0.path'] ?? '';
-		$this->db_port = $conf['database.0.port'];
+		//load the conf file
+		$this->load();
 
+		//set the server variables
+		$this->define_project_paths();
+
+		//check if the configuration is stored in the framework structure
+		$this->within_framework = strpos(dirname(__DIR__, 2), $this->file) !== false;
 	}
 
 	/**
-	 * Find the path to the config.php
+	 * Implement backwards compatibility for db_* configuration values
+	 * @param string $property
+	 * @return mixed
+	 */
+	public function __get(string $property) {
+		switch($property) {
+			case 'db_type':
+				return $this->get('database.0.type');
+			case 'db_host':
+				return $this->get('database.0.host');
+			case 'db_port':
+				return $this->get('database.0.port');
+			case 'db_name':
+				return $this->get('database.0.name');
+			case 'db_username':
+				return $this->get('database.0.username');
+			case 'db_password':
+				return $this->get('database.0.password');
+			case 'config_path':
+				return $this->path();
+		}
+	}
+
+	/**
+	 * Returns the string representation of the configuration file
+	 * @return string configuration
+	 */
+	public function __toString(): string {
+		$sb = "";
+		foreach ($this->configuration as $key => $value) {
+			$sb .= "$key = '$value'\n";
+		}
+		return $sb;
+	}
+
+	// loads the config.conf file or provides a backward compatibility layer if a config.php file was used
+	private function load() {
+
+		//check for deprecated version so we can use 'require' instead
+		if ($this->deprecated) {
+			//allow global variables to be set in the old config.php file
+			global $db_type, $db_host, $db_port, $db_name, $db_username, $db_password;
+
+			//load the config.php file
+			require_once $this->file;
+
+			//convert the old properties to the new standard
+//			$this->configuration['#database system settings'] = '';
+			$this->configuration['database.0.type'] = $db_type;
+			$this->configuration['database.0.host'] = $db_host;
+			$this->configuration['database.0.port'] = $db_port;
+			$this->configuration['database.0.name'] = $db_name;
+			$this->configuration['database.0.username'] = $db_username;
+			$this->configuration['database.0.password'] = $db_password;
+			$this->configuration['database.0.sslmode'] = 'prefer';
+
+			//remove from the global namespace
+			unset($db_type, $db_host, $db_port, $db_name, $db_username, $db_password);
+
+			//set defaults for the new config.conf file
+//			$this->configuration['#database switch settings'] = '';
+			$this->configuration['database.1.type'] = 'sqlite';
+			$this->configuration['database.1.path'] = '/var/lib/freeswitch/db';
+			$this->configuration['database.1.name'] = 'core.db';
+//			$this->configuration['#general settings'] = '';
+			$this->configuration['document.root'] = '/var/www/fusionpbx';
+			$this->configuration['project.path'] = '';
+			$this->configuration['temp.dir'] = '/tmp';
+			$this->configuration['php.dir'] = PHP_BINDIR;
+			$this->configuration['php.bin'] = basename(PHP_BINARY);
+//			$this->configuration['#cache settings'] = '';
+			$this->configuration['cache.method'] = 'file';
+			$this->configuration['cache.location'] = '/var/cache/fusionpbx';
+			$this->configuration['cache.settings'] = 'true';
+//			$this->configuration['#switch settings'] = '';
+			$this->configuration['switch.conf.dir'] = '/etc/freeswitch';
+			$this->configuration['switch.sounds.dir'] = '/usr/share/freeswitch/sounds';
+			$this->configuration['switch.database.dir'] = '/var/lib/freeswitch/db';
+			$this->configuration['switch.recordings.dir'] = '/var/lib/freeswitch/recordings';
+			$this->configuration['switch.storage.dir'] = '/var/lib/freeswitch/storage';
+			$this->configuration['switch.voicemail.dir'] = '/var/lib/freeswitch/storage/voicemail';
+			$this->configuration['switch.scripts.dir'] = '/usr/share/freeswitch/scripts';
+			$this->configuration['switch.event_socket.host'] = 'fs';
+			$this->configuration['switch.event_socket.port'] = '8021';
+			$this->configuration['switch.event_socket.password'] = 'ClueCon';
+//			$this->configuration['#switch xml'] = '';
+			$this->configuration['xml_handler.fs_path'] = 'false';
+			$this->configuration['xml_handler.reg_as_number_alias'] = 'false';
+			$this->configuration['xml_handler.number_as_presence_id']  =' true';
+//			$this->configuration['#error reporting hide show all errors except notices and warnings'] = '';
+			$this->configuration['error.reporting'] = 'none';
+		} else {
+			//use native php parsing function
+			$conf = parse_ini_file($this->file);
+			//save the loaded and parsed conf file to the object
+			$this->configuration = $conf;
+		}
+
+	}
+
+	private function define_project_paths() {
+		// Load the document root
+		$doc_root = $this->get('document.root', '/var/www/fusionpbx');
+		$doc_path = $this->get('document.path', '');
+		//set the server variables and define project path constant
+		if (!empty($doc_path)) {
+			if (!defined('PROJECT_PATH')) { define("PROJECT_PATH", $doc_path); }
+			if (!defined('PROJECT_ROOT')) { define("PROJECT_ROOT", $doc_root.'/'.$doc_path); }
+		}
+		else {
+			if (!defined('PROJECT_PATH')) { define("PROJECT_PATH", ''); }
+			if (!defined('PROJECT_ROOT')) { define("PROJECT_ROOT", $doc_root); }
+		}
+
+		// internal definitions to the framework
+		$_SERVER["PROJECT_PATH"] = PROJECT_PATH;
+		$_SERVER["PROJECT_ROOT"] = PROJECT_ROOT;
+
+		// tell php where the framework is
+		$_SERVER["DOCUMENT_ROOT"] = PROJECT_ROOT;
+
+		// have php search for any libraries in the now defined root
+		set_include_path(PROJECT_ROOT);
+	}
+
+	/**
+	 * Find the path to the config.conf file
 	 * @var string $config_path - full path to the config.php file
 	 */
-	public function find() {
-
+	private function find() {
 		//find the file
-			if (file_exists("/etc/fusionpbx/config.conf")) {
-				$this->config_path = "/etc/fusionpbx/config.conf";
-			}
-			elseif (file_exists("/usr/local/etc/fusionpbx/config.conf")) {
-				$this->config_path = "/usr/local/etc/fusionpbx/config.conf";
-			}
-			elseif (file_exists($_SERVER["PROJECT_ROOT"]."/resources/config.php")) {
-				$this->config_path = $_SERVER["PROJECT_ROOT"]."/resources/config.php";
-			}
-			elseif (file_exists("/etc/fusionpbx/config.php")) {
-				$this->config_path = "/etc/fusionpbx/config.php";
-			}
-			elseif (file_exists("/usr/local/etc/fusionpbx/config.php")) {
-				$this->config_path = "/usr/local/etc/fusionpbx/config.php";
-			}
-			else {
-				$this->config_path = '';
-			}
+		if (file_exists("/etc/fusionpbx/config.conf")) {
+			$this->file = "/etc/fusionpbx/config.conf";
+		}
+		elseif (file_exists("/usr/local/etc/fusionpbx/config.conf")) {
+			$this->file = "/usr/local/etc/fusionpbx/config.conf";
+		}
+		elseif (file_exists("/etc/fusionpbx/config.php")) {
+			$this->file = "/etc/fusionpbx/config.php";
+			$this->deprecated = true;
+		}
+		elseif (file_exists("/usr/local/etc/fusionpbx/config.php")) {
+			$this->file = "/usr/local/etc/fusionpbx/config.php";
+			$this->deprecated = true;
+		}
+		// use the current web directory to find it as a last resort
+		elseif (file_exists(dirname(__DIR__, 2) . "/resources/config.php")) {
+			$this->file = "/var/www/fusionpbx/resources/config.php";
+			$this->deprecated = true;
+		}
+	}
 
-		//return the path
-			return $this->config_path;
+	/**
+	 * Returns true if the configuration was found to be using a .php extension instead of the
+	 * config.conf configuration
+	 * @return bool
+	 */
+	public function compatibility_enabled(): bool {
+		return $this->deprecated;
 	}
 
 	/**
 	 * Determine whether the config file exists
 	 */
 	public function exists() {
-		$this->find();
-		if (!empty($this->config_path)) {
+		if (!empty($this->file)) {
 			return true;
 		}
 		else {
 			return false;
 		}
 	}
+
+	/**
+	 * Returns true if the configuration was found within the same directory structure as the framework or false
+	 * if the configuration file was found outside of the framework directory structure
+	 * @return bool true if the configuration was found in the framework false otherwise
+	 */
+	public function exists_in_framework(): bool {
+		return $this->within_framework;
+	}
+
+	/**
+	 * Get a configuration value using a key in the configuration file
+	 * @param string $key key to match
+	 * @param string $default_value if no matching key is found, then this value will be returned
+	 * @return string returns a value in the conf file or an empty string
+	 */
+	public function get(string $key, string $default_value = ''): string {
+		if (array_key_exists($key, $this->configuration)) {
+			return $this->configuration[$key];
+		}
+		return $default_value;
+	}
+
+	/**
+	 * Returns the config path or an empty string
+	 * @return string
+	 */
+	public function path(): string {
+		return dirname($this->file);
+	}
+
+	/**
+	 * Returns the file name only of the configuration file
+	 * @return string
+	 */
+	public function filename(): string {
+		return basename($this->file);
+	}
+
+	/**
+	 * Returns the path fully resolved of any symlinks and the file name
+	 * @return string
+	 */
+	public function path_and_filename(): string {
+		return realpath($this->path()) . '/' . $this->filename();
+	}
+
+	/**
+	 * Returns the array of configuration settings
+	 * @return array
+	 */
+	public function configuration(): array {
+		return $this->configuration;
+	}
+
 }
-/*
+
+
+//Examples:
+//~~~~~~~~
 $config = new config;
-$config_exists = $config->exists();
-$config_path = $config->find();
-$config->get();
-$db_type = $config->db_type;
-$db_name = $config->db_name;
-$db_username = $config->db_username;
-$db_password = $config->db_password;
-$db_host = $config->db_host;
-$db_path = $config->db_path;
-$db_port = $config->db_port;
-echo "config_path: ".$config_path."\n";
-if ($config_exists) {
-	echo "config_exists: true\n";
-} else {
-	echo "config_exists: false\n";
+echo "Config path: " . $config->path() . "\n";
+echo "Config file: " . $config->filename() . "\n";
+echo "Full path and filename: " . $config->path_and_filename() . "\n";
+echo "Compatibility Mode: " . $config->compatibility_enabled() ? 'true' : 'false';
+if ($config->compatibility_enabled()) {
+	//do something to create a new one or notify user
 }
-echo "db_type: ".$db_type."\n";
-echo "db_name: ".$db_name."\n";
-echo "db_username: ".$db_username."\n";
-echo "db_password: ".$db_password."\n";
-echo "db_host: ".$db_host."\n";
-echo "db_path: ".$db_path."\n";
-echo "db_port: ".$db_port."\n";
-*/
+
+// show old style configuration options
+echo "db_type: ".$config->db_type."\n";
+echo "db_name: ".$config->db_name."\n";
+echo "db_username: ".$config->db_username."\n";
+echo "db_password: ".$config->db_password."\n";
+echo "db_host: ".$config->db_host."\n";
+echo "db_path: ".$config->db_path."\n";
+echo "db_port: ".$config->db_port."\n";
+
+// use current style configuration options
+echo "database.0.type: " . $config->get('database.0.type') . "\n";
+
+// use a default value
+echo "admin.name: " . $config->get('admin.name', 'admin') . "\n";
+
+// get all configuration options by printing the object
+echo "config settings: " . $config . "\n";
+
+// get all configuration options as an array
+var_dump($config->configuration());
+
+//*/
 
 ?>
