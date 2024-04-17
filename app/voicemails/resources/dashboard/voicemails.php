a7948946f2a7d3aba56b08760a0ce900cbb95e81
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-04-17 12:03:08 -0700

    Update voicemails.php

--- app/voicemails/resources/dashboard/voicemails.php
+++ app/voicemails/resources/dashboard/voicemails.php
@@ -82,7 +82,7 @@
 						},
 						title: {
 							text: '<?php echo $text['label-new_messages']; ?>',
-							color: '<?php echo $row['dashboard_heading_text_color'] ?? $_SESSION['dashboard']['chart_heading_text_color']['text']; ?>'
+							color: '<?php echo $dashboard_heading_text_color; ?>'
 						}
 					}
 				},
@@ -93,7 +93,7 @@
 						ctx.font = chart_text_size + 'px ' + chart_text_font;
 						ctx.textBaseline = 'middle';
 						ctx.textAlign = 'center';
-						ctx.fillStyle = '<?php echo $row['dashboard_number_text_color'] ?? $_SESSION['dashboard']['number_text_color']['text']; ?>';
+						ctx.fillStyle = '<?php echo $dashboard_number_text_color; ?>';
 						ctx.fillText(options.text, width / 2, top + (height / 2));
 						ctx.save();
 					}
