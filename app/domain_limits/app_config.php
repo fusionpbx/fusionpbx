b9aad4c416d1f315498d1662c2c62e0f5b3a332b
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-05-08 10:37:18 -0700

    Update app_config.php

--- app/domain_limits/app_config.php
+++ app/domain_limits/app_config.php
@@ -7,7 +7,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "dashboard_domain_limits_chart_color_used";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "#03c04a";
-		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "false";
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "";
 		$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "a679e2fe-a865-498c-bdd1-d0fe3981ac56";
@@ -15,7 +15,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "dashboard_domain_limits_chart_color_remaining";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "#d4d4d4";
-		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "false";
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "";
 
 ?>
