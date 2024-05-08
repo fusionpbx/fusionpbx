a2d099c85acc5d897876989b87a214570a83cfc4
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-05-08 10:09:18 -0700

    Update app_config.php

--- app/switch/app_config.php
+++ app/switch/app_config.php
@@ -52,7 +52,7 @@
 		$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "15ab0540-c281-4793-baf9-3a2f6dc909c5";
 		$apps[$x]['default_settings'][$y]['default_setting_category'] = "theme";
-		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "switch_status_chart_main_color";
+		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "dashboard_switch_status_chart_main_color";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "#2a9df4";
 		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
@@ -60,7 +60,7 @@
 		$y++;
 		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "1e4db354-27d7-45e1-9513-ef5e23f66335";
 		$apps[$x]['default_settings'][$y]['default_setting_category'] = "theme";
-		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "switch_status_chart_sub_color";
+		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "dashboard_switch_status_chart_sub_color";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "#d4d4d4";
 		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
