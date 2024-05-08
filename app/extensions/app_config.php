826f260c549cd96ab26c60e50f0c629b78353567
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-05-08 10:39:17 -0700

    Update app_config.php

--- app/extensions/app_config.php
+++ app/extensions/app_config.php
@@ -355,7 +355,7 @@
 		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "call_timeout";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "numeric";
 		$apps[$x]['default_settings'][$y]['default_setting_value'] = "30";
-		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "false";
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
 		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Set the ring time (delay in seconds) before sending a call to voicemail";
 
 	//cache details
