41d04a010830300d8f3e408a1f93b4b8f783487b
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-05-10 08:14:06 -0700

    Update index.php

--- core/dashboard/index.php
+++ core/dashboard/index.php
@@ -357,7 +357,7 @@ function toggle_grid_row_end(dashboard_name) {
 		$dashboard_name = $row['dashboard_name'];
 		$dashboard_icon = $row['dashboard_icon'] ?? '';
 		$dashboard_url  = $row['dashboard_url'] ?? '';
-		$dashboard_chart_type = $row['dashboard_chart_type'] ?? ($dashboard_name == 'New Messages' || $dashboard_name == 'Missed Valls' || $dashboard_name == 'Recent Calls') ? 'none' : 'doughnut';
+		$dashboard_chart_type = $row['dashboard_chart_type'] ?? 'doughnut';
 		$dashboard_number_text_color = $row['dashboard_number_text_color'] ?? $settings->get('theme', 'dashboard_number_text_color');
 		$dashboard_details_state = $row['dashboard_details_state'];
 		$grid_row_end = ($dashboard_details_state == "expanded" || empty($dashboard_details_state)) ? "grid-row-end: span 5;" : "grid-row-end: span 2;";
