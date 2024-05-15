7c926a9ba6c45f24538f8ffb47a33b624cb6d4c1
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-05-15 11:19:57 -0700

    Update dashboard_edit.php

--- core/dashboard/dashboard_edit.php
+++ core/dashboard/dashboard_edit.php
@@ -638,7 +638,7 @@
 	}
 	if (empty($dashboard_detail_background_color) || count($dashboard_detail_background_color) < 2) {
 		echo "	<input type='text' class='formfld colorpicker' name='dashboard_detail_background_color[]' value='' onclick=\"document.getElementById('detail_second_input').style.display = 'block';\">\n";
-		if (empty($dashboard_background_color)) {
+		if (empty($dashboard_detail_background_color)) {
 			echo "	<input id='detail_second_input' style='display: none;' type='text' class='formfld colorpicker' name='dashboard_detail_background_color[]'>\n";
 		}
 	}
