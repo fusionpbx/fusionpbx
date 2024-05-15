04c23a111d2cf080eaecba3faa87b7f0f912b5bf
nemerald-voip <info@nemerald.com>
2024-05-15 00:22:23 -0700

    updated billing suspension script

--- app/switch/resources/scripts/billing_suspension.lua
+++ app/switch/resources/scripts/billing_suspension.lua
@@ -1,26 +1,32 @@
 -- Set the API endpoint URL and Bearer token from the argv values
+
 domain_uuid = argv[1]
 
-local Database = require "resources.functions.database";
+if not domain_uuid then
+    freeswitch.consoleLog("error", "[check-suspended] Missing domain_uuid argument")
+    return
+end
 
---check if the session is ready
-if not session:ready() then return end
+local Database = require "resources.functions.database"
 
-local dbh = Database.new('system');
+local dbh = Database.new('system')
 
-local sql = "select domain_setting_value ";
-sql = sql .. " from  v_domain_settings";
-sql = sql .. " where domain_setting_category = 'company' ";
-sql = sql .. " AND domain_uuid = :domain_uuid ";
-sql = sql .. " AND domain_setting_subcategory = 'billing_suspension' ";
+local sql = [[
+    select domain_setting_value from v_domain_settings where domain_setting_category = 'company' and domain_uuid = :domain_uuid and domain_setting_subcategory = 'billing_suspension'
+]]
 
-local params = {domain_uuid=domain_uuid};
-freeswitch.consoleLog("notice", "[BILLING SUSPENSION] sql: " .. sql);
-local response = dbh:first_row(sql, params);
+local params = {domain_uuid = domain_uuid}
+freeswitch.consoleLog("notice", "[check-suspended] sql: " .. sql)
+local response = dbh:first_row(sql, params)
+dbh:release()
 
-
-freeswitch.consoleLog("notice", "[BILLING SUSPENSION] value: " .. response.domain_setting_value);
-session:setVariable("billing_suspended", response.domain_setting_value);
+if response then
+    freeswitch.consoleLog("notice", "[check-suspended] value: " .. response.domain_setting_value)
+    -- session:setVariable("billing_suspended", response.domain_setting_value)
+    stream:write(response.domain_setting_value)
+else
+    freeswitch.consoleLog("notice", "[check-suspended] No result found for domain_uuid: " .. domain_uuid)
+end
 
 --session:execute("set", "billing_suspended=" .. response.domain_setting_value .. "");
 
