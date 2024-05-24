234ed304641e73e84373595eff482f2671b19dcf
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-05-24 12:00:37 -0700

    fixed php error

--- core/dashboard/index.php
+++ core/dashboard/index.php
@@ -196,7 +196,7 @@
 
 //display login message
 	if (if_group("superadmin") && isset($_SESSION['login']['message']['text']) && $_SESSION['login']['message']['text'] != '') {
-		echo "<div class='login_message' width='100%'><b>".$text['login-message_attention']."</b>&nbsp;&nbsp;".$_SESSION['login']['message']['text']."&nbsp;&nbsp;(<a href='?msg=dismiss'>".$text['login-message_dismiss']."</a>)</div>\n";
+		echo "<div class='login_message' width='100%'><b>".($text['login-message_attention'] ?? '')."</b>&nbsp;&nbsp;".$_SESSION['login']['message']['text']."&nbsp;&nbsp;(<a href='?msg=dismiss'>".$text['login-message_dismiss']."</a>)</div>\n";
 	}
 
 ?>
