8a0f6c07647accab2d3f4a30e9a62d2360002209
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-06-03 10:30:44 -0700

    Update domain_limits.php

--- app/domain_limits/resources/dashboard/domain_limits.php
+++ app/domain_limits/resources/dashboard/domain_limits.php
@@ -114,7 +114,7 @@
 			echo "		<span class='hud_title'>".$text['label-domain_limits']."</span>\n";
 
 		//doughnut chart
-			if ($dashboard_chart_type == "doughnut" || !isset($dashboard_chart_type)) {
+			if (!isset($dashboard_chart_type) || $dashboard_chart_type == "doughnut") {
 				echo "<div class='hud_chart' style='width: 275px;'><canvas id='domain_limits_chart'></canvas></div>\n";
 
 				echo "<script>\n";
