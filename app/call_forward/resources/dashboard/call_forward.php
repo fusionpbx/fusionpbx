c24b79bbba16b2140d0dadd756c456f4734cdf10
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-06-03 10:30:12 -0700

    Update call_forward.php

--- app/call_forward/resources/dashboard/call_forward.php
+++ app/call_forward/resources/dashboard/call_forward.php
@@ -106,7 +106,7 @@
 	echo "		<span class='hud_title'>".$text['header-call_forward']."</span>\n";
 
 //doughnut chart
-	if ($dashboard_chart_type == "doughnut" || !isset($dashboard_chart_type)) {
+	if (!isset($dashboard_chart_type) || $dashboard_chart_type == "doughnut") {
 		echo "<div class='hud_chart' style='width: 275px;'><canvas id='call_forward_chart'></canvas></div>\n";
 
 		echo "<script>\n";
