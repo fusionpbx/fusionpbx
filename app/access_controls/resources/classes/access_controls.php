0e7ce15bcaa81912304ec3df4ea86da49fe9f3d9
Tim Fry <tim@fusionpbx.com>
2024-06-24 20:16:22 -0300

    use class constants where appropriate

--- app/access_controls/resources/classes/access_controls.php
+++ app/access_controls/resources/classes/access_controls.php
@@ -9,25 +9,19 @@ if (!class_exists('access_controls')) {
 
 	class access_controls {
 
+		const APP_NAME = 'access_controls';
+		const APP_UUID = '1416a250-f6e1-4edc-91a6-5c9b883638fd';
+		const LIST_PAGE = 'access_controls.php';
+
 		/**
 		 * declare private variables
 		 */
-		private $app_name;
-		private $app_uuid;
-		private $permission_prefix;
-		private $list_page;
-		private $table;
-		private $uuid_prefix;
 
 		/**
 		 * called when the object is created
 		 */
 		public function __construct() {
 
-			//assign private variables
-			$this->app_name = 'access_controls';
-			$this->app_uuid = '1416a250-f6e1-4edc-91a6-5c9b883638fd';
-			$this->list_page = 'access_controls.php';
 		}
 
 		/**
@@ -35,12 +29,12 @@ if (!class_exists('access_controls')) {
 		 */
 		public function delete($records) {
 
-			//assign private variables
-			$this->permission_prefix = 'access_control_';
-			$this->table = 'access_controls';
-			$this->uuid_prefix = 'access_control_';
+			//assign local variables
+			$permission_prefix = 'access_control_';
+			$table = 'access_controls';
+			$uuid_prefix = 'access_control_';
 
-			if (permission_exists($this->permission_prefix . 'delete')) {
+			if (permission_exists($permission_prefix . 'delete')) {
 
 				//add multi-lingual support
 				$language = new text;
@@ -50,7 +44,7 @@ if (!class_exists('access_controls')) {
 				$token = new token;
 				if (!$token->validate($_SERVER['PHP_SELF'])) {
 					message::add($text['message-invalid_token'], 'negative');
-					header('Location: ' . $this->list_page);
+					header('Location: ' . self::LIST_PAGE);
 					exit;
 				}
 
@@ -60,8 +54,8 @@ if (!class_exists('access_controls')) {
 					//build the delete array
 					foreach ($records as $x => $record) {
 						if ($record['checked'] == 'true' && is_uuid($record['uuid'])) {
-							$array[$this->table][$x][$this->uuid_prefix . 'uuid'] = $record['uuid'];
-							$array['access_control_nodes'][$x][$this->uuid_prefix . 'uuid'] = $record['uuid'];
+							$array[$table][$x][$uuid_prefix . 'uuid'] = $record['uuid'];
+							$array['access_control_nodes'][$x][$uuid_prefix . 'uuid'] = $record['uuid'];
 						}
 					}
 
@@ -74,8 +68,8 @@ if (!class_exists('access_controls')) {
 
 						//execute delete
 						$database = new database;
-						$database->app_name = $this->app_name;
-						$database->app_uuid = $this->app_uuid;
+						$database->app_name = self::APP_NAME;
+						$database->app_uuid = self::APP_UUID;
 						$database->delete($array);
 						unset($array);
 
@@ -99,12 +93,12 @@ if (!class_exists('access_controls')) {
 
 		public function delete_nodes($records) {
 
-			//assign private variables
-			$this->permission_prefix = 'access_control_node_';
-			$this->table = 'access_control_nodes';
-			$this->uuid_prefix = 'access_control_node_';
+			//assign local variables
+			$permission_prefix = 'access_control_node_';
+			$table = 'access_control_nodes';
+			$uuid_prefix = 'access_control_node_';
 
-			if (permission_exists($this->permission_prefix . 'delete')) {
+			if (permission_exists($permission_prefix . 'delete')) {
 
 				//add multi-lingual support
 				$language = new text;
@@ -114,7 +108,7 @@ if (!class_exists('access_controls')) {
 				$token = new token;
 				if (!$token->validate('/app/access_controls/access_control_nodes.php')) {
 					message::add($text['message-invalid_token'], 'negative');
-					header('Location: ' . $this->list_page);
+					header('Location: ' . self::LIST_PAGE);
 					exit;
 				}
 
@@ -124,7 +118,7 @@ if (!class_exists('access_controls')) {
 					//build the delete array
 					foreach ($records as $x => $record) {
 						if ($record['checked'] == 'true' && is_uuid($record['uuid'])) {
-							$array[$this->table][$x][$this->uuid_prefix . 'uuid'] = $record['uuid'];
+							$array[$table][$x][$uuid_prefix . 'uuid'] = $record['uuid'];
 						}
 					}
 
@@ -133,8 +127,8 @@ if (!class_exists('access_controls')) {
 
 						//execute delete
 						$database = new database;
-						$database->app_name = $this->app_name;
-						$database->app_uuid = $this->app_uuid;
+						$database->app_name = self::APP_NAME;
+						$database->app_uuid = self::APP_UUID;
 						$database->delete($array);
 						unset($array);
 
@@ -159,11 +153,11 @@ if (!class_exists('access_controls')) {
 		public function copy($records) {
 
 			//assign private variables
-			$this->permission_prefix = 'access_control_';
-			$this->table = 'access_controls';
-			$this->uuid_prefix = 'access_control_';
+			$permission_prefix = 'access_control_';
+			$table = 'access_controls';
+			$uuid_prefix = 'access_control_';
 
-			if (permission_exists($this->permission_prefix . 'add')) {
+			if (permission_exists($permission_prefix . 'add')) {
 
 				//add multi-lingual support
 				$language = new text;
@@ -173,7 +167,7 @@ if (!class_exists('access_controls')) {
 				$token = new token;
 				if (!$token->validate($_SERVER['PHP_SELF'])) {
 					message::add($text['message-invalid_token'], 'negative');
-					header('Location: ' . $this->list_page);
+					header('Location: ' . self::LIST_PAGE);
 					exit;
 				}
 
@@ -191,8 +185,8 @@ if (!class_exists('access_controls')) {
 					if (is_array($uuids) && @sizeof($uuids) != 0) {
 
 						//primary table
-						$sql = "select * from v_" . $this->table . " ";
-						$sql .= "where " . $this->uuid_prefix . "uuid in (" . implode(', ', $uuids) . ") ";
+						$sql = "select * from v_" . $table . " ";
+						$sql .= "where " . $uuid_prefix . "uuid in (" . implode(', ', $uuids) . ") ";
 						$database = new database;
 						$rows = $database->select($sql, $parameters, 'all');
 						if (is_array($rows) && @sizeof($rows) != 0) {
@@ -201,11 +195,11 @@ if (!class_exists('access_controls')) {
 								$primary_uuid = uuid();
 
 								//copy data
-								$array[$this->table][$x] = $row;
+								$array[$table][$x] = $row;
 
 								//overwrite
-								$array[$this->table][$x][$this->uuid_prefix . 'uuid'] = $primary_uuid;
-								$array[$this->table][$x]['access_control_description'] = trim($row['access_control_description'] . ' (' . $text['label-copy'] . ')');
+								$array[$table][$x][$uuid_prefix . 'uuid'] = $primary_uuid;
+								$array[$table][$x]['access_control_description'] = trim($row['access_control_description'] . ' (' . $text['label-copy'] . ')');
 
 								//nodes sub table
 								$sql_2 = "select * from v_access_control_nodes where access_control_uuid = :access_control_uuid";
@@ -241,8 +235,8 @@ if (!class_exists('access_controls')) {
 
 						//save the array
 						$database = new database;
-						$database->app_name = $this->app_name;
-						$database->app_uuid = $this->app_uuid;
+						$database->app_name = self::APP_NAME;
+						$database->app_uuid = self::APP_UUID;
 						$database->save($array);
 						unset($array);
 
