26320b50cf4d30a310324d94ed5424524797601c
Antonio Fernandez <tfernandez.sc2@gmail.com>
2024-06-28 11:55:43 -0400

    bug fix in device_by.php (#7027)

--- app/provision/resources/functions/device_by.php
+++ app/provision/resources/functions/device_by.php
@@ -13,7 +13,7 @@ function device_by_mac($device_address) {
 function device_by_ext($ext, $domain) {
 	$sql = "select t1.* ";
 	$sql .= "from v_devices t1 ";
-	$sql .- "inner join v_device_lines t2 on t1.device_uuid = t2.device_uuid ";
+	$sql .= "inner join v_device_lines t2 on t1.device_uuid = t2.device_uuid ";
 	$sql .= "inner join v_domains t3 on t2.domain_uuid = t3.domain_uuid ";
 	$sql .= "where t2.user_id = :ext ";
 	$sql .= "and t3.domain_name = :domain ";
