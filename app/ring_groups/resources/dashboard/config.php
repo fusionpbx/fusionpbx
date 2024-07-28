3f4ca099ab5a83a0a665abf55c4c7278e53cf284
FusionPBX <mark@fusionpbx.com>
2024-07-27 19:19:45 -0600

    Add dashboard ring groups

--- app/ring_groups/resources/dashboard/config.php
+++ app/ring_groups/resources/dashboard/config.php
@@ -1,5 +1,6 @@
 <?php
 
+//ring group forward
 $array['dashboard'][$x]['dashboard_uuid'] = 'b5da1d72-2cc7-422e-b05c-f07659aed419';
 $array['dashboard'][$x]['dashboard_name'] = 'Ring Group Forward';
 $array['dashboard'][$x]['dashboard_path'] = 'app/ring_groups/resources/dashboard/ring_group_forward.php';
@@ -18,5 +19,45 @@ $y++;
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = 'f7ef37b1-8134-48d1-b25a-d1a50bf0e339';
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'b5da1d72-2cc7-422e-b05c-f07659aed419';
 $array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'user';
+$x++;
+
+//ring groups icon
+$array['dashboard'][$x]['dashboard_uuid'] = '97e69ceb-89f6-43a0-9a92-c89c5b02e6ee';
+$array['dashboard'][$x]['dashboard_name'] = 'Ring Groups';
+$array['dashboard'][$x]['dashboard_path'] = 'core/dashboard/resources/dashboard/icon.php';
+$array['dashboard'][$x]['dashboard_icon'] = 'fa-users';
+$array['dashboard'][$x]['dashboard_url'] = '/app/ring_groups/ring_groups.php';
+$array['dashboard'][$x]['dashboard_target'] = 'self';
+$array['dashboard'][$x]['dashboard_width'] = '';
+$array['dashboard'][$x]['dashboard_height'] = '';
+$array['dashboard'][$x]['dashboard_content'] = '';
+$array['dashboard'][$x]['dashboard_content_text_align'] = '';
+$array['dashboard'][$x]['dashboard_content_details'] = '';
+$array['dashboard'][$x]['dashboard_chart_type'] = '';
+$array['dashboard'][$x]['dashboard_heading_text_color'] = '';
+$array['dashboard'][$x]['dashboard_heading_text_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_heading_background_color'] = '';
+$array['dashboard'][$x]['dashboard_heading_background_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_number_background_color'] = '';
+$array['dashboard'][$x]['dashboard_number_text_color'] = '#1256c1';
+$array['dashboard'][$x]['dashboard_number_text_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_background_color'] ='["#ffffff"]';
+$array['dashboard'][$x]['dashboard_background_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_detail_background_color'] = '';
+$array['dashboard'][$x]['dashboard_column_span'] = '1';
+$array['dashboard'][$x]['dashboard_row_span'] = '1';
+$array['dashboard'][$x]['dashboard_details_state'] = 'disabled';
+$array['dashboard'][$x]['dashboard_order'] = '30';
+$array['dashboard'][$x]['dashboard_enabled'] = 'true';
+$array['dashboard'][$x]['dashboard_description'] = '';
+$y = 0;
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = 'ded58e5c-2f87-4f54-80c3-32bb411b034a';
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = '2f0ef341-724e-4578-84bb-803e0fc0b14c';
+$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'superadmin';
+$y++;
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = '42f0327f-c2a5-44bc-9c8b-82ca9c8c4e92';
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = '2f0ef341-724e-4578-84bb-803e0fc0b14c';
+$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'admin';
+$y++;
 
 ?>
