89ad2172f3f200f8fd4bf2db4fb9a3d785b5d7ac
FusionPBX <mark@fusionpbx.com>
2024-07-29 22:11:39 -0600

    Update config.php

--- app/call_forward/resources/dashboard/config.php
+++ app/call_forward/resources/dashboard/config.php
@@ -1,9 +1,31 @@
 <?php
 
-$x++;
+//call forward
 $array['dashboard'][$x]['dashboard_uuid'] = 'ba60799a-1c40-44a8-80ef-c2be4f4692fb';
 $array['dashboard'][$x]['dashboard_name'] = 'Call Forward';
-$array['dashboard'][$x]['dashboard_path'] = 'app/call_forward/resources/dashboard/call_forward.php';
+$array['dashboard'][$x]['dashboard_path'] = 'core/dashboard/resources/dashboard/icon.php';
+$array['dashboard'][$x]['dashboard_icon'] = 'fa-forward';
+$array['dashboard'][$x]['dashboard_url'] = '/app/call_forward/resources/dashboard/call_forward.php';
+$array['dashboard'][$x]['dashboard_target'] = 'self';
+$array['dashboard'][$x]['dashboard_width'] = '';
+$array['dashboard'][$x]['dashboard_height'] = '';
+$array['dashboard'][$x]['dashboard_content'] = '';
+$array['dashboard'][$x]['dashboard_content_text_align'] = '';
+$array['dashboard'][$x]['dashboard_content_details'] = '';
+$array['dashboard'][$x]['dashboard_chart_type'] = '';
+$array['dashboard'][$x]['dashboard_heading_text_color'] = '';
+$array['dashboard'][$x]['dashboard_heading_text_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_heading_background_color'] = '';
+$array['dashboard'][$x]['dashboard_heading_background_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_number_background_color'] = '';
+$array['dashboard'][$x]['dashboard_number_text_color'] = '#F9FBFE';
+$array['dashboard'][$x]['dashboard_number_text_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_background_color'] ='';
+$array['dashboard'][$x]['dashboard_background_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_detail_background_color'] = '';
+$array['dashboard'][$x]['dashboard_column_span'] = '1';
+$array['dashboard'][$x]['dashboard_row_span'] = '1';
+$array['dashboard'][$x]['dashboard_details_state'] = 'disabled';
 $array['dashboard'][$x]['dashboard_order'] = '130';
 $array['dashboard'][$x]['dashboard_enabled'] = 'true';
 $array['dashboard'][$x]['dashboard_description'] = 'Call routing for extension using Call forward, Follow Me and Do Not Disturb.';
@@ -21,9 +43,9 @@ $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'ba60799a-1c
 $array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'user';
 $x++;
 
-//call forward icon
+//follow me icon
 $array['dashboard'][$x]['dashboard_uuid'] = '714e513d-be78-4c63-8f8d-951d48a7fc0e';
-$array['dashboard'][$x]['dashboard_name'] = 'Call Forward';
+$array['dashboard'][$x]['dashboard_name'] = 'Follow Me';
 $array['dashboard'][$x]['dashboard_path'] = 'core/dashboard/resources/dashboard/icon.php';
 $array['dashboard'][$x]['dashboard_icon'] = 'fa-forward';
 $array['dashboard'][$x]['dashboard_url'] = '/app/call_forward/call_forward.php';
