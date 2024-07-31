19fe2770a2df9ea7ba514c066254f7585f03e0e0
FusionPBX <markjcrane@gmail.com>
2024-07-30 22:18:29 -0600

    Update dashboard permissions

--- app/fax/resources/dashboard/config.php
+++ app/fax/resources/dashboard/config.php
@@ -30,17 +30,9 @@ $array['dashboard'][$x]['dashboard_order'] = '50';
 $array['dashboard'][$x]['dashboard_enabled'] = 'true';
 $array['dashboard'][$x]['dashboard_description'] = '';
 $y = 0;
-$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = 'd400d674-6427-40aa-8ac0-d6c2042912cf';
-$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'ea3f5a8a-fc93-47d7-a632-a733522affc3';
-$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'superadmin';
-$y++;
-$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = 'e258a8b7-38e4-4faa-a788-db4b1340730b';
-$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'ea3f5a8a-fc93-47d7-a632-a733522affc3';
-$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'admin';
-$y++;
-$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = 'cb7bd2f0-6e3d-4624-b576-ef818a974250';
-$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'ea3f5a8a-fc93-47d7-a632-a733522affc3';
-$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'user';
-$y++;
+//$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = 'd400d674-6427-40aa-8ac0-d6c2042912cf';
+//$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'ea3f5a8a-fc93-47d7-a632-a733522affc3';
+//$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'superadmin';
+//$y++;
 
 ?>
