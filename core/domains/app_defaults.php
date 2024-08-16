83efb46e8ed8e5f4a7c9a075c978e89595d878e1
FusionPBX <mark@fusionpbx.com>
2024-08-16 14:58:12 -0600

    Update app_defaults.php

--- core/domains/app_defaults.php
+++ core/domains/app_defaults.php
@@ -27,11 +27,11 @@
 //process this only one time
 	if ($domains_processed == 1) {
 
-		//set domains with enabled status of empty or null to true
-			$sql = "update v_domains set domain_enabled = 'true' ";
-			$sql .= "where domain_enabled = '' or domain_enabled is null ";
-			$database->execute($sql, null);
-			unset($sql);
+		//set domain enable to true
+		$sql = "update v_domains set domain_enabled = 'true' ";
+		$sql .= "where domain_enabled is null ";
+		$database->execute($sql, null);
+		unset($sql);
 
 	}
  
