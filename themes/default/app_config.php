4fbf0902edaeeea7a01540e8ea043be4b3a3b381
norwayfun <temuri.doghonadze@gmail.com>
2024-08-26 09:42:24 +0200

    Translating theme to Georgian

--- themes/default/app_config.php
+++ themes/default/app_config.php
@@ -20,7 +20,7 @@
 		$apps[$x]['description']['fr-fr'] = "Thème par Défaut";
 		$apps[$x]['description']['he-il'] = "";
 		$apps[$x]['description']['it-it'] = "";
-		$apps[$x]['description']['ka-ge'] = "";
+		$apps[$x]['description']['ka-ge'] = "ნაგულისხმევი თემა";
 		$apps[$x]['description']['nl-nl'] = "";
 		$apps[$x]['description']['pl-pl'] = "";
 		$apps[$x]['description']['pt-br'] = "";
