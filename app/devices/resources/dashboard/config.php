006d967f71c231dbb77fa8991e5acdce09b30e68
FusionPBX <mark@fusionpbx.com>
2024-08-29 16:28:53 -0600

    Add dashboard device icon

--- app/devices/resources/dashboard/config.php
+++ app/devices/resources/dashboard/config.php
@@ -1,14 +1,80 @@
 <?php
 
+//device keys
 $array['dashboard'][$x]['dashboard_uuid'] = 'f1c722d5-b714-4fa2-9664-5f6d24d44661';
 $array['dashboard'][$x]['dashboard_name'] = 'Device Keys';
 $array['dashboard'][$x]['dashboard_path'] = 'devices/device_keys';
+$array['dashboard'][$x]['dashboard_icon'] = '';
+$array['dashboard'][$x]['dashboard_url'] = '';
+$array['dashboard'][$x]['dashboard_target'] = 'self';
+$array['dashboard'][$x]['dashboard_width'] = '';
+$array['dashboard'][$x]['dashboard_height'] = '';
+$array['dashboard'][$x]['dashboard_content'] = '';
+$array['dashboard'][$x]['dashboard_content_text_align'] = '';
+$array['dashboard'][$x]['dashboard_content_details'] = '';
+$array['dashboard'][$x]['dashboard_chart_type'] = '';
+$array['dashboard'][$x]['dashboard_label_enabled'] = 'true';
+$array['dashboard'][$x]['dashboard_label_text_color'] = '';
+$array['dashboard'][$x]['dashboard_label_text_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_label_background_color'] = '';
+$array['dashboard'][$x]['dashboard_label_background_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_number_background_color'] = '';
+$array['dashboard'][$x]['dashboard_number_text_color'] = '';
+$array['dashboard'][$x]['dashboard_number_text_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_background_color'] ='';
+$array['dashboard'][$x]['dashboard_background_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_detail_background_color'] = '';
+$array['dashboard'][$x]['dashboard_column_span'] = '1';
+$array['dashboard'][$x]['dashboard_row_span'] = '2';
+$array['dashboard'][$x]['dashboard_details_state'] = 'expanded';
 $array['dashboard'][$x]['dashboard_order'] = '150';
 $array['dashboard'][$x]['dashboard_enabled'] = 'false';
-$array['dashboard'][$x]['dashboard_description'] = '';
+$array['dashboard'][$x]['dashboard_description'] = 'Used to manage phone provisioned device keys.';
 $y = 0;
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = '0d04d50e-aa49-40d2-9e98-ed21f9070f6f';
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'f1c722d5-b714-4fa2-9664-5f6d24d44661';
 $array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'user';
+$x++;
+
+//devices icon
+$array['dashboard'][$x]['dashboard_uuid'] = 'd93648da-af16-4018-9182-76e8820ffe56';
+$array['dashboard'][$x]['dashboard_name'] = 'Devices';
+$array['dashboard'][$x]['dashboard_path'] = 'dashboard/icon';
+$array['dashboard'][$x]['dashboard_icon'] = 'fa-mobile-retro';
+$array['dashboard'][$x]['dashboard_url'] = '/app/devices/devices.php';
+$array['dashboard'][$x]['dashboard_target'] = 'self';
+$array['dashboard'][$x]['dashboard_width'] = '';
+$array['dashboard'][$x]['dashboard_height'] = '';
+$array['dashboard'][$x]['dashboard_content'] = '';
+$array['dashboard'][$x]['dashboard_content_text_align'] = '';
+$array['dashboard'][$x]['dashboard_content_details'] = '';
+$array['dashboard'][$x]['dashboard_chart_type'] = '';
+$array['dashboard'][$x]['dashboard_label_enabled'] = 'true';
+$array['dashboard'][$x]['dashboard_label_text_color'] = '';
+$array['dashboard'][$x]['dashboard_label_text_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_label_background_color'] = '';
+$array['dashboard'][$x]['dashboard_label_background_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_number_background_color'] = '';
+$array['dashboard'][$x]['dashboard_number_text_color'] = '#0292ff';
+$array['dashboard'][$x]['dashboard_number_text_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_background_color'] ='';
+$array['dashboard'][$x]['dashboard_background_color_hover'] = '';
+$array['dashboard'][$x]['dashboard_detail_background_color'] = '';
+$array['dashboard'][$x]['dashboard_column_span'] = '1';
+$array['dashboard'][$x]['dashboard_row_span'] = '1';
+$array['dashboard'][$x]['dashboard_details_state'] = 'disabled';
+$array['dashboard'][$x]['dashboard_order'] = '50';
+$array['dashboard'][$x]['dashboard_enabled'] = 'true';
+$array['dashboard'][$x]['dashboard_description'] = '';
+$y = 0;
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = '84452fd4-2409-427b-8382-96bfb38fdf49';
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'd93648da-af16-4018-9182-76e8820ffe56';
+$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'superadmin';
+$y++;
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = '5d026f9d-3fbb-4d4a-b654-bef32ea8bdef';
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'd93648da-af16-4018-9182-76e8820ffe56';
+$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'admin';
+$y++;
 
 ?>
+
