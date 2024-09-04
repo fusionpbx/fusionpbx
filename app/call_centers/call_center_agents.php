6d5466b72d0530b6ff7664e89ac0476c1112bbd9
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-04 15:41:01 -0700

    Update call_center_agents.php

--- app/call_centers/call_center_agents.php
+++ app/call_centers/call_center_agents.php
@@ -184,6 +184,7 @@
 	echo "<input type='hidden' id='action' name='action' value=''>\n";
 	echo "<input type='hidden' name='search' value=\"".escape($search)."\">\n";
 
+	echo "<div class='card'>\n";
 	echo "<table class='list'>\n";
 	echo "<tr class='list-header'>\n";
 	if (permission_exists('call_center_agent_delete')) {
@@ -277,6 +278,7 @@
 	}
 
 	echo "</table>\n";
+	echo "</div>\n";
 	echo "<br />\n";
 	echo "<div align='center'>".$paging_controls."</div>\n";
 
