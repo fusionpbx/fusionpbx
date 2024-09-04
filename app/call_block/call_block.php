3576a29a9831f8d9770d763857666c1fcc724757
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-04 15:39:34 -0700

    Update call_block.php

--- app/call_block/call_block.php
+++ app/call_block/call_block.php
@@ -277,6 +277,7 @@
 	echo "<input type='hidden' id='action' name='action' value=''>\n";
 	echo "<input type='hidden' name='search' value=\"".escape($search)."\">\n";
 
+	echo "<div class='card'>\n";
 	echo "<table class='list'>\n";
 	echo "<tr class='list-header'>\n";
 	if (permission_exists('call_block_add') || permission_exists('call_block_edit') || permission_exists('call_block_delete')) {
@@ -392,6 +393,7 @@
 	}
 
 	echo "</table>\n";
+	echo "</div>\n";
 	echo "<br />\n";
 	echo "<div align='center'>".$paging_controls."</div>\n";
 
