7e6eac4d0055944f56bbf418c31ee566affb1614
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-05 09:16:47 -0700

    Update xml_cdr_search.php

--- app/xml_cdr/xml_cdr_search.php
+++ app/xml_cdr/xml_cdr_search.php
@@ -118,7 +118,8 @@
 	echo "	</div>\n";
 	echo "	<div style='clear: both;'></div>\n";
 	echo "</div>\n";
-	
+
+	echo "<div class='card'>\n";
 	echo "<table cellpadding='0' cellspacing='0' border='0' width='100%'>\n";
 	echo "	<tr>\n";
 	echo "		<td width='50%' style='vertical-align: top;'>\n";
@@ -341,6 +342,7 @@
 	echo "		</td>";
 	echo "	</tr>";
 	echo "</table>";
+	echo "</div>\n";
 	echo "<br><br>";
 	
 	echo "</form>";
