36e04b0700f398429a64e1720b80cbd570532f47
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-05 10:29:03 -0700

    Update contact_url_edit.php

--- app/contacts/contact_url_edit.php
+++ app/contacts/contact_url_edit.php
@@ -242,6 +242,7 @@
 	}
 	echo "<br /><br />\n";
 
+	echo "<div class='card'>\n";
 	echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
 
 	echo "<tr>\n";
@@ -314,6 +315,7 @@
 	echo "</tr>\n";
 
 	echo "</table>";
+	echo "</div>\n";
 	echo "<br><br>";
 
 	echo "<input type='hidden' name='contact_uuid' value='".escape($contact_uuid ?? '')."'>\n";
