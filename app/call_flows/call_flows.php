f4691e93bf8b73b7438a709c72ad531b69db52cb
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-05 09:19:54 -0700

    Update call_flows.php

--- app/call_flows/call_flows.php
+++ app/call_flows/call_flows.php
@@ -215,6 +215,7 @@
 	echo "<input type='hidden' id='toggle_field' name='toggle_field' value=''>\n";
 	echo "<input type='hidden' name='search' value=\"".escape($search)."\">\n";
 
+	echo "<div class='card'>\n";
 	echo "<table class='list'>\n";
 	echo "<tr class='list-header'>\n";
 	if (permission_exists('call_flow_add') || permission_exists('call_flow_edit') || permission_exists('call_flow_delete')) {
@@ -298,6 +299,7 @@
 	}
 
 	echo "</table>\n";
+	echo "</div>\n";
 	echo "<br />\n";
 	echo "<div align='center'>".$paging_controls."</div>\n";
 
