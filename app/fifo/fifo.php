e2d0c8d4d4bb0025052a028c5dd957e2dcc1b59f
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-05 14:09:52 -0700

    Update fifo.php

--- app/fifo/fifo.php
+++ app/fifo/fifo.php
@@ -236,6 +236,7 @@
 	echo "<input type='hidden' id='action' name='action' value=''>\n";
 	echo "<input type='hidden' name='search' value=\"".escape($search ?? '')."\">\n";
 
+	echo "<div class='card'>\n";
 	echo "<table class='list'>\n";
 	echo "<tr class='list-header'>\n";
 	if (permission_exists('fifo_add') || permission_exists('fifo_edit') || permission_exists('fifo_delete')) {
@@ -309,6 +310,7 @@
 	}
 
 	echo "</table>\n";
+	echo "</div>\n";
 	echo "<br />\n";
 	echo "<div align='center'>".$paging_controls."</div>\n";
 	echo "<input type='hidden' name='".$token['name']."' value='".$token['hash']."'>\n";
