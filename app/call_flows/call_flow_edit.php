425486d074a2157a53794cd7af398727c2f010fa
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-05 09:21:34 -0700

    Update call_flow_edit.php

--- app/call_flows/call_flow_edit.php
+++ app/call_flows/call_flow_edit.php
@@ -458,6 +458,7 @@
 	echo "	<div style='clear: both;'></div>\n";
 	echo "</div>\n";
 
+	echo "<div class='card'>\n";
 	echo "<table width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
 
 	echo "<tr>\n";
@@ -793,6 +794,7 @@
 	echo "</tr>\n";
 
 	echo "</table>";
+	echo "</div>\n";
 	echo "<br /><br />";
 
 	if ($action == "update") {
