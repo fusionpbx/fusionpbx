6e5a8779f0dfc94fb5a2883116376e52a7229ce2
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-05 09:59:09 -0700

    Update conference_profiles.php

--- app/conference_profiles/conference_profiles.php
+++ app/conference_profiles/conference_profiles.php
@@ -164,6 +164,7 @@
 	echo "<input type='hidden' id='action' name='action' value=''>\n";
 	echo "<input type='hidden' name='search' value=\"".escape($search)."\">\n";
 
+	echo "<div class='card'>\n";
 	echo "<table class='list'>\n";
 	echo "<tr class='list-header'>\n";
 	if (permission_exists('conference_profile_add') || permission_exists('conference_profile_edit') || permission_exists('conference_profile_delete')) {
@@ -222,6 +223,7 @@
 	}
 
 	echo "</table>\n";
+	echo "</div>\n";
 	echo "<br />\n";
 	echo "<div align='center'>".!empty($paging_controls)."</div>\n";
 	echo "<input type='hidden' name='".$token['name']."' value='".$token['hash']."'>\n";
