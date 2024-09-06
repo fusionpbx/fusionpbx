8ae00a0856c878ae26a57a273fdc9bbca3ee0697
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-06 15:14:28 -0700

    Update voicemails.php

--- app/voicemails/voicemails.php
+++ app/voicemails/voicemails.php
@@ -204,7 +204,7 @@
 
 //show the content
 	echo "<div class='action_bar' id='action_bar'>\n";
-	echo "	<div class='heading'><b>".$text['title-voicemails']." (".$num_rows.")</b></div>\n";
+	echo "	<div class='heading'><b>".$text['title-voicemails']."</b><div class='count'>".number_format($num_rows)."</div></div>\n";
 	echo "	<div class='actions'>\n";
 	if (permission_exists('voicemail_import')) {
 		echo button::create(['type'=>'button','label'=>$text['button-import'],'icon'=>$_SESSION['theme']['button_icon_import'],'style'=>'','link'=>'voicemail_imports.php']);
