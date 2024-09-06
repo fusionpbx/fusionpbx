e90112206f9a93a2ad59b54e34afa12ca7a2a52b
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-06 10:08:15 -0700

    Update device_vendors.php

--- app/devices/device_vendors.php
+++ app/devices/device_vendors.php
@@ -159,7 +159,7 @@
 	echo "<input type='hidden' id='action' name='action' value=''>\n";
 	echo "<input type='hidden' name='search' value=\"".escape($search)."\">\n";
 
-
+	echo "<div class='card'>\n";
 	echo "<table class='list'>\n";
 	echo "<tr class='list-header'>\n";
 	if (permission_exists('device_vendor_edit') || permission_exists('device_vendor_delete')) {
@@ -210,6 +210,7 @@
 	unset($result);
 
 	echo "</table>\n";
+	echo "</div>\n";
 	echo "<br />\n";
 	echo "<div align='center'>".$paging_controls."</div>\n";
 
