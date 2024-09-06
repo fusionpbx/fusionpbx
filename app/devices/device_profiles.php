fb81733cec7693255a46548226f9fc5b8979ccf9
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-06 15:00:38 -0700

    Update device_profiles.php

--- app/devices/device_profiles.php
+++ app/devices/device_profiles.php
@@ -154,7 +154,7 @@
 
 //show the content
 	echo "<div class='action_bar' id='action_bar'>\n";
-	echo "	<div class='heading'><b>".$text['title-device_profiles']." (".$num_rows.")</b></div>\n";
+	echo "	<div class='heading'><b>".$text['title-device_profiles']."</b><div class='count'>".number_format($num_rows)."</div></div>\n";
 	echo "	<div class='actions'>\n";
 	echo button::create(['type'=>'button','label'=>$text['button-back'],'icon'=>$_SESSION['theme']['button_icon_back'],'id'=>'btn_back','style'=>'margin-right: 15px;','link'=>'devices.php']);
 	if (permission_exists('device_profile_add')) {
