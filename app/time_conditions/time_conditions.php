b7ae4aa152054dbdf9bc8d090be08ded11d34b23
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-06 15:13:32 -0700

    Update time_conditions.php

--- app/time_conditions/time_conditions.php
+++ app/time_conditions/time_conditions.php
@@ -140,7 +140,7 @@
 
 //show the content
 	echo "<div class='action_bar' id='action_bar'>\n";
-	echo "	<div class='heading'><b>".$text['header-time_conditions']." (".$num_rows.")</b></div>\n";
+	echo "	<div class='heading'><b>".$text['header-time_conditions']."</b><div class='count'>".number_format($num_rows)."</div></div>\n";
 	echo "	<div class='actions'>\n";
 	if (permission_exists('time_condition_add')) {
 		echo button::create(['type'=>'button','label'=>$text['button-add'],'icon'=>$_SESSION['theme']['button_icon_add'],'id'=>'btn_add','link'=>'time_condition_edit.php']);
