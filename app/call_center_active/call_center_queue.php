4ba5f3a0e07d6cfc3e6f04197daa87c15524ccb1
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-09 14:25:24 -0700

    Update streams.php (#7127)
    * Update streams.php
    
    * Update call_center_queue.php
    
    * Update menu.php
    
    * Update vars.php
    
    * Update dashboard.php
    
    * Update dashboard_edit.php

--- app/call_center_active/call_center_queue.php
+++ app/call_center_active/call_center_queue.php
@@ -87,7 +87,7 @@
 
 //show the content
 	echo "<div class='action_bar' id='action_bar'>\n";
-	echo "	<div class='heading'><b>".$text['header-active_call_center']." (".$num_rows.")</b></div>\n";
+	echo "	<div class='heading'><b>".$text['header-active_call_center']."</b><div class='count'>".number_format($num_rows)."</div></div>\n";
 	echo "	<div class='actions'>\n";
 	echo 		"<form id='form_search' class='inline' method='get'>\n";
 	echo 		"<input type='text' class='txt list-search' name='search' id='search' value=\"".escape($search)."\" placeholder=\"".$text['label-search']."\" onkeydown='list_search_reset();'>";
