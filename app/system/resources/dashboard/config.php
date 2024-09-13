69c0827669fd31fd299e331de0d7ee65f073f9fb
FusionPBX <mark@fusionpbx.com>
2024-09-13 15:14:05 -0600

    Add system status
    Disable system cpu and disk usage dashboard widgets

--- app/system/resources/dashboard/config.php
+++ app/system/resources/dashboard/config.php
@@ -1,12 +1,24 @@
 <?php
 
 $x++;
-$array['dashboard'][$x]['dashboard_uuid'] = 'c9b1f67a-d78a-4299-a16e-594a4b7cd246';
+$array['dashboard'][$x]['dashboard_uuid'] = 'ab48bcca-5cb1-40f0-b36f-ba1ad510ba5a';
 $array['dashboard'][$x]['dashboard_name'] = 'System Status';
 $array['dashboard'][$x]['dashboard_path'] = 'system/system_status';
 $array['dashboard'][$x]['dashboard_order'] = '90';
 $array['dashboard'][$x]['dashboard_enabled'] = 'true';
 $array['dashboard'][$x]['dashboard_description'] = 'System information like Disk usage.';
+$y = 0;
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = '5e1998c1-d3be-42f0-80ac-659e2e7934cb';
+$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'c388de65-85ad-4662-bbc3-a65fe9ddc319';
+$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'superadmin';
+$x++;
+
+$array['dashboard'][$x]['dashboard_uuid'] = 'beade936-846b-4f02-986c-a2de6fa762c2';
+$array['dashboard'][$x]['dashboard_name'] = 'System Disk Usage';
+$array['dashboard'][$x]['dashboard_path'] = 'system/system_disk_usage';
+$array['dashboard'][$x]['dashboard_order'] = '100';
+$array['dashboard'][$x]['dashboard_enabled'] = 'false';
+$array['dashboard'][$x]['dashboard_description'] = 'System Disk Usage.';
 $y = 0;
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = '625971b9-4892-4948-97e6-8bb8d409f4e1';
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'c9b1f67a-d78a-4299-a16e-594a4b7cd246';
@@ -17,7 +29,7 @@ $array['dashboard'][$x]['dashboard_uuid'] = 'beade936-846b-4f02-986c-a2de6fa762c
 $array['dashboard'][$x]['dashboard_name'] = 'System CPU Status';
 $array['dashboard'][$x]['dashboard_path'] = 'system/system_cpu_status';
 $array['dashboard'][$x]['dashboard_order'] = '100';
-$array['dashboard'][$x]['dashboard_enabled'] = 'true';
+$array['dashboard'][$x]['dashboard_enabled'] = 'false';
 $array['dashboard'][$x]['dashboard_description'] = 'System CPU information.';
 $y = 0;
 $array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = '41befc82-c65a-499e-9e29-513a3dd7974a';
