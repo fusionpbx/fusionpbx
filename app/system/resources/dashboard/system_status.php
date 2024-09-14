7edc65fce4f9db5f2305d44d8c5165a4426dbc20
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-14 09:56:18 -0700

    Fixed alignment on dashboard system status progress bars (#7132)

--- app/system/resources/dashboard/system_status.php
+++ app/system/resources/dashboard/system_status.php
@@ -178,25 +178,25 @@
 	if ($dashboard_chart_type == "progress_bar") {
 		//cpu usage
 		if ($dashboard_row_span > 1) {
-			echo "	<span class='hud_title cpu_usage' style='text-align: left; font-size: 11px; line-height: 1.8; font-weight: unset; padding-left: 9%;'>".$text['label-processor_usage']."</span>\n";
+			echo "	<span class='hud_title cpu_usage' style='text-align: left; font-size: 11px; line-height: 1.8; font-weight: unset; padding-left: 10%;'>".$text['label-processor_usage']."</span>\n";
 			echo "	<div class='progress_container' style='width: 80%; height: 15px; border-radius: 4px; background: ".($settings->get('theme', 'dashboard_cpu_usage_chart_sub_color') ?? '#d4d4d4').";'>\n";
 			echo "		<div class='progress_bar' style='width: ".$percent_cpu."%; height: 15px; border-radius: 4px; font-size: x-small; color: ".$row['dashboard_number_text_color']."; background: ".($settings->get('theme', 'dashboard_cpu_usage_chart_main_color') ?? '#03c04a').";'>".round($percent_cpu)."%</div>\n";
 			echo "	</div>\n";
-			echo "	<div style='height: 20px'>&nbsp;</div>\n";
+			echo "	<div style='width: 100%; height: 15px'>&nbsp;</div>\n";
 		}
 
 		//disk usage
 		if ($dashboard_row_span >= 1) {
-			echo "	<span class='hud_title' style='text-align: left; font-size: 11px; line-height: 1.8; font-weight: unset; padding-left: 9%;'>".$text['label-disk_usage']."</span>\n";
+			echo "	<span class='hud_title' style='text-align: left; font-size: 11px; line-height: 1.8; font-weight: unset; padding-left: 10%;'>".$text['label-disk_usage']."</span>\n";
 			echo "	<div class='progress_container' style='width: 80%; height: 15px; border-radius: 4px; background: ".($settings->get('theme', 'dashboard_disk_usage_chart_sub_color') ?? '#d4d4d4').";'>\n";
 			echo "		<div class='progress_bar' style='width: ".$percent_disk_usage."%; height: 15px; border-radius: 4px; font-size: x-small; color: ".$row['dashboard_number_text_color']."; background: ".($settings->get('theme', 'dashboard_disk_usage_chart_main_color') ?? '#03c04a').";'>".round($percent_disk_usage)."%</div>\n";
 			echo "	</div>\n";
-			echo "	<div style='height: 20px'>&nbsp;</div>\n";
+			echo "	<div style='width: 100%; height: 15px'>&nbsp;</div>\n";
 		}
 
 		//percent memory
 		if ($dashboard_row_span > 1) {
-			echo "	<span class='hud_title' style='text-align: left; font-size: 11px; line-height: 1.8; font-weight: unset; padding-left: 9%;'>".$text['label-memory_usage']."</span>\n";
+			echo "	<span class='hud_title' style='text-align: left; font-size: 11px; line-height: 1.8; font-weight: unset; padding-left: 10%;'>".$text['label-memory_usage']."</span>\n";
 			echo "	<div class='progress_container' style='width: 80%; height: 15px; border-radius: 4px; background: ".($settings->get('theme', 'dashboard_disk_usage_chart_sub_color') ?? '#d4d4d4').";'>\n";
 			echo "		<div class='progress_bar' style='width: ".round($memory_details['memory_percent'])."%; height: 15px; border-radius: 4px; font-size: x-small; color: ".$row['dashboard_number_text_color']."; background: ".($settings->get('theme', 'dashboard_disk_usage_chart_main_color') ?? '#03c04a').";'>".round($memory_details['memory_percent'])."%</div>\n";
 			echo "	</div>\n";
