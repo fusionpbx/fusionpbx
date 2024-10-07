ef3ab0b13e7208ccf86690e8d9573e989dd339b7
FusionPBX <mark@fusionpbx.com>
2024-10-07 11:39:28 -0600

    Add agent state to indicate wrap up time

--- app/call_center_active/call_center_active_inc.php
+++ app/call_center_active/call_center_active_inc.php
@@ -221,9 +221,18 @@
 									$talk_time = $agent_row['talk_time'];
 									$ready_time = $agent_row['ready_time'];
 
+									//calcudate the length since status change and bridge end			
+									$last_status_change_length = time() - $last_status_change;
+									$last_bridge_end_length = time() - $last_bridge_end;
+
+									//set the state to wrap up time
+									if ($last_bridge_end_length < $wrap_up_time) {
+										$state = 'Wrap Up Time';
+									}
+
 									//format the seconds to hh:mm:ss
-									$last_status_change_length = format_seconds(time() - $last_status_change);
-									$last_bridge_end_length = format_seconds(time() - $last_bridge_end);
+									$last_status_change_length_formatted = format_seconds($last_status_change_length);
+									$last_bridge_end_length_formatted = format_seconds($last_bridge_end_length);
 
 									if (permission_exists('call_center_agent_edit')) {
 										$list_row_url = "../call_centers/call_center_agent_edit.php?id=".$agent_uuid;
@@ -241,8 +250,8 @@
 									echo "<td>".escape($agent_extension)."</td>\n";
 									echo "<td>".escape($status)."</td>\n";
 									echo "<td>".escape($state)."</td>\n";
-									echo "<td>".escape($last_status_change_length)."</td>\n";
-									echo "<td>".escape($last_bridge_end_length)."</td>\n";
+									echo "<td>".escape($last_status_change_length_formatted)."</td>\n";
+									echo "<td>".escape($last_bridge_end_length_formatted)."</td>\n";
 									echo "<td class='center'>".escape($no_answer_count)."</td>\n";
 									echo "<td class='center'>".escape($calls_answered)."</td>\n";
 									echo "<td>".escape($tier_state)."</td>\n";
