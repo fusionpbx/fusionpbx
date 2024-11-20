57914dd1cf8eb0200bbae23b269a32dc7cb8d8cc
nemerald-voip <info@nemerald.com>
2024-11-19 21:08:09 -0800

    added whitelist scripts

--- app/switch/resources/scripts/is_whitelisted.lua
+++ app/switch/resources/scripts/is_whitelisted.lua
@@ -1,13 +1,61 @@
+local number = argv[1]  -- Number to be dialed
+
 --get the variables
 if (session:ready()) then
-    --session:setAutoHangup(false);
     domain_uuid = session:getVariable("domain_uuid");
-    call_direction = session:getVariable("call_direction");
-    caller_id_name = session:getVariable("caller_id_name");
-    caller_id_number = session:getVariable("caller_id_number");
-    destination_number = session:getVariable("destination_number");
-    context = session:getVariable("context");
-    call_block = session:getVariable("call_block");
-    extension_uuid = session:getVariable("extension_uuid");
-    hold_music = session:getVariable("hold_music");
+    -- extension_uuid = session:getVariable("extension_uuid");
+end
+
+
+--connect to the database
+local Database = require "resources.functions.database";
+dbh = Database.new('system');
+
+-- Function to check if a number is whitelisted
+function is_whitelisted(number)
+    local is_allowed = false
+    -- local sql = string.format("SELECT COUNT(*) AS count FROM whitelisted_numbers WHERE number = '%s' AND domain_uuid = '%s'", number, domain_uuid)
+
+     -- SQL query to handle both full-length numbers and extensions
+     local sql = string.format([[
+        SELECT COUNT(*) AS count 
+        FROM whitelisted_numbers 
+        WHERE 
+            (
+                -- Full-length numbers: remove leading '1' for comparison
+                (LENGTH(number) > 9 AND 
+                 CASE 
+                     WHEN number LIKE '1%%' THEN SUBSTR(number, 2) 
+                     ELSE number 
+                 END = '%s')
+                OR 
+                -- Short numbers (extensions): match exactly
+                (LENGTH(number) <= 9 AND number = '%s')
+            )
+        AND domain_uuid = '%s'
+    ]], number, number, domain_uuid)
+
+    
+    dbh:query(sql, function(row)
+        if row["count"] == "1" then
+            is_allowed = true
+        end
+    end)
+
+    return is_allowed
+end
+
+-- Check whitelist
+if is_whitelisted(number) then
+    freeswitch.consoleLog("INFO", "[is_whitelisted] Call to " .. number .. " is allowed. \n")
+    return "allowed"  -- Allow the call to proceed
+else
+    freeswitch.consoleLog("NOTICE", "[is_whitelisted] " .. number .. " is not in the whitelist. Call is rejected \n")
+    
+    -- If session is active, hang up
+    if session ~= nil and session:ready() then
+        session:hangup("CALL_REJECTED")  -- Hang up with the specified cause
+    end
+
+    return "not_whitelisted"
 end
