e440927d2724ff81801218e84f04adf748ab48f7
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-12-11 14:49:21 -0700

    Update index.php

--- core/dashboard/index.php
+++ core/dashboard/index.php
@@ -94,6 +94,7 @@
 	$sql .= "dashboard_label_background_color_hover, ";
 	$sql .= "dashboard_number_text_color, ";
 	$sql .= "dashboard_number_text_color_hover, ";
+	$sql .= "dashboard_number_background_color, ";
 	$sql .= "dashboard_background_color, ";
 	$sql .= "dashboard_background_color_hover, ";
 	$sql .= "dashboard_detail_background_color, ";
@@ -536,6 +537,7 @@ function toggle_grid_row_end_all() {
 		$dashboard_chart_type = $row['dashboard_chart_type'] ?? "doughnut";
 		$dashboard_label_text_color = $row['dashboard_label_text_color'] ?? $settings->get('theme', 'dashboard_label_text_color');
 		$dashboard_number_text_color = $row['dashboard_number_text_color'] ?? $settings->get('theme', 'dashboard_number_text_color');
+		$dashboard_number_background_color = $row['dashboard_number_background_color'] ?? $settings->get('theme', 'dashboard_number_background_color');
 		$dashboard_details_state = $row['dashboard_details_state'] ?? "expanded";
 		$dashboard_row_span = $row['dashboard_row_span'] ?? 2;
 
@@ -561,6 +563,7 @@ function toggle_grid_row_end_all() {
 		$dashboard_chart_type = preg_replace($text_pattern, '', $dashboard_chart_type);
 		$dashboard_label_text_color = preg_replace($text_pattern, '', $dashboard_label_text_color);
 		$dashboard_number_text_color = preg_replace($text_pattern, '', $dashboard_number_text_color);
+		$dashboard_number_background_color = preg_replace($text_pattern, '', $dashboard_number_background_color);
 		$dashboard_details_state = preg_replace($text_pattern, '', $dashboard_details_state);
 		$dashboard_row_span = preg_replace($number_pattern, '', $dashboard_row_span);
 		$dashboard_path = preg_replace($text_pattern, '', strtolower($row['dashboard_path']));
