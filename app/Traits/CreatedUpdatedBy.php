1b6083f0954cd8deb35faab572bdb4ff66495e21
Daniel Lucio <luis.daniel.lucio@gmail.com>
2025-02-08 07:51:36 -0500

    Created/Budated By Trait

---
+++ app/Traits/CreatedUpdatedBy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Traits;
+
+trait CreatedUpdatedBy
+{
+    public static function bootCreatedUpdatedBy()
+    {
+        // updating created_by and updated_by when model is created
+        static::creating(function ($model) {
+            if (!$model->isDirty('created_by')) {
+                $model->created_by = auth()->user()->user_uuid;
+            }
+            if (!$model->isDirty('updated_by')) {
+                $model->updated_by = auth()->user()->user_uuid;
+            }
+        });
+
+        // updating updated_by when model is updated
+        static::updating(function ($model) {
+            if (!$model->isDirty('updated_by')) {
+                $model->updated_by = auth()->user()->user_uuid;
+            }
+        });
+    }
+}
