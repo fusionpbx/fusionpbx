e9105f4824d791822c7b3ece43860ffb919cbcb8
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2025-02-09 00:35:48 -0500

    Create MusicOnHold.php

---
+++ app/Models/MusicOnHold.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Traits\HasUniqueIdentifier;
+
+class MusicOnHold extends Model
+{
+	use HasApiTokens, HasFactory, Notifiable, HasUniqueIdentifier;
+	protected $table = 'v_music_on_hold';
+	protected $primaryKey = 'music_on_hold_uuid';
+	public $incrementing = false;
+	protected $keyType = 'string';	// TODO, check if UUID is valid
+	const CREATED_AT = 'insert_date';
+	const UPDATED_AT = 'update_date';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+	protected $fillable = [
+      'music_on_hold_name',
+      'music_on_hold_path',
+      'music_on_hold_rate',
+      'music_on_hold_shuffle',
+      'music_on_hold_channels',
+      'music_on_hold_interval',
+      'music_on_hold_timer_name',
+      'music_on_hold_chime_list',
+      'music_on_hold_chime_freq',
+      'music_on_hold_chime_max',
+	];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+	protected $hidden = [
+	];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+	protected $casts = [
+	];
+
+	public function domain(): BelongsTo {
+		return $this->belongsTo(Domain::class, 'domain_uuid', 'domain_uuid');
+	}
+
+}
