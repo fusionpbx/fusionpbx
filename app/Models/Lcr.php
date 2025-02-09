51a0023b42a2ecf543967dcc6b4dec0d7e201bd0
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2025-02-08 23:57:28 -0500

    Create Lcr.php

---
+++ app/Models/Lcr.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Traits\HasUniqueIdentifier;
+
+class Lcr extends Model
+{
+	use HasApiTokens, HasFactory, Notifiable, HasUniqueIdentifier;
+	protected $table = 'v_lcr';
+	protected $primaryKey = 'lcr_uuid';
+	public $incrementing = false;
+	protected $keyType = 'string';	// TODO, check if UUID is valid
+	const CREATED_AT = 'insert_date';
+	const UPDATED_AT = 'update_date';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+	protected $fillable = [
+    'digits',
+    'origination_digits',
+    'rate',
+    'intrastate_rate',
+    'intralata_rate',
+    'lead_strip',
+    'trail_strip',
+    'prefix',
+    'suffix',
+    'lcr_profile',
+    'date_start',
+    'date_end',
+    'quality',
+    'reliability',
+    'cid',
+    'enabled',
+    'description',
+    'connect_increment',
+    'talk_increment',
+    'lcr_direction',
+    'currency',
+    'connect_rate',
+	];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+	protected $hidden = [
+	];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+	protected $casts = [
+	];
+
+}
