b519dbdcf02b7b765036719c757f11f56e7e39d7
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2025-02-08 23:03:28 -0500

    Create VoicemailMessage.php

---
+++ app/Models/VoicemailMessage.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Traits\HasUniqueIdentifier;
+
+class VoicemailGreeting extends Model
+{
+	use HasApiTokens, HasFactory, Notifiable, HasUniqueIdentifier;
+	protected $table = 'v_voicemail_messages';
+	protected $primaryKey = 'voicemail_message_uuid';
+	public $incrementing = false;
+	protected $keyType = 'string';	// TODO, check if UUID is valid
+	const CREATED_AT = 'insert_date';
+	const UPDATED_AT = 'update_date';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+	protected $fillable = [
+    'created_epoch',
+    'read_epoch',
+    'caller_id_name',
+    'caller_id_number',
+    'message_length',
+    'message_status',
+    'message_priority',
+    'message_intro_base64',
+    'message_base64',
+    'message_transcription',
+	];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+	protected $hidden = [
+	];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+	protected $casts = [
+	];
+
+	public function domain(): BelongsTo {
+		return $this->belongsTo(Domain::class, 'domain_uuid', 'domain_uuid');
+	}
+
+	public function voicemail(): BelongsTo {
+		return $this->belongsTo(Voicemail::class, 'voicemail_uuid', 'voicemail_uuid');
+	}
+
+}
