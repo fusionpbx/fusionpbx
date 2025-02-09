97c7fd1a3d4e99c3ff047ada406c848d1ff5d44d
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2025-02-09 00:17:13 -0500

    Update UserSetting.php

--- app/Models/UserSetting.php
+++ app/Models/UserSetting.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Traits\HasUniqueIdentifier;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserSetting extends Model
 {
