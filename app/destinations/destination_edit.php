4a112cab24ee8bb1e3a19e86914057c6d16a9655
Alex <40072887+alexdcrane@users.noreply.github.com>
2025-02-25 12:42:46 -0700

    Update destination_edit.php

--- app/destinations/destination_edit.php
+++ app/destinations/destination_edit.php
@@ -2097,7 +2097,7 @@
 
 	//enabled
 	echo "<tr>\n";
-	echo "<td class='vncell' valign='top' align='left' nowrap='nowrap'>\n";
+	echo "<td class='vncellreq' valign='top' align='left' nowrap='nowrap'>\n";
 	echo "	".$text['label-destination_enabled']."\n";
 	echo "</td>\n";
 	echo "<td class='vtable' align='left'>\n";
