b22da6c98ffd23ee36660d587fec1a046d5c94b1
Luis Daniel Lucio Quiroz <dlucio@okay.com.mx>
2025-02-27 18:50:25 -0500

    login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use app\Http\Middleware\Authenticate;
+use app\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
