486b88cb2bd218ca733daf292404f1aac36c59e2
Luis Daniel Lucio Quiroz <dlucio@okay.com.mx>
2025-02-27 16:03:16 -0500

    work on the auth

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,13 +4,16 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Closure;
+use Symfony\Component\HttpFoundation\Response;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+
+    public function handle(Request $request, Closure $next): Response
     {
         $authenticated = $request->session()->get('authenticated', false);
 
