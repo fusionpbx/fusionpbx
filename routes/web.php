a75103ac53440374d27c3c21c95d4990bfcd3ab1
Daniel Lucio <luis.daniel.lucio@gmail.com>
2025-03-06 14:03:41 -0500

    no CSRF

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,6 @@ Route::middleware('guest')->group(function () {
 
 
 Route::post('/curl/xml_handler/configuration', function (Request $request){
-    $xml = ModXMLCURLController::configuration($request);
-    return response($xml, 200)->header('Content-Type','text/xml');
+    $xml = new ModXMLCURLController;
+    return response($xml->configuration($request), 200)->header('Content-Type','text/xml');
 })->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
