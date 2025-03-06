b9f34a3a751eab1bce988feca660c44d5b8c7ae6
Daniel Lucio <luis.daniel.lucio@gmail.com>
2025-03-06 14:01:31 -0500

    no CSRF

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Middleware\Authenticate;
+
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ModXMLCURLController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
