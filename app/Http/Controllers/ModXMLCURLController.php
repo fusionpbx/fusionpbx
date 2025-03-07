4a97e613764ef854b4c8b5fa1ed86b258c6e27de
Daniel Lucio <luis.daniel.lucio@gmail.com>
2025-03-06 20:58:22 -0500

    session support

--- app/Http/Controllers/ModXMLCURLController.php
+++ app/Http/Controllers/ModXMLCURLController.php
@@ -30,6 +30,7 @@ use App\Models\Variable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Storage;
 
 use XMLWriter;
