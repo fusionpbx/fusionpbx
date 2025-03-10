f381d1f4bc99d2ed580d7173f4c788b4075ff2f2
Matías Cánepa <matias.canepa@gmail.com>
2025-03-10 16:51:50 -0300

    fixed menu model format

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -2,29 +2,47 @@
 
 namespace App\Models;
 
+use App\Traits\HasUniqueIdentifier;
+use App\Traits\GetTableName;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Menu extends Model
 {
-	use HasFactory;
+	use HasApiTokens, HasFactory, Notifiable, HasUniqueIdentifier, GetTableName;
+	protected $table = 'menu';
+	protected $primaryKey = 'id';
+	public $incrementing = false;
+	protected $keyType = 'int';
+	const CREATED_AT = 'insert_date';
+	const UPDATED_AT = 'update_date';
 
-	protected $table = "menu";
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+	protected $fillable = [
+		'name',
+		'url',
+		'parent_id',
+		'order',
+	];
 
-	protected $fillable = ["name", "url", "parent_id", "order"];
-
-	public function parent()
-	{
-		return $this->belongsTo(Menu::class, "parent_id");
+	public function parent(): BelongsTo {
+		return $this->belongsTo(Menu::class, 'parent_id');
 	}
 
-	public function children()
-	{
-		return $this->hasMany(Menu::class, "parent_id")->orderBy("order");
+	public function children(): HasMany	{
+		return $this->hasMany(Menu::class, 'parent_id')->orderBy('order');
 	}
 
 	public function childrenRecursive()
 	{
-		return $this->hasMany(Menu::class, "parent_id")->with("childrenRecursive")->orderBy("order");
+		return $this->hasMany(Menu::class, 'parent_id')->with('childrenRecursive')->orderBy('order');
 	}
 }
