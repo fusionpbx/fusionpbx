817fd64ba483bd28494d6261b68e1842c5a17172
chansizzle <14916599+chansizzle@users.noreply.github.com>
2025-03-12 09:40:44 -0600

    Add CDR Details, Call Recordings Play, and Call Recordings Download to emergency logs (#7301)
    * Create download.php
    
    download.php is required to play and download call recordings
    
    * add CDR Details and Call Recording to emergency logs
    
    * add CDR Details and Call Recording to emergency logs
    
    * combine call details column and call status column
    
    removed the call details column
    make the call status column be the hyperlink to the cdr details page
    
    * combine call details column and call status column
    
    removed the call details column
    make the call status column be the hyperlink to the cdr details page

---
+++ app/emergency/download.php
@@ -0,0 +1,48 @@
+<?php
+/*
+	FusionPBX
+	Version: MPL 1.1
+
+	The contents of this file are subject to the Mozilla Public License Version
+	1.1 (the "License"); you may not use this file except in compliance with
+	the License. You may obtain a copy of the License at
+	http://www.mozilla.org/MPL/
+
+	Software distributed under the License is distributed on an "AS IS" basis,
+	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	for the specific language governing rights and limitations under the
+	License.
+
+	The Original Code is FusionPBX
+
+	The Initial Developer of the Original Code is
+	Mark J Crane <markjcrane@fusionpbx.com>
+	Portions created by the Initial Developer are Copyright (C) 2016-2020
+	the Initial Developer. All Rights Reserved.
+
+	Contributor(s):
+	Mark J Crane <markjcrane@fusionpbx.com>
+*/
+
+//includes files
+	require_once dirname(__DIR__, 2) . "/resources/require.php";
+	require_once "resources/check_auth.php";
+
+//check permisions
+	if (permission_exists('call_recording_play') || permission_exists('call_recording_download')) {
+		//access granted
+	}
+	else {
+		echo "access denied";
+		exit;
+	}
+
+//download
+	if (is_uuid($_GET['id'])) {
+		$obj = new call_recordings;
+		$obj->recording_uuid = $_GET['id'];
+		$obj->binary = isset($_GET['binary']) ? true : false;
+		$obj->download();
+	}
+
+?>
