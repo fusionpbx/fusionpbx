73e675761ecea0598cf43cf79b0825c01014c3a3
FusionPBX <mark@fusionpbx.com>
2025-03-13 10:06:36 -0600

    Fix Call Detail Records with invalid xml

--- app/xml_cdr/resources/classes/xml_cdr.php
+++ app/xml_cdr/resources/classes/xml_cdr.php
@@ -339,6 +339,12 @@
 			//replace xml tag name <set api_hangup_hook> with <api_hangup_hook>
 				$xml_string = preg_replace('/(<\/?)(set )([^>]*>)/', '$1$3', $xml_string);
 
+			//replace xml tag name <^^,default_language> with <default_language>
+				$xml_string = preg_replace('/(<\/?)(\^\^,)([^>]*>)/', '$1$3', $xml_string);
+
+			//replace xml tag name <nolocal:operator> with <operator>
+				$xml_string = preg_replace('/(<\/?)(nolocal:)([^>]*>)/', '$1$3', $xml_string);
+
 			//disable xml entities
 				if (PHP_VERSION_ID < 80000) { libxml_disable_entity_loader(true); }
 
