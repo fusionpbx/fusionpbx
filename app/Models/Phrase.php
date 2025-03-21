413d932a7362c86fd2ae20bb8eeda895ae6f9c49
Daniel Lucio <luis.daniel.lucio@gmail.com>
2025-03-21 08:28:30 -0400

    last function of xmlhandler, missing phrase models, domain model update

---
+++ app/Models/Phrase.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\GetTableName;
+use App\Traits\HasUniqueIdentifier;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
+use Laravel\Sanctum\HasApiTokens;
+
+class Phrase extends Model
+{
+	use HasApiTokens, HasFactory, Notifiable, HasUniqueIdentifier, GetTableName;
+	protected $table = 'v_phrases';
+	protected $primaryKey = 'phrase_uuid';
+	public $incrementing = false;
+	protected $keyType = 'string';	// TODO, check if UUID is valid
+	const CREATED_AT = 'insert_date';
+	const UPDATED_AT = 'update_date';
+
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+	protected $fillable = [
+        'domain_uuid',
+        'phrase_name',
+        'phrase_language',
+        'phrase_enabled',
+        'phrase_description',
+	];
+
+	public function domain(): BelongsTo {
+		return $this->belongsTo(Domain::class, 'domain_uuid', 'domain_uuid');
+	}
+
+    public function details(): HasMany {
+		return $this->hasMany(PhraseDetail::class, 'phrase_uuid', 'phrase_uuid');
+	}
+}
---
+++ app/Models/PhraseDetail.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Phrase;
+use App\Traits\HasUniqueIdentifier;
+use App\Traits\GetTableName;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class PhraseDetail extends Model
+{
+	use HasApiTokens, HasFactory, Notifiable, HasUniqueIdentifier, GetTableName;
+	protected $table = 'v_phrase_details';
+	protected $primaryKey = 'phrase_detail_uuid';
+	public $incrementing = false;
+	protected $keyType = 'string';	// TODO, check if UUID is valid
+	const CREATED_AT = 'insert_date';
+	const UPDATED_AT = 'update_date';
+
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+	protected $fillable = [
+        'phrase_uuid',
+        'domain_uuid',
+        'phrase_detail_group',
+        'phrase_detail_tag',
+        'phrase_detail_pattern',
+        'phrase_detail_function',
+        'phrase_detail_data',
+        'phrase_detail_method',
+        'phrase_detail_type',
+        'phrase_detail_order'
+	];
+
+
+	public function phrase(): BelongsTo {
+		return $this->belongsTo(Phrase::class, 'phrase_uuid', 'phrase_uuid');
+	}
+}
