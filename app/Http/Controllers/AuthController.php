7add659e96fc337b9027fd88c0341a2356f55c73
Daniel Lucio <luis.daniel.lucio@gmail.com>
2025-03-24 18:03:27 -0400

    Set the session domain when logging

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
@@ -28,12 +29,16 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials, $request->filled('remember'))) {
             $request->session()->regenerate();
+            $domain = Auth::user()->domain();
+            Session::put('domain_uuid', $domain->domain_uuid);
+            Session::put('domain_name', $domain->domain_name);
+            Session::put('domain_description', !empty($domain->domain_description) ? $domain->domain_description : $domain->domain_name);
             return redirect()->intended('/dashboard');
         }
 
         return back()
             ->withInput($request->only('user_email', 'remember'))
-            ->with('error', 'Las credenciales proporcionadas son incorrectas.');
+            ->with('error', __('Wrong Username or Password.'));
     }
 
     public function logout(Request $request)
