7b535c907340482113a310cbc30238a26ffedd24
Daniel Lucio <luis.daniel.lucio@gmail.com>
2025-03-25 09:15:12 -0400

    CDR work, new routes

--- app/Http/Controllers/ModFormatCDRController.php
+++ app/Http/Controllers/ModFormatCDRController.php
@@ -17,5 +17,11 @@ class ModFormatCDRController extends Controller
         if(App::hasDebugModeEnabled()){
             Log::notice('['.__FILE__.':'.__LINE__.']['.__CLASS__.']['.__METHOD__.'] input: '.print_r($request->toArray(), true));
         }
+
+        // TODO: Check for Authentication
+
+        // Detect Format
+        $default_settings = new DefaultSettingController;
+        $format = $default_settings->get('config', 'format_cdr.format', 'text') ? 'xml';
     }
 }
