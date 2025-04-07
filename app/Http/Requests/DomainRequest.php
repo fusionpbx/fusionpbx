49b931e4ca0690a336e7d42f6c2f0423303e1e7f
Daniel Lucio <luis.daniel.lucio@gmail.com>
2025-04-07 16:29:20 -0400

    unique domains names

--- app/Http/Requests/DomainRequest.php
+++ app/Http/Requests/DomainRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Domain;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class DomainRequest extends FormRequest
 {
@@ -14,10 +16,17 @@ class DomainRequest extends FormRequest
 	public function rules(): array
 	{
 		return [
-			"domain_name" => "required|string|max:255",
-			"domain_description" => "sometimes|nullable|string|max:255",
-			"domain_enabled" => "nullable",
-			"domain_parent_uuid" => "nullable|uuid",
+			"domain_name" => [
+                                "bail",
+                                "required",
+                                Rule::unique('domains')->ignore($this->route('domain') ? $this->route('domain')->domain_uuid : null),
+                                "unique:App\Models\Domain,domain_name",
+                                "string",
+                                "max:255",
+                              ],
+			"domain_description" => "bail|sometimes|nullable|string|max:255",
+			"domain_enabled" => "nullable|in:true,false",
+			"domain_parent_uuid" => "bail|nullable|uuid",
 		];
 	}
 }
