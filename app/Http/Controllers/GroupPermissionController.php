3e7c1b8c0c681312d7faef719d64a1742fe86af6
Daniel Lucio <luis.daniel.lucio@gmail.com>
2025-04-09 12:47:29 -0400

    missing facade

--- app/Http/Controllers/GroupPermissionController.php
+++ app/Http/Controllers/GroupPermissionController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use App\Http\Requests\GroupPermissionRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class GroupPermissionController extends Controller
