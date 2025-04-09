10a6c457dc039dad8e4f678c8b446d2c01f6ef63
Daniel Lucio <luis.daniel.lucio@gmail.com>
2025-04-09 13:06:11 -0400

    route

--- app/Http/Controllers/GroupPermissionController.php
+++ app/Http/Controllers/GroupPermissionController.php
@@ -98,7 +98,7 @@ class GroupPermissionController extends Controller
         return view('pages.permission.index', compact(
             'search',
             'permissionsByApp',
-            'groupUuid',
+            'group_uuid',
             'group',
             'filter',
             'permissions'
