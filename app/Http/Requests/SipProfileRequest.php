401a21ceede4ce477a14bd93377d8ff3bdb6d17d
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2025-04-28 07:46:17 -0400

    Update SipProfileRequest.php

--- app/Http/Requests/SipProfileRequest.php
+++ app/Http/Requests/SipProfileRequest.php
@@ -22,17 +22,17 @@ class SipProfileRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'sip_profile_name' => 'required|string|max:255',
-            'sip_profile_hostname' => 'nullable|string|max:255',
-            'sip_profile_enabled' => 'required|boolean',
-            'sip_profile_description' => 'required|string',
-            'domains.*.sip_profile_domain_name' => 'nullable|string|max:255',
-            'domains.*.sip_profile_domain_alias' => 'required|in:true,false',
-            'domains.*.sip_profile_domain_parse' => 'required|in:true,false',
-            'settings.*.sip_profile_setting_name' => 'nullable|string|max:255',
-            'settings.*.sip_profile_setting_value' => 'nullable|string',
-            'settings.*.sip_profile_setting_enabled' => 'required|in:true,false',
-            'settings.*.sip_profile_setting_description' => 'nullable|string',
+            'sip_profile_name' => 'bail|required|string|alpha_dash:ascii|max:255',
+            'sip_profile_hostname' => 'bail|nullable|string|max:255|alpha_dash:ascii',
+            'sip_profile_enabled' => 'bail|required|boolean',
+            'sip_profile_description' => 'bail|required|string',
+            'domains.*.sip_profile_domain_name' => 'sometimes|bail|nullable|string|alpha_dash:ascii|max:255',
+            'domains.*.sip_profile_domain_alias' => 'sometimes|bail|required|in:true,false',
+            'domains.*.sip_profile_domain_parse' => 'sometimes|bail|required|in:true,false',
+            'settings.*.sip_profile_setting_name' => 'sometimes|bail|nullable|string|alpha_dash:ascii|max:255',
+            'settings.*.sip_profile_setting_value' => 'sometimes|bail|nullable|alpha_dash:ascii|string',
+            'settings.*.sip_profile_setting_enabled' => 'sometimes|bail|required|in:true,false',
+            'settings.*.sip_profile_setting_description' => 'sometimes|bail|nullable|string',
         ];
     }
 }
