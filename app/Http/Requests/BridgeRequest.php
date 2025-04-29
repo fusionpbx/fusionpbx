d8e69bb02b427ffcff9aa5a8940281294c72bdb3
Daniel Lucio <luis.daniel.lucio@gmail.com>
2025-04-29 11:58:32 -0400

    fix some requests

--- app/Http/Requests/BridgeRequest.php
+++ app/Http/Requests/BridgeRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class BridgeRequest extends FormRequest
 {
@@ -14,7 +15,13 @@ class BridgeRequest extends FormRequest
 	public function rules(): array
 	{
 		return [
-			"bridge_name" => "bail|required|string|max:255",
+			"bridge_name" => [
+                    'bail',
+                    'required',
+                    'string',
+                    'max:255',
+                    Rule::unique('App\Models\User','username')->where(fn (Builder $query) => $query->where('domain_uuid', Session::get('domain_uuid'))),
+                              ],
 			"bridge_destination" => "bail|required|string|max:255",
 			"bridge_enabled" => "bail|nullable|in:true,false",
 			"bridge_description" => "bail|nullable|string|max:255",
