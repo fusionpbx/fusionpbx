33739cc5dd0e3f4bb6c19e5e34d96eb4c1634913
root <root@chi-pbx-dev.hornblower.com>
2025-05-29 17:40:50 +0000

    missing use

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\API\DomainAPIController;
+use App\Http\Controllers\API\ExtensionAPIController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\UserActivationController;
 use App\Http\Middleware\VerifyAuthenticationKey;
