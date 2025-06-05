cfd862cf643b515919b4688e5c04c834d57e7b26
FusionPBX <mark@fusionpbx.com>
2025-06-05 10:20:43 -0600

    Update xml_cdr.php

--- app/xml_cdr/resources/classes/xml_cdr.php
+++ app/xml_cdr/resources/classes/xml_cdr.php
@@ -1272,7 +1272,7 @@
 
 					//get the application array
 					if (!empty($destination_array) && !empty($row["caller_profile"]["destination_number"])) {
-						if ($this->call_direction == 'outbound') {
+						if ($this->call_direction == 'outbound' && !empty($row["caller_profile"]["username"])) {
 							$app = $this->find_app($destination_array, urldecode($row["caller_profile"]["username"]));
 						}
 						else {
