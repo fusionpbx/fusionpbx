844c8c514343f61dc58ef62e4b8216584c60fe29
Daniel Lucio <luis.daniel.lucio@gmail.com>
2025-06-20 12:25:01 -0400

    debug

--- app/Rules/ISO639.php
+++ app/Rules/ISO639.php
@@ -4,6 +4,8 @@ namespace App\Rules;
 
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
 
 class ISO639 implements ValidationRule
 {
@@ -20,15 +22,24 @@ class ISO639 implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $data = config('iso639');
-        $filtered_data = array_column($data, $this->index);
-        $element = array_find($filtered_data, function (string $v) use ($value){
-            return ($value == $v);
-        });
-
-        if (is_null($element))
+        $countryData = config('iso639');
+        if(App::hasDebugModeEnabled()){
+            Log::debug('['.__FILE__.':'.__LINE__.']['.__CLASS__.']['.__METHOD__.'] $countryData: '.print_r($countryData, true));
+        }
+        if (isset($countryData))
         {
-            $fail('The :attribute does not have a valid ISO639 value.');
+            $filtered_data = array_column($countryData, $this->index);
+            if(App::hasDebugModeEnabled()){
+                Log::debug('['.__FILE__.':'.__LINE__.']['.__CLASS__.']['.__METHOD__.'] filtered_data: '.print_r($filtered_data, true));
+            }
+            $element = array_find($filtered_data, function (string $v) use ($value){
+                return ($value == $v);
+            });
+
+            if (is_null($element))
+            {
+                $fail('The :attribute does not have a valid ISO639 value.');
+            }
         }
     }
 }
