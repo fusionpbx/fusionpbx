a597ce94b37f945baf5ccd0f3a22e57a12ad27da
root <root@chi-pbx-dev.hornblower.com>
2025-07-09 14:12:05 +0000

    number only required if not using the CDR

--- app/Http/Requests/CallBlockRequest.php
+++ app/Http/Requests/CallBlockRequest.php
@@ -19,7 +19,7 @@ class CallBlockRequest extends FormRequest
 			"extension_uuid" => "bail|sometimes|nullable|uuid|exists:App\Models\Extension,extension_uuid",
 			"call_block_name" => "bail|sometimes|string|max:255",
 			"call_block_country_code" => ['nullable','numeric','integer','min:1','min_digits:1','max_digits:3', new E164(config('freeswitch.CHECK_COUNTRY_CODE'), '*')],
-			"call_block_number" => "bail|required|integer|min_digits:1|max_digits:14",
+			"call_block_number" => "bail|required_without:selected_xml_cdrs|integer|min_digits:1|max_digits:14",
 			"call_block_action" => "bail|nullable|string",
 			"call_block_enabled" => "bail|nullable|in:true,false",
 			"call_block_description" => "bail|nullable|string|max:255",
