7e27cd25a1e580d70fbfa016eca569f57b0ed6fc
FusionPBX <mark@fusionpbx.com>
2025-07-13 19:13:50 -0600

    fix spelling mistake in xml_cdr method name
    Change the method name from moved_to_failed to move_to_failed

--- app/xml_cdr/resources/classes/xml_cdr.php
+++ app/xml_cdr/resources/classes/xml_cdr.php
@@ -1519,7 +1519,7 @@ if (!class_exists('xml_cdr')) {
 			}
 		}
 
-		public function moved_to_failed($failed_file) {
+		public function move_to_failed($failed_file) {
 			$xml_cdr_dir = $this->setting->get('switch', 'log', '/var/log/freeswitch').'/xml_cdr';
 			if (!file_exists($xml_cdr_dir.'/failed')) {
 				if (!mkdir($xml_cdr_dir.'/failed', 0660, true)) {
