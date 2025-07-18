180cc601845a87877b9f959eaf8f954e9361cdbd
Daniel Lucio <luis.daniel.lucio@gmail.com>
2025-07-18 15:08:37 -0400

    github FS work

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,7 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+
         'templates' => [
             'driver' => 'github',
             'token' => env('COOLPBX_TEMPLATE_REPOSITORY_TOKEN', ''),
