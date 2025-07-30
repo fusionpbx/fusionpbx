6ad8326463e45006dc052071b4986353803e6eb4
Mendel <44873735+rabbidiesel@users.noreply.github.com>
2025-07-30 10:03:39 -0400

    Added Algo Devices and Default settingd (#7434)

--- app/algo/app_config.php
+++ app/algo/app_config.php
@@ -31,14 +31,64 @@
 		$apps[$x]['description']['uk-ua'] = "";
 
 	//default settings
-    /*
 		$y=0;
-		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "21896284-7c04-46f0-91c4-66fca8719b7f";
+		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "e874cf2e-b58a-4833-b1c5-921ee3419681";
 		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
-		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "algo_provision_url";
+		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "algo_8186_version_firmware";
 		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
-		$apps[$x]['default_settings'][$y]['default_setting_value'] = 'https://{$domain_name}/app/provision/';
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = '5.5f';
 		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "false";
-		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Provisioning URL for Algo";
+		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Algo 8186 Firmware Version";
 		$y++;
-    */
+		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "a1cbb813-b6a2-4e5e-87a5-8e7572b0ba87";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
+		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "algo_8188_version_firmware";
+		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = '5.5f';
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "false";
+		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Algo 8188 Firmware Version";
+		$y++;
+		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "646d81ec-eaa9-426f-a32d-1876dc4b75ed";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
+		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "algo_8189_version_firmware";
+		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = '5.5f';
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "false";
+		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Algo 8189 Firmware Version";
+		$y++;
+		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "75c1afc0-1f8e-4dd8-bc2b-6f27a7818aff";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
+		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "algo_download_method";
+		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = 'https';
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "false";
+		$apps[$x]['default_settings'][$y]['default_setting_description'] = "How to get the provisioning and firmware files.
+		TFTP, FTP, HTTP, HTTPS";
+		$y++;
+		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "dc5ecda5-6f3a-4967-811e-9e9fe7690b87";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
+		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "algo_firmware_path";
+		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = 'firmware/algo';
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "false";
+		$apps[$x]['default_settings'][$y]['default_setting_description'] = "HTTP or HTTPS for downloading";
+		$y++;
+		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "3a79d794-0009-48ef-bd65-0f5dd28579f9";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
+		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "algo_inbound_timeout";
+		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = '240';
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
+		$apps[$x]['default_settings'][$y]['default_setting_description'] = "In seconds, How long can a page last.";
+		$y++;
+		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "e7d51f9f-b9f2-494f-b99a-ece2ffcf7cfe";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "provision";
+		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "algo_multicast_mode";
+		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = '240';
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
+		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Algo Multicast Modes
+		0 - None
+		1 - Transmitter (Sender)
+		2- Receiver (Listener).";
+		$y++;
