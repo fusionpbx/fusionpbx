f9be2b3d623a98e835dc51a02b985bb244ccd51c
Alex <40072887+alexdcrane@users.noreply.github.com>
2025-09-15 14:26:36 -0700

    Update user_edit.php

--- core/users/user_edit.php
+++ core/users/user_edit.php
@@ -997,20 +997,21 @@
 		$user_groups = $database->select($sql, $parameters, 'all');
 		if (is_array($user_groups)) {
 			echo "<table cellpadding='0' cellspacing='0' border='0'>\n";
-			if (permission_exists('user_group_delete') || if_group("superadmin")) {
+			if (permission_exists('user_group_delete')) {
 				echo "	<input type='hidden' id='action' name='action' value=''>\n";
 				echo "	<input type='hidden' id='group_uuid' name='group_uuid' value=''>\n";
 			}
+			$x = 0;
 			foreach($user_groups as $field) {
 				if (!empty($field['group_name'])) {
 					echo "<tr>\n";
 					echo "	<td class='vtable' style='white-space: nowrap; padding-right: 30px;' nowrap='nowrap'>";
 					echo escape($field['group_name']).((!empty($field['group_domain_uuid'])) ? "@".$_SESSION['domains'][$field['group_domain_uuid']]['domain_name'] : null);
 					echo "	</td>\n";
-					if (permission_exists('user_group_delete') || if_group("superadmin")) {
+					if (permission_exists('user_group_delete')) {
 						echo "	<td class='list_control_icons' style='width: 25px;'>\n";
-						echo button::create(['type'=>'button','icon'=>'fas fa-minus','id'=>'btn_delete','class'=>'default list_control_icon','name'=>'btn_delete','onclick'=>"modal_open('modal-delete-group-".escape($field['group_name'])."','btn_delete');"]);
-						echo modal::create(['id'=>'modal-delete-group-'.escape($field['group_name']),'type'=>'delete','actions'=>button::create(['type'=>'button','label'=>$text['button-continue'],'icon'=>'check','id'=>'btn_delete','style'=>'float: right; margin-left: 15px;','collapse'=>'never','onclick'=>"modal_close(); list_action_set('delete'); document.getElementById('group_uuid').value = '".escape($field['group_uuid'])."'; list_form_submit('frm');"])]);
+						echo button::create(['type'=>'button','icon'=>'fas fa-minus','id'=>'btn_delete','class'=>'default list_control_icon','name'=>'btn_delete','onclick'=>"modal_open('modal-delete-group-$x','btn_delete');"]);
+						echo modal::create(['id'=>'modal-delete-group-'.$x,'type'=>'delete','actions'=>button::create(['type'=>'button','label'=>$text['button-continue'],'icon'=>'check','id'=>'btn_delete','style'=>'float: right; margin-left: 15px;','collapse'=>'never','onclick'=>"modal_close(); list_action_set('delete'); document.getElementById('group_uuid').value = '".escape($field['group_uuid'])."'; list_form_submit('frm');"])]);
 						echo "	</td>\n";
 					}
 					echo "</tr>\n";
@@ -1018,6 +1019,7 @@
 						$assigned_groups[] = $field['group_uuid'];
 					}
 				}
+				$x++;
 			}
 			echo "</table>\n";
 		}
