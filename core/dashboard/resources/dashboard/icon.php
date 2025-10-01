afe5a051168476faaab940584803922863e9bdea
Alex <40072887+alexdcrane@users.noreply.github.com>
2025-10-01 09:25:04 -0700

    Dashboard: Adjust icon widget clickable area (#7536)
    * Dashboard: Adjust icon widget clickable area
    
    * Update domains.php
    
    * Update registrations.php

--- core/dashboard/resources/dashboard/icon.php
+++ core/dashboard/resources/dashboard/icon.php
@@ -23,12 +23,12 @@
 
 //dashboard icon
 	echo "<div class='hud_box'>\n";
-	echo "	<div class='hud_content' ".(empty($widget_details_state) || $widget_details_state != "disabled" ? "onclick=\"$('#hud_icon_details').slideToggle('fast');\"" : null).">\n";
+	echo "	<div class='hud_content' onclick=\"".(empty($widget_details_state) || $widget_details_state == "disabled" ? "window.open('".$widget_url."', '".$widget_target."', '".$window_parameters."'); return false;" : "$('#hud_icon_details').slideToggle('fast');")."\">\n";
 	echo "		<span class='hud_title'><a style='padding: 10px 0;' onclick=\"window.open('".$widget_url."', '".$widget_target."', '".$window_parameters."'); return false;\">".escape($widget_label)."</a></span>\n";
 	echo "		<span class='hud_stat'><a style='padding: 10px 20px;' onclick=\"window.open('".$widget_url."', '".$widget_target."', '".$window_parameters."'); return false;\"><i class=\"fas ".$widget_icon."\"></i></a></span>\n";
 	echo "	</div>\n";
 
-	if (empty($widget_details_state) || $widget_details_state != "disabled") {
+	if (!empty($widget_details_state) && $widget_details_state != "disabled") {
 		echo "	<div class='hud_details hud_box' id='hud_icon_details' style='padding: 20px; 10%; overflow: auto; ".(!empty($row['widget_detail_background_color']) ? "background: ".$row['widget_detail_background_color'].";" : null)."'>".str_replace("\r", '<br>', escape($widget_content_details))."</div>\n";
 	}
 	echo "	<span class='hud_expander' onclick=\"$('#hud_icon_details').slideToggle('fast');\"><span class='fas fa-ellipsis-h'></span></span>";
