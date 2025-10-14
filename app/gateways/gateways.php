1dc1ea49fcae706de9b89eb57ae56b7035813938
FusionPBX <markjcrane@gmail.com>
2025-10-14 17:17:38 -0600

    Add condition domain_uuid not empty

--- app/gateways/gateways.php
+++ app/gateways/gateways.php
@@ -300,7 +300,7 @@
 			$list_row_url = '';
 			if (permission_exists('gateway_edit')) {
 				$list_row_url = "gateway_edit.php?id=".urlencode($row['gateway_uuid']);
-				if ($row['domain_uuid'] != $_SESSION['domain_uuid'] && permission_exists('domain_select')) {
+				if (!empty($row['domain_uuid']) && $row['domain_uuid'] != $_SESSION['domain_uuid'] && permission_exists('domain_select')) {
 					$list_row_url .= '&domain_uuid='.urlencode($row['domain_uuid']).'&domain_change=true';
 				}
 			}
