919fb241b59fbc3b30389937e6629804f741a1fe
FusionPBX <mark@fusionpbx.com>
2025-10-15 14:36:05 -0600

    Enhance domain UUID check in settings list

--- app/sofia_global_settings/sofia_global_settings.php
+++ app/sofia_global_settings/sofia_global_settings.php
@@ -210,7 +210,7 @@
 			$list_row_url = '';
 			if (permission_exists('sofia_global_setting_edit')) {
 				$list_row_url = "sofia_global_setting_edit.php?id=".urlencode($row['sofia_global_setting_uuid']);
-				if ($row['domain_uuid'] != $_SESSION['domain_uuid'] && permission_exists('domain_select')) {
+				if (!empty($row['domain_uuid']) && $row['domain_uuid'] != $_SESSION['domain_uuid'] && permission_exists('domain_select')) {
 					$list_row_url .= '&domain_uuid='.urlencode($row['domain_uuid']).'&domain_change=true';
 				}
 			}
