9aec2c822f7ff364338b8e0e983336ea05491032
FusionPBX <mark@fusionpbx.com>
2025-10-15 15:15:05 -0600

    Improve domain UUID check in stream listing

--- app/streams/streams.php
+++ app/streams/streams.php
@@ -252,7 +252,7 @@
 			$list_row_url = '';
 			if (permission_exists('stream_edit')) {
 				$list_row_url = "stream_edit.php?id=".urlencode($row['stream_uuid']);
-				if ($row['domain_uuid'] != $_SESSION['domain_uuid'] && permission_exists('domain_select')) {
+				if (!empty($row['domain_uuid']) && $row['domain_uuid'] != $_SESSION['domain_uuid'] && permission_exists('domain_select')) {
 					$list_row_url .= '&domain_uuid='.urlencode($row['domain_uuid']).'&domain_change=true';
 				}
 			}
