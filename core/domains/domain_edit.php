6dacd1d35afbd8a4bdfe2712194ad1424b493bac
Alex <40072887+alexdcrane@users.noreply.github.com>
2025-11-11 13:14:01 -0700

    Update domain_edit.php

--- core/domains/domain_edit.php
+++ core/domains/domain_edit.php
@@ -529,6 +529,9 @@
 		unset($sql, $parameters, $row);
 	}
 
+//set the defaults
+	$domain_enabled = $domain_enabled ?? true;
+
 //create token
 	$object = new token;
 	$token = $object->create($_SERVER['PHP_SELF']);
