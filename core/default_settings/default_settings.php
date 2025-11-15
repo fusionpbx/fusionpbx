debc9a9b8c6cb0bb59c5eebe739ea1b565b2385b
markjcrane <markjcrane@gmail.com>
2025-11-15 12:12:40 -0700

    Update the SQL query to get the Categories

--- core/default_settings/default_settings.php
+++ core/default_settings/default_settings.php
@@ -47,8 +47,9 @@
 	$language = new text;
 	$text = $language->get();
 
-//get the http post data
+//set the variables
 	$search = $_REQUEST['search'] ?? '';
+	$show = $_REQUEST['show'] ?? '';
 	$default_setting_category = $_REQUEST['default_setting_category'] ?? '';
 	if (!empty($_POST['default_settings'])) {
 		$action = $_POST['action'];
@@ -59,10 +60,6 @@
 		$domain_uuid = '';
 		$default_settings = '';
 	}
-
-//set additional variables
-	$search = !empty($_GET["search"]) ? $_GET["search"] : '';
-	$show = !empty($_GET["show"]) ? $_GET["show"] : '';
 
 //sanitize the variables
 	$action = preg_replace('#[^a-zA-Z0-9_\-\.]#', '', $action);
@@ -167,25 +164,11 @@
 
 //get default setting categories
 	$sql = "select ";
-	$sql .= "distinct(d1.default_setting_category), ";
-	$sql .= "( ";
-	$sql .= "	select ";
-	$sql .= "	count(d2.default_setting_category) ";
-	$sql .= "	from v_default_settings as d2 ";
-	$sql .= "	where d2.default_setting_category = d1.default_setting_category ";
-	if (!empty($search)) {
-		$sql .= "	and (";
-		$sql .= "		lower(d2.default_setting_category) like :search ";
-		$sql .= "		or lower(d2.default_setting_subcategory) like :search ";
-		$sql .= "		or lower(d2.default_setting_name) like :search ";
-		$sql .= "		or lower(d2.default_setting_value) like :search ";
-		$sql .= "		or lower(d2.default_setting_description) like :search ";
-		$sql .= "	) ";
-		$parameters['search'] = '%'.$search.'%';
-	}
-	$sql .= ") as quantity ";
-	$sql .= "from v_default_settings as d1 ";
-	$sql .= "order by d1.default_setting_category asc ";
+	$sql .= "d.default_setting_category, ";
+	$sql .= "count(d.default_setting_category) as quantity ";
+	$sql .= "from v_default_settings as d ";
+	$sql .= "group by d.default_setting_category ";
+	$sql .= "order by d.default_setting_category asc ";
 	$rows = $database->select($sql, $parameters ?? null, 'all');
 	if (!empty($rows) && @sizeof($rows) != 0) {
 		foreach ($rows as $row) {
