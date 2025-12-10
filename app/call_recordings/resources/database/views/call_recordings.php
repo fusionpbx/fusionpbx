1483cc1ac709afcd19538cbc0743ed58b959ba28
FusionPBX <markjcrane@gmail.com>
2025-12-10 12:08:34 -0700

    Call Recording Transcription multiple changes     - Use the new transcribe_queue class and method     - Move call recording transcription to xml_cdr_transcripts     - Remove transcribe_target table, keys and colum name

--- app/call_recordings/resources/database/views/call_recordings.php
+++ app/call_recordings/resources/database/views/call_recordings.php
@@ -1,14 +1,16 @@
 <?php
 
 	$view['name'] = "view_call_recordings";
-	$view['version'] = "20250919";
+	$view['version'] = "20251210";
 	$view['description'] = "Show the call recordings details from the call detail records database.";
-	$view['sql'] = "    select domain_uuid, xml_cdr_uuid as call_recording_uuid, \n";
+	$view['sql'] = "    SELECT c.domain_uuid, c.xml_cdr_uuid AS call_recording_uuid, \n";
 	$view['sql'] .= "	caller_id_name, caller_id_number, caller_destination, destination_number, \n";
-	$view['sql'] .= "	record_name as call_recording_name, record_path as call_recording_path, \n";
-	$view['sql'] .= "	record_transcription as call_recording_transcription, \n";
-	$view['sql'] .= "	duration as call_recording_length, start_stamp as call_recording_date, direction as call_direction \n";
-	$view['sql'] .= "	from v_xml_cdr \n";
-	$view['sql'] .= "	where record_name is not null \n";
-	$view['sql'] .= "	and record_path is not null \n";
-	$view['sql'] .= "	order by start_stamp desc \n";
+	$view['sql'] .= "	record_name AS call_recording_name, record_path AS call_recording_path, \n";
+	$view['sql'] .= "	t.transcript_json AS call_recording_transcription, \n";
+	$view['sql'] .= "	record_transcription, \n";
+	$view['sql'] .= "	duration AS call_recording_length, start_stamp AS call_recording_date, direction AS call_direction \n";
+	$view['sql'] .= "	FROM v_xml_cdr as c, v_xml_cdr_transcripts as t \n";
+	$view['sql'] .= "	WHERE record_name IS NOT NULL \n";
+	$view['sql'] .= "	AND record_path is not null \n";
+	$view['sql'] .= "	AND c.xml_cdr_uuid = t.xml_cdr_uuid \n";
+	$view['sql'] .= "	ORDER BY start_stamp desc \n";
