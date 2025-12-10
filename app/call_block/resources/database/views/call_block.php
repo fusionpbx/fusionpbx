144932f9c0fdae2a79889d1ae10b1d1c073499c9
volga629-1 <59034879+volga629-1@users.noreply.github.com>
2025-12-10 13:24:34 -0500

    View statements fixes (#7657)
    * Missing open statement 
    
    Missing open statement 
    
    ```
    $view['sql'] .= "); \n";
    
    ```
    
    * Double SELECT will break query
    
    Double SELECT break query
    
    * Forgot update version
    
    Forgot update version
    
    * No matching ( 
    
    No matching  opening 
    
    ```
    
    FROM
    v_contacts as c
    );
    
    ```

--- app/call_block/resources/database/views/call_block.php
+++ app/call_block/resources/database/views/call_block.php
@@ -1,12 +1,30 @@
 <?php
 
-	$view['name'] = "view_call_block";
-	$view['version'] = "20250919";
-	$view['description'] = "Show the call block details with extension information.";
-	$view['sql'] = "	select domain_uuid, group_uuid, group_name, ";
-    $view['sql'] .= "	select c.domain_uuid, call_block_uuid, c.call_block_direction, c.extension_uuid, c.call_block_name, c.call_block_country_code, \n";
-	$view['sql'] .= "	c.call_block_number, e.extension, e.number_alias, c.call_block_count, c.call_block_app, c.call_block_data, c.date_added, \n";
-	$view['sql'] .= "	c.call_block_enabled, c.call_block_description, c.insert_date, c.insert_user, c.update_date, c.update_user \n";
-	$view['sql'] .= "	from v_call_block as c \n";
-	$view['sql'] .= " left join v_extensions as e \n";
-	$view['sql'] .= "	on c.extension_uuid = e.extension_uuid \n";
+$view['name'] = "view_call_block";
+$view['version'] = "20251207";
+$view['description'] = "Show the call block details with extension information.";
+
+$view['sql']  = "SELECT \n";
+$view['sql'] .= "    c.domain_uuid, \n";
+$view['sql'] .= "    c.call_block_uuid, \n";
+$view['sql'] .= "    c.call_block_direction, \n";
+$view['sql'] .= "    c.extension_uuid, \n";
+$view['sql'] .= "    c.call_block_name, \n";
+$view['sql'] .= "    c.call_block_country_code, \n";
+$view['sql'] .= "    c.call_block_number, \n";
+$view['sql'] .= "    e.extension, \n";
+$view['sql'] .= "    e.number_alias, \n";
+$view['sql'] .= "    c.call_block_count, \n";
+$view['sql'] .= "    c.call_block_app, \n";
+$view['sql'] .= "    c.call_block_data, \n";
+$view['sql'] .= "    c.date_added, \n";
+$view['sql'] .= "    c.call_block_enabled, \n";
+$view['sql'] .= "    c.call_block_description, \n";
+$view['sql'] .= "    c.insert_date, \n";
+$view['sql'] .= "    c.insert_user, \n";
+$view['sql'] .= "    c.update_date, \n";
+$view['sql'] .= "    c.update_user \n";
+$view['sql'] .= "FROM v_call_block AS c \n";
+$view['sql'] .= "LEFT JOIN v_extensions AS e \n";
+$view['sql'] .= "    ON c.extension_uuid = e.extension_uuid;\n";
+
