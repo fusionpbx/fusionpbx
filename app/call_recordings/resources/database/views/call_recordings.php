d2cd86dc9a280767cec601293bd4d382a2fc21c3
markjcrane <markjcrane@gmail.com>
2025-12-10 13:06:11 -0700

    Remove record_transcription replaced by transcript_json

--- app/call_recordings/resources/database/views/call_recordings.php
+++ app/call_recordings/resources/database/views/call_recordings.php
@@ -3,11 +3,10 @@
 	$view['name'] = "view_call_recordings";
 	$view['version'] = "20251210";
 	$view['description'] = "Show the call recordings details from the call detail records database.";
-	$view['sql'] = "    SELECT c.domain_uuid, c.xml_cdr_uuid AS call_recording_uuid, \n";
+	$view['sql'] = "	SELECT c.domain_uuid, c.xml_cdr_uuid AS call_recording_uuid, \n";
 	$view['sql'] .= "	caller_id_name, caller_id_number, caller_destination, destination_number, \n";
 	$view['sql'] .= "	record_name AS call_recording_name, record_path AS call_recording_path, \n";
 	$view['sql'] .= "	t.transcript_json AS call_recording_transcription, \n";
-	$view['sql'] .= "	record_transcription, \n";
 	$view['sql'] .= "	duration AS call_recording_length, start_stamp AS call_recording_date, direction AS call_direction \n";
 	$view['sql'] .= "	FROM v_xml_cdr as c, v_xml_cdr_transcripts as t \n";
 	$view['sql'] .= "	WHERE record_name IS NOT NULL \n";
