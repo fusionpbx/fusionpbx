178f83d6bef0f701731a1bee56dcadf169d0dbc6
volga629-1 <59034879+volga629-1@users.noreply.github.com>
2025-12-10 13:24:34 -0500

    View statements fixes (#7657)
    * Missing open statement 
    
    Missing open statement 
    
    ```
    $view['sql'] .= "); \n";
    
    ```
    
    * Double SELECT will break query
    
    Double SELECT break query
    
    * Forgot update version
    
    Forgot update version
    
    * No matching ( 
    
    No matching  opening 
    
    ```
    
    FROM
    v_contacts as c
    );
    
    ```

--- app/extensions/resources/database/views/extensions.php
+++ app/extensions/resources/database/views/extensions.php
@@ -1,15 +1,14 @@
 <?php
 
 $view['name'] = "view_extensions";
-$view['version'] = "20250920";
+$view['version'] = "20251207";
 $view['description'] = "Show the extensions with assigned users";
-$view['sql'] = "SELECT \n";
-$view['sql'] .= "e.*, \n";
-$view['sql'] .= "( \n";
-$view['sql'] .= "	SELECT json_agg(u.*) \n";
-$view['sql'] .= "	FROM v_extension_users as u \n";
-$view['sql'] .= "	WHERE u.extension_uuid = e.extension_uuid \n";
-$view['sql'] .= ") AS extension_users \n";
-$view['sql'] .= "FROM \n";
-$view['sql'] .= "	v_extensions as e \n";
-$view['sql'] .= "); \n";
+
+$view['sql']  = "SELECT \n";
+$view['sql'] .= "    e.*, \n";
+$view['sql'] .= "    ( \n";
+$view['sql'] .= "        SELECT json_agg(u.*) \n";
+$view['sql'] .= "        FROM v_extension_users AS u \n";
+$view['sql'] .= "        WHERE u.extension_uuid = e.extension_uuid \n";
+$view['sql'] .= "    ) AS extension_users \n";
+$view['sql'] .= "FROM v_extensions AS e;\n";
