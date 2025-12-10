9c322f58e2455eac6cf25704833001c67acb3f17
markjcrane <markjcrane@gmail.com>
2025-12-10 13:30:09 -0700

    Remove deprecated view definitions in app_defaults.php Views are being created by the database class views method.

--- app/call_recordings/app_defaults.php
+++ app/call_recordings/app_defaults.php
@@ -26,22 +26,6 @@
 
 if ($domains_processed == 1) {
 
-	//create the user view combines username, organization, contact first and last name
-	$database->execute("DROP VIEW view_call_recordings;", null);
-	$sql = "CREATE VIEW view_call_recordings AS ( \n";
-	$sql .= "	select domain_uuid, xml_cdr_uuid as call_recording_uuid, \n";
-	$sql .= "	caller_id_name, caller_id_number, caller_destination, destination_number, \n";
-	$sql .= "	record_name as call_recording_name, record_path as call_recording_path, \n";
-	$sql .= "	record_transcription as call_recording_transcription, \n";
-	$sql .= "	duration as call_recording_length, start_stamp as call_recording_date, direction as call_direction \n";
-	$sql .= "	from v_xml_cdr \n";
-	$sql .= "	where record_name is not null \n";
-	$sql .= "	and record_path is not null \n";
-	$sql .= "	order by start_stamp desc \n";
-	$sql .= "); \n";
-	$database->execute($sql, null);
-	unset($sql);
-
 }
 
 ?>
