66cb2ecefbdf64925491eb5b88978f6301a945e5
Alex <40072887+alexdcrane@users.noreply.github.com>
2025-12-15 09:39:18 -0700

    Update devices.php

--- app/devices/devices.php
+++ app/devices/devices.php
@@ -83,17 +83,6 @@
 	$search = strtolower($_REQUEST["search"] ?? '');
 	$fields = strtolower($_REQUEST["fields"] ?? '');
 
-//set the time zone
-	$time_zone = $settings->get('domain', 'time_zone', date_default_timezone_get());
-
-//set the time format options: 12h, 24h
-	if ($settings->get('domain', 'time_format') == '24h') {
-		$time_format = 'HH24:MI:SS';
-	}
-	else {
-		$time_format = 'HH12:MI:SS am';
-	}
-
 //get total devices count from the database
 	$sql = "select count(*) from v_devices ";
 	$sql .= "where domain_uuid = :domain_uuid ";
@@ -187,6 +176,17 @@
 	list($paging_controls, $rows_per_page) = paging($num_rows, $param ?? '', $rows_per_page);
 	list($paging_controls_mini, $rows_per_page) = paging($num_rows, $param ?? '', $rows_per_page, true);
 	$offset = $rows_per_page * $page;
+
+//set the time zone
+	$time_zone = $settings->get('domain', 'time_zone', date_default_timezone_get());
+
+//set the time format options: 12h, 24h
+	if ($settings->get('domain', 'time_format') == '24h') {
+		$time_format = 'HH24:MI:SS';
+	}
+	else {
+		$time_format = 'HH12:MI:SS am';
+	}
 
 //get the list
 	$sql = "select ";
