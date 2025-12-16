cc531b5e2cfbc458af96c4f69c3cc70c753f88d8
Alex <40072887+alexdcrane@users.noreply.github.com>
2025-12-16 09:47:15 -0700

    Update xml_cdr_statistics_inc.php

--- app/xml_cdr/xml_cdr_statistics_inc.php
+++ app/xml_cdr/xml_cdr_statistics_inc.php
@@ -485,20 +485,20 @@
 		$start_stamp_begin_formatted = ($settings->get('domain', 'time_format') == '24h' ? $start_stamp_begin : DateTime::createFromFormat('Y-m-d h:i a', $start_stamp_begin)->format('Y-m-d H:i'));
 	}
 	if (!empty($start_stamp_end)) {
-		$start_end_stamp_formatted = ($settings->get('domain', 'time_format') == '24h' ? $start_stamp_end : DateTime::createFromFormat('Y-m-d h:i a', $start_stamp_end)->format('Y-m-d H:i'));
+		$start_stamp_end_formatted = ($settings->get('domain', 'time_format') == '24h' ? $start_stamp_end : DateTime::createFromFormat('Y-m-d h:i a', $start_stamp_end)->format('Y-m-d H:i'));
 	}
 	if (!empty($start_stamp_begin) && !empty($start_stamp_end)) {
 		$sql .= "and c.start_stamp between :start_stamp_begin and :start_stamp_end \n";
-		$parameters['start_stamp_begin'] = $start_stamp_begin.':00.000';
-		$parameters['start_stamp_end'] = $start_stamp_end.':59.999';
+		$parameters['start_stamp_begin'] = $start_stamp_begin_formatted.':00.000';
+		$parameters['start_stamp_end'] = $start_stamp_end_formatted.':59.999';
 	}
 	else if (!empty($start_stamp_begin)) {
 		$sql .= "and c.start_stamp >= :start_stamp_begin \n";
-		$parameters['start_stamp_begin'] = $start_stamp_begin.':00.000';
+		$parameters['start_stamp_begin'] = $start_stamp_begin_formatted.':00.000';
 	}
 	else if (!empty($start_stamp_end)) {
 		$sql .= "and c.start_stamp <= :start_stamp_end \n";
-		$parameters['start_stamp_end'] = $start_stamp_end.':59.999';
+		$parameters['start_stamp_end'] = $start_stamp_end_formatted.':59.999';
 	}
 	if (!empty($answer_stamp_begin) && !empty($answer_stamp_end)) {
 		$sql .= "and c.answer_stamp between :answer_stamp_begin and :answer_stamp_end \n";
