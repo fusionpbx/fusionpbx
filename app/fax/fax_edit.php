d1b6a55959322c32b3d4f4d8acb8f8ff4d954190
Alex <40072887+alexdcrane@users.noreply.github.com>
2026-01-02 13:55:53 -0700

    Update fax_edit.php

--- app/fax/fax_edit.php
+++ app/fax/fax_edit.php
@@ -29,7 +29,7 @@
 	require_once "resources/check_auth.php";
 
 //check permissions
-	if (!(permission_exists('fax_extension_add') || !permission_exists('fax_extension_edit') || permission_exists('fax_extension_delete'))) {
+	if (!(permission_exists('fax_extension_add') || permission_exists('fax_extension_edit') || permission_exists('fax_extension_delete'))) {
 		echo "access denied";
 		exit;
 	}
