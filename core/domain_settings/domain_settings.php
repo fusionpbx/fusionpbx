5238afe7dcf98e5e11bbb7c23014a27f7288d111
FusionPBX <markjcrane@gmail.com>
2026-01-31 10:45:33 -0700

    Only show the masked password if there is a password defined.

--- core/domain_settings/domain_settings.php
+++ core/domain_settings/domain_settings.php
@@ -314,7 +314,7 @@
 				echo "		".$text['label-'.escape($row['domain_setting_value'])];
 			}
 			else if ($subcategory == 'password' || (substr_count($subcategory, '_password') > 0 && $subcategory != 'input_text_font_password') || substr_count($subcategory, '_key') || substr_count($subcategory, '_secret') > 0) {
-				echo "		".str_repeat('*', strlen(escape($row['domain_setting_value'])));
+				echo "		".str_repeat('*', empty($row['domain_setting_value']) ? 0 : 10); //use the same number of characters to mask the password length
 			}
 			else if ($category == 'theme' && $subcategory == 'button_icons' && $name == 'text') {
 				echo "		".$text['option-button_icons_'.$row['domain_setting_value']]."\n";
