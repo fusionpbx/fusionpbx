211d157b92d815ca48e519f2e30a47fb3f6f0a8b
FusionPBX <markjcrane@gmail.com>
2026-01-31 10:45:33 -0700

    Only show the masked password if there is a password defined.

--- core/default_settings/default_settings.php
+++ core/default_settings/default_settings.php
@@ -518,7 +518,7 @@
 				echo "		[...]\n";
 			}
 			else if ($subcategory == 'password' || (substr_count($subcategory, '_password') > 0 && $subcategory != 'input_text_font_password') || substr_count($subcategory, '_key') > 0 || substr_count($subcategory, '_secret') > 0) {
-				echo "		".str_repeat('*', 10);	//use the same number of characters to mask the password length
+				echo "		".str_repeat('*', empty($row['default_setting_value']) ? 0 : 10); //use the same number of characters to mask the password length
 			}
 			else if ($category == 'theme' && $subcategory == 'button_icons' && $name == 'text') {
 				echo "		".$text['option-button_icons_'.$row['default_setting_value']]."\n";
