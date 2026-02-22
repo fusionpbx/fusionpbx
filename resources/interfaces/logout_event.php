362c35f1fb1ad3d9b04e1f323a113be4918b6572
Tim Fry <tim@fusionpbx.com>
2026-02-22 14:59:11 +0000

    Use settings object in interface signature

--- resources/interfaces/logout_event.php
+++ resources/interfaces/logout_event.php
@@ -1,10 +1,5 @@
 <?php
 
 interface logout_event {
-<<<<<<< HEAD
 	public static function on_logout(settings $settings);
 }
-=======
-	public static function on_logout();
-}
->>>>>>> bad884891f14f6ec02c401c0f65d2f818b3e4a9e
