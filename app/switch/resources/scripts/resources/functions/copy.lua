1a915bb21cd3897c981f9e69160d8f2878739821
FusionPBX <markjcrane@gmail.com>
2026-03-31 12:31:58 -0600

    Fix the gsub for to limit to allowed characters

--- app/switch/resources/scripts/resources/functions/copy.lua
+++ app/switch/resources/scripts/resources/functions/copy.lua
@@ -1,8 +1,9 @@
 --add the copy function
 	function copy(src,dst)
 		--retrieve allowed characters and then use it to sanitize the dir variable
-		local allowed_chars = os.getenv("ALLOWED_CHARS") or "^%a%d%-%._~/"
-		dir = dir:gsub("[^" .. allowed_chars .. "]", "")
+		local allowed_chars = os.getenv("ALLOWED_CHARS") or "^%a%d%-%._~/";
+		src = src:gsub("[^" .. allowed_chars .. "]", "");
+		dst = dst:gsub("[^" .. allowed_chars .. "]", "");
 
 		if (package.config:sub(1,1) == "/") then
 			--unix
