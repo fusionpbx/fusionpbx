840970e0947b458393ce95de0d567687230b27cb
FusionPBX <markjcrane@gmail.com>
2026-04-04 19:30:54 +0000

    Add permission music_on_hold_map

--- app/music_on_hold/music_on_hold_map.php
+++ app/music_on_hold/music_on_hold_map.php
@@ -30,7 +30,7 @@
 	require_once "resources/check_auth.php";
 
 //check permissions
-	if (!permission_exists('music_on_hold_add)) {
+	if (!permission_exists('music_on_hold_map')) {
 		echo "access denied";
 		exit;
 	}
