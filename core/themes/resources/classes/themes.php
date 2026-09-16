09a11ef1a46428c28e0dacfc243522e31d86ab50
Alex <40072887+alexdcrane@users.noreply.github.com>
2026-09-16 22:31:10 +0000

    Update themes.php

--- core/themes/resources/classes/themes.php
+++ core/themes/resources/classes/themes.php
@@ -425,7 +425,7 @@
 					}
 				}
 				if (is_array($uuids) && @sizeof($uuids) != 0) {
-					$sql  = "select " . $this->name . "_uuid as uuid, " . $this->toggle_field . " as toggle from v_" . $this->table . " ";
+					$sql  = "select " . $this->name . "_uuid as uuid, cast(" . $this->toggle_field . " as text) as toggle from v_" . $this->table . " ";
 					$sql  .= "where " . $this->name . "_uuid in (" . implode(', ', $uuids) . ") ";
 					$rows = $this->database->select($sql, $parameters ?? null, 'all');
 					if (is_array($rows) && @sizeof($rows) != 0) {
