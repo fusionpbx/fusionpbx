a50ed6f012f459c2c99ae08a8215987146678c5d
Alex <40072887+alexdcrane@users.noreply.github.com>
2026-09-17 21:39:23 +0000

    Update default_settings.php

--- core/default_settings/default_settings.php
+++ core/default_settings/default_settings.php
@@ -563,7 +563,7 @@
 					echo "		".$text['option-'.$row['default_setting_value']]."\n";
 				}
 			}
-			else if (substr_count($subcategory, "_color") > 0 && ($name == "text" || $name == 'array')) {
+			else if ((substr_count($subcategory, "_color") > 0 || str_starts_with($row['default_setting_value'] ?? '', "rgb") || str_starts_with($row['default_setting_value'] ?? '', "#")) && ($name == "text" || $name == 'array')) {
 				echo "		".(img_spacer('15px', '15px', 'background: '.escape($row['default_setting_value']).'; margin-right: 4px; vertical-align: middle; border: 1px solid '.(color_adjust($row['default_setting_value'], -0.18)).'; padding: -1px;'));
 				echo "<span style=\"font-family: 'Courier New'; line-height: 6pt;\">".escape($row['default_setting_value'])."</span>\n";
 			}
