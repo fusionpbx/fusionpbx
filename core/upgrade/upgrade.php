67b124cdc41bf75ca9fee72aa123c4693d3b9690
FusionPBX <markjcrane@gmail.com>
2026-09-23 12:02:50 -0600

    SECURITY Add escape shell arguments and htmlspecialchars

--- core/upgrade/upgrade.php
+++ core/upgrade/upgrade.php
@@ -82,7 +82,7 @@
 
 		// Make the config directory
 		if (isset($config_path)) {
-			system('mkdir -p '.$config_path);
+			system('mkdir -p ' . escapeshellarg($config_path));
 		}
 		else {
 			echo "Config directory not found\n";
@@ -111,6 +111,9 @@
 		$conf .= "temp.dir = /tmp\n";
 		$conf .= "php.dir = ".$php_dir."\n";
 		$conf .= "php.bin = php\n";
+		$conf .= "\n";
+		$conf .= "#security settings\n";
+		$conf .= "security.header.content_security_policy.frame_ancestors =\n";
 		$conf .= "\n";
 		$conf .= "#session settings\n";
 		$conf .= "session.cookie_httponly = true\n";
@@ -415,20 +418,22 @@
 
 		// Send a message to the console
 			if ($display_type === 'text') {
-				echo "[ Update ] Table and field structure.\n";
+				echo "[ Update ] Database Schema\n";
 			}
 
 		// Update the table and field structure.
 			$response = $schema->upgrade("text");
 			if ($display_type === 'text') {
 				foreach(explode("\n", $response) as $row) {
-					echo trim($row)."\n";
+					if (!empty($row)) {
+						echo trim($row)."\n";
+					}
 				}
 			}
 
 		// Send a message to the console
 			if ($display_type === 'text') {
-				echo "[ Update ] Application defaults.\n";
+				echo "[ Update ] Application Defaults\n";
 			}
 
 		// Update php fpm service
@@ -678,7 +683,7 @@ function update_php_fpm(settings $settings) {
 
 			// Prepared systemd to use the update
 			system('systemctl daemon-reload');
-			system('systemctl restart php'.$php_version.'-fpm');
+			system('systemctl restart ' . escapeshellarg('php' . $php_version . '-fpm'));
 		}
 
 	}
@@ -740,7 +745,7 @@ function update_file_permissions($text, settings $settings) {
 			}
 
 			// Update the file ownership to use the web server user
-			exec("chown -R www-data:www-data $dir");
+			exec("chown -R www-data:www-data " . escapeshellarg($dir));
 		}
 	} else {
 		echo ($text['label-not_running_as_root'] ?? "Not root user - operation skipped")."\n";
@@ -789,10 +794,11 @@ function upgrade_services($text, settings $settings) {
 				system("systemctl start " . escapeshellarg($service_name));
 			}
 			if (stristr(PHP_OS, 'FreeBSD')) {
-				system("cp " . $file . " /usr/local/etc/rc.d/".$service_name);
-				system("sysrc " . $service_name . "_enable=\"YES\"");
-				system("chmod 755 /usr/local/etc/rc.d/" . $service_name);
-				system("service " . $service_name . " start");
+				$service_name = preg_replace('/[^a-zA-Z0-9_-]/', '', $service_name);
+				system("cp " . escapeshellarg($file) . " /usr/local/etc/rc.d/" . escapeshellarg($service_name));
+				system("sysrc " . escapeshellarg($service_name) . "_enable=YES");
+				system("chmod 755 /usr/local/etc/rc.d/" . escapeshellarg($service_name));
+				system("service " . escapeshellarg($service_name) . " start");
 			}
 		}
 	}
@@ -834,10 +840,10 @@ function stop_services($text, settings $settings) {
 			}
 			// Stop the service
 			if (stristr(PHP_OS, 'Linux')) {
-				system("systemctl stop " . $service_name);
+				system("systemctl stop " . escapeshellarg($service_name));
 			}
 			if (stristr(PHP_OS, 'FreeBSD')) {
-				system("service " . $service_name . " stop");
+				system("service " . escapeshellarg($service_name) . " stop");
 			}
 		}
 	}
@@ -878,10 +884,10 @@ function restart_services($text, settings $settings) {
 			}
 			// Restart the service
 			if (stristr(PHP_OS, 'Linux')) {
-				system("systemctl restart ".$service_name);
+				system("systemctl restart " . escapeshellarg($service_name));
 			}
 			if (stristr(PHP_OS, 'FreeBSD')) {
-				system("service " . $service_name . " restart");
+				system("service " . escapeshellarg($service_name) . " restart");
 			}
 		}
 	}
